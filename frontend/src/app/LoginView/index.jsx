ce1f48f53ce1f935ec56be1cae50728c63d67107
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-09-29 23:58:53 -0700

    added Login and 404 page

---
+++ frontend/src/app/LoginView/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class LoginPage extends React.Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <main>
+        <div class="grid-container">
+          <h1>Login Page</h1>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ frontend/src/app/NotFoundView/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class NotFoundViewComponent extends React.Component {
+  render() {
+    return (
+      <main>
+        <div class="grid-container">
+          <h1> 404 not found</h1>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default NotFoundViewComponent;
