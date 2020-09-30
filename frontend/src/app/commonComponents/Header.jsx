797349ff7f476cd9fce526040be27a475b1bae97
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-09-29 23:58:23 -0700

    added footer and header components

--- frontend/src/app/commonComponents/Header.jsx
+++ frontend/src/app/commonComponents/Header.jsx
@@ -1,49 +1,13 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// import { userPropTypes } from "../propTypes";
-// import "../../styles/App.css";
-// import Axios from "axios";
-// import Button from "./Button";
 
 class Header extends React.Component {
-  //   static propTypes = {
-  //     user: userPropTypes
-  //   };
-
   render() {
-    // let logout = null;
-    // let userSection = null;
-
-    // if (this.props.user) {
-    //   logout = (
-    //     <Button
-    //       onClick={async () => {
-    //         await Axios.get(`/logout`);
-    //         window.location.href = "/";
-    //       }}
-    //       label="Logout"
-    //       addClass="usa-button"
-    //     />
-    //   );
-
-    //   userSection = (
-    //     <span className="smeqa-rr-nav__user">
-    //       <FontAwesomeIcon icon="user" /> {this.props.user.user.email}
-    //     </span>
-    //   );
-    // }
-
     return (
-      <header className="usa-header usa-header--basic">
+      <header className="usa-header usa-header--basic prime-nav">
         <div className="usa-nav-container">
           <div className="usa-nav-bar">
-            <a href="/" className="">
-              <h1 className="">PRIME Data Input MVP</h1>
-            </a>
+            <h1 className="app-title">PRIME Data Input MVP</h1>
           </div>
-          {/* {userSection} */}
-          {/* {logout} */}
         </div>
       </header>
     );
