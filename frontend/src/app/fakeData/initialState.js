a420c8c0acf169fb1adaa8392421747426ec12b3
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-10-17 11:16:59 -0700

    misc cleanup

--- frontend/src/app/fakeData/initialState.js
+++ frontend/src/app/fakeData/initialState.js
@@ -1,8 +1,8 @@
 import { initialPatientState } from "./patients";
 import { initialTestResultsState } from "./testResults";
-import { initialOrganizationState } from "./orgs";
-import { initialDevicesState } from "./devices";
-import { initialUserState } from "./users";
+// import { initialOrganizationState } from "./orgs";
+// import { initialDevicesState } from "./devices";
+// import { initialUserState } from "./users";
 import { initialTestQueueState } from "./testQueue";
 
 export const initialState = {
