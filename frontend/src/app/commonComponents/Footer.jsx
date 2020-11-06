87bc6ecd0ada8106a68ef5de99e15f0dbc81b670
peterwaterman <peter.waterman@hq.dhs.gov>
2020-11-06 13:48:12 -0500

    uncomment footer

--- frontend/src/app/commonComponents/Footer.jsx
+++ frontend/src/app/commonComponents/Footer.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import usdsLogo from "../../img/usds-logo.png";
+import React from "react";
+import usdsLogo from "../../img/usds-logo.png";
 
-// class Footer extends React.Component {
-//   render() {
-//     return (
-//       <footer className="usa-footer usa-footer--slim">
-//         <div className="usa-footer__primary-section">
-//           <div className="grid-container">
-//             <div className="usa-footer__primary-container grid-row">
-//               <img
-//                 className="usa-footer__logo-img"
-//                 src={usdsLogo}
-//                 alt="USDS logo"
-//                 onClick={() => {
-//                   if (window.confirm("Reset Demo Data?")) {
-//                     localStorage.clear();
-//                     window.location.reload(false);
-//                   }
-//                 }}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </footer>
-//     );
-//   }
-// }
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="usa-footer usa-footer--slim">
+        <div className="usa-footer__primary-section">
+          <div className="grid-container">
+            <div className="usa-footer__primary-container grid-row">
+              <img
+                className="usa-footer__logo-img"
+                src={usdsLogo}
+                alt="USDS logo"
+                onClick={() => {
+                  if (window.confirm("Reset Demo Data?")) {
+                    localStorage.clear();
+                    window.location.reload(false);
+                  }
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
 
-// export default Footer;
+export default Footer;
