79d8e96072860d776ceda2ba22a4b1a90416bddc
peterwaterman <peter.waterman@hq.dhs.gov>
2020-11-06 13:23:56 -0500

    set initial device state for user testing

--- frontend/src/app/fakeData/devices.js
+++ frontend/src/app/fakeData/devices.js
@@ -1,12 +1,18 @@
 import { DEVICE_TYPES } from "../devices/constants";
 
-let randomDeviceId = Object.keys(DEVICE_TYPES)[0];
+//let randomDeviceId = Object.keys(DEVICE_TYPES)[1];
+// Adding specific devices for user testing by request from H&K
 
 export const initialDevicesState = {
-  [randomDeviceId]: {
-    displayName: DEVICE_TYPES[randomDeviceId],
-    deviceManufacturer: "Abbott",
-    deviceModel: "ID NOW",
+  deviceId2: {
+    displayName: "BD Veritor",
+    deviceManufacturer: "BD",
+    deviceModel: "Veritor",
     isDefault: true,
+  },
+  deviceId3: {
+    displayName: "Abbott Binax Now",
+    deviceManufacturer: "Abbott",
+    deviceModel: "Binax Now",
   },
 };
