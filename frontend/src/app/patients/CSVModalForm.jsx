dad89333f1a5c16763fe55197a4494161dd015bc
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-11-06 22:17:37 -0800

    rudimentary styles on patient table

--- frontend/src/app/patients/CSVModalForm.jsx
+++ frontend/src/app/patients/CSVModalForm.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
-// import PropTypes from "prop-types";
+import moment from "moment";
 import Modal from "react-modal";
+import { v4 as uuidv4 } from "uuid";
+
+import Alert from "../commonComponents/Alert";
 import Button from "../commonComponents/Button";
+import { displayFullName } from "../utils";
 
 Modal.setAppElement("#root");
 
@@ -9,6 +13,125 @@ const CSVModalForm = ({ isOpen, onClose, data }) => {
   useEffect(() => {
     Modal.setAppElement("#root");
   });
+
+  const PreviewTable = (data) => {
+    if (!data) {
+      return null;
+    }
+
+    // there are lots of rows -- do we render them all?
+    let rows = data.map((row) => {
+      const {
+        patientID,
+        patientLastName,
+        patientFirstName,
+        patientMiddleName,
+        // patientSuffix,
+        patientRace,
+        patientDOB,
+        patientGender,
+        patientEthnicity,
+        // patientStreet,
+        // patientStreet2,
+        // patientCity,
+        // patientCounty,
+        // patientState,
+        // patientZipCode,
+        // patientPhoneNumber,
+        // patientEmail,
+        // patientAge,
+        // employedInHealthcare,
+        // typeOfHealthcareProfessional,
+        // residentCongregateSetting,
+        // patientResidencyType,
+      } = { ...row };
+
+      return (
+        <tr key={`patient-${uuidv4()}`}>
+          <th scope="row">
+            {displayFullName(
+              patientFirstName,
+              patientMiddleName,
+              patientLastName
+            )}
+          </th>
+          <td>{patientID}</td>
+          <td>{moment(patientDOB).format("MMM DD YYYY")}</td>
+          <td>{patientGender}</td>
+          <td>{patientEthnicity}</td>
+          <td>{patientRace}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table className="usa-table usa-table--borderless width-full">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Unique ID</th>
+            <th scope="col">Date of Birth</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Ethnicity</th>
+            <th scope="col">Race</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  };
+
+  const ErrorTable = (data) => {
+    if (!data) {
+      return null;
+    }
+    let rows = data.map((errorRow) => {
+      const { message, row } = { ...errorRow };
+      const { patientID, patientLastName, patientFirstName } = {
+        ...errorRow.original,
+      };
+
+      return (
+        <tr key={`patient-${uuidv4()}`}>
+          <th scope="row">
+            {displayFullName(patientFirstName, "", patientLastName)}
+          </th>
+          <td>{patientID}</td>
+          <td>{row}</td>
+          <td>{message}</td>
+        </tr>
+      );
+    });
+
+    // alerts styles are currently broken
+    const alert = null;
+    //   <Alert
+    //     type="warning"
+    //     title="The following recrods will not be added due to exact matches found in the system"
+    //     body={`${data.length} records will not be included in the upload.`}
+    //   />
+    // );
+
+    return (
+      <React.Fragment>
+        {alert}
+        <p>
+          {`The following ${data.length} will not be added due to exact matches found in the system`}{" "}
+        </p>
+        <table className="usa-table usa-table--borderless width-full">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Unique ID</th>
+              <th scope="col">Row #</th>
+              <th scope="col">Message</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </React.Fragment>
+    );
+  };
 
   return (
     <Modal
@@ -27,22 +150,12 @@ const CSVModalForm = ({ isOpen, onClose, data }) => {
           <Button type="button" onClick={() => {}} label="Confirm Import" />
         </div>
         <div>
-          <h2>Good Rows:</h2>
-          {
-            // note this doesn't handle duplicate keys well we
-            data.data &&
-              data.data.map((r) => <p key={r.patientID}>{JSON.stringify(r)}</p>)
-          }
+          <h2>Preview:</h2>
+          {PreviewTable(data.data)}
         </div>
         <div>
           <h2>Bad Rows:</h2>
-          {
-            // note this doesn't handle duplicate keys well we
-            data.badRows &&
-              data.badRows.map((r) => (
-                <p key={r.original.patientID}>{JSON.stringify(r)}</p>
-              ))
-          }
+          {ErrorTable(data.badRows)}
         </div>
         <div className="grid-row">
           <Button type="button" onClick={onClose} label="Cancel" />
