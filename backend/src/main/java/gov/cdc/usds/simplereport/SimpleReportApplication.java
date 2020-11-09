1434b13eda65f6c8f41e7f9027398fbbb6aa28c1
Ben Warfield <bwarfield@cdc.gov>
2020-11-09 15:07:40 -0500

    Added basic Spring Boot app skeleton.

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/SimpleReportApplication.java
@@ -0,0 +1,13 @@
+package gov.cdc.usds.simplereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReportApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/gov/cdc/usds/simplereport/SimpleReportApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.usds.simplereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
