77f8dab52464086a28a7fca4d34b7661590625a5
Ben Warfield <bwarfield@cdc.gov>
2020-11-10 16:21:34 -0500

    Implemented trivial graphql mutations trivially.

--- backend/src/main/java/gov/cdc/usds/simplereport/GraphqlConfiguration.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/GraphqlConfiguration.java
@@ -5,12 +5,16 @@ import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.LocalDate;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import gov.cdc.usds.simplereport.api.DummyDataRepo;
+import gov.cdc.usds.simplereport.api.model.Device;
+import gov.cdc.usds.simplereport.api.model.Patient;
 import graphql.GraphQL;
+import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -36,13 +40,40 @@ public class GraphqlConfiguration {
 
 	@Bean
     public RuntimeWiring buildWiring() {
+		DataFetcher<String> deviceAdder = context -> {
+			String newId = context.getArgument("deviceId");
+			String displayName = context.getArgument("displayName");
+			String deviceManufacturer = context.getArgument("deviceManufacturer");
+			String deviceModel = context.getArgument("deviceModel");
+			boolean isDefault = context.getArgument("isDefault");
+			DummyDataRepo.addDevice(new Device(newId, displayName, deviceManufacturer, deviceModel, isDefault));
+			return newId;
+		};
+
+		DataFetcher<String> patientAdder = context -> {
+			String patientId = context.getArgument("patientId");
+			String firstName = context.getArgument("firstName");
+			String middleName = context.getArgument("middleName");
+			String lastName = context.getArgument("lastName");
+			String birthDate = context.getArgument("birthDate");
+			String address = context.getArgument("address");
+			String phone = context.getArgument("phone");
+			LocalDate realDate = LocalDate.parse(birthDate);
+			DummyDataRepo.addPatient(new Patient(patientId, firstName, middleName, lastName, realDate, address, phone));
+			return patientId;
+		};
 		return RuntimeWiring.newRuntimeWiring()
 			.type(
 					newTypeWiring("Query")
 						.dataFetcher("patient", DummyDataRepo.patientFetcher())
 						.dataFetcher("device", DummyDataRepo.deviceFetcher())
 						.build()
-					)
+			     )
+			.type(
+					newTypeWiring("Mutation")
+						.dataFetcher("addDevice", deviceAdder)
+						.dataFetcher("addPatient", patientAdder)
+			     )
 			.build();
 	}
 }
