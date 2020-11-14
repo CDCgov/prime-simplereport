60aa5081b6531c1a104df5667b935fa028753e49
Ben Warfield <bwarfield@cdc.gov>
2020-11-14 00:56:31 -0500

    Rearranged db setup to allow easier nuking of schema.
    Split out the schema creation from DB creation, and made it runnable
    from a shell command, to enable simpler testing and debugging of
    database migrations.

--- backend/db-setup/create-db.sh
+++ backend/db-setup/create-db.sh
@@ -2,15 +2,4 @@ createuser -U "$POSTGRES_USER" -w simple_report_migrations
 createuser -U "$POSTGRES_USER" -w simple_report_app
 createdb -U "$POSTGRES_USER" -w simple_report --maintenance-db="$POSTGRES_DB"
 
-psql -v ON_ERROR_STOP=1  -U "$POSTGRES_USER" simple_report <<-SQL
-    CREATE SCHEMA IF NOT EXISTS simple_report;
-    ALTER ROLE simple_report_app PASSWORD 'api123';
-    ALTER ROLE simple_report_migrations PASSWORD 'migrations456';
-    GRANT ALL PRIVILEGES ON SCHEMA simple_report TO simple_report_migrations;
-    GRANT USAGE ON SCHEMA simple_report TO simple_report_app;
-    ALTER DEFAULT PRIVILEGES FOR USER simple_report_migrations IN SCHEMA simple_report
-        GRANT SELECT, INSERT, DELETE, UPDATE, TRUNCATE ON TABLES TO simple_report_app;
-    ALTER DEFAULT PRIVILEGES FOR USER simple_report_migrations IN SCHEMA simple_report
-        GRANT SELECT, UPDATE ON SEQUENCES TO simple_report_app;
-    GRANT USAGE ON LANGUAGE plpgsql to simple_report_app;
-SQL
+psql -v ON_ERROR_STOP=1  -U "$POSTGRES_USER" simple_report -f /usr/local/lib/reset-db.sql
