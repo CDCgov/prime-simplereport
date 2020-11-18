71bc4fae7990d8b132c685dd0c38513c58b7371e
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-11-17 20:48:40 -0800

    refactored devices

--- frontend/src/app/Settings/Settings.jsx
+++ frontend/src/app/Settings/Settings.jsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { gql, useQuery } from "@apollo/client";
+// import { useDispatch } from "react-redux";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { v4 as uuidv4 } from "uuid";
 
 import Button from "../commonComponents/Button";
 import DeviceSettings from "./DeviceSettings";
 import OrderingProviderSettings from "./OrderingProviderSettings";
 import OrganizationSettings from "./OrganizationSettings";
-import { generateDeviceSettings } from "../devices/utils";
-import { updateSettings } from "../Settings/state/settingsActions";
+// import { updateSettings } from "../Settings/state/settingsActions";
 import { useEffect } from "react";
 
-const getDevicesQuery = gql`
+const getSettings = gql`
   {
-    device {
-      id
-      displayName
-      deviceModel
-      deviceManufacturer
-      isDefault
-    }
     organization {
       id
       cliaNumber
@@ -32,33 +25,92 @@ const getDevicesQuery = gql`
       orderingProviderCounty
       orderingProviderState
       orderingProviderPhone
+      defaultDevice
+      devices {
+        id
+      }
     }
   }
 `;
 
+const setSettings = gql`
+  mutation(
+    $name: String
+    $clia: String
+    $orderingProviderName: String
+    $orderingProviderNPI: String
+    $orderingProviderStreet: String
+    $orderingProviderStreetTwo: String
+    $orderingProviderCity: String
+    $orderingProviderCounty: String
+    $orderingProviderState: String
+    $orderingProviderZipCode: String
+    $orderingProviderPhone: String
+    $devices: [String]
+    $defaultDevice: String
+  ) {
+    updateOrganization(
+      testingFacilityName: $name
+      cliaNumber: $clia
+      orderingProviderName: $orderingProviderName
+      orderingProviderNPI: $orderingProviderNPI
+      orderingProviderStreet: $orderingProviderStreet
+      orderingProviderStreetTwo: $orderingProviderStreetTwo
+      orderingProviderCity: $orderingProviderCity
+      orderingProviderCounty: $orderingProviderCounty
+      orderingProviderState: $orderingProviderState
+      orderingProviderZipCode: $orderingProviderZipCode
+      orderingProviderPhone: $orderingProviderPhone
+      devices: $devices
+      defaultDevice: $defaultDevice
+    )
+  }
+`;
+
 const Settings = () => {
-  const dispatch = useDispatch();
-  const { data, loading, error } = useQuery(getDevicesQuery);
+  // const dispatch = useDispatch();
+  const {
+    data: settings,
+    loading: isLoadingSettings,
+    error: errorFetchingSettings,
+  } = useQuery(getSettings);
+
+  const [saveSettings, { as }] = useMutation(setSettings);
 
   const [deviceSettings, updateDeviceSettings] = useState({});
   const [orgSettings, updateOrgSettings] = useState({});
 
   useEffect(() => {
-    if (data) {
-      updateDeviceSettings(generateDeviceSettings(data.device));
-      updateOrgSettings(data.organization);
+    if (settings) {
+      let supportedDevices = settings.organization.devices.reduce(
+        (acc, device) => {
+          let name = `device-${uuidv4()}`;
+          acc[name] = device.id;
+          return acc;
+        },
+        {}
+      );
+
+      let deviceSettings = {
+        supportedDevices: supportedDevices,
+        defaultDeviceId: settings.organization.defaultDevice,
+      };
+      updateDeviceSettings(deviceSettings);
     }
-  }, [data]);
+  }, [settings]);
 
   const onSaveSettings = () => {
-    dispatch(updateSettings(deviceSettings, orgSettings));
+    console.log("deviceSettings", deviceSettings);
+    console.log("orgSettings", orgSettings);
+    // dispatch(updateSettings(deviceSettings, orgSettings));
   };
 
-  if (error) {
-    return <p> There was an error </p>;
-  }
-  if (loading) {
+  if (isLoadingSettings) {
     return <p> Loading... </p>;
+  }
+  if (errorFetchingSettings) {
+    console.log(errorFetchingSettings);
+    return <p> There was an error </p>;
   }
 
   return (
