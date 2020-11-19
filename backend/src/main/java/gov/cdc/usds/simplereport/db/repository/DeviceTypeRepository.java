f613fd4ee5904634d1b78940b1e252694c0507fb
Ben Warfield <bwarfield@cdc.gov>
2020-11-19 16:30:30 -0500

    Reversed some breaking repository changes.

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTypeRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTypeRepository.java
@@ -5,5 +5,4 @@ import gov.cdc.usds.simplereport.db.model.DeviceType;
 /** Specification of EternalEntityRepository for {@link DeviceType} manipulation. */
 public interface DeviceTypeRepository extends EternalEntityRepository<DeviceType> {
 
-  public DeviceType findByInternalId(String internalId);
 }
