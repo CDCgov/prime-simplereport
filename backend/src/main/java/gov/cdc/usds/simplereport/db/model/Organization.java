d46886235aeeeb9ba0acc27b6b1e665a0b806abe
Ben Warfield <bwarfield@cdc.gov>
2020-11-18 23:18:59 -0500

    Added and tested many-to-many relationship between org and device.

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/Organization.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/Organization.java
@@ -1,8 +1,15 @@
 package gov.cdc.usds.simplereport.db.model;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.NaturalId;
@@ -21,11 +28,28 @@ public class Organization extends EternalEntity {
 	@JoinColumn(name = "default_device_type")
 	private DeviceType defaultDeviceType;
 
-	public Organization(String facilityName, String externalId, DeviceType defaultDeviceType) {
+	@ManyToMany
+	@JoinTable(
+		name = "facility_device_type",
+		joinColumns = @JoinColumn(name="organization_id"),
+		inverseJoinColumns = @JoinColumn(name="device_type_id")
+	)
+	private Set<DeviceType> configuredDevices = new HashSet<>();
+
+	public Organization(String facilityName, String externalId) {
 		super();
 		this.facilityName = facilityName;
 		this.externalId = externalId;
+	}
+	public Organization(String facilityName, String externalId, DeviceType defaultDeviceType) {
+		this(facilityName, externalId);
 		this.defaultDeviceType = defaultDeviceType;
+	}
+
+	public Organization(String facilityName, String externalId, DeviceType defaultDeviceType,
+			Collection<DeviceType> configuredDevices) {
+		this(facilityName, externalId, defaultDeviceType);
+		this.configuredDevices.addAll(configuredDevices);
 	}
 
 	public String getFacilityName() {
@@ -38,5 +62,28 @@ public class Organization extends EternalEntity {
 
 	public DeviceType getDefaultDeviceType() {
 		return defaultDeviceType;
+	}
+
+	public Set<DeviceType> getDeviceTypes() {
+		// this might be better done on the DB side, but that seems like a recipe for weird behaviors
+		return configuredDevices.stream()
+			.filter(e -> !e.isDeleted())
+			.collect(Collectors.toSet());
+	}
+
+	public void addDeviceType(DeviceType newDevice) {
+		configuredDevices.add(newDevice);
+	}
+
+	public void addDefaultDeviceType(DeviceType newDevice) {
+		this.defaultDeviceType = newDevice;
+		addDeviceType(newDevice);
+	}
+
+	public void removeDeviceType(DeviceType existingDevice) {
+		configuredDevices.remove(existingDevice);
+		if (defaultDeviceType != null && existingDevice.getInternalId().equals(defaultDeviceType.getInternalId())) {
+			defaultDeviceType = null;
+		}
 	}
 }
--- backend/src/test/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepositoryTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepositoryTest.java
@@ -4,17 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import gov.cdc.usds.simplereport.db.model.DeviceType;
 import gov.cdc.usds.simplereport.db.model.Organization;
 
 public class OrganizationRepositoryTest extends BaseRepositoryTest {
 
 	@Autowired
 	private OrganizationRepository _repo;
+	@Autowired
+	private DeviceTypeRepository _devices;
 
 	@Test
 	public void createAndFindSomething() {
@@ -24,5 +29,19 @@ public class OrganizationRepositoryTest extends BaseRepositoryTest {
 		Optional<Organization> sameOrg = _repo.findByExternalId("12345");
 		assertTrue(sameOrg.isPresent());
 		assertEquals(sameOrg.get().getInternalId(), saved.getInternalId());
+	}
+
+	@Test
+	public void smokeTestDeviceOperations() {
+		Set<DeviceType> configuredDevices = new HashSet<>();
+		DeviceType bill = new DeviceType("Bill", "Weasleys", "1");
+		configuredDevices.add(_devices.save(bill));
+		configuredDevices.add(_devices.save(new DeviceType("Percy", "Weasleys", "2")));
+		Organization saved = _repo.save(new Organization("My Office", "650", null, configuredDevices));
+		Organization found = _repo.findByExternalId(saved.getExternalId()).get();
+		assertEquals(2, found.getDeviceTypes().size());
+		found.addDefaultDeviceType(bill);
+		_repo.save(found);
+		found = _repo.findById(saved.getInternalId()).get();
 	}
 }
