81b85d969195afedb8d8a23c576e98fa821e4191
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2020-11-21 08:22:00 -0800

    Quickfix to make nav menu show up and function on narrow screens (#135)
    * Quickfix to make nav menu show up and function on narrow screens
    
    Fixes issue #101
    
    * Fix warning on `<img role`
    
    * prettier on the .scss
    
    Co-authored-by: Neil Sharma <neil.s.sharma@omb.eop.gov>

--- frontend/src/app/commonComponents/Header.jsx
+++ frontend/src/app/commonComponents/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PATIENT_TERM_PLURAL_CAP } from "../../config/constants";
+import classNames from "classnames";
+import iconClose from "../../img/close-gray-60.svg";
 
 const Header = ({ organizationId }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
   return (
     <header className="usa-header usa-header--basic">
       <div className="usa-nav-container">
@@ -14,12 +17,30 @@ const Header = ({ organizationId }) => {
               <Link to="/">{process.env.REACT_APP_TITLE}</Link>
             </em>
           </div>
+          <button
+            onClick={() => setMenuVisible(!menuVisible)}
+            className="usa-menu-btn"
+          >
+            Menu
+          </button>
         </div>
-        <nav aria-label="Primary navigation" className="usa-nav prime-nav">
+        <nav
+          aria-label="Primary navigation"
+          className={classNames("usa-nav", "prime-nav", {
+            "is-visible": menuVisible,
+          })}
+        >
+          <button
+            className="usa-nav__close prime-nav-close-button"
+            onClick={() => setMenuVisible(false)}
+          >
+            <img src={iconClose} alt="close menu" />
+          </button>
           <ul className="usa-nav__primary usa-accordion">
             <li className="usa-nav__primary-item">
               <NavLink
                 to={`/organization/${organizationId}/queue`}
+                onClick={() => setMenuVisible(false)}
                 activeClassName="active-nav-item"
                 activeStyle={{
                   color: "white",
@@ -31,6 +52,7 @@ const Header = ({ organizationId }) => {
             <li className="usa-nav__primary-item">
               <NavLink
                 to={`/organization/${organizationId}/results`}
+                onClick={() => setMenuVisible(false)}
                 activeClassName="active-nav-item"
                 activeStyle={{
                   color: "white",
@@ -42,6 +64,7 @@ const Header = ({ organizationId }) => {
             <li className="usa-nav__primary-item">
               <NavLink
                 to={`/organization/${organizationId}/patients`}
+                onClick={() => setMenuVisible(false)}
                 activeClassName="active-nav-item"
                 activeStyle={{
                   color: "white",
@@ -50,13 +73,28 @@ const Header = ({ organizationId }) => {
                 {PATIENT_TERM_PLURAL_CAP}
               </NavLink>
             </li>
+
+            <li className="usa-nav__primary-item prime-settings-hidden">
+              <NavLink
+                to={`/organization/${organizationId}/settings`}
+                onClick={() => setMenuVisible(false)}
+                activeClassName="active-nav-item"
+                activeStyle={{
+                  color: "white",
+                }}
+              >
+                <FontAwesomeIcon icon={"cog"} size="2x" />
+              </NavLink>
+            </li>
           </ul>
         </nav>
+
         <nav aria-label="Primary navigation" className="usa-nav prime-nav">
           <ul className="usa-nav__primary usa-accordion">
             <li className="usa-nav__primary-item">
               <NavLink
                 to={`/organization/${organizationId}/settings`}
+                onClick={() => setMenuVisible(false)}
                 activeClassName="active-nav-item"
                 activeStyle={{
                   color: "white",
