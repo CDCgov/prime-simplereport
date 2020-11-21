45923c6e43f761ca734055d78be58773ee4b3e56
Tim <hello@timbest.net>
2020-11-21 12:39:21 -0500

    add suffix

--- backend/src/test/java/gov/cdc/usds/simplereport/db/repository/TestOrderRepositoryTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/db/repository/TestOrderRepositoryTest.java
@@ -50,7 +50,7 @@ public class TestOrderRepositoryTest extends BaseRepositoryTest {
 	public void testLifeCycle() {
 		Provider mccoy = _providers.save(new Provider("Doc", "NCC1701", null, "(1) (111) 2222222"));
 		Organization gtown = _orgRepo.save(new Organization("Georgetown", "gt", null, mccoy));
-		Person hoya = _personRepo.save(new Person(gtown, "lookupId", "Joe", null, "Schmoe", LocalDate.now(), null, "(123) 456-7890", "", "", "", "", "", false, false));
+		Person hoya = _personRepo.save(new Person(gtown, "lookupId", "Joe", null, "Schmoe", null, LocalDate.now(), null, "(123) 456-7890", "", "", "", "", "", false, false));
 		TestOrder order = _repo.save(new TestOrder(hoya, gtown));
 		flush();
 		TestEvent ev = _events.save(new TestEvent(TestResult.POSITIVE, null, hoya, mccoy));
