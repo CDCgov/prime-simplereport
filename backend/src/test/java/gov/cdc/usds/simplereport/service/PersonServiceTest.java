fffed9fde3d20944e56683a2c4983fbc5fae27d6
Ben Warfield <bwarfield@cdc.gov>
2020-11-21 00:58:09 -0500

    Added tests for patient-list fetch.
    Somewhat laboriously added two different test cases, one of which should have failed
    but initially did not, and the other of which kindly did fail continuously, for
    the problem caused by mis-defining the "street" column in an address.
    
    Also imported the database-truncating tool to wipe the DB during tests, making
    it even more imperative to eventually stop using the dev DB for testing.

---
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/PersonServiceTest.java
@@ -0,0 +1,42 @@
+package gov.cdc.usds.simplereport.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import gov.cdc.usds.simplereport.db.model.Person;
+import gov.cdc.usds.simplereport.db.repository.OrganizationRepository;
+import gov.cdc.usds.simplereport.db.repository.PersonRepository;
+import gov.cdc.usds.simplereport.db.repository.ProviderRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("dev")
+public class PersonServiceTest {
+
+	private PersonService _service;
+
+	public PersonServiceTest(@Autowired OrganizationRepository orgRepo, @Autowired ProviderRepository providerRepo, @Autowired PersonRepository repo) {
+		OrganizationService os= new OrganizationService(orgRepo, providerRepo);
+		_service = new PersonService(os, repo);
+	}
+
+	@Test
+	public void roundTrip() {
+		_service.addPatient("FOO", "Fred", null, "Fosbury", LocalDate.of(1865, 12, 25), "123 Main", "Apartment 3", "Hicksville", "NY",
+			"11801", "(888) GET-BENT", null, null, "Nassau", "1600M", null, null, false, false);
+		_service.addPatient("BAR", "Basil", null, "Barnacle", LocalDate.of(1865, 12, 25), "13 Main",null, "Hicksville", "NY",
+				"11801", "(888) GET-BENT", null, null, "Nassau", "1600M", null, null, false, false);
+		List<Person> all = _service.getPatients();
+		assertEquals(2, all.size());
+	}
+
+}
