0d9e64a9621f47b90eadd7f201dcfd03f7669ccb
Tim <hello@timbest.net>
2020-11-21 15:36:11 -0500

    wire up survey response

--- backend/src/main/java/gov/cdc/usds/simplereport/SimpleReportApplication.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/SimpleReportApplication.java
@@ -3,6 +3,9 @@ package gov.cdc.usds.simplereport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import javax.servlet.Filter;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableJpaAuditing // this will move to a separate configuration object when we create user audit trails
@@ -11,4 +14,15 @@ public class SimpleReportApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleReportApplication.class, args);
     }
+
+    /**
+   * Register the {@link OpenEntityManagerInViewFilter} so that the
+   * GraphQL-Servlet can handle lazy loads during execution.
+   *
+   * @return
+   */
+  @Bean
+  public Filter OpenFilter() {
+    return new OpenEntityManagerInViewFilter();
+  }
 }
