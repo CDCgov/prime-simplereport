613964bffdd24e910da5a3706ed6e124fcfd3855
Neil Sharma <neil.s.sharma@omb.eop.gov>
2020-11-20 23:32:22 -0800

    removed another unused import

--- backend/src/main/java/gov/cdc/usds/simplereport/service/OrganizationService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/OrganizationService.java
@@ -2,7 +2,6 @@ package gov.cdc.usds.simplereport.service;
 
 import java.util.Optional;
 import java.util.List;
-import java.util.Iterator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
