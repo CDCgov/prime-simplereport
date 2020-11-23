d81226deb38fedd46bacb6dfcc3dedc802dd0903
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-11-22 20:02:31 -0500

    Remove unused imports.

--- backend/src/main/java/gov/cdc/usds/simplereport/api/upload/CSVUploadController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/upload/CSVUploadController.java
@@ -4,8 +4,6 @@ import gov.cdc.usds.simplereport.service.UploadService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
