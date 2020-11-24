416e9ebac57ad13f388a98f91b56cc88160c50cc
Ben Warfield <bwarfield@cdc.gov>
2020-11-24 00:50:09 -0500

    Missed unused imports.

--- backend/src/test/java/gov/cdc/usds/simplereport/service/PersonServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/PersonServiceTest.java
@@ -7,10 +7,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import gov.cdc.usds.simplereport.db.model.Person;
 import gov.cdc.usds.simplereport.db.repository.BaseRepositoryTest;
