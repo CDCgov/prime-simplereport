2ba63469569bcaecc107187b8fced2da3fb2390f
Tim <hello@timbest.net>
2020-12-02 15:46:51 -0500

    fix date loading

--- frontend/src/app/commonComponents/DateInput.jsx
+++ frontend/src/app/commonComponents/DateInput.jsx
@@ -1,97 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { v4 as uuidv4 } from "uuid";
+import classnames from "classnames";
+import useUniqueId from "../commonComponents/useUniqueIds";
 
 const DateInput = ({
-  values,
+  value = "",
   label,
-  legend,
-  hint,
-  names,
+  name,
+  placeholder,
   onChange,
-  disabled,
+  type,
+  addClass,
+  min,
+  max,
+  disabled
 }) => {
-  let dayId = uuidv4();
-  let monthId = uuidv4();
-  let yearId = uuidv4();
+  let [newId] = useUniqueId("textinput", 1);
+
+  const labelElem = label ? (
+    <label className="usa-label" htmlFor={newId}>
+      {label}
+    </label>
+  ) : null;
 
   return (
-    <React.Fragment>
-      <fieldset className="usa-fieldset">
-        <legend className="usa-legend">{legend}</legend>
-        <span className="usa-hint" id="dobHint">
-          {hint}
-        </span>
-        <div className="usa-memorable-date">
-          <div className="usa-form-group usa-form-group--month">
-            <label className="usa-label" htmlFor={dayId}>
-              Month
-            </label>
-            <input
-              aria-describedby="dobHint"
-              className="usa-input usa-input--inline"
-              id={dayId}
-              name={names.day}
-              type="text"
-              maxLength="2"
-              step="1"
-              min="0"
-              max="31"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              value={values.day}
-              disabled={disabled}
-            />
-          </div>
-          <div className="usa-form-group usa-form-group--day">
-            <label className="usa-label" htmlFor={monthId}>
-              Day
-            </label>
-            <input
-              className="usa-input usa-input--inline"
-              aria-describedby="dobHint"
-              id={monthId}
-              name={names.month}
-              type="text"
-              maxLength="2"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              value={values.month}
-              disabled={disabled}
-            />
-          </div>
-          <div className="usa-form-group usa-form-group--year">
-            <label className="usa-label" htmlFor={yearId}>
-              Year
-            </label>
-            <input
-              className="usa-input usa-input--inline"
-              aria-describedby="dobHint"
-              id={yearId}
-              name={names.year}
-              type="text"
-              minLength="4"
-              maxLength="4"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              value={values.year}
-              disabled={disabled}
-            />
-          </div>
-        </div>
-      </fieldset>
-    </React.Fragment>
+    <div className={classnames("prime-text-input", addClass)}>
+      {labelElem}
+      <input
+        autoComplete="off"
+        className="usa-input"
+        id={newId}
+        name={name}
+        type={type || "date"}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        value={value ?? ""}
+        max={max ?? null}
+        min={min ?? null}
+        disabled={disabled}
+      />
+    </div>
   );
 };
 
 DateInput.propTypes = {
-  legend: PropTypes.string,
-  hint: PropTypes.string,
-  //   values: PropTypes.string,
+  value: PropTypes.string,
   label: PropTypes.string,
-  //   names: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
   onChange: PropTypes.func,
-  disabled: PropTypes.bool,
 };
 
 export default DateInput;
