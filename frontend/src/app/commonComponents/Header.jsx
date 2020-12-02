49d82ba16db010f100f422e2e8ce7a7a668c4493
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2020-12-02 14:15:08 -0800

    Basic support for showing logged in user info the the top nav
    - User initials and user details on click
    - Logout stubbed out. Need to get details for Okta logout since there seems to be several ways to do it. Right now it alerts.
    - User details also shown in "mobile" side-menu view.
    - Minor fixes to layout:
      - close boxes from Test Queue tab had z-index so high it would overlap mobile menu.
      - logo in header was so big it was forcing the whole top nave to be taller than it should be.
      - Used consistent close box for topnav (but it's visibly different from close box for Test Queue delete, which is probably good).

--- frontend/src/app/commonComponents/Header.jsx
+++ frontend/src/app/commonComponents/Header.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PATIENT_TERM_PLURAL_CAP } from "../../config/constants";
 import classNames from "classnames";
-import iconClose from "../../img/close-gray-60.svg";
+import { gql, useQuery } from "@apollo/client";
+import Button from "./Button";
+
+const WHOAMI_QUERY = gql`
+  {
+    whoami {
+      firstName
+      middleName
+      lastName
+      suffix
+      organization {
+        testingFacilityName
+      }
+    }
+  }
+`;
 
 const Header = ({ organizationId }) => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [staffDetailsVisible, setUserDetailsVisible] = useState(false);
+  const { data: whoamidata } = useQuery(WHOAMI_QUERY, {
+    fetchPolicy: "no-cache",
+  });
+  const [staffInitials, setStaffInitials] = useState("");
+  const [staffName, setStaffName] = useState("");
+  const [facilityName, setFacilityName] = useState("");
+  useEffect(() => {
+    if (!whoamidata || !whoamidata.whoami) return;
+    const whoami = whoamidata.whoami;
+    setStaffName(formatFullName(whoami));
+
+    const ch1 = whoami.firstName ? whoami.firstName.charAt(0) : "";
+    const ch2 = whoami.middleName ? whoami.middleName.charAt(0) : "";
+    const ch3 = whoami.lastName ? whoami.lastName.charAt(0) : "";
+    setStaffInitials(`${ch1}${ch2}${ch3}`);
+
+    setFacilityName(whoami.organization.testingFacilityName);
+  }, [whoamidata]);
+
+  const formatFullName = (whoami) => {
+    // this trick will not include spaces if middlename is blank.
+    let result = whoami.firstName;
+    result += whoami.middleName ? ` ${whoami.middleName}` : "";
+    result += whoami.lastName ? ` ${whoami.lastName}` : "";
+    result += whoami.suffix ? `, ${whoami.suffix}` : "";
+    return result;
+  };
+
+  const logout = () => {
+    // todo: do logout here. maybe https://developer.okta.com/docs/guides/sign-users-out/react/sign-out-of-your-app/
+    debugger;
+    alert("OKTA logout here");
+  };
   return (
     <header className="usa-header usa-header--basic">
       <div className="usa-nav-container">
@@ -30,12 +79,13 @@ const Header = ({ organizationId }) => {
             "is-visible": menuVisible,
           })}
         >
-          <button
-            className="usa-nav__close prime-nav-close-button"
+          <span
+            className="fa-layers fa-fw fa-2x usa-nav__close prime-nav-close-button"
             onClick={() => setMenuVisible(false)}
           >
-            <img src={iconClose} alt="close menu" />
-          </button>
+            <FontAwesomeIcon icon={"window-close"} />
+          </span>
+
           <ul className="usa-nav__primary usa-accordion">
             <li className="usa-nav__primary-item">
               <NavLink
@@ -74,6 +124,29 @@ const Header = ({ organizationId }) => {
               </NavLink>
             </li>
 
+            <li className="usa-nav__primary-item prime-staff-infobox-sidemenu prime-settings-hidden">
+              <span className="fa-layers fa-fw">
+                <FontAwesomeIcon icon={"user"} />
+              </span>
+              <span>{staffInitials}</span>
+            </li>
+
+            <li className="usa-nav__primary-item usa-sidenav prime-staff-infobox-sidemenu prime-settings-hidden">
+              <ul className="usa-sidenav__sublist prime-sidenav_inset">
+                <li className="usa-sidenav__item">{staffName}</li>
+                <li className="usa-sidenav__item">{facilityName}</li>
+                <li className="usa-sidenav__item">
+                  <Button
+                    label=" Switch users"
+                    secondaryInverse
+                    big
+                    icon="sign-out-alt"
+                    onClick={() => logout()}
+                  />
+                </li>
+              </ul>
+            </li>
+
             <li className="usa-nav__primary-item prime-settings-hidden">
               <NavLink
                 to={`/organization/${organizationId}/settings`}
@@ -83,7 +156,7 @@ const Header = ({ organizationId }) => {
                   color: "white",
                 }}
               >
-                <FontAwesomeIcon icon={"cog"} size="2x" />
+                <FontAwesomeIcon icon={"cog"} size="2x" /> Settings
               </NavLink>
             </li>
           </ul>
@@ -91,6 +164,51 @@ const Header = ({ organizationId }) => {
 
         <nav aria-label="Primary navigation" className="usa-nav prime-nav">
           <ul className="usa-nav__primary usa-accordion">
+            <li className="usa-nav__primary-item">
+              <NavLink
+                to={`#`}
+                isActive={() => false}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setUserDetailsVisible(!staffDetailsVisible);
+                }}
+                activeClassName="active-nav-item"
+                activeStyle={{
+                  color: "white",
+                }}
+              >
+                <FontAwesomeIcon icon={"user"} size="2x" />
+                <span className={"prime-username-icon-span"}>
+                  {staffInitials}
+                </span>
+              </NavLink>
+              <div
+                aria-label="Primary navigation"
+                className={classNames("usa-nav", "prime-staff-infobox", {
+                  "is-prime-staff-infobox-visible": staffDetailsVisible,
+                })}
+              >
+                <ul className="usa-sidenav__sublist prime-sidenav_inset">
+                  <li className="usa-sidenav__item">{staffName}</li>
+                  <li className="usa-sidenav__item">{facilityName}</li>
+                  <li className="usa-sidenav__item">
+                    <Button
+                      label={" Switch users"}
+                      secondaryInverse
+                      big
+                      icon="sign-out-alt"
+                      onClick={() => logout()}
+                    />
+                  </li>
+                </ul>
+                <span
+                  className="fa-layers fa-fw fa-2x prime-close-button-popup"
+                  onClick={() => setUserDetailsVisible(false)}
+                >
+                  <FontAwesomeIcon icon={"window-close"} />
+                </span>
+              </div>
+            </li>
             <li className="usa-nav__primary-item">
               <NavLink
                 to={`/organization/${organizationId}/settings`}
