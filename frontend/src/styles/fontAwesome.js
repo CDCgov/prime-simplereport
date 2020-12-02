1b6504732a53a0b01a4cb6f2e3fdbc137456c024
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2020-12-02 14:15:08 -0800

    Basic support for showing logged in user info the the top nav
    - User initials and user details on click
    - Logout stubbed out. Need to get details for Okta logout since there seems to be several ways to do it. Right now it alerts.
    - User details also shown in "mobile" side-menu view.
    - Minor fixes to layout:
      - close boxes from Test Queue tab had z-index so high it would overlap mobile menu.
      - logo in header was so big it was forcing the whole top nave to be taller than it should be.
      - Used consistent close box for topnav (but it's visibly different from close box for Test Queue delete, which is probably good).

--- frontend/src/styles/fontAwesome.js
+++ frontend/src/styles/fontAwesome.js
@@ -21,6 +21,8 @@ import {
   faTimesCircle,
   faCircle,
   faCog,
+  faSignOutAlt,
+  faWindowClose,
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
@@ -43,5 +45,7 @@ library.add(
   faArrowLeft,
   faTimesCircle,
   faCircle,
-  faCog
+  faCog,
+  faSignOutAlt,
+  faWindowClose
 );
