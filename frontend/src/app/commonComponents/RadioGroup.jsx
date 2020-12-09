0a41d64172e55cde48fa61c5efc88c0e85d51092
Tim Best <53869143+timothybest-usds@users.noreply.github.com>
2020-12-09 13:18:10 -0500

    Tim best/require feilds (#267)
    * mark required fields as required
    
    * undefiend -> undefined
    
    * remove device unselect
    
    unselecting the device and submiting the form will break the server. If we want this feature we should add it back in with frontend form submit handling
    
    * prettier
    
    Co-authored-by: Tim <hello@timbest.net>

--- frontend/src/app/commonComponents/RadioGroup.jsx
+++ frontend/src/app/commonComponents/RadioGroup.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
 import classnames from "classnames";
 
+import Required from "../commonComponents/Required";
+
 const RadioGroup = ({
   onChange,
   buttons,
@@ -13,6 +15,7 @@ const RadioGroup = ({
   type,
   legend,
   displayLegend = false,
+  required = false,
 }) => {
   // Note: list Affirmations before negations: Yes before No.
   const radioGroupItems = buttons.map((button) => {
@@ -50,6 +53,12 @@ const RadioGroup = ({
     <fieldset className="usa-fieldset prime-radios">
       <legend className={displayLegend ? "usa-legend" : "usa-sr-only"}>
         {legend}
+        {required ? (
+          <span>
+            {" "}
+            <Required />
+          </span>
+        ) : null}
       </legend>
       <ul
         className={
---
+++ frontend/src/app/commonComponents/Required.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Required: React.FC<{}> = () => (
+  <span style={{ color: "#E35024" }}>*</span>
+);
+
+export default Required;
