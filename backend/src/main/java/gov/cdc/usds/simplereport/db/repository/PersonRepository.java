0dcdbe55c8346f504913ee3f6e4c4ea1bb7d8bda
Tim <hello@timbest.net>
2020-12-10 10:33:39 -0500

    update query

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
@@ -20,6 +20,6 @@ public interface PersonRepository extends EternalEntityRepository<Person> {
     @Query(BASE_QUERY + " and internalId = :id and organization = :org")
     public Optional<Person> findByIDAndOrganization(UUID id, Organization org);
 
-    @Query(BASE_QUERY + " where e.organization = :org and (e.facility is null OR e.facility = :fac)")
+    @Query(BASE_QUERY + " AND e.organization = :org AND (e.facility IS NULL OR e.facility = :fac)")
     public List<Person> findByFacilityAndOrganization(Facility fac, Organization org);
 }
