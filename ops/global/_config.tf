a34beb001cce360110d372636bc6148bd07d7fa9
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-12-15 20:23:43 -0500

    Initial migration of global services
    Moved global services into the management RG.
    
    Now, we just need to migrate everything else.

--- ops/global/_config.tf
+++ ops/global/_config.tf
@@ -5,10 +5,19 @@ terraform {
     container_name       = "sr-tfstate"
     key                  = "global/terraform.tfstate"
   }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    okta = {
+      source = "oktadeveloper/okta"
+    }
+  }
+  required_version = ">= 0.13"
 }
 
 provider "azurerm" {
-  version = "~>2.0"
   features {}
   skip_provider_registration = true
 }
