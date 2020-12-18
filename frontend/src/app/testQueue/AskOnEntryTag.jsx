a51d35b5b9d6c3d05c050cb120dd70e570f88dbc
Dave Methvin <david.w.methvin@omb.eop.gov>
2020-12-18 10:36:14 -0500

    Fix PENDING/COMPLETE question status on refresh

--- frontend/src/app/testQueue/AskOnEntryTag.jsx
+++ frontend/src/app/testQueue/AskOnEntryTag.jsx
@@ -1,39 +1,23 @@
 import React from "react";
 
-export const areAnswersComplete = (answerDict) => {
-  if (!answerDict.noSymptoms) {
-    let symptomFound = false;
-
-    try {
-      const symptoms = JSON.parse(answerDict.symptoms);
-      Object.values(symptoms).forEach((val) => {
-        if (val) {
-          symptomFound = true;
-        }
-      });
-    } catch (e) {
-      console.error("expected json response. found:", e);
-    }
-
-    if (!symptomFound) {
-      return false;
-    }
-    if (answerDict.symptomOnset) {
-      const onsetDate = answerDict.symptomOnset;
-      if (!onsetDate) {
-        return false;
-      }
-    }
-  }
-  if (!answerDict.firstTest) {
-    if (!answerDict.priorTestDate) {
-      return false;
-    }
-    if (!answerDict.priorTestType || !answerDict.priorTestResult) {
+export const areAnswersComplete = (answers) => {
+  if (!answers.noSymptoms) {
+    const symptoms = JSON.parse(answers.symptoms);
+    const filled = Object.values(symptoms).some((v) => v === true);
+    if (!filled || !answers.symptomOnset) {
       return false;
     }
   }
-  if (!answerDict.pregnancy) {
+  if (!answers.firstTest) {
+    if (
+      !answers.priorTestDate ||
+      !answers.priorTestType ||
+      !answers.priorTestResult
+    ) {
+      return false;
+    }
+  }
+  if (!answers.pregnancy) {
     return false;
   }
   return true;
