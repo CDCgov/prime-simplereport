ab2761c4fdc888dec9ce7bea7fe3c2e6bb344795
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2020-12-21 09:55:21 -0800

    SonarCloud warns that `SimpleDateFormat` isn't threadsafe.
    It's just temp code, but it's nice to keep SonarCloud happy.

--- backend/src/main/java/gov/cdc/usds/simplereport/service/ScheduledTasksService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/ScheduledTasksService.java
@@ -11,23 +11,22 @@ import java.util.Date;
 @Service
 public class ScheduledTasksService {
     private static final Logger LOG = LoggerFactory.getLogger(ScheduledTasksService.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     private static final int FIXED_DELAY_MS = 1000*60*5;  // fixed period in milliseconds after last task ends
 
     @Scheduled(fixedDelay = FIXED_DELAY_MS)
     public void reportCurrentTime() {
         LOG.info("Fixed puts a delay between tasks, so they can run as long as needed without overlapping.\n" +
-                " The time is now {}", dateFormat.format(new Date()));
+                " The time is now {}", new SimpleDateFormat("HH:mm:ss").format(new Date()));
     }
 
     // see https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/scheduling/support/CronExpression.html
     @Scheduled(cron = "0 0 10 * * *", zone="America/Los_Angeles") // 22 "America/New_York"
     public void runDaily() {
-        LOG.info("Daily run. The time is now {}", dateFormat.format(new Date()));
+        LOG.info("Daily run. The time is now {}", new SimpleDateFormat("HH:mm:ss").format(new Date()));
     }
 
     @Scheduled(cron = "*/60 * * * * *", zone="America/Los_Angeles") // 22 "America/New_York"
     public void quickCronTest() {
-        LOG.info("quickCronTest. The time is now {}", dateFormat.format(new Date()));
+        LOG.info("quickCronTest. The time is now {}", new SimpleDateFormat("HH:mm:ss").format(new Date()));
     }
 }
