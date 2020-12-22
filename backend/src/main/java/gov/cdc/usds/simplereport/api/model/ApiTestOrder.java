df68dd636d5c6fdc1c86229773f4974028f2bbdb
Tim Best <tBest@cdc.gov>
2020-12-22 16:21:43 -0500

    move TestResult API model to data resolver

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/ApiTestOrder.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/ApiTestOrder.java
@@ -6,83 +6,84 @@ import java.time.LocalDate;
 
 import org.json.JSONObject;
 
-import gov.cdc.usds.simplereport.db.model.TestOrder;
 import gov.cdc.usds.simplereport.db.model.DeviceType;
+import gov.cdc.usds.simplereport.db.model.TestOrder;
+import gov.cdc.usds.simplereport.db.model.Person;
 import gov.cdc.usds.simplereport.db.model.auxiliary.AskOnEntrySurvey;
 
 
 public class ApiTestOrder {
 
-	private TestOrder order;
-	private AskOnEntrySurvey survey;
+    private TestOrder order;
+    private AskOnEntrySurvey survey;
 
-	public ApiTestOrder(TestOrder order) {
-		super();
-		this.order = order;
-		this.survey = order.getAskOnEntrySurvey().getSurvey();
-	}
+    public ApiTestOrder(TestOrder order) {
+        super();
+        this.order = order;
+        this.survey = order.getAskOnEntrySurvey().getSurvey();
+    }
 
-	public String getInternalId() {
-		return order.getInternalId().toString();
-	}
+    public String getInternalId() {
+        return order.getInternalId().toString();
+    }
 
-	public Date getDateAdded() {
-		return order.getCreatedAt();
-	}
+    public Date getDateAdded() {
+        return order.getCreatedAt();
+    }
 
-	public String getPregnancy() {
-		return survey.getPregnancy();
-	}
+    public String getPregnancy() {
+        return survey.getPregnancy();
+    }
 
-	public Boolean getNoSymptoms() {
-		return survey.getNoSymptoms();
-	}
+    public Boolean getNoSymptoms() {
+        return survey.getNoSymptoms();
+    }
 
-	public String getSymptoms() {
-		Map<String, Boolean> s = survey.getSymptoms();
-		JSONObject obj = new JSONObject();
-		for (Map.Entry<String,Boolean> entry : s.entrySet()) {
-			obj.put(entry.getKey(), entry.getValue().toString());
-		}
-		return obj.toString();
-	}
+    public String getSymptoms() {
+        Map<String, Boolean> s = survey.getSymptoms();
+        JSONObject obj = new JSONObject();
+        for (Map.Entry<String,Boolean> entry : s.entrySet()) {
+            obj.put(entry.getKey(), entry.getValue().toString());
+        }
+        return obj.toString();
+    }
 
-	public LocalDate getSymptomOnset() {
-		return survey.getSymptomOnsetDate();
-	}
+    public LocalDate getSymptomOnset() {
+        return survey.getSymptomOnsetDate();
+    }
 
-	public Boolean getFirstTest() {
-		return survey.getFirstTest();
-	}
+    public Boolean getFirstTest() {
+        return survey.getFirstTest();
+    }
 
-	public LocalDate getPriorTestDate() {
-		return survey.getPriorTestDate();
-	}
+    public LocalDate getPriorTestDate() {
+        return survey.getPriorTestDate();
+    }
 
-	public String getPriorTestType() {
-		return survey.getPriorTestType();
-	}
+    public String getPriorTestType() {
+        return survey.getPriorTestType();
+    }
 
-	public String getPriorTestResult() {
-		return survey.getPriorTestResult() == null ? "" : survey.getPriorTestResult().toString();
-	}
+    public String getPriorTestResult() {
+        return survey.getPriorTestResult() == null ? "" : survey.getPriorTestResult().toString();
+    }
 
-	public DeviceType getDeviceType() {
-		return order.getDeviceType();
-	}
+    public DeviceType getDeviceType() {
+        return order.getDeviceType();
+    }
 
-	public Patient getPatient() {
-		return new Patient(order.getPatient());
-	}
+    public Person getPatient() {
+        return order.getPatient();
+    }
 
-	public String getResult() {
-		if (order.getTestResult() == null) {
-			return "";
-		}
-		return order.getTestResult().toString();
-	}
+    public String getResult() {
+        if (order.getTestResult() == null) {
+            return "";
+        }
+        return order.getTestResult().toString();
+    }
 
-	public Date getDateTested() {
-		return order.getDateTested();
-	}
+    public Date getDateTested() {
+        return order.getDateTested();
+    }
 }
