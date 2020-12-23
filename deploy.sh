0f219a1ed8528c7089cc99070ea6fae9e078dbf3
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-23 12:45:13 -0500

    test commit2

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,5 @@ fi
 
 if [ "$1" == "demo" ]; then
   git checkout demo
-  echo "Deploying the static landing page to $1"
-  sed -i 's,https://hhs-prime.okta.com/oauth2/default/v1/authorize?client_id=0oa5ahrdfSpxmNZO74h6&amp;response_type=token+id_token&amp;scope=openid&amp;scope=simple_report&amp;redirect_uri=https://simplereport.cdc.gov/app&amp;nonce=thisisnotsafe&amp;state=thisisbogus,https://demo.simplereport.gov/app,' _site/index.html
-  az storage blob upload-batch -s _site/ -d '$web' --account-name simplereportdemoapp --destination-path '/'
+  git reset --hard $GIT_SHA
 fi
