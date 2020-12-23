b23d4c226758b5ba2a1534df88add46fa07455f5
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-23 12:48:06 -0500

    test pushing to demo

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ git pull
 if [ $(git tag -l "$GIT_SHA") ]; then
     echo "Tag already exists."
 else
-    git checkout main
-    git pull origin main
+#    git checkout main
+#    git pull origin main
     git tag $GIT_SHA
     git push origin $GIT_SHA
 fi
@@ -15,4 +15,5 @@ fi
 if [ "$1" == "demo" ]; then
   git checkout demo
   git reset --hard $GIT_SHA
+  git push origin demo
 fi
