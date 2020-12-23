0e45e910fbd6ecfd7587d7f2ada17a0750c1c44b
Ben Warfield <bwarfield@cdc.gov>
2020-12-23 15:57:45 -0500

    Added helper models for IDP-based authorization.
    Created role enumeration and wrote code to convert authority names in our
    specified format to a POJO.

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/authorization/OrganizationRole.java
@@ -0,0 +1,5 @@
+package gov.cdc.usds.simplereport.config.authorization;
+
+public enum OrganizationRole {
+    USER, ADMIN; // ?
+}
---
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/authorization/OrganizationRoles.java
@@ -0,0 +1,26 @@
+package gov.cdc.usds.simplereport.config.authorization;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class OrganizationRoles {
+
+    private String organizationExternalId;
+    private Set<OrganizationRole> grantedRoles;
+
+    public OrganizationRoles(String organizationExternalId, Collection<OrganizationRole> grantedRoles) {
+        super();
+        this.organizationExternalId = organizationExternalId;
+        this.grantedRoles = EnumSet.copyOf(grantedRoles);
+    }
+
+    public String getOrganizationExternalId() {
+        return organizationExternalId;
+    }
+
+    public Set<OrganizationRole> getGrantedRoles() {
+        return Collections.unmodifiableSet(grantedRoles);
+    }
+}
