a4167fc0755a89da1f47ec31b66cb2a3a4893f5b
Tim Best <tBest@cdc.gov>
2020-12-23 18:17:26 -0500

    test test results

---
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/TestResultTest.java
@@ -0,0 +1,65 @@
+package gov.cdc.usds.simplereport.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yannbriancon.interceptor.HibernateQueryInterceptor;
+
+import gov.cdc.usds.simplereport.db.model.Facility;
+import gov.cdc.usds.simplereport.db.model.Organization;
+import gov.cdc.usds.simplereport.db.model.Person;
+import gov.cdc.usds.simplereport.db.model.TestEvent;
+import gov.cdc.usds.simplereport.service.OrganizationService;
+import gov.cdc.usds.simplereport.service.TestOrderService;
+import gov.cdc.usds.simplereport.test_util.TestDataFactory;
+
+public class TestResultTest extends BaseApiTest {
+
+    @Autowired
+    private TestDataFactory _dataFactory;
+    @Autowired
+    private OrganizationService _orgService;
+    @Autowired
+    private TestOrderService _testOrderService;
+
+    private Organization _org;
+    private Facility _site;
+
+    @BeforeEach
+    public void init() {
+        truncateDb();
+        _org = _orgService.getCurrentOrganization();
+        _site = _orgService.getFacilities(_org).get(0);
+    }
+
+    @Test
+    public void fetchTestResults() throws Exception {
+        Person p = _dataFactory.createFullPerson(_org);
+        TestEvent _o = _dataFactory.createTestEvent(p, _site);
+        TestEvent _o1 = _dataFactory.createTestEvent(p, _site);
+        TestEvent _o2 = _dataFactory.createTestEvent(p, _site);
+  
+        ObjectNode variables = getFacilityScopedArguments();
+        ArrayNode testResults = fetchTestResults(variables);
+
+        assertEquals(3, testResults.size());
+    }
+
+    private ObjectNode getFacilityScopedArguments() {
+        return JsonNodeFactory.instance.objectNode()
+                .put("facilityId", _site.getInternalId().toString());
+    }
+
+    private ArrayNode fetchTestResults(ObjectNode variables) {
+        return (ArrayNode) runQuery("test-result-query", variables).get("testResults");
+    }
+
+}
