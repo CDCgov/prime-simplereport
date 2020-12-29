d385c76583e789a7bb81b86d2e40bdc1ed6e19c8
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2020-12-29 16:48:52 -0500

    privatelink setup for dev/test/demo databases to allow private connections (#445)
    refactored the dev environment to match other tf envs

--- ops/dev/persistent/network.tf
+++ ops/dev/persistent/network.tf
@@ -1,10 +1,8 @@
-# Create the virtual network and the persistent subnets
 resource "azurerm_virtual_network" "vn" {
   name                = "simple-report-${local.env}-network"
-  resource_group_name = data.azurerm_resource_group.test.name
-  location            = data.azurerm_resource_group.test.location
-  address_space = [
-  "10.3.0.0/16"]
+  resource_group_name = data.azurerm_resource_group.dev.name
+  location            = data.azurerm_resource_group.dev.location
+  address_space       = ["10.1.0.0/16"]
 
   tags = local.management_tags
 }
@@ -12,24 +10,25 @@ resource "azurerm_virtual_network" "vn" {
 # VMs subnet
 resource "azurerm_subnet" "vms" {
   name                                           = "${local.env}-vms"
-  resource_group_name                            = data.azurerm_resource_group.test.name
+  resource_group_name                            = data.azurerm_resource_group.dev.name
   virtual_network_name                           = azurerm_virtual_network.vn.name
-  address_prefixes                               = ["10.3.252.0/24"]
+  address_prefixes                               = ["10.1.252.0/24"]
   enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_subnet" "lbs" {
   name                 = "${local.project}-${local.name}-${local.env}-lb"
-  resource_group_name  = data.azurerm_resource_group.test.name
+  resource_group_name  = data.azurerm_resource_group.dev.name
   virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes     = ["10.3.254.0/24"]
+  address_prefixes     = ["10.1.254.0/24"]
 }
 
 resource "azurerm_subnet" "webapp" {
   name                 = "${local.project}-${local.name}-${local.env}-webapp"
-  resource_group_name  = data.azurerm_resource_group.test.name
+  resource_group_name  = data.azurerm_resource_group.dev.name
   virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes     = ["10.3.100.0/24"]
+  address_prefixes = [
+  "10.1.100.0/24"]
 
   lifecycle {
     ignore_changes = [delegation]
--- ops/services/postgres_db/_output.tf
+++ ops/services/postgres_db/_output.tf
@@ -2,6 +2,10 @@ output "server_name" {
   value = azurerm_postgresql_server.db.name
 }
 
+output "server_id" {
+  value = azurerm_postgresql_server.db.id
+}
+
 output "server_fqdn" {
   value = azurerm_postgresql_server.db.fqdn
 }
