e8ced604d8b9d8b792a22f288ba63d0be414bdfc
Ben Warfield <bwarfield@cdc.gov>
2021-01-02 23:14:03 -0500

    # This is a combination of 2 commits. # This is the 1st commit message:
    DRAFT outline of Java/DB changes to support specimen types.
    
    # This is the commit message #2:
    
    Added specimen location code/name, and SpecimenTypeRepository.

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/SpecimenTypeRepository.java
@@ -0,0 +1,7 @@
+package gov.cdc.usds.simplereport.db.repository;
+
+import gov.cdc.usds.simplereport.db.model.SpecimenType;
+
+public interface SpecimenTypeRepository extends EternalEntityRepository<SpecimenType> {
+
+}
---
+++ backend/src/main/java/gov/cdc/usds/simplereport/validators/NumericCode.java
@@ -0,0 +1,42 @@
+package gov.cdc.usds.simplereport.validators;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * A constraint to force a value to be a nullable numeric string of length at
+ * least 8 (adjust this value up or down if it turns out that is not the minimum
+ * length for valid CLIA numbers and SNOMED codes).
+ */
+
+@Constraint(validatedBy = {})
+@NotNull
+@Pattern(regexp = "\\d+")
+@Size(min = NumericCode.MIN_DIGITS)
+@Documented
+@Retention(RUNTIME)
+@Target({ FIELD, PARAMETER, LOCAL_VARIABLE, ANNOTATION_TYPE })
+public @interface NumericCode {
+
+    public static final int MIN_DIGITS = 8;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String message() default "Parameter must be a numeric code string";
+
+}
