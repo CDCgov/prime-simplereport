56c8cc9829342f5c885bf0e0608986d37302b390
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-01-04 11:59:52 -0500

    placeholder for slack webhook

--- ops/dev/main.tf
+++ ops/dev/main.tf
@@ -38,6 +38,7 @@ module "simple_report_api" {
     SPRING_DATASOURCE_URL                            = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sr_dev_db_jdbc.id})"
     DATAHUB_API_KEY                                  = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.datahub_api_key.id})"
     APPLICATIONINSIGHTS_CONNECTION_STRING            = "InstrumentationKey=${data.azurerm_application_insights.app_insights.instrumentation_key};IngestionEndpoint=https://eastus-1.in.applicationinsights.azure.com/"
+    SECRET_SLACK_NOTIFY_WEBHOOK_URL                  = ""
   }
 }
 
