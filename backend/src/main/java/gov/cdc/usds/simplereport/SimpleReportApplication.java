cc943d67c1fe2dbf390cbbefb650ca7fe618d078
Ben Warfield <bwarfield@cdc.gov>
2021-01-06 00:26:41 -0500

    Rearranged lazy-loading support and added a naive health check.
    Moved the open-in-view filter configuration out of the application and into
    a standard Spring web configurer, explicitly disabled the open-in-view
    interceptor (which was useless), and added a health check on /health
    just for fun.

--- backend/src/main/java/gov/cdc/usds/simplereport/SimpleReportApplication.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/SimpleReportApplication.java
@@ -1,12 +1,8 @@
 package gov.cdc.usds.simplereport;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 import gov.cdc.usds.simplereport.config.AuthorizationProperties;
 import gov.cdc.usds.simplereport.config.InitialSetupProperties;
@@ -23,15 +19,4 @@ public class SimpleReportApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleReportApplication.class, args);
     }
-
-    /**
-   * Register the {@link OpenEntityManagerInViewFilter} so that the
-   * GraphQL-Servlet can handle lazy loads during execution.
-   *
-   * @return
-   */
-  @Bean
-  public Filter OpenFilter() {
-    return new OpenEntityManagerInViewFilter();
-  }
 }
---
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/WebConfiguration.java
@@ -0,0 +1,37 @@
+package gov.cdc.usds.simplereport.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnWebApplication
+public class WebConfiguration implements WebMvcConfigurer {
+
+    public static final String HEALTH_CHECK = "/health";
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addStatusController(HEALTH_CHECK, HttpStatus.OK);
+    }
+
+    /**
+     * We want (at least for now) to be able to lazy-load JPA relationships in the
+     * graphql resolver, even after exiting service-layer transactions.
+     *
+     * Since the graphql servlet does not register a handler with Spring, the Spring
+     * HandlerInterceptor does not work: instead, register a servlet filter with the
+     * same effect.
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean<OpenEntityManagerInViewFilter> openInViewFilter() {
+        return new FilterRegistrationBean<OpenEntityManagerInViewFilter>(new OpenEntityManagerInViewFilter());
+    }
+}
