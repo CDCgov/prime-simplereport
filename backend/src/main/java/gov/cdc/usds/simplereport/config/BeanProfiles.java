f47744c39fd42fe098966bdd9f10bfe8699698c7
Ben Warfield <bwarfield@cdc.gov>
2021-01-05 21:03:25 -0500

    Turned constant-holder from an interface to a class.
    Sonar thinks having this be an interface was a bad idea,
    and upon consideration I agree.

--- backend/src/main/java/gov/cdc/usds/simplereport/config/BeanProfiles.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/BeanProfiles.java
@@ -1,22 +1,25 @@
 package gov.cdc.usds.simplereport.config;
 
 /**
- * Constant-holding interface to describe the bean profiles available in this
+ * Constant-holding class to describe the bean profiles available in this
  * application.
  */
-public interface BeanProfiles {
+public final class BeanProfiles {
 
     /** Profile for testing authentication and authorization */
-    String AUTHORIZATION_DEV = "auth-dev";
+    public static final String AUTHORIZATION_DEV = "auth-dev";
     /** Profile for mocking and bypassing most security (DEV OR TEST ONLY) */
-    String NO_SECURITY = "no-security";
+    public static final String NO_SECURITY = "no-security";
     /**
      * Profile where we assume there is a single tenant and do not check for roles
      * in the authentication token.
      */
-    String SINGLE_TENANT = "single-tenant";
+    public static final String SINGLE_TENANT = "single-tenant";
     /**
      * Profile for local development, sometimes also used for QA/Demo environments.
      */
-    String DEVELOPMENT = "dev";
+    public static final String DEVELOPMENT = "dev";
+
+    private BeanProfiles() { /* no instances! */ }
+
 }
