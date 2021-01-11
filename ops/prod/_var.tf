78f984c9d3832e1722e868ae8ee92fe0ac783b48
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-01-11 10:05:39 -0500

    prod db & storage account

---
+++ ops/prod/_var.tf
@@ -0,0 +1,4 @@
+//variable "acr_image_tag" {
+//  description = "Simple report Api ACR tag to deploy"
+//  type        = string
+//}
---
+++ ops/prod/main.tf
@@ -0,0 +1,49 @@
+locals {
+  project = "prime"
+  name    = "simple-report"
+  env     = "prod"
+  management_tags = {
+    prime-app      = "simple-report"
+    environment    = local.env
+    resource_group = "${local.project}-${local.name}-${local.env}"
+  }
+}
+
+# Frontend React App
+resource "azurerm_storage_account" "app" {
+  account_replication_type  = "GRS" # Cross-regional redundancy
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  name                      = "simplereport${local.env}app"
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  location                  = data.azurerm_resource_group.rg.location
+  enable_https_traffic_only = false
+  min_tls_version           = "TLS1_2"
+
+  static_website {
+    index_document = "index.html"
+  }
+  tags = local.management_tags
+}
+
+resource "azurerm_cdn_profile" "cdn_profile" {
+  name                = "${local.name}-${local.env}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Standard_Microsoft"
+  tags                = local.management_tags
+}
+
+resource "azurerm_cdn_endpoint" "cdn_endpoint" {
+  name                          = "${local.name}-${local.env}"
+  profile_name                  = azurerm_cdn_profile.cdn_profile.name
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  location                      = data.azurerm_resource_group.rg.location
+  origin_host_header            = azurerm_storage_account.app.primary_web_host
+  querystring_caching_behaviour = "IgnoreQueryString"
+
+  origin {
+    name      = "${local.name}-${local.env}-static"
+    host_name = azurerm_storage_account.app.primary_web_host
+  }
+}
