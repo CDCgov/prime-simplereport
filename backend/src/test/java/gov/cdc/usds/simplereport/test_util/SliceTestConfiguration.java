6ae84e3ade0960b606c34dc8f8129574e2f4da52
Ben Warfield <bwarfield@cdc.gov>
2021-01-14 16:15:29 -0500

    Refactored service tests that have an authorization component.
    Added explicit annotation-based configuration for admin and non-admin
    users in tests, and eliminated split between admin and non-admin tests.
    
    Also removed admin user from injectable configuration, as it was not being
    used anywhere outside of tests.

--- backend/src/test/java/gov/cdc/usds/simplereport/test_util/SliceTestConfiguration.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/test_util/SliceTestConfiguration.java
@@ -6,10 +6,23 @@ import gov.cdc.usds.simplereport.config.simplereport.AdminEmailList;
 import gov.cdc.usds.simplereport.config.simplereport.DataHubConfig;
 import gov.cdc.usds.simplereport.service.ApiUserService;
 import gov.cdc.usds.simplereport.service.OrganizationInitializingService;
+import gov.cdc.usds.simplereport.service.model.IdentityAttributes;
 import gov.cdc.usds.simplereport.service.model.IdentitySupplier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 /**
  * Bean creation and wiring required to get slice tests to run without a full
@@ -20,8 +33,39 @@ import org.springframework.context.annotation.Import;
 @EnableConfigurationProperties({InitialSetupProperties.class, AdminEmailList.class, DataHubConfig.class})
 public class SliceTestConfiguration {
 
+    public static final String SITE_ADMIN_USER = "ruby@example.com";
+    public static final String STANDARD_USER = "bob@example.com";
+
+    private static final Map<String, IdentityAttributes> TEST_USERS = Map.of(
+            STANDARD_USER, new IdentityAttributes(STANDARD_USER, "Bobbity", "Bob", "Bobberoo", null),
+            SITE_ADMIN_USER, new IdentityAttributes(SITE_ADMIN_USER, "Ruby", "Raven", "Reynolds", null));
+
     @Bean
-    public IdentitySupplier dummyIdentityProvider(InitialSetupProperties props) {
-        return props::getDefaultUser;
+    public IdentitySupplier testIdentityProvider() {
+        return () -> {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth == null) {
+                // needed for running the commandlinerunner that we don't actually need
+                return TEST_USERS.get(STANDARD_USER);
+            }
+            String username = auth.getName();
+            LoggerFactory.getLogger(SliceTestConfiguration.class).warn("Found username {} for auth {}", username, auth);
+            return TEST_USERS.get(username);
+        };
     }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.METHOD, ElementType.TYPE })
+    @WithMockUser(username = STANDARD_USER, authorities = { "TEST-TENANT:DIS_ORG:USER" })
+    @Inherited
+    public @interface WithSimpleReportStandardUser {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.METHOD, ElementType.TYPE })
+    @WithMockUser(username = SITE_ADMIN_USER, authorities = { "TEST-TENANT:DIS_ORG:USER" })
+    @Inherited
+    public @interface WithSimpleReportSiteAdminUser {
+    }
+
 }
