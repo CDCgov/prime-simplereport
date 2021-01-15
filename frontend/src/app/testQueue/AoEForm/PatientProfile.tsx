2e99b0bd01d6290fed0908a4b8e3a81bcfcb90d0
Maya Benari <hello@maya-benari.com>
2021-01-15 12:42:26 -0800

    Copy update

--- frontend/src/app/testQueue/AoEForm/PatientProfile.tsx
+++ frontend/src/app/testQueue/AoEForm/PatientProfile.tsx
@@ -27,7 +27,7 @@ const PatientProfile = (props: Props) => {
       <div className="usa-alert usa-alert--info margin-bottom-3">
         <div className="usa-alert__body">
           <p className="usa-alert__text">
-            Confirm your profile info is correct. If it's incorrect, tell the
+            Confirm that your profile info is correct. If it's incorrect, tell the
             test administrator.
           </p>
         </div>
