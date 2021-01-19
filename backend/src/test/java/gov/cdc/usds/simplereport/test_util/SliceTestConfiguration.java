183c96d44161adb1035ba087ba89b93c6f31d20d
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-01-19 10:41:32 -0500

    more PR changes

--- backend/src/test/java/gov/cdc/usds/simplereport/test_util/SliceTestConfiguration.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/test_util/SliceTestConfiguration.java
@@ -2,22 +2,25 @@ package gov.cdc.usds.simplereport.test_util;
 
 import gov.cdc.usds.simplereport.config.AuditingConfig;
 import gov.cdc.usds.simplereport.config.InitialSetupProperties;
+import gov.cdc.usds.simplereport.config.AuthorizationProperties;
 import gov.cdc.usds.simplereport.config.simplereport.AdminEmailList;
 import gov.cdc.usds.simplereport.config.simplereport.DataHubConfig;
 import gov.cdc.usds.simplereport.service.ApiUserService;
+import gov.cdc.usds.simplereport.service.OktaService;
 import gov.cdc.usds.simplereport.service.OrganizationInitializingService;
 import gov.cdc.usds.simplereport.service.model.IdentitySupplier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import com.okta.spring.boot.sdk.config.OktaClientProperties;
 
 /**
  * Bean creation and wiring required to get slice tests to run without a full
  * application context being created. This is not annotated with a Spring
  * stereotype because we very much do not want it to be picked up automatically!
  */
-@Import({ TestDataFactory.class, AuditingConfig.class, ApiUserService.class, OrganizationInitializingService.class })
-@EnableConfigurationProperties({InitialSetupProperties.class, AdminEmailList.class, DataHubConfig.class})
+@Import({ TestDataFactory.class, AuditingConfig.class, OktaService.class, ApiUserService.class, OrganizationInitializingService.class })
+@EnableConfigurationProperties({InitialSetupProperties.class, OktaClientProperties.class, AuthorizationProperties.class, AdminEmailList.class, DataHubConfig.class})
 public class SliceTestConfiguration {
 
     @Bean
