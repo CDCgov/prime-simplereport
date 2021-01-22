7b0d91f97843bf050f40e7ed6983b93dfa9091bc
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-01-22 15:55:56 -0500

    resolving Spring/Okta inconsistenies

--- backend/src/test/java/gov/cdc/usds/simplereport/service/OktaServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/OktaServiceTest.java
@@ -67,9 +67,7 @@ public class OktaServiceTest extends BaseServiceTest<OktaService> {
 
         User user = _oktaClient.listUsers(AMOS.getUsername(), null, null, null, null).single();
         assertEquals(user.getProfile().getFirstName(), AMOS.getFirstName());
-        assertEquals(user.getProfile().getMiddleName(), AMOS.getMiddleName());
         assertEquals(user.getProfile().getLastName(), AMOS.getLastName());
-        assertEquals(user.getProfile().getHonorificSuffix(), AMOS.getSuffix());
         assertEquals(user.getProfile().getLogin(), AMOS.getUsername());
         assertEquals(user.getProfile().getEmail(), AMOS.getUsername());
 
@@ -95,9 +93,7 @@ public class OktaServiceTest extends BaseServiceTest<OktaService> {
 
         User user = _oktaClient.listUsers(BRAD.getUsername(), null, null, null, null).single();
         assertEquals(user.getProfile().getFirstName(), BRAD.getFirstName());
-        assertEquals(user.getProfile().getMiddleName(), BRAD.getMiddleName());
         assertEquals(user.getProfile().getLastName(), BRAD.getLastName());
-        assertEquals(user.getProfile().getHonorificSuffix(), BRAD.getSuffix());
         assertEquals(user.getProfile().getLogin(), BRAD.getUsername());
         assertEquals(user.getProfile().getEmail(), BRAD.getUsername());
 
