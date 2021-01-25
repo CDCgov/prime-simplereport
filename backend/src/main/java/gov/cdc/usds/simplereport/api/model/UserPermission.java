1f28e50af639645dafab0c9d8dd786bcfa615f06
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-01-25 17:51:20 -0500

    As useful as the placeholder values were, I've replaced them.

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/UserPermission.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/UserPermission.java
@@ -1,7 +1,11 @@
 package gov.cdc.usds.simplereport.api.model;
 
 public enum UserPermission {
-    THIS, 
-	THAT,
-	MORE;
+	READ_PATIENT_LIST,
+	READ_RESULT_LIST,
+	EDIT_PATIENT,
+	EDIT_FACILITY,
+	EDIT_ORGANIZATION,
+	START_TEST,
+	UPDATE_TEST;
 }
