faf0e16ebf1eebd42cefe9a97d236761fa85508e
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-01-26 17:07:22 -0500

    quieting problematic okta integration tests

--- backend/src/test/java/gov/cdc/usds/simplereport/api/BaseApiTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/BaseApiTest.java
@@ -28,6 +28,7 @@ import gov.cdc.usds.simplereport.config.AuthorizationProperties;
 import gov.cdc.usds.simplereport.config.authorization.OrganizationRole;
 import gov.cdc.usds.simplereport.config.authorization.OrganizationRoles;
 import gov.cdc.usds.simplereport.service.AuthorizationService;
+import gov.cdc.usds.simplereport.service.OktaService;
 import gov.cdc.usds.simplereport.service.OrganizationInitializingService;
 import gov.cdc.usds.simplereport.service.model.IdentitySupplier;
 import gov.cdc.usds.simplereport.test_util.DbTruncator;
@@ -59,15 +60,17 @@ public abstract class BaseApiTest {
     @Autowired
     protected GraphQLTestTemplate _template; // screw delegation
 
-    @MockBean
-    protected AuthorizationService _authService;
-    @MockBean
-    protected IdentitySupplier _supplier;
-
     @Autowired
     protected AuthorizationProperties _authorizationProperties;
     @Autowired
     private OktaClientProperties _oktaClientProperties;
+
+    @MockBean
+    protected AuthorizationService _authService;
+    @MockBean
+    protected IdentitySupplier _supplier;
+    @MockBean
+    protected OktaService _oktaService;
 
     protected Client _oktaClient;
 
