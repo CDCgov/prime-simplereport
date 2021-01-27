a2df71e04a6f0545879adce8ed8c0f1bbbfda486
Ben Warfield <bwarfield@cdc.gov>
2021-01-27 00:35:01 -0500

    Added role description to whoami output.
    Refactored the "find the applicable role" code to make it more coherent,
    and tweaked descriptions to make sense for the requirements of #659.

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/User.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/User.java
@@ -1,35 +1,46 @@
 package gov.cdc.usds.simplereport.api.model;
 
 import java.util.Optional;
+import java.util.ArrayList;
 import java.util.List;
 
 import gov.cdc.usds.simplereport.config.authorization.UserPermission;
 import gov.cdc.usds.simplereport.db.model.ApiUser;
 import gov.cdc.usds.simplereport.db.model.Organization;
-import gov.cdc.usds.simplereport.db.model.auxiliary.PersonName;
+import gov.cdc.usds.simplereport.service.model.CurrentOrganizationRoles;
 
 public class User {
 
-	private String id;
+    private ApiUser wrapped;
 	private Optional<Organization> org;
-	private PersonName nameInfo;
-	private String email;
 	private Boolean isAdmin;
+    private String roleDescription;
 	private List<UserPermission> permissions;
 
-	public User(ApiUser user, Optional<Organization> org, Boolean isAdmin, List<UserPermission> permissions) {
+    public User(ApiUser user, Optional<CurrentOrganizationRoles> orgwrapper, boolean isAdmin) {
+        this.wrapped = user;
+        this.org = orgwrapper.map(CurrentOrganizationRoles::getOrganization);
+        this.permissions = new ArrayList<>();
+        this.isAdmin = isAdmin;
+        if (orgwrapper.isPresent()) {
+            permissions.addAll(orgwrapper.get().getGrantedPermissions());
+            roleDescription = orgwrapper.get().getEffectiveRole().get().getDescription();
+        }
+        if (isAdmin) {
+            roleDescription = "Super Admin";
+        }
+    }
+
+    public User(ApiUser user, Optional<Organization> org, Boolean isAdmin, List<UserPermission> permissions) {
 		super();
-		this.id = user.getInternalId().toString();
+        this.wrapped = user;
 		this.org = org;
-		this.nameInfo = user.getNameInfo();
-		// Note: we assume a user's email and login username are the same thing.
-		this.email = user.getLoginEmail();
 		this.isAdmin = isAdmin;
 		this.permissions = permissions;
 	}
 
 	public String getId() {
-		return id;
+        return wrapped.getInternalId().toString();
 	}
 
 	public Optional<Organization> getOrganization() {
@@ -37,23 +48,24 @@ public class User {
 	}
 
 	public String getFirstName() {
-		return nameInfo.getFirstName();
+        return wrapped.getNameInfo().getFirstName();
 	}
 
 	public String getMiddleName() {
-		return nameInfo.getMiddleName();
+        return wrapped.getNameInfo().getMiddleName();
 	}
 
 	public String getLastName() {
-		return nameInfo.getLastName();
+        return wrapped.getNameInfo().getLastName();
 	}
 
 	public String getSuffix() {
-		return nameInfo.getSuffix();
+        return wrapped.getNameInfo().getSuffix();
 	}
 
+    // Note: we assume a user's email and login username are the same thing.
 	public String getEmail() {
-		return email;
+        return wrapped.getLoginEmail();
 	}
 
 	public Boolean getIsAdmin() {
@@ -63,4 +75,8 @@ public class User {
 	public List<UserPermission> getPermissions() {
 		return permissions;
 	}
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
 }
