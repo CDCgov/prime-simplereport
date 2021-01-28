624eea828121241f697a27741193e5feae3a8428
Nick Clyde <nick@clyde.tech>
2021-01-28 13:45:31 -0800

    Fix backend test?

--- backend/src/test/java/gov/cdc/usds/simplereport/service/TestOrderServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/TestOrderServiceTest.java
@@ -26,131 +26,132 @@ import gov.cdc.usds.simplereport.test_util.SliceTestConfiguration.WithSimpleRepo
 @SuppressWarnings("checkstyle:MagicNumber")
 public class TestOrderServiceTest extends BaseServiceTest<TestOrderService> {
 
-    @Autowired
-    private DeviceTypeRepository _deviceTypeRepo;
-    @Autowired
-    private OrganizationService _organizationService;
-    @Autowired
-    private PersonService _personService;
-    @Autowired
-    private HibernateQueryInterceptor hibernateQueryInterceptor;
-    @Autowired
-    private TestDataFactory _dataFactory;
+        @Autowired
+        private DeviceTypeRepository _deviceTypeRepo;
+        @Autowired
+        private OrganizationService _organizationService;
+        @Autowired
+        private PersonService _personService;
+        @Autowired
+        private HibernateQueryInterceptor hibernateQueryInterceptor;
+        @Autowired
+        private TestDataFactory _dataFactory;
 
-    @BeforeEach
-    void setupData() {
-        initSampleData();
-    }
-    
-    @Test
-    public void addPatientToQueue() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.",
-                LocalDate.of(1865, 12, 25), "123 Main", "Apartment 3", "Syracuse", "NY", "11801",
-                "8883334444", "STAFF", null, "Nassau", null, null, null, false, false);
+        @BeforeEach
+        void setupData() {
+                initSampleData();
+        }
 
-        _service.addPatientToQueue(facility.getInternalId(), p, "",
-                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
-                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
+        @Test
+        public void addPatientToQueue() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.", LocalDate.of(1865, 12, 25),
+                                "123 Main", "Apartment 3", "Syracuse", "NY", "11801", "8883334444", "STAFF", null,
+                                "Nassau", null, null, null, false, false);
 
-        List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
-        assertEquals(1, queue.size());
-    }
+                _service.addPatientToQueue(facility.getInternalId(), p, "", Collections.<String, Boolean>emptyMap(),
+                                false, LocalDate.of(1865, 12, 25), "", TestResult.POSITIVE, LocalDate.of(1865, 12, 25),
+                                false);
 
-    @Test
-    @WithSimpleReportStandardUser
-    public void addTestResult() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.",
-                LocalDate.of(1865, 12, 25), "123 Main", "Apartment 3", "Syracuse", "NY", "11801",
-                "8883334444", "STAFF", null, "Nassau", null, null, null, false, false);
-        _service.addPatientToQueue(facility.getInternalId(), p, "",
-                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
-                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
-        DeviceType devA = _deviceTypeRepo.save(new DeviceType("A", "B", "C", "D", "E"));
+                List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
+                assertEquals(1, queue.size());
+        }
 
-        _service.addTestResult(devA.getInternalId().toString(), TestResult.POSITIVE,
-                p.getInternalId().toString(), null);
+        @Test
+        @WithSimpleReportStandardUser
+        public void addTestResult() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.", LocalDate.of(1865, 12, 25),
+                                "123 Main", "Apartment 3", "Syracuse", "NY", "11801", "8883334444", "STAFF", null,
+                                "Nassau", null, null, null, false, false);
+                _service.addPatientToQueue(facility.getInternalId(), p, "", Collections.<String, Boolean>emptyMap(),
+                                false, LocalDate.of(1865, 12, 25), "", TestResult.POSITIVE, LocalDate.of(1865, 12, 25),
+                                false);
+                DeviceType devA = _deviceTypeRepo.save(new DeviceType("A", "B", "C", "D", "E"));
 
-        List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
-        assertEquals(0, queue.size());
-    }
+                _service.addTestResult(devA.getInternalId().toString(), TestResult.POSITIVE,
+                                p.getInternalId().toString(), null);
 
-    @Test
-    @WithSimpleReportStandardUser
-    public void editTestResult_standardUser_ok() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.",
-                LocalDate.of(1865, 12, 25), "123 Main", "Apartment 3", "Syracuse", "NY", "11801",
-                "8883334444", "STAFF", null, "Nassau", null, null, null, false, false);
-        TestOrder o = _service.addPatientToQueue(facility.getInternalId(), p, "",
-                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
-                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
-        DeviceType devA = _deviceTypeRepo.save(new DeviceType("A", "B", "C", "D", "E"));
+                List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
+                assertEquals(0, queue.size());
+        }
 
-        _service.editQueueItem(o.getInternalId().toString(), devA.getInternalId().toString(),
-                TestResult.POSITIVE.toString(), null);
+        @Test
+        @WithSimpleReportStandardUser
+        public void editTestResult_standardUser_ok() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _personService.addPatient(null, "FOO", "Fred", null, "", "Sr.", LocalDate.of(1865, 12, 25),
+                                "123 Main", "Apartment 3", "Syracuse", "NY", "11801", "8883334444", "STAFF", null,
+                                "Nassau", null, null, null, false, false);
+                TestOrder o = _service.addPatientToQueue(facility.getInternalId(), p, "",
+                                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
+                                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
+                DeviceType devA = _deviceTypeRepo.save(new DeviceType("A", "B", "C", "D", "E"));
 
-        List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
-        assertEquals(1, queue.size());
-        assertEquals(TestResult.POSITIVE, queue.get(0).getTestResult());
-        assertEquals(devA.getInternalId(), queue.get(0).getDeviceType().getInternalId());
-    }
+                _service.editQueueItem(o.getInternalId().toString(), devA.getInternalId().toString(),
+                                TestResult.POSITIVE.toString(), null);
 
-    @Test
-    @WithSimpleReportEntryOnlyUser
-    public void editTestResult_entryOnlyUser_ok() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _dataFactory.createFullPerson(org);
-        TestOrder o = _service.addPatientToQueue(facility.getInternalId(), p, "",
-                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
-                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
-        DeviceType devA = _dataFactory.getGenericDevice();
+                List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
+                assertEquals(1, queue.size());
+                assertEquals(TestResult.POSITIVE, queue.get(0).getTestResult());
+                assertEquals(devA.getInternalId(), queue.get(0).getDeviceType().getInternalId());
+        }
 
-        _service.editQueueItem(o.getInternalId().toString(), devA.getInternalId().toString(),
-                TestResult.POSITIVE.toString(), null);
+        @Test
+        @WithSimpleReportEntryOnlyUser
+        public void editTestResult_entryOnlyUser_ok() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _dataFactory.createFullPerson(org);
+                TestOrder o = _service.addPatientToQueue(facility.getInternalId(), p, "",
+                                Collections.<String, Boolean>emptyMap(), false, LocalDate.of(1865, 12, 25), "",
+                                TestResult.POSITIVE, LocalDate.of(1865, 12, 25), false);
+                DeviceType devA = _dataFactory.getGenericDevice();
 
-        List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
-        assertEquals(1, queue.size());
-        assertEquals(TestResult.POSITIVE, queue.get(0).getTestResult());
-        assertEquals(devA.getInternalId(), queue.get(0).getDeviceType().getInternalId());
-    }
-    @Test
-    @WithSimpleReportStandardUser
-    public void fetchTestResults_standardUser_ok() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _dataFactory.createFullPerson(org);
-        TestEvent _e = _dataFactory.createTestEvent(p, facility);
+                _service.editQueueItem(o.getInternalId().toString(), devA.getInternalId().toString(),
+                                TestResult.POSITIVE.toString(), null);
 
-        // Count queries with one order
-        hibernateQueryInterceptor.startQueryCount();
-        _service.getTestResults(facility.getInternalId().toString());
-        assertEquals(9, hibernateQueryInterceptor.getQueryCount());
+                List<TestOrder> queue = _service.getQueue(facility.getInternalId().toString());
+                assertEquals(1, queue.size());
+                assertEquals(TestResult.POSITIVE, queue.get(0).getTestResult());
+                assertEquals(devA.getInternalId(), queue.get(0).getDeviceType().getInternalId());
+        }
 
-        // Count queries with three order
-        TestEvent _e1 = _dataFactory.createTestEvent(p, facility);
-        TestEvent _e2 = _dataFactory.createTestEvent(p, facility);
-        hibernateQueryInterceptor.startQueryCount();
-        _service.getTestResults(facility.getInternalId().toString());
-        assertEquals(9, hibernateQueryInterceptor.getQueryCount());
-    }
+        @Test
+        @WithSimpleReportStandardUser
+        public void fetchTestResults_standardUser_ok() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _dataFactory.createFullPerson(org);
+                TestEvent _e = _dataFactory.createTestEvent(p, facility);
 
-    @Test
-    @WithSimpleReportEntryOnlyUser
-    void fetchTestResults_entryOnlyUser_error() {
-        Organization org = _organizationService.getCurrentOrganization();
-        Facility facility = _organizationService.getFacilities(org).get(0);
-        Person p = _dataFactory.createFullPerson(org);
-        _dataFactory.createTestEvent(p, facility);
+                // Count queries with one order
+                hibernateQueryInterceptor.startQueryCount();
+                _service.getTestResults(facility.getInternalId().toString());
+                assertEquals(10, hibernateQueryInterceptor.getQueryCount());
 
-        // https://github.com/CDCgov/prime-simplereport/issues/677
-        // assertSecurityError(() ->
-        // _service.getTestResults(facility.getInternalId().toString()));
-        assertSecurityError(() -> _service.getTestResults(p));
-    }
+                // Count queries with three order
+                TestEvent _e1 = _dataFactory.createTestEvent(p, facility);
+                TestEvent _e2 = _dataFactory.createTestEvent(p, facility);
+                hibernateQueryInterceptor.startQueryCount();
+                _service.getTestResults(facility.getInternalId().toString());
+                assertEquals(12, hibernateQueryInterceptor.getQueryCount());
+        }
+
+        @Test
+        @WithSimpleReportEntryOnlyUser
+        void fetchTestResults_entryOnlyUser_error() {
+                Organization org = _organizationService.getCurrentOrganization();
+                Facility facility = _organizationService.getFacilities(org).get(0);
+                Person p = _dataFactory.createFullPerson(org);
+                _dataFactory.createTestEvent(p, facility);
+
+                // https://github.com/CDCgov/prime-simplereport/issues/677
+                // assertSecurityError(() ->
+                // _service.getTestResults(facility.getInternalId().toString()));
+                assertSecurityError(() -> _service.getTestResults(p));
+        }
 }
