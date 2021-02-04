4b51319300f32c88d4f595532647576fe756e89c
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-02-04 12:11:18 -0500

    simplified function code

--- ops/services/app_functions/slack_forwarding/functions/AlertRouter/index.ts
+++ ops/services/app_functions/slack_forwarding/functions/AlertRouter/index.ts
@@ -1,4 +1,3 @@
-import {AlertSchema, isAlertSchema} from './schema';
 import {AzureFunction, Context, HttpRequest} from '@azure/functions';
 const fetch = require('node-fetch');
 const util = require('util')
@@ -6,9 +5,11 @@ const util = require('util')
 let APP_INSIGHT_URL = 'https://portal.azure.com/#@cdc.onmicrosoft.com/resource/subscriptions/7d1e3999-6577-4cd5-b296-f518e5c8e677/resourcegroups/prime-simple-report-management/providers/microsoft.insights/components/prime-simple-report-global-insights/overview';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    // context will allow logging statements to appear in Azure
     context.log('HTTP trigger function processed a request.');
     context.log('Alert received:: ', util.inspect(req.body, {showHidden: false, depth: null}));
     context.log(JSON.stringify(req.body))
+
     const webhook = process.env["SLACK_WEBHOOK"];
     console.log("Connecting to: ", webhook);
     if (webhook === undefined) {
@@ -19,9 +20,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         return
     }
 
-    if (req.body !== undefined && isAlertSchema(req.body)) {
-        // I'm not really sure why the type guards aren't working correctly, but this is pretty simple
-        // Send to Slack
+    if (req.body !== undefined) {
         context.log("sending to slack");
         const res = await sendToSlack(webhook, req.body);
 
@@ -39,7 +38,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     }
 };
 
-async function sendToSlack(url: string, alert: AlertSchema) {
+async function sendToSlack(url: string, alert: any) {
 
     return fetch(url, {
         method: 'post',
@@ -47,15 +46,7 @@ async function sendToSlack(url: string, alert: AlertSchema) {
     });
 }
 
-function generateSlackMessage(alert: AlertSchema): any {
-
-    let notificationTime;
-    if (alert.data.essentials.monitorCondition == 'Resolved') {
-        notificationTime = new Date(alert.data.essentials.resolvedDateTime);
-    } else {
-       notificationTime = new Date(alert.data.essentials.firedDateTime);
-    }
-
+function generateSlackMessage(alert: any): any {
     // Generate the heading text
     let alert_type;
     if (alert.data.alertContext.conditionType === 'WebtestLocationAvailabilityCriteria'){
