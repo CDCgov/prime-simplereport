1c4d109dfd45bc2d921b0af41b47df0026e981cb
Emmanuel Apau <67923993+emmanuel-apau@users.noreply.github.com>
2021-02-11 13:24:55 -0500

    Network/Http alerts + metabase module (#800)
    * added metabase module
    added mem_threshold variable
    added http 4xx & 5xx error alerts

--- ops/services/app_gateway/main.tf
+++ ops/services/app_gateway/main.tf
@@ -270,6 +270,7 @@ resource "azurerm_application_gateway" "load_balancer" {
       identity,
       rewrite_rule_set,
       url_path_map,
+      ssl_policy,
       # comment if you are creating the gateway for the first time
       request_routing_rule,
     ]
