eb91c9b66275289fb8303196d6cdf2bda8d0833e
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-02-16 15:43:02 -0800

    Update ManagePatients.tsx
    Prettier nit

--- frontend/src/app/patients/ManagePatients.tsx
+++ frontend/src/app/patients/ManagePatients.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import moment from "moment";
 import { displayFullName } from "../utils";
