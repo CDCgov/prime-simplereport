79fc87d3be47ed24ea3d9ac4ee651d13fd9f1330
Neil Sharma <neil.s.sharma@omb.eop.gov>
2021-02-17 11:47:03 -0800

    query

--- frontend/src/app/Settings/Users/ManageUsersContainer.tsx
+++ frontend/src/app/Settings/Users/ManageUsersContainer.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-// import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import ManageUsers from "./ManageUsers";
 import { UserRole } from "../../permissions";
 import { useSelector } from "react-redux";
 
 // TODO this hasn't been implemented yet in the backend
-// const GET_USERS = gql`
-//   query GetUsers {
-//     getUsers {
-//       id
-//       roleDescription
-//       permissions
-//       firstName
-//       middleName
-//       lastName
-//       email
-//     }
-//   }
-// `;
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      roleDescription
+      permissions
+      firstName
+      middleName
+      lastName
+      email
+    }
+  }
+`;
 
 export interface SettingsUser {
   id: string;
@@ -26,6 +26,10 @@ export interface SettingsUser {
   email: string;
   isAdmin: boolean;
   facilities?: UserFacilitySetting[]; // TODO: update this when the graphql query is defined. Should just be the facility id and name.
+}
+
+interface UserData {
+  users: SettingsUser[];
 }
 
 export interface UserFacilitySetting {
@@ -103,23 +107,34 @@ const deleteUser = (userId: string) => {
   // TODO: perform, graphql mutation
 };
 
-const ManageUsersContainer: React.FC<any> = () => {
+// const ManageUsersContainer: React.FC<any> = () => {
+const ManageUsersContainer: any = () => {
   const loggedInUser = useSelector((state) => (state as any).user as User);
   loggedInUser.id = "111"; // TODO: delete this
-  //   const { data, loading, error } = useQuery<SettingsData, {}>(GET_FACILITIES, {
-  //     fetchPolicy: "no-cache",
-  //   });
 
-  //   if (loading) {
-  //     return <p> Loading... </p>;
-  //   }
-  //   if (error) {
-  //     return error;
-  //   }
+  // const { data, loading, error } = useQuery<SettingsData, {}>(GET_USERS, {
+  //   fetchPolicy: "no-cache",
+  // });
 
-  //   if (data === undefined) {
-  //     return <p>Error: Users not found</p>;
-  //   }
+  const { data, loading, error } = useQuery<UserData, {}>(GET_USERS, {
+    fetchPolicy: "no-cache",
+  });
+
+  if (loading) {
+    console.log("loading");
+    return <p> Loading... </p>;
+  }
+  if (error) {
+    console.log(error);
+    return error;
+  }
+
+  if (data === undefined) {
+    console.log("users:", data);
+    return <p>Error: Users not found</p>;
+  } else {
+    console.log("data", data);
+  }
 
   return (
     <ManageUsers
