471571ac5ff8987de42dc86b54d4656d75c30940
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-02-18 17:54:43 -0500

    lint fix

--- backend/src/test/java/gov/cdc/usds/simplereport/service/DeviceTypeServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/DeviceTypeServiceTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
