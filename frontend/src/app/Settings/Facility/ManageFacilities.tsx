130b245a880abc8279d5dde14622f083210d413c
Neil Sharma <35268641+sharmaneil@users.noreply.github.com>
2021-02-18 12:34:00 -0800

    Access Control V1 & V2 - Manage Users (Front-End only) (#698)
    * dummy manage users with routing
    
    * jank edit users flow
    
    * skeleton layout for users
    
    * added user role type
    
    * added dummy current user
    
    * misc users code
    
    * cleanup
    
    * style changes
    
    * moved data to container
    
    * added modal
    
    * styled the modal
    
    * added a feature flag
    
    * renamed nav to settingsnav
    
    * moved settings to settings page with subroutes
    
    * updated settings
    
    * misc fixes
    
    * added facility dropdowns, turned off by feature flag
    
    * added key to facilities list
    
    * flagged v1 until graphql query is in
    
    * fixed build issue with facilities setting
    
    * Update frontend/src/app/Settings/Users/InProgressModal.tsx
    
    Co-authored-by: Tim Best <53869143+timothybest-usds@users.noreply.github.com>
    
    * Update frontend/src/app/Settings/Settings.tsx
    
    Co-authored-by: Tim Best <53869143+timothybest-usds@users.noreply.github.com>
    
    * prompts if you navigate away
    
    * refactored role form into separate component
    
    * cleanup
    
    * feature flag
    
    * moved facilitiesform to separate component
    
    * moved facilitiesform to separate component
    
    * added rough UI for facility mgmt
    
    * minor facilities fix
    
    * cleanup
    
    * fixed firefox modal
    
    * styled skeleton create user modal
    
    * moved form to separate component
    
    * refactored away isEdited into its own state
    
    * content for dropdown
    
    * wired in new user mutation handler
    
    * wired in delete user
    
    * fixed feature flag issue on new user
    
    * query
    
    * wired in getUsers query
    
    * moved side nav to separate container
    
    * wired in graphql query for getting facilities
    
    * fixed lint errors
    
    * incremental cleanup of dummy data
    
    * added success alert to create user forms
    
    * added a delete user modal
    
    * updated flags
    
    Co-authored-by: Tim Best <53869143+timothybest-usds@users.noreply.github.com>

--- frontend/src/app/Settings/Facility/ManageFacilities.tsx
+++ frontend/src/app/Settings/Facility/ManageFacilities.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-import Nav from "../Nav";
-
 interface Props {
   facilities: Facility[];
 }
@@ -13,48 +11,43 @@ const ManageFacilities: React.FC<Props> = ({ facilities }) => {
     (state) => (state as any).facility.id as string
   );
   return (
-    <main className="prime-home">
-      <div className="grid-container">
-        <Nav />
-        <div className="grid-row">
-          <div className="prime-container usa-card__container">
-            <div className="usa-card__header">
-              <h2>Manage Facilities</h2>
-              <NavLink
-                className="usa-button usa-button--inverse"
-                to="/settings/add-facility/"
-              >
-                + New facility
-              </NavLink>
-            </div>
-            <div className="usa-card__body">
-              <table className="usa-table usa-table--borderless width-full">
-                <thead>
-                  <tr>
-                    <th scope="col">Facility name</th>
-                    <th scope="col">CLIA number</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {facilities.map((facility) => (
-                    <tr key={facility.id}>
-                      <td>
-                        <NavLink
-                          to={`/settings/facility/${facility.id}?facility=${activeFacilityId}`}
-                        >
-                          {facility.name}
-                        </NavLink>
-                      </td>
-                      <td>{facility.cliaNumber}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <div className="grid-row">
+      <div className="prime-container usa-card__container">
+        <div className="usa-card__header">
+          <h2>Manage Facilities</h2>
+          <NavLink
+            className="usa-button usa-button--inverse"
+            to="/settings/add-facility/"
+          >
+            + New facility
+          </NavLink>
+        </div>
+        <div className="usa-card__body">
+          <table className="usa-table usa-table--borderless width-full">
+            <thead>
+              <tr>
+                <th scope="col">Facility name</th>
+                <th scope="col">CLIA number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {facilities.map((facility) => (
+                <tr key={facility.id}>
+                  <td>
+                    <NavLink
+                      to={`/settings/facility/${facility.id}?facility=${activeFacilityId}`}
+                    >
+                      {facility.name}
+                    </NavLink>
+                  </td>
+                  <td>{facility.cliaNumber}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
--- frontend/src/app/Settings/SettingsNav.tsx
+++ frontend/src/app/Settings/SettingsNav.tsx
@@ -5,6 +5,7 @@ const getFacilityIdFromUrl = () => {
   const queryParams = new URLSearchParams(window.location.search);
   return queryParams.has("facility") ? queryParams.get("facility") : null;
 };
+
 const SettingsNav = () => {
   return (
     <nav className="prime-secondary-nav" aria-label="Secondary navigation">
@@ -12,22 +13,30 @@ const SettingsNav = () => {
         <li className="usa-nav__secondary-item">
           <NavLink
             to={`/settings/?facility=${getFacilityIdFromUrl()}`}
-            onClick={() => 4}
             activeClassName="active"
             exact={true}
           >
             Manage Organization
           </NavLink>
-        </li>{" "}
+        </li>
         <li className="usa-nav__secondary-item">
           <NavLink
             to={`/settings/facilities/?facility=${getFacilityIdFromUrl()}`}
-            onClick={() => 4}
             activeClassName="active"
           >
             Manage Facilities
           </NavLink>
         </li>
+        {process.env.REACT_APP_USER_SETTINGS_ENABLED === "true" ? (
+          <li className="usa-nav__secondary-item">
+            <NavLink
+              to={`/settings/users/?facility=${getFacilityIdFromUrl()}`}
+              activeClassName="active"
+            >
+              Manage users
+            </NavLink>
+          </li>
+        ) : null}
       </ul>
     </nav>
   );
