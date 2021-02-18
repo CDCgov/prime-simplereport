b90f66ce043a223f132e0a731be1a1f73557198a
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-02-18 17:58:24 -0500

    another lint fix

--- backend/src/test/java/gov/cdc/usds/simplereport/db/repository/BaseRepositoryTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/db/repository/BaseRepositoryTest.java
@@ -1,7 +1,5 @@
 package gov.cdc.usds.simplereport.db.repository;
 
-import org.junit.jupiter.api.BeforeEach;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
