193818a977db1a3f91ea12707ba33f92d88c2fe5
Maya Benari <hello@maya-benari.com>
2021-02-19 13:46:22 -0800

    Fix compile warning

--- frontend/src/app/commonComponents/RadioGroup.tsx
+++ frontend/src/app/commonComponents/RadioGroup.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import classnames from "classnames";
 import useUniqueId from "./useUniqueIds";
 import Required from "./Required";
