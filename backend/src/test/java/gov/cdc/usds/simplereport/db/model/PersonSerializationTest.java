02cc1fe8839881d1c381438db3e564b5cadbd786
Ben Warfield <bwarfield@cdc.gov>
2021-02-19 22:19:52 -0500

    Added a bit more serialization testing for the Person object.

--- backend/src/test/java/gov/cdc/usds/simplereport/db/model/PersonSerializationTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/db/model/PersonSerializationTest.java
@@ -1,7 +1,7 @@
 package gov.cdc.usds.simplereport.db.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -14,12 +14,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.boot.test.json.ObjectContent;
 
 import gov.cdc.usds.simplereport.db.model.auxiliary.StreetAddress;
 
 @JsonTest
 class PersonSerializationTest {
+
+    private static final int BIRTH_YEAR = 2000;
+    private static final int BIRTH_MONTH = 3;
+    private static final int BIRTH_DAY = 31;
 
     @Autowired
     private JacksonTester<Person> _tester;
@@ -43,16 +48,33 @@ class PersonSerializationTest {
     }
 
     @Test
-    void serialize_withOrgAndFacility_noOrgOrFacility() throws IOException {
-        // consts are to keep style check happy othewise it complains about
-        // "magic numbers"
-        final int BIRTH_YEAR = 2000;
-        final int BIRTH_MONTH = 3;
-        final int BIRTH_DAY = 31;
+    void serialize_withoutFacility_valuesStored() throws IOException {
         Organization fakeOrg = new Organization("ABC", "123");
-        Person p = new Person(fakeOrg, null, "John", "Jacob", "Jingleheimer-Jones", "Jr.",
-                LocalDate.of(BIRTH_YEAR, BIRTH_MONTH, BIRTH_DAY), null, "1234556", null, "a@b.c", "marathon", "generic",
-                "Male-ish", true, false);
+        Person p = makeSerializablePerson(fakeOrg);
+        JsonContent<Person> serialized = _tester.write(p);
+        assertThat(serialized).extractingJsonPathStringValue("firstName")
+                .isEqualTo("John");
+        assertThat(serialized).extractingJsonPathStringValue("lastName")
+                .isEqualTo("Jingleheimer-Jones");
+        assertThat(serialized).extractingJsonPathStringValue("middleName")
+                .isEqualTo("Jacob");
+        assertThat(serialized).extractingJsonPathStringValue("suffix")
+                .isEqualTo("Jr.");
+        assertThat(serialized).extractingJsonPathStringValue("birthDate")
+                .isEqualTo(String.format("%4d-%02d-%02d", BIRTH_YEAR, BIRTH_MONTH, BIRTH_DAY));
+        assertThat(serialized).extractingJsonPathArrayValue("address.street")
+                .hasSize(3)
+                .isEqualTo(List.of("1600 Pennsylvania Avenue", "Right Side Door", "Down the Steps"));
+        assertThat(serialized).extractingJsonPathMapValue("facility")
+                .isNull();
+        assertThat(serialized).extractingJsonPathMapValue("organization")
+                .isNull();
+    }
+
+    @Test
+    void serialize_withFacility_noOrgOrFacility() throws IOException {
+        Organization fakeOrg = new Organization("ABC", "123");
+        Person p = makeSerializablePerson(fakeOrg);
         Provider mccoy = new Provider("Doc", "", "", "", "NCC1701", null, "(1) (111) 2222222");
         DeviceSpecimen ds = new DeviceSpecimen(
                 new DeviceType("Bill", "Weasleys", "1", "12345-6", "E"),
@@ -60,9 +82,22 @@ class PersonSerializationTest {
         StreetAddress addy = new StreetAddress(Collections.singletonList("Moon Base"), "Luna City", "THE MOON", "", "");
         p.setFacility(new Facility(fakeOrg, "Nice Place", "YOUGOTHERE", addy, "555-867-5309", "facility@test.com",
                 mccoy, ds, List.of(ds)));
-        String json = _tester.write(p).getJson();
-        assertFalse(json.contains("organization"));
-        assertFalse(json.contains("facility"));
+        JsonContent<Person> serialized = _tester.write(p);
+        assertThat(serialized).extractingJsonPathStringValue("lastName")
+                .isEqualTo("Jingleheimer-Jones");
+        assertThat(serialized).extractingJsonPathMapValue("facility")
+                .isNull();
+        assertThat(serialized).extractingJsonPathMapValue("organization")
+                .isNull();
+    }
+
+    private Person makeSerializablePerson(Organization fakeOrg) {
+        StreetAddress addy = new StreetAddress(List.of("1600 Pennsylvania Avenue", "Right Side Door", "Down the Steps"),
+                "Washington", "DC", "20001", "DC");
+        Person p = new Person(fakeOrg, null, "John", "Jacob", "Jingleheimer-Jones", "Jr.",
+                LocalDate.of(BIRTH_YEAR, BIRTH_MONTH, BIRTH_DAY), addy, "1234556", null, "a@b.c", "marathon", "generic",
+                "Male-ish", true, false);
+        return p;
     }
 
     private void assertAlexanderHamilton(ObjectContent<Person> ob) {
