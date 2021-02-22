b4b5e08dc784f20b2e5fca640ada356024a64036
Maya Benari <hello@maya-benari.com>
2021-02-22 14:34:56 -0800

    Add line height class to link

--- frontend/src/app/patients/PatientForm.tsx
+++ frontend/src/app/patients/PatientForm.tsx
@@ -599,7 +599,7 @@ const PatientForm = (props: Props) => {
         <FormGroup title="Demographics">
         {props.isPxpView && (
           <Button
-            className="usa-button--unstyled margin-top-1 margin-bottom-2"
+            className="usa-button--unstyled margin-top-1 margin-bottom-2 line-height-sans-2"
             onClick={() => setHelpModalOpen(true)}
             label="Why are we asking for this information?"
           />
