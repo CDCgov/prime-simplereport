eefcca6b1e62a3378d607312cbdf5dcbff3fea9f
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-02-24 12:37:36 -0500

    Update user maanagement logic to focus on newly added user

--- frontend/src/app/Settings/Users/ManageUsers.tsx
+++ frontend/src/app/Settings/Users/ManageUsers.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Prompt } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -37,7 +37,7 @@ interface Props {
   updateUserRole: (variables: any) => Promise<any>;
   addUserToOrg: (variables: any) => Promise<any>;
   deleteUser: (variables: any) => Promise<any>;
-  getUsers: () => void;
+  getUsers: () => Promise<any>;
 }
 
 export type SettingsUsers = { [id: string]: SettingsUser };
@@ -50,6 +50,12 @@ const sortUsers = (users: SettingsUsers) =>
     return nameA > nameB ? 1 : -1;
   });
 
+const getSettingsUser = (users: SettingsUser[]) =>
+  users.reduce((acc: SettingsUsers, user: SettingsUser) => {
+    acc[user.id] = user;
+    return acc;
+  }, {});
+
 const ManageUsers: React.FC<Props> = ({
   users,
   loggedInUser,
@@ -59,21 +65,7 @@ const ManageUsers: React.FC<Props> = ({
   deleteUser,
   getUsers,
 }) => {
-  let settingsUsers: SettingsUsers = users.reduce(
-    (acc: SettingsUsers, user: SettingsUser) => {
-      acc[user.id] = user;
-      return acc;
-    },
-    {}
-  );
-
-  const [usersState, updateUsersState] = useState<SettingsUsers>(settingsUsers);
-
-  const sortedUsers = sortUsers(usersState);
-
-  const [activeUserId, updateActiveUserId] = useState<string>(
-    sortedUsers[0].id
-  );
+  const [activeUser, updateActiveUser] = useState<SettingsUser>();
   const [nextActiveUserId, updateNextActiveUserId] = useState<string | null>(
     null
   );
@@ -81,11 +73,24 @@ const ManageUsers: React.FC<Props> = ({
   const [showAddUserModal, updateShowAddUserModal] = useState(false);
   const [showDeleteUserModal, updateShowDeleteUserModal] = useState(false);
   const [isUserEdited, updateIsUserEdited] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
   const [error, setError] = useState<Error>();
+  // Maintain deleted user ID on client while request is in flight
+  const [deletedUserId, setDeletedUserId] = useState<string>();
+  // Maintain added user on client while request is in flight
+  const [addedUserId, setAddedUserId] = useState<string>();
 
   if (error) {
     throw error;
   }
+
+  // Local users will be more up-to-date than users, which only gets updated after server requests complete
+  let localUsers = [...users];
+  if (deletedUserId) {
+    localUsers = localUsers.filter(({ id }) => id !== deletedUserId);
+  }
+  const usersState: SettingsUsers = getSettingsUser(localUsers);
+  const sortedUsers = sortUsers(usersState);
 
   // only updates the local state
   function updateUser<T>(
@@ -93,12 +98,9 @@ const ManageUsers: React.FC<Props> = ({
     key: string, // the field to update
     value: T // value of the field to update
   ) {
-    updateUsersState({
-      ...usersState,
-      [userId]: {
-        ...usersState[userId],
-        [key]: value,
-      },
+    updateActiveUser({
+      ...usersState[userId],
+      [key]: value,
     });
     updateIsUserEdited(true);
   }
@@ -109,44 +111,38 @@ const ManageUsers: React.FC<Props> = ({
       updateNextActiveUserId(nextActiveUserId);
       updateShowInProgressModal(true);
     } else {
-      updateActiveUserId(nextActiveUserId);
+      updateActiveUser(usersState[nextActiveUserId]);
     }
   };
 
   // proceed with changing the active user
-  const onContinueChangeActiveUser = (currentActiveUserId: string) => {
-    updateShowInProgressModal(false);
-
-    updateActiveUserId(nextActiveUserId as string);
-    resetUser(currentActiveUserId);
-  };
-
-  // throw away unsaved edits
-  const resetUser = (userId: string) => {
-    updateUsersState({
-      ...usersState,
-      [userId]: settingsUsers[userId],
-    });
+  const onContinueChangeActiveUser = () => {
     updateIsUserEdited(false);
+    updateShowInProgressModal(false);
+    if (nextActiveUserId) {
+      updateActiveUser(usersState[nextActiveUserId]);
+    }
   };
 
-  const handleUpdateUser = (userId: string) => {
-    const selectedRoleDescription = usersState[userId]
-      .roleDescription as RoleDescription;
+  const handleUpdateUser = () => {
+    if (!activeUser) {
+      return;
+    }
+    setIsUpdating(true);
+    const selectedRoleDescription = activeUser.roleDescription as RoleDescription;
     const selectedOrganizationRole = RoleDescriptionToOrgRole[
       selectedRoleDescription
     ] as OrganizationRole;
     updateUserRole({
       variables: {
-        id: userId,
+        id: activeUser.id,
         role: selectedOrganizationRole,
       },
     })
       .then(() => {
+        getUsers();
         updateIsUserEdited(false);
-
-        const user = usersState[userId];
-
+        const user = usersState[activeUser.id];
         const fullName = displayFullNameInOrder(
           user.firstName,
           user.middleName,
@@ -162,88 +158,87 @@ const ManageUsers: React.FC<Props> = ({
           />
         );
       })
-      .catch(setError);
+      .catch(setError)
+      .finally(() => {
+        setIsUpdating(false);
+      });
   };
 
-  const handleAddUserToOrg = (newUserInvite: NewUserInvite) => {
+  const handleAddUserToOrg = async (newUserInvite: NewUserInvite) => {
     // TODO: validate form
-    const { firstName, lastName, email } = { ...newUserInvite };
-    addUserToOrg({
-      variables: {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-      },
-    })
-      .then(() => {
-        const fullName = displayFullNameInOrder(firstName, "", lastName);
-        showNotification(
-          toast,
-          <Alert
-            type="success"
-            title={`Invitation sent to ${fullName}`}
-            body={`They will receive an invitation to create an account at the email address provided`}
-          />
-        );
-        updateShowAddUserModal(false);
-        getUsers();
-      })
-      .catch(setError);
+    try {
+      setIsUpdating(true);
+      const { firstName, lastName, email } = { ...newUserInvite };
+      const { data } = await addUserToOrg({
+        variables: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        },
+      });
+      await getUsers();
+      const fullName = displayFullNameInOrder(firstName, "", lastName);
+      showNotification(
+        toast,
+        <Alert
+          type="success"
+          title={`Invitation sent to ${fullName}`}
+          body={`They will receive an invitation to create an account at the email address provided`}
+        />
+      );
+      updateShowAddUserModal(false);
+      setAddedUserId(data?.addUserToCurrentOrg?.id);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setIsUpdating(false);
+    }
   };
 
-  const handleDeleteUser = (userId: string) => {
-    deleteUser({
-      variables: {
-        id: userId,
-        deleted: true,
-      },
-    })
-      .then((data) => {
-        const deletedUserId = data.data.setUserIsDeleted.id;
-        const user = usersState[deletedUserId];
+  const handleDeleteUser = async (userId: string) => {
+    try {
+      const data = await deleteUser({
+        variables: {
+          id: userId,
+          deleted: true,
+        },
+      });
+      const deletedUserId = data.data.setUserIsDeleted.id;
+      const user = usersState[deletedUserId];
 
-        const fullName = displayFullNameInOrder(
-          user.firstName,
-          user.middleName,
-          user.lastName
-        );
-
-        showNotification(
-          toast,
-          <Alert
-            type="success"
-            title={`User account removed for ${fullName}`}
-          />
-        );
-
-        // remove the deleted user from the UI
-        const { [deletedUserId]: value, ...usersMinusDeleted } = usersState;
-        updateUsersState(usersMinusDeleted);
-        const firstUserId =
-          sortedUsers[0].id === deletedUserId
-            ? sortedUsers[1].id
-            : sortedUsers[0].id;
-        updateActiveUserId(firstUserId); // arbitrarily pick the first user as the next active.
-      })
-      .catch(setError);
-
-    const user = usersState[userId];
-
-    const fullName = displayFullNameInOrder(
-      user.firstName,
-      user.middleName,
-      user.lastName
-    );
-
-    let successAlert = (
-      <Alert type="success" title={`User account removed for ${fullName}`} />
-    );
-
-    showNotification(toast, successAlert);
-    updateShowDeleteUserModal(false);
+      const fullName = displayFullNameInOrder(
+        user.firstName,
+        user.middleName,
+        user.lastName
+      );
+      updateShowDeleteUserModal(false);
+      setDeletedUserId(userId);
+      showNotification(
+        toast,
+        <Alert type="success" title={`User account removed for ${fullName}`} />
+      );
+      await getUsers();
+      updateActiveUser(sortedUsers[0]); // arbitrarily pick the first user as the next active.
+      setDeletedUserId(undefined);
+    } catch (e) {
+      setError(e);
+    }
   };
 
-  const activeUser: SettingsUser = usersState[activeUserId];
+  // Default to first user
+  useEffect(() => {
+    if (!activeUser && sortedUsers.length) {
+      updateActiveUser(sortedUsers[0]);
+    }
+  }, [activeUser, sortedUsers]);
+
+  // Navigate to addeed user, if applicable
+  useEffect(() => {
+    if (addedUserId && usersState[addedUserId]) {
+      updateActiveUser(usersState[addedUserId]);
+      setAddedUserId(undefined);
+    }
+  }, [addedUserId, usersState]);
 
   return (
     <div className="prime-container usa-card__container">
@@ -257,28 +252,34 @@ const ManageUsers: React.FC<Props> = ({
           />
         ) : null}
       </div>
-      {!activeUser ? (
+      {!activeUser || !localUsers.length ? (
         <div className="usa-card__body">
-          <p> There are no users in this organization </p>
+          {!localUsers.length ? (
+            <p>There are no users in this organization</p>
+          ) : (
+            <p>Loading data</p>
+          )}
         </div>
       ) : (
         <div className="usa-card__body">
           <div className="grid-row">
             <UsersSideNav
-              activeUserId={activeUserId}
+              activeUserId={activeUser.id || ""}
               users={sortedUsers}
               onChangeActiveUser={onChangeActiveUser}
             />
             <div className="tablet:grid-col padding-left-2">
               <div className="user-header grid-row flex-row flex-align-center">
                 <h2 className="display-inline-block margin-y-1">
-                  {displayFullNameInOrder(
-                    activeUser.firstName,
-                    activeUser.middleName,
-                    activeUser.lastName
-                  )}
+                  {activeUser
+                    ? displayFullNameInOrder(
+                        activeUser.firstName,
+                        activeUser.middleName,
+                        activeUser.lastName
+                      )
+                    : "Loading..."}
                 </h2>
-                {activeUser.id === loggedInUser.id ? (
+                {activeUser?.id === loggedInUser.id ? (
                   <span className="usa-tag margin-left-1 bg-base-lighter text-ink">
                     YOU
                   </span>
@@ -289,13 +290,16 @@ const ManageUsers: React.FC<Props> = ({
                   Admins have full access to conduct tests, manage results and
                   profiles, and manage settings and users
                 </p>
-                <UserRoleSettingsForm
-                  activeUser={activeUser}
-                  loggedInUser={loggedInUser}
-                  onUpdateUser={updateUser}
-                />
+                {activeUser && (
+                  <UserRoleSettingsForm
+                    activeUser={activeUser}
+                    loggedInUser={loggedInUser}
+                    onUpdateUser={updateUser}
+                  />
+                )}
 
-                {process.env.REACT_APP_VIEW_USER_FACILITIES === "true" ? (
+                {process.env.REACT_APP_VIEW_USER_FACILITIES === "true" &&
+                activeUser ? (
                   <UserFacilitiesSettingsForm
                     activeUser={activeUser}
                     allFacilities={allFacilities}
@@ -311,27 +315,28 @@ const ManageUsers: React.FC<Props> = ({
                     className="flex-align-self-start display-inline-block"
                     onClick={() => updateShowDeleteUserModal(true)}
                     label="Remove user"
-                    disabled={loggedInUser.id === activeUser.id}
+                    disabled={loggedInUser.id === activeUser.id || isUpdating}
                   />
                 ) : null}
                 <Button
                   type="button"
-                  onClick={() => handleUpdateUser(activeUserId)}
-                  label="Save changes"
+                  onClick={() => handleUpdateUser()}
+                  label={isUpdating ? "Saving..." : "Save changes"}
                   disabled={
                     // enabled only if the user has been edited AND the loggedInUser is an org admin or super admin
                     !isUserEdited ||
                     !["Admin user", "Admin user (SU)"].includes(
                       loggedInUser.roleDescription
-                    )
+                    ) ||
+                    isUpdating
                   }
                 />
               </div>
 
-              {showInProgressModal ? (
+              {showInProgressModal && activeUser ? (
                 <InProgressModal
                   onClose={() => updateShowInProgressModal(false)}
-                  onContinue={() => onContinueChangeActiveUser(activeUserId)}
+                  onContinue={() => onContinueChangeActiveUser()}
                 />
               ) : null}
               {isUserEdited ? (
@@ -343,6 +348,7 @@ const ManageUsers: React.FC<Props> = ({
               {showAddUserModal &&
               process.env.REACT_APP_ADD_NEW_USER_ENABLED === "true" ? (
                 <CreateUserModal
+                  isUpdating={isUpdating}
                   onClose={() => updateShowAddUserModal(false)}
                   onSubmit={handleAddUserToOrg}
                 />
