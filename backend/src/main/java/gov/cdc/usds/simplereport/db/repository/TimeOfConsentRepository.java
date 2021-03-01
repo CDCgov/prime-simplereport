726d89e34b2c5294cbc8d325ef4040345790c098
Nick Clyde <nick@clyde.tech>
2021-03-01 10:27:18 -0800

    Fix repository

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/TimeOfConsentRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/TimeOfConsentRepository.java
@@ -1,11 +1,14 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
 
 import gov.cdc.usds.simplereport.db.model.TimeOfConsent;
 import gov.cdc.usds.simplereport.db.model.PatientLink;
 
-public interface TimeOfConsentRepository extends EternalAuditedEntityRepository<TimeOfConsent> {
+public interface TimeOfConsentRepository extends CrudRepository<TimeOfConsent, UUID> {
 
     public List<TimeOfConsent> findAllByPatientLink(PatientLink pl);
 }
