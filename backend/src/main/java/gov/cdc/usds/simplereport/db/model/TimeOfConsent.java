d749b2b05b9f31dee6d6c50ce941e1aee24f8301
Nick Clyde <nick@clyde.tech>
2021-03-01 10:46:17 -0800

    Use SystemManagedEntity

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/TimeOfConsent.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/TimeOfConsent.java
@@ -1,28 +1,14 @@
 package gov.cdc.usds.simplereport.db.model;
 
-import java.util.Date;
-import java.util.UUID;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
-public class TimeOfConsent {
-
-    @Column(updatable = false, nullable = false)
-    @Id
-    @GeneratedValue(generator = "UUID4")
-    private UUID internalId;
-
-    @Column(updatable = false)
-    @CreatedDate
-    private Date createdAt;
+@DynamicUpdate
+public class TimeOfConsent extends SystemManagedEntity {
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "patient_link_id", nullable = false)
@@ -33,14 +19,6 @@ public class TimeOfConsent {
 
     public TimeOfConsent(PatientLink patientLink) {
         this.patientLink = patientLink;
-    }
-
-    public UUID getInternalId() {
-        return internalId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
     }
 
     public PatientLink getPatientLink() {
