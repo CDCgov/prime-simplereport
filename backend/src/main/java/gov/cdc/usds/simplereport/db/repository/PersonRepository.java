92f2f364b0991c2abb5470a1f1d4ddfc10b8d477
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-03-05 10:05:20 -0800

    Cleanup unused model functions

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
@@ -1,6 +1,5 @@
 package gov.cdc.usds.simplereport.db.repository;
 
-import gov.cdc.usds.simplereport.db.model.Facility;
 import gov.cdc.usds.simplereport.db.model.Organization;
 import gov.cdc.usds.simplereport.db.model.Person;
 import java.util.List;
@@ -17,25 +16,6 @@ public interface PersonRepository extends EternalAuditedEntityRepository<Person>
   public List<Person> findAll(Specification<Person> searchSpec, Pageable p);
 
   public int count(Specification<Person> searchSpec);
-
-  @Query(BASE_ALLOW_DELETED_QUERY + " e.organization = :org AND e.isDeleted = :isDeleted")
-  public Page<Person> findAllByOrganization(Organization org, boolean isDeleted, Pageable pageable);
-
-  @Query(
-      "SELECT COUNT(e) FROM #{#entityName} e WHERE e.organization = :org AND e.isDeleted = :isDeleted")
-  public long countAllByOrganization(Organization org, boolean isDeleted);
-
-  // NOTE: e.facility = NULL allows some patients to be in all facilities (e.g. staff)
-  @Query(
-      BASE_ALLOW_DELETED_QUERY
-          + " (e.facility = :fac OR e.facility IS NULL) AND e.organization = :org AND e.isDeleted = :isDeleted")
-  public Page<Person> findByFacilityAndOrganization(
-      Facility fac, Organization org, boolean isDeleted, Pageable pageable);
-
-  // NOTE: e.facility = NULL allows some patients to be in all facilities  (e.g. staff)
-  @Query(
-      "SELECT COUNT(e) FROM #{#entityName} e WHERE (e.facility = :fac OR e.facility IS NULL) AND e.organization = :org AND e.isDeleted = :isDeleted")
-  public long countAllByFacilityAndOrganization(Facility fac, Organization org, boolean isDeleted);
 
   @Query(
       BASE_ALLOW_DELETED_QUERY
