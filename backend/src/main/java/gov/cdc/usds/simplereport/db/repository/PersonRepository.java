67fb54a16fda271731b128d78a4d6cb4fd24e291
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-03-05 10:21:50 -0800

    Ran spotlist

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
@@ -5,7 +5,6 @@ import gov.cdc.usds.simplereport.db.model.Person;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.Query;
