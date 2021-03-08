e2aa68a968a8672c45e5aae05309224827713418
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-03-08 18:17:58 -0500

    made my changes more ~spotless~

--- backend/src/main/java/gov/cdc/usds/simplereport/config/authorization/OrganizationRole.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/authorization/OrganizationRole.java
@@ -22,9 +22,9 @@ public enum OrganizationRole implements Comparable<OrganizationRole> {
   MEMBER("Member", EnumSet.noneOf(UserPermission.class)),
   /**
    * This is the role that gives you access to all facilities in your organization; admins also have
-   * all-facility access by default. NOTE: this role does not give you any meaningful
-   * permissions besides the ability to exercise other permissioned features on all facilities, 
-   * and thus must be accompanied by other roles for a user to be able to use the app.
+   * all-facility access by default. NOTE: this role does not give you any meaningful permissions
+   * besides the ability to exercise other permissioned features on all facilities, and thus must be
+   * accompanied by other roles for a user to be able to use the app.
    */
   ALL_FACILITIES("All-facility-access user", EnumSet.of(UserPermission.ACCESS_ALL_FACILITIES)),
   /**
