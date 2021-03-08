026e10deea24bef6f5bb0735524180c86dc71db6
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-03-08 10:29:37 -0500

    Add patient search debounce

--- frontend/src/app/testQueue/addToQueue/SearchResults.tsx
+++ frontend/src/app/testQueue/addToQueue/SearchResults.tsx
@@ -10,6 +10,7 @@ interface SearchResultsProps {
   facilityId: string;
   patientsInQueue: string[];
   shouldShowSuggestions: boolean;
+  loading: boolean;
 }
 
 const SearchResults = ({
@@ -18,6 +19,7 @@ const SearchResults = ({
   facilityId,
   patientsInQueue,
   shouldShowSuggestions,
+  loading,
 }: SearchResultsProps) => {
   const [dialogPatient, setDialogPatient] = useState(null);
   const [canAddToQueue, setCanAddToQueue] = useState(false);
@@ -26,46 +28,52 @@ const SearchResults = ({
     return patientsInQueue.indexOf(patientId) === -1;
   };
   let results;
-  if (patients.length === 0) {
-    results = <h3>No results</h3>;
-    if (canAddToQueue) setCanAddToQueue(false);
+
+  if (!shouldShowSuggestions) {
+    results = null;
   } else {
     results = (
       <div className="usa-card__container shadow-3 results-dropdown">
         <div className="usa-card__body">
-          <table className="usa-table usa-table--borderless">
-            <thead>
-              <tr>
-                <th scope="col">Full name</th>
-                <th scope="col">Date of birth</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {patients.map((p) => (
-                <tr key={p.internalId}>
-                  <td>
-                    {displayFullName(p.firstName, p.middleName, p.lastName)}
-                  </td>
-                  <td>{p.birthDate}</td>
-                  <td>
-                    {canAddToTestQueue(p.internalId) ? (
-                      <Button
-                        variant="unstyled"
-                        label="Begin test"
-                        onClick={() => {
-                          setDialogPatient(p);
-                          setCanAddToQueue(canAddToTestQueue(p.internalId));
-                        }}
-                      />
-                    ) : (
-                      "Test in progress"
-                    )}
-                  </td>
+          {loading ? (
+            <p>Searching...</p>
+          ) : patients.length === 0 ? (
+            <p>No results</p>
+          ) : (
+            <table className="usa-table usa-table--borderless">
+              <thead>
+                <tr>
+                  <th scope="col">Full name</th>
+                  <th scope="col">Date of birth</th>
+                  <th scope="col">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {patients.map((p) => (
+                  <tr key={p.internalId}>
+                    <td>
+                      {displayFullName(p.firstName, p.middleName, p.lastName)}
+                    </td>
+                    <td>{p.birthDate}</td>
+                    <td>
+                      {canAddToTestQueue(p.internalId) ? (
+                        <Button
+                          variant="unstyled"
+                          label="Begin test"
+                          onClick={() => {
+                            setDialogPatient(p);
+                            setCanAddToQueue(canAddToTestQueue(p.internalId));
+                          }}
+                        />
+                      ) : (
+                        "Test in progress"
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     );
