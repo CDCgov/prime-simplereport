060f2c4667fdf652d3c23a3e6abe1c47b021e613
Nick Clyde <nick@clyde.tech>
2021-03-09 12:07:31 -0800

    Add **

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 const rootDir = process.cwd();
 
 module.exports = {
-  "frontend/*.{js,ts,jsx,tsx}": (files) => {
+  "frontend/**/*.{js,ts,jsx,tsx}": (files) => {
     process.chdir(`${rootDir}/frontend`);
     return [
       `npx eslint --fix ${files.join(" ")}`,
       `npx prettier --write ${files.join(" ")}`,
     ];
   },
-  "backend/*.java": () => {
+  "backend/**/*.java": () => {
     process.chdir(`${rootDir}/backend`);
     return "./gradlew spotlessApply";
   },
