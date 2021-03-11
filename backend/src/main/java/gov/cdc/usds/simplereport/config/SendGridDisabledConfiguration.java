6db13134deaf409d9399293f03646b5f46ab6eab
Adam Weber <adam@skylight.digital>
2021-03-11 15:23:40 -0600

    Restore inner class for disabling SendGrid

--- backend/src/main/java/gov/cdc/usds/simplereport/config/SendGridDisabledConfiguration.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/SendGridDisabledConfiguration.java
@@ -1,14 +1,25 @@
 package gov.cdc.usds.simplereport.config;
 
+import java.io.IOException;
+
 import com.sendgrid.helpers.mail.Mail;
 import gov.cdc.usds.simplereport.service.email.EmailProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class SendGridDisabledConfiguration {
   @ConditionalOnMissingBean
   EmailProvider defaultToBypassSendGrid() {
-    return (Mail mail) -> null;
+    return new SendGridDisabledConfiguration.DisabledSendgrid();
+  }
+
+  @Component
+  class DisabledSendgrid implements EmailProvider {
+    @Override
+    public String send(Mail mail) throws IOException {
+      return null;
+    }
   }
 }
