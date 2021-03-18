c301c66d2974e94de7e0ccd4b0aca683b057a4b9
Nick Clyde <nick@clyde.tech>
2021-03-18 14:00:11 -0700

    Add TestResult react test

---
+++ frontend/src/patientApp/timeOfTest/TestResult.test.tsx
@@ -0,0 +1,100 @@
+import { MockedProvider } from "@apollo/client/testing";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render } from "@testing-library/react";
+
+import TestResult from "./TestResult";
+
+const mockStore = configureStore([]);
+
+describe("TestResult", () => {
+  it("should show a positive result", () => {
+    const store = mockStore({
+      plid: "foo",
+      patient: {
+        firstName: "Bob",
+        lastName: "Barker",
+        lastTest: {
+          deviceType: "Testing device",
+          dateTested: "08/27/2020",
+          result: "POSITIVE",
+        },
+      },
+    });
+    const { container, getByText } = render(
+      <Router history={createMemoryHistory()}>
+        <Provider store={store}>
+          <MockedProvider mocks={[]} addTypename={false}>
+            <TestResult />
+          </MockedProvider>
+        </Provider>
+      </Router>
+    );
+
+    expect(getByText("SARS-CoV-2 result")).toBeInTheDocument();
+    expect(getByText("Bob Barker")).toBeInTheDocument();
+    expect(getByText("Test result")).toBeInTheDocument();
+    expect(getByText("Positive")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+  it("should show a negative result", () => {
+    const store = mockStore({
+      plid: "foo",
+      patient: {
+        firstName: "Bob",
+        lastName: "Barker",
+        lastTest: {
+          deviceType: "Testing device",
+          dateTested: "08/27/2020",
+          result: "NEGATIVE",
+        },
+      },
+    });
+    const { container, getByText } = render(
+      <Router history={createMemoryHistory()}>
+        <Provider store={store}>
+          <MockedProvider mocks={[]} addTypename={false}>
+            <TestResult />
+          </MockedProvider>
+        </Provider>
+      </Router>
+    );
+
+    expect(getByText("SARS-CoV-2 result")).toBeInTheDocument();
+    expect(getByText("Bob Barker")).toBeInTheDocument();
+    expect(getByText("Test result")).toBeInTheDocument();
+    expect(getByText("Negative")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+  it("should show an inconclusive result", () => {
+    const store = mockStore({
+      plid: "foo",
+      patient: {
+        firstName: "Bob",
+        lastName: "Barker",
+        lastTest: {
+          deviceType: "Testing device",
+          dateTested: "08/27/2020",
+          result: "UNDETERMINED",
+        },
+      },
+    });
+    const { container, getByText } = render(
+      <Router history={createMemoryHistory()}>
+        <Provider store={store}>
+          <MockedProvider mocks={[]} addTypename={false}>
+            <TestResult />
+          </MockedProvider>
+        </Provider>
+      </Router>
+    );
+
+    expect(getByText("SARS-CoV-2 result")).toBeInTheDocument();
+    expect(getByText("Bob Barker")).toBeInTheDocument();
+    expect(getByText("Test result")).toBeInTheDocument();
+    expect(getByText("Inconclusive")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
