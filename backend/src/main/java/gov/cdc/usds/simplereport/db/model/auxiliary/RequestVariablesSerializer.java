3cce57279ec4f24ec138c944c7a162eb4b99e223
Tim Best <tBest@cdc.gov>
2021-03-23 17:30:53 -0400

    revet to working loop

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/auxiliary/RequestVariablesSerializer.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/auxiliary/RequestVariablesSerializer.java
@@ -9,6 +9,7 @@ import gov.cdc.usds.simplereport.api.model.errors.AuditLogFailureException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.servlet.http.Part;
@@ -37,9 +38,10 @@ public class RequestVariablesSerializer extends JsonSerializer<Map<String, Objec
   @Override
   public void serialize(Map<String, Object> value, JsonGenerator gen, SerializerProvider arg2)
       throws IOException, JsonProcessingException {
-    Map<String, Object> parsedValue =
-        value.entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> parseValue(e.getValue())));
+    Map<String, Object> parsedValue = new HashMap<>();
+    for (Map.Entry<String, Object> entry : value.entrySet()) {
+      parsedValue.put(entry.getKey(), parseValue(entry.getValue()));
+    }
     gen.writeObject(parsedValue);
   }
 }
