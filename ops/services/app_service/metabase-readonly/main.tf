488dae51e445f373e592eb6dd18de08809f00aaa
emmastephenson <emma.stephenson.24@gmail.com>
2021-03-23 15:56:33 -0700

    terraform formatting

--- ops/services/app_service/metabase-readonly/main.tf
+++ ops/services/app_service/metabase-readonly/main.tf
@@ -67,33 +67,33 @@ resource "null_resource" "add_readonly_db_user" {
 }
 
 resource "azurerm_app_service" "metabase-readonly" {
-    name = var.name
-    resource_group_name = var.resource_group_name
-    location = var.resource_group_location
-    app_service_plan_id = var.app_service_plan_id
-    https_only = true
-    site_config {
-        always_on = true
-        linux_fx_version = "DOCKER|metabase/metabase-readonly"
-    }
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  app_service_plan_id = var.app_service_plan_id
+  https_only          = true
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOCKER|metabase/metabase-readonly"
+  }
 
-    app_settings = {
-        "MB_DB_USER"                     = data.azurerm_key_vault_secret.postgres_readonly_user.value
-        "MB_DB_PASS"                     = data.azurerm_key_vault_secret.postgres_readonly_pass.value
-        "MB_DB_NAME"                     = var.postgres_db_name
-        "MB_DB_TYPE"                     = "postgres"
-        "MB_DB_HOST"                     = var.postgres_server_name
-        "MB_DB_PORT"                     = var.postgres_port
-        "WEBSITE_VNET_ROUTE_ALL"         = 1
-        "WEBSITE_DNS_SERVER"             = "168.63.129.16"
-        "APPINSIGHTS_INSTRUMENTATIONKEY" = var.ai_instrumentation_key
-     }
+  app_settings = {
+    "MB_DB_USER"                     = data.azurerm_key_vault_secret.postgres_readonly_user.value
+    "MB_DB_PASS"                     = data.azurerm_key_vault_secret.postgres_readonly_pass.value
+    "MB_DB_NAME"                     = var.postgres_db_name
+    "MB_DB_TYPE"                     = "postgres"
+    "MB_DB_HOST"                     = var.postgres_server_name
+    "MB_DB_PORT"                     = var.postgres_port
+    "WEBSITE_VNET_ROUTE_ALL"         = 1
+    "WEBSITE_DNS_SERVER"             = "168.63.129.16"
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = var.ai_instrumentation_key
+  }
 
-     identity {
+  identity {
     type = "SystemAssigned"
   }
 
-   logs {
+  logs {
     http_logs {
       file_system {
         retention_in_days = 7
