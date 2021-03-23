595137b3f0b4ebb3b24fe003f53509de492cbecd
emmastephenson <emma.stephenson.24@gmail.com>
2021-03-23 15:45:11 -0700

    add documentation for adding Azure secret

--- ops/services/app_service/metabase-readonly/vault.tf
+++ ops/services/app_service/metabase-readonly/vault.tf
@@ -13,4 +13,4 @@ data "azurerm_key_vault_secret" "postgres_readonly_pass" {
 # We could also set this as a random password, as is done in
 # ops/services/database/main.tf
 # If this is kept non-random, we'll need to manually add it as a secret to the Vault (requires an 
-# Azure login: )
+# Azure login: https://docs.microsoft.com/en-us/azure/key-vault/secrets/quick-create-cli)
