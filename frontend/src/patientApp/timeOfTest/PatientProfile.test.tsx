eb7713386c0cd061d2782304b445cb6b81f137cc
Nick Clyde <nick@clyde.tech>
2021-03-26 11:00:56 -0700

    Fix test

--- frontend/src/patientApp/timeOfTest/PatientProfile.test.tsx
+++ frontend/src/patientApp/timeOfTest/PatientProfile.test.tsx
@@ -1,6 +1,15 @@
 import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 
 import PatientProfile from "./PatientProfile";
+
+const mockStore = configureStore([]);
+const mockContainer = (store: any, patient: any) => (
+  <Provider store={store}>
+    <PatientProfile patient={patient} />
+  </Provider>
+);
 
 describe("PatientProfile", () => {
   it("snapshot", () => {
@@ -9,7 +18,10 @@ describe("PatientProfile", () => {
       lastName: "Doe",
       street: "24 Dreary Ln",
     };
-    const component = renderer.create(<PatientProfile patient={patient} />);
+    const store = mockStore({
+      plid: "foo",
+    });
+    const component = renderer.create(mockContainer(store, patient));
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
