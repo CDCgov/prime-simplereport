87e8f32a711c5cca34aa0361011401a6be838f6d
emmastephenson <emma.stephenson.24@gmail.com>
2021-03-31 14:36:18 -0700

    linters again

--- frontend/src/app/commonComponents/__test__/ErrorPage.test.tsx
+++ frontend/src/app/commonComponents/__test__/ErrorPage.test.tsx
@@ -1,8 +1,7 @@
-import { getByText, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import ErrorPage from "../ErrorPage";
 
-import SettingsNav from "../ErrorPage";
+import ErrorPage from "../ErrorPage";
 
 describe("ErrorPage", () => {
   it("displays the correct error message", () => {
@@ -12,8 +11,14 @@ describe("ErrorPage", () => {
       </MemoryRouter>
     );
     expect(getByText("Something went wrong :(")).toBeInTheDocument();
-    expect(getByText("Please try refreshing your browser.")).toBeInTheDocument();
-    expect(getByText("If the problem continues, contact support@simplereport.gov for support.")).toBeInTheDocument();
+    expect(
+      getByText("Please try refreshing your browser.")
+    ).toBeInTheDocument();
+    expect(
+      getByText(
+        "If the problem continues, contact support@simplereport.gov for support."
+      )
+    ).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
 });
