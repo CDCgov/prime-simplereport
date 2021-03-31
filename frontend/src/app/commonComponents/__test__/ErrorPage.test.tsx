4c068ccda991b4d7fa3d246c98f5e29ce666bb68
emmastephenson <emma.stephenson.24@gmail.com>
2021-03-31 14:33:31 -0700

    Add screenshot tests for error page.

---
+++ frontend/src/app/commonComponents/__test__/ErrorPage.test.tsx
@@ -0,0 +1,19 @@
+import { getByText, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import ErrorPage from "../ErrorPage";
+
+import SettingsNav from "../ErrorPage";
+
+describe("ErrorPage", () => {
+  it("displays the correct error message", () => {
+    const { container, getByText } = render(
+      <MemoryRouter>
+        <ErrorPage />
+      </MemoryRouter>
+    );
+    expect(getByText("Something went wrong :(")).toBeInTheDocument();
+    expect(getByText("Please try refreshing your browser.")).toBeInTheDocument();
+    expect(getByText("If the problem continues, contact support@simplereport.gov for support.")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
