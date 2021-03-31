b2ddb95c06e9fa97663ed8cd38052db6966d5361
Maya Benari <hello@maya-benari.com>
2021-03-31 16:48:57 -0700

    Update error front end

--- frontend/src/app/commonComponents/ErrorPage.tsx
+++ frontend/src/app/commonComponents/ErrorPage.tsx
@@ -1,23 +1,33 @@
+import USAGovBanner from "../commonComponents/USAGovBanner";
 import siteLogo from "../../img/simplereport-logo-color.svg";
-import "./ErrorPage.scss";
 
 const ErrorPage = () => {
   return (
-    <main>
-      <div className="error-img-container">
-        <div className="usa-logo">
-          <img src={siteLogo} alt="{process.env.REACT_APP_TITLE}" />
+    <>
+      <header>
+        <USAGovBanner />
+        <div className="border-bottom border-base-lighter padding-y-1">
+          <div className="grid-container">
+            <div className="maxw-card-lg">
+              <img src={siteLogo} alt="{process.env.REACT_APP_TITLE}" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="grid-container maxw-tablet error-text-container">
-        <h1 className="error-h1">Something went wrong :(</h1>
-        <p>Please try refreshing your browser.</p>
-        <p>
-          If the problem continues, contact support@simplereport.gov for
-          support.
-        </p>
-      </div>
-    </main>
+      </header>
+      <main className="usa-section">
+        <div className="grid-container usa-prose">
+          <h1 className="">Something went wrong :(</h1>
+          <p>Please try refreshing your browser.</p>
+          <p>
+            If the problem continues, contact{" "}
+            <a href="mailto:support@simplereport.gov">
+              support@simplereport.gov
+            </a>{" "}
+            for support.
+          </p>
+        </div>
+      </main>
+    </>
   );
 };
 
