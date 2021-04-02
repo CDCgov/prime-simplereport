86e22fc5d99af237acf0f27e8f430981caa1a78f
emmastephenson <emma.stephenson.24@gmail.com>
2021-04-02 10:35:06 -0700

    linters

--- frontend/src/app/PrimeErrorBoundary.test.tsx
+++ frontend/src/app/PrimeErrorBoundary.test.tsx
@@ -1,48 +1,48 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { Component } from "react";
+
 import PrimeErrorBoundary from "./PrimeErrorBoundary";
 import ErrorPage from "./commonComponents/ErrorPage";
 
-import { Component } from "react";
 
 jest.mock("./AppInsights", () => {
-    return {
-        appInsights: {trackException: jest.fn()}
-    };
-  });
+  return {
+    appInsights: { trackException: jest.fn() },
+  };
+});
 
-  interface State {}
-  interface Props {}
-  
-  class ComponentOfDeath extends Component<Props, State> {
-    public state: State = {}
+interface State {}
+interface Props {}
 
-    public componentDidMount() {
-      throw new Error("a really terrible uncaught exception!");
-    }
-    public render() {
-      return <h1>Component of doooom</h1>
-    }
+class ComponentOfDeath extends Component<Props, State> {
+  public state: State = {};
+
+  public componentDidMount() {
+    throw new Error("a really terrible uncaught exception!");
   }
+  public render() {
+    return <h1>Component of doooom</h1>;
+  }
+}
 
 describe("PrimeErrorBoundary", () => {
-    beforeEach( () => {
-        jest.spyOn(console, "error").mockImplementation(() => {});
-    }); 
+  beforeEach(() => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
 
-    afterEach(() => {
-        jest.spyOn(console, "error").mockRestore();
-    })
+  afterEach(() => {
+    jest.spyOn(console, "error").mockRestore();
+  });
 
   it("renders the error page on component error", () => {
-      const { container } = render(
-          <MemoryRouter>
-              <PrimeErrorBoundary onError={(error: any) => 
-                <ErrorPage></ErrorPage>}>
-                    <ComponentOfDeath></ComponentOfDeath>
-              </PrimeErrorBoundary>
-          </MemoryRouter>
-      )
-      expect(container).toHaveTextContent("Something went wrong :(");
+    const { container } = render(
+      <MemoryRouter>
+        <PrimeErrorBoundary onError={(error: any) => <ErrorPage></ErrorPage>}>
+          <ComponentOfDeath></ComponentOfDeath>
+        </PrimeErrorBoundary>
+      </MemoryRouter>
+    );
+    expect(container).toHaveTextContent("Something went wrong :(");
   });
 });
