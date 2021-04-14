ef80e98501d8855eb5c90d154038ee868a710e03
Adam Weber <adam@skylight.digital>
2021-04-13 20:12:26 -0700

    Use AuditedEntity; use PhoneType in GQL schema

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/PhoneNumber.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/PhoneNumber.java
@@ -1,23 +1,15 @@
 package gov.cdc.usds.simplereport.db.model;
 
 import gov.cdc.usds.simplereport.db.model.auxiliary.PhoneType;
-import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class PhoneNumber {
-  @Id
-  @Column
-  @GeneratedValue(generator = "UUID4")
-  private UUID id;
-
+public class PhoneNumber extends AuditedEntity {
   @ManyToOne(optional = false)
   @JoinColumn(name = "person_internal_id")
   private Person person;
