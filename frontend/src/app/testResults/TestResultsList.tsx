34e79a0940617edca580e7368cbd167f66e74498
Nick Clyde <nick@clyde.tech>
2021-04-14 10:32:17 -0700

    Add filter by patient to test results page

--- frontend/src/app/testResults/TestResultsList.tsx
+++ frontend/src/app/testResults/TestResultsList.tsx
@@ -1,8 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
-import React, { useState } from "react";
+import { gql, useLazyQuery, useQuery } from "@apollo/client";
+import React, {
+  ChangeEventHandler,
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { useSelector } from "react-redux";
 import moment from "moment";
 import classnames from "classnames";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
 
 import { PATIENT_TERM_CAP } from "../../config/constants";
 import { displayFullName, displayFullNameInOrder } from "../utils";
@@ -14,11 +22,20 @@ import { ActionsMenu } from "../commonComponents/ActionsMenu";
 import { getUrl } from "../utils/url";
 import Pagination from "../commonComponents/Pagination";
 import { TEST_RESULT_DESCRIPTIONS } from "../constants";
+import "./TestResultsList.scss";
+import Button from "../commonComponents/Button";
+import { useDebounce } from "../testQueue/addToQueue/useDebounce";
+import {
+  MIN_SEARCH_CHARACTER_COUNT,
+  SEARCH_DEBOUNCE_TIME,
+} from "../testQueue/constants";
+import SearchInput from "../testQueue/addToQueue/SearchInput";
+import { QUERY_PATIENT } from "../testQueue/addToQueue/AddToQueueSearch";
+import { Patient } from "../patients/ManagePatients";
 
 import TestResultPrintModal from "./TestResultPrintModal";
 import TestResultCorrectionModal from "./TestResultCorrectionModal";
-
-import "./TestResultsList.scss";
+import TestResultsByPatientSearch from "./TestResultsByPatientSearch";
 
 type Results = keyof typeof TEST_RESULT_DESCRIPTIONS;
 
@@ -62,6 +79,46 @@ export const testResultQuery = gql`
   }
 `;
 
+export const testResultByPatientQuery = gql`
+  query GetPatientResults($patientId: ID!, $pageNumber: Int, $pageSize: Int) {
+    testResultsByPatient(
+      patientId: $patientId
+      pageNumber: $pageNumber
+      pageSize: $pageSize
+    ) {
+      internalId
+      dateTested
+      result
+      correctionStatus
+      deviceType {
+        internalId
+        name
+      }
+      patient {
+        internalId
+        firstName
+        middleName
+        lastName
+        birthDate
+        gender
+        lookupId
+      }
+      createdBy {
+        nameInfo {
+          firstName
+          middleName
+          lastName
+        }
+      }
+      patientLink {
+        internalId
+      }
+      symptoms
+      noSymptoms
+    }
+  }
+`;
+
 interface Props {
   data: any;
   trackAction: () => void;
@@ -69,6 +126,8 @@ interface Props {
   page: number;
   entriesPerPage: number;
   totalEntries: number;
+  setSelectedPatientId: Dispatch<SetStateAction<string>>;
+  facilityId: string;
 }
 
 function hasSymptoms(noSymptoms: boolean, symptoms: string) {
@@ -90,9 +149,43 @@ export const DetachedTestResultsList: any = ({
   page,
   entriesPerPage,
   totalEntries,
+  setSelectedPatientId,
+  facilityId,
 }: Props) => {
   const [printModalId, setPrintModalId] = useState(undefined);
   const [markErrorId, setMarkErrorId] = useState(undefined);
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [queryString, debounced, setDebounced] = useDebounce("", {
+    debounceTime: SEARCH_DEBOUNCE_TIME,
+    runIf: (q) => q.length >= MIN_SEARCH_CHARACTER_COUNT,
+  });
+
+  const allowQuery = debounced.length >= MIN_SEARCH_CHARACTER_COUNT;
+
+  const [queryPatients, { data: patientData }] = useLazyQuery(QUERY_PATIENT, {
+    fetchPolicy: "no-cache",
+    variables: { facilityId, namePrefixMatch: queryString },
+  });
+
+  useEffect(() => {
+    if (queryString.trim() !== "") {
+      queryPatients();
+    }
+  }, [queryString, queryPatients]);
+
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setDebounced(event.target.value);
+  };
+
+  const onSearchClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+  };
+
+  const onPatientSelect = (patient: Patient) => {
+    setDebounced("");
+    setSelectedPatientId(patient.internalId);
+  };
 
   if (printModalId) {
     return (
@@ -200,7 +293,42 @@ export const DetachedTestResultsList: any = ({
                   {totalEntries}
                 </span>
               </h2>
+              <div>
+                <Button
+                  variant={!showFilters ? "outline" : undefined}
+                  className={showFilters ? "sr-active-button" : undefined}
+                  icon={faSlidersH}
+                  onClick={() => {
+                    if (showFilters) {
+                      setDebounced("");
+                    }
+                    setShowFilters(!showFilters);
+                  }}
+                >
+                  {showFilters ? "Clear filters" : "Filter"}
+                </Button>
+              </div>
             </div>
+            {showFilters && (
+              <div
+                id="test-results-search-by-patient-input"
+                className="display-flex flex-row position-relative bg-base-lightest padding-x-3 padding-y-2"
+              >
+                <SearchInput
+                  onSearchClick={onSearchClick}
+                  onInputChange={onInputChange}
+                  queryString={debounced}
+                  disabled={!allowQuery}
+                  placeholder={"Filter test results by a specific patient"}
+                />
+                <TestResultsByPatientSearch
+                  patients={patientData?.patients || []}
+                  onPatientSelect={onPatientSelect}
+                  shouldShowSuggestions={allowQuery}
+                  loading={debounced !== queryString}
+                />
+              </div>
+            )}
             <div className="usa-card__body">
               <table className="usa-table usa-table--borderless width-full">
                 <thead>
@@ -237,24 +365,62 @@ export const resultsCountQuery = gql`
     testResultsCount(facilityId: $facilityId)
   }
 `;
+export const resultsCountByPatientQuery = gql`
+  query GetResultsCountByPatient($patientId: ID!) {
+    testResultsCountByPatient(patientId: $patientId)
+  }
+`;
 
 const TestResultsList = (
   props: Omit<
     Props,
-    InjectedQueryWrapperProps | "pageCount" | "entriesPerPage" | "totalEntries"
+    | InjectedQueryWrapperProps
+    | "pageCount"
+    | "entriesPerPage"
+    | "totalEntries"
+    | "facilityId"
+    | "setSelectedPatientId"
   >
 ) => {
   const activeFacilityId = useSelector(
     (state) => (state as any).facility.id as string
   );
 
+  const [selectedPatientId, setSelectedPatientId] = useState<string>("");
+
+  const entriesPerPage = 20;
+  const pageNumber = props.page || 1;
+
+  let query, countQuery, countQueryVariables;
+  const queryVariables: {
+    patientId?: string;
+    facilityId?: string;
+    pageNumber: number;
+    pageSize: number;
+  } = {
+    pageNumber: pageNumber - 1,
+    pageSize: entriesPerPage,
+  };
+
+  if (selectedPatientId) {
+    query = testResultByPatientQuery;
+    countQuery = resultsCountByPatientQuery;
+    queryVariables.patientId = selectedPatientId;
+    countQueryVariables = { patientId: selectedPatientId };
+  } else {
+    query = testResultQuery;
+    countQuery = resultsCountQuery;
+    queryVariables.facilityId = activeFacilityId;
+    countQueryVariables = { facilityId: activeFacilityId };
+  }
+
   const {
     data: totalResults,
     loading,
     error,
     refetch: refetchCount,
-  } = useQuery(resultsCountQuery, {
-    variables: { facilityId: activeFacilityId },
+  } = useQuery(countQuery, {
+    variables: countQueryVariables,
     fetchPolicy: "no-cache",
   });
 
@@ -270,18 +436,12 @@ const TestResultsList = (
   }
 
   const totalEntries = totalResults.testResultsCount;
-  const entriesPerPage = 20;
-  const pageNumber = props.page || 1;
 
   return (
     <QueryWrapper<Props>
-      query={testResultQuery}
+      query={query}
       queryOptions={{
-        variables: {
-          facilityId: activeFacilityId,
-          pageNumber: pageNumber - 1,
-          pageSize: entriesPerPage,
-        },
+        variables: queryVariables,
       }}
       onRefetch={refetchCount}
       Component={DetachedTestResultsList}
@@ -290,6 +450,8 @@ const TestResultsList = (
         page: pageNumber,
         totalEntries,
         entriesPerPage,
+        setSelectedPatientId,
+        facilityId: activeFacilityId,
       }}
     />
   );
