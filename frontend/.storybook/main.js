916dfe96c6a684e20016ad35df7adc5f46df8f60
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-04-21 21:02:29 -0400

    Prevent css-loader from erroneous url loading

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,19 +7,26 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
   ],
+  // Overrides CRA webpack settings:
+  // https://github.com/facebook/create-react-app/issues/9870#issuecomment-719666170
   webpackFinal: async (config) => {
-    // Force SASS loader to look for USWDS files
     config.module.rules.forEach(
       (r) =>
-        r.use &&
-        r.use.forEach((u) => {
-          if (/sass-loader/.test(u.loader)) {
-            u.options = {
-              sassOptions: {
-                includePaths: ["./node_modules/uswds/dist/scss/"],
-              },
-            };
-          }
+        r.oneOf &&
+        r.oneOf.forEach((u) => {
+          u.use?.forEach((el, i) => {
+            if (/\/css-loader/.test(el?.loader)) {
+              el.options = {
+                ...el.options,
+                url: (url) => {
+                  if (/\.(ttf|eot|woff|woff2|svg|png)$/.test(url)) {
+                    return false;
+                  }
+                  return true;
+                },
+              };
+            }
+          });
         })
     );
     return config;
