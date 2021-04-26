bbba76d0383129585618bda02d6cae22173015fc
Jeremy Zitomer <jeremy.a.zitomer@omb.eop.gov>
2021-04-26 18:59:10 -0400

    work in progress...

--- frontend/src/app/admin/Organization/PendingOrganizationsList.tsx
+++ frontend/src/app/admin/Organization/PendingOrganizationsList.tsx
@@ -12,6 +12,7 @@ import Alert from "../../commonComponents/Alert";
 import { showNotification } from "../../utils";
 import Checkboxes from "../../commonComponents/Checkboxes";
 import Button from "../../commonComponents/Button";
+import { Console } from "console";
 
 export const ORGANIZATIONS_QUERY = gql`
   query GetUnverifiedOrganizations($identityVerified: Boolean) {
@@ -37,86 +38,101 @@ interface Props {
   refetch: () => void;
 }
 
-function orgRows(orgs: any, refetch: () => void) {
-  if (orgs.length === 0) {
-    return (
-      <tr>
-        <td>No results</td>
-      </tr>
-    );
-  }
-
-  return [...orgs].map((o) => {
-    const [identityVerified, setIdentityVerified] = useState(
-      o.identityVerified
-    );
-    const [verifyIdentity] = useMutation(SET_ORG_IDENTITY_VERIFIED_MUTATION);
-    const submitIdentityVerified = () => {
-      verifyIdentity({
-        variables: {
-          externalId: o.externalId,
-          verified: identityVerified,
-        },
-      })
-        .then(() => {
-          const alert = (
-            <Alert
-              type="success"
-              title={o.name + " identity verified"}
-              body=""
-            />
-          );
-          showNotification(toast, alert);
-        })
-        .finally(refetch);
-    };
-
-    return (
-      <tr
-        key={o.id}
-        //title=""
-        className={classnames("sr-org-row")}
-      >
-        <th scope="row">
-          {o.name}
-          <span className="display-block text-base font-ui-2xs">
-            External ID: {o.externalId}
-          </span>
-        </th>
-        <td>
-          <Checkboxes
-            onChange={(e) => setIdentityVerified(e.target.checked)}
-            name="identity_verified"
-            legend=""
-            boxes={[
-              {
-                value: "1",
-                label: "Identity Verified",
-                checked: identityVerified,
-              },
-            ]}
-          />
-        </td>
-        <td>
-          <Button
-            type="button"
-            onClick={submitIdentityVerified}
-            label="Save Changes"
-            disabled={!identityVerified}
-          />
-        </td>
-      </tr>
-    );
-  });
-}
-
 export const DetachedPendingOrganizationsList: any = ({
   data,
   refetch,
 }: Props) => {
   const orgs = data?.organizations || [];
 
-  const rows = orgRows(orgs, refetch);
+  var externalIdVerifiedMap = new Map([...orgs].map((o) => [o.externalId, o.identityVerified]));
+
+  // const verifyByExternalId = (externalId: String) => {
+  //   const [modifiedData, setModifiedData] = useState(externalIdVerifiedMap.get(externalId))
+  //   return setModifiedData;
+  // }
+
+  console.log(externalIdVerifiedMap);
+  function orgRows(orgs: any) {
+    if (orgs.length === 0) {
+      return (
+        <tr>
+          <td>No results</td>
+        </tr>
+      );
+    }
+    
+    var rows = [];
+    for (var i in orgs) {
+      const o = orgs[i];
+      const [modifiedData, setVerified] = useState(externalIdVerifiedMap.get(o.externalId));
+      rows.push(
+        <tr
+          key={o.id}
+          //title=""
+          className={classnames("sr-org-row")}
+        >
+          <th scope="row">
+            {o.name}
+          </th>
+          <th scope="row">
+            {o.externalId}
+          </th>
+          <td>
+            <Checkboxes
+              onChange={(e) => {setVerified(e.target.checked)}}
+              name="identity_verified"
+              legend=""
+              boxes={[
+                {
+                  value: "1",
+                  label: "Identity Verified",
+                  checked: externalIdVerifiedMap.get(o.externalId),
+                },
+              ]}
+            />
+          </td>
+          {/* <td>
+            <Button
+              type="button"
+              onClick={submitIdentityVerified}
+              label="Save Changes"
+              disabled={!identityVerified}
+            />
+          </td> */}
+        </tr>
+      );
+    }
+  }
+
+  const rows = orgRows(orgs);
+
+  const [verifyIdentity] = useMutation(SET_ORG_IDENTITY_VERIFIED_MUTATION);
+
+  const submitIdentityVerified = () => {
+    const verifyAllIdentities = new Promise(() => 
+      {externalIdVerifiedMap.forEach((externalId,verified) => {
+        verifyIdentity({
+          variables: {
+            externalId: externalId,
+            verified: verified,
+          },
+        })
+      })}
+    );
+
+    verifyAllIdentities
+      .then(() => {
+        const alert = (
+          <Alert
+            type="success"
+            title={"Blah identity verified"}
+            body=""
+          />
+        );
+        showNotification(toast, alert);
+      })
+      .finally(refetch);
+    };
 
   return (
     <main className="prime-home">
@@ -125,14 +141,22 @@ export const DetachedPendingOrganizationsList: any = ({
           <div className="prime-container card-container sr-pending-organizations-list">
             <div className="usa-card__header">
               <h2>Organizations Pending Identity Verification</h2>
+              <div>
+                <Button
+                  className="sr-active-button"
+                  onClick={submitIdentityVerified}
+                >
+                  Save Changes
+                </Button>
+              </div>
             </div>
             <div className="usa-card__body">
               <table className="usa-table usa-table--borderless width-full">
                 <thead>
                   <tr>
                     <th scope="col">Name</th>
+                    <th scope="col">External ID</th>
                     <th scope="col">Verify Identity</th>
-                    <th scope="col"></th>
                   </tr>
                 </thead>
                 <tbody>{rows}</tbody>
