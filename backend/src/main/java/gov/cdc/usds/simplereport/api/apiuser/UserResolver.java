2a4d31f6f454fc8751e98a6e969cef5a5c071271
Brett Maden <brett.m.maden@omb.eop.gov>
2021-04-27 16:57:50 -0400

    remove UserIdentity wrapper

--- backend/src/main/java/gov/cdc/usds/simplereport/api/apiuser/UserResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/apiuser/UserResolver.java
@@ -1,12 +1,11 @@
 package gov.cdc.usds.simplereport.api.apiuser;
 
 import gov.cdc.usds.simplereport.api.model.User;
-import gov.cdc.usds.simplereport.api.model.UserIdentity;
+import gov.cdc.usds.simplereport.db.model.ApiUser;
 import gov.cdc.usds.simplereport.service.ApiUserService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 /** Resolver for the graphql User type */
@@ -23,10 +22,8 @@ public class UserResolver implements GraphQLQueryResolver {
     return new User(_userService.getCurrentUserInfo());
   }
 
-  public List<UserIdentity> getUsers() {
-    return _userService.getUsersInCurrentOrg().stream()
-        .map(UserIdentity::new)
-        .collect(Collectors.toList());
+  public List<ApiUser> getUsers() {
+    return _userService.getUsersInCurrentOrg();
   }
 
   public User getUser(UUID userId) {
