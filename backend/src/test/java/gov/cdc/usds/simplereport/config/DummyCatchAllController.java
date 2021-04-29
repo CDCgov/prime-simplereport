ad74be21055ddfeb2de5a464bb3e172077ded143
emmastephenson <emma.stephenson.24@gmail.com>
2021-04-29 16:04:58 -0700

    add further database backing for spring sessions

--- backend/src/test/java/gov/cdc/usds/simplereport/config/DummyCatchAllController.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/config/DummyCatchAllController.java
@@ -2,7 +2,6 @@ package gov.cdc.usds.simplereport.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
--- backend/src/test/java/gov/cdc/usds/simplereport/config/SecurityConfigurationTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/config/SecurityConfigurationTest.java
@@ -1,18 +1,17 @@
 package gov.cdc.usds.simplereport.config;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import gov.cdc.usds.simplereport.logging.AuditLoggingAdvice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
-import gov.cdc.usds.simplereport.logging.AuditLoggingAdvice;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @WebMvcTest(
     controllers = DummyCatchAllController.class,
@@ -29,14 +28,14 @@ public class SecurityConfigurationTest {
 
   @Test
   void defaultPathIsAllowed() throws Exception {
-    MockHttpServletRequestBuilder builder = 
-      get("/");
-      this._mockMvc.perform(builder).andExpect(status().isOk());
+    MockHttpServletRequestBuilder builder = get("/");
+    this._mockMvc.perform(builder).andExpect(status().isOk());
   }
 
   @Test
   void postToAccountRequestIsAllowed() throws Exception {
-    MockHttpServletRequestBuilder builder = post(WebConfiguration.ACCOUNT_REQUEST + "/foo-facility");
+    MockHttpServletRequestBuilder builder =
+        post(WebConfiguration.ACCOUNT_REQUEST + "/foo-facility");
     this._mockMvc.perform(builder).andExpect(status().isOk());
   }
 
