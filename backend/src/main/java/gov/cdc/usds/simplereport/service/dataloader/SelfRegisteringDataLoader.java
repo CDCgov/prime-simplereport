36a4f0dafb5aacc3f1f6f721bcebfc305d6b1564
Adam Weber <adam@skylight.digital>
2021-04-28 20:00:21 -0700

    Use HTML, not Markdown, in Javadocs

--- backend/src/main/java/gov/cdc/usds/simplereport/service/dataloader/SelfRegisteringDataLoader.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/dataloader/SelfRegisteringDataLoader.java
@@ -7,7 +7,8 @@ import org.dataloader.DataLoaderRegistry;
 /**
  * In order for the GraphQL DataResolvers to be able to access DataLoaders from the
  * DataLoaderRegistry, each DataLoader must have a unique key. In order for our DataLoaders to
- * _self-register_ with the DataLoaderRegistry, they must also know their own key at instantiation.
+ * <em>self-register</em> with the DataLoaderRegistry, they must also know their own key at
+ * instantiation.
  */
 public abstract class SelfRegisteringDataLoader<K, V> extends DataLoader<K, V> {
   public SelfRegisteringDataLoader(
