7d9e2c87ffddbd583a15e1f6d162b4f815cdc514
Maya Benari <hello@maya-benari.com>
2021-04-29 17:16:44 -0700

    upgrade storybook and add a11y add-on

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
+    "@storybook/addon-links",
     "@storybook/preset-create-react-app",
   ],
 };
