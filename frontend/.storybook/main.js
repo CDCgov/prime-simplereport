cf98bddabf84fc100fa1b329000ab5a9d3b757d9
Maya Benari <hello@maya-benari.com>
2021-04-29 17:22:17 -0700

    update addon registration order

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
     "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/preset-create-react-app",
   ],
 };
