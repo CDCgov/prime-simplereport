98ff8798abfc95a1c2a58f823bd03c30e89baf4b
emmastephenson <emma.stephenson.24@gmail.com>
2021-04-30 13:04:46 -0700

    add model for user account creation

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/UserAccountCreationRequest.java
@@ -0,0 +1,29 @@
+package gov.cdc.usds.simplereport.api.model.useraccountcreation;
+
+public class UserAccountCreationRequest {
+    
+    private String password;
+    private String recoveryQuestion;
+    private String recoveryAnswer;
+
+    public UserAccountCreationRequest(String password) {
+        this.password = password;
+    }
+
+    public UserAccountCreationRequest(String recoveryQuestion, String recoveryAnswer) {
+        this.recoveryQuestion = recoveryQuestion;
+        this.recoveryAnswer = recoveryAnswer;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getRecoveryQuestion() {
+        return this.recoveryQuestion;
+    }
+
+    public String getRecoveryAnswer() {
+        return this.recoveryAnswer;
+    }
+}
