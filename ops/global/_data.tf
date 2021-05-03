a0606dcfead9f841464a2f8967382f99b0a993b0
Ben Warfield <bwarfield@cdc.gov>
2021-05-03 14:05:14 -0400

    Import "Everyone" group into global configuration.
    We need everyone to have an MFA login requirement even after redeploys.

--- ops/global/_data.tf
+++ ops/global/_data.tf
@@ -6,3 +6,7 @@ data "azurerm_key_vault_secret" "slack_metrics_webhook" {
   key_vault_id = azurerm_key_vault.sr.id
   name         = "simple-report-global-slack-webhook"
 }
+
+data "okta_group" "everyone" {
+  name = "Everyone"
+}
