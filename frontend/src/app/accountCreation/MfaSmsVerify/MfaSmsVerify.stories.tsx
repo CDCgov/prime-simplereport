da56d58f6794a964d44075ae712fec1f6001f586
Nick Clyde <nick@clyde.tech>
2021-05-03 15:09:43 -0700

    Add error states and validation for account creation components  (#1484)
    * Start adding error states
    
    * remove extra stories
    
    * use legend on radio group
    
    * update pw helper
    
    * upgrade storybook and add a11y add-on
    
    * update addon registration order
    
    * update pw helper design
    
    * Add error states and validation for account creation components
    
    * update error message copy
    
    Co-authored-by: Maya Benari <hello@maya-benari.com>
    Co-authored-by: Maya Benari <maya@skylight.digital>

--- frontend/src/app/accountCreation/MfaSmsVerify/MfaSmsVerify.stories.tsx
+++ frontend/src/app/accountCreation/MfaSmsVerify/MfaSmsVerify.stories.tsx
@@ -12,8 +12,3 @@ const Template: Story = (args) => <MfaSmsVerify {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
-
-export const ErrorState = Template.bind({});
-ErrorState.args = {
-  passwordError: "error",
-};
--- frontend/src/app/accountCreation/SecurityQuestion/SecurityQuestion.test.tsx
+++ frontend/src/app/accountCreation/SecurityQuestion/SecurityQuestion.test.tsx
@@ -9,12 +9,30 @@ describe("SecurityQuestion", () => {
   });
 
   it("can choose a security question and type an answer", () => {
-    userEvent.selectOptions(screen.getByLabelText("Security question"), [
-      "What’s your favorite book?",
-    ]);
-    fireEvent.change(screen.getByLabelText("Answer"), {
+    userEvent.selectOptions(
+      screen.getByLabelText("Security question", { exact: false }),
+      ["What’s your favorite book?"]
+    );
+    fireEvent.change(screen.getByLabelText("Answer", { exact: false }), {
       target: { value: "Lord of the Rings" },
     });
     expect(screen.getByText("What’s your favorite book?")).toBeInTheDocument();
+  });
+
+  it("requires a security question", () => {
+    fireEvent.change(screen.getByLabelText("Answer", { exact: false }), {
+      target: { value: "Lord of the Rings" },
+    });
+    fireEvent.click(screen.getByText("Continue"));
+    expect(screen.getByText("Enter a security question")).toBeInTheDocument();
+  });
+
+  it("requires a security answer", () => {
+    userEvent.selectOptions(
+      screen.getByLabelText("Security question", { exact: false }),
+      ["What’s your favorite book?"]
+    );
+    fireEvent.click(screen.getByText("Continue"));
+    expect(screen.getByText("Enter a security answer")).toBeInTheDocument();
   });
 });
