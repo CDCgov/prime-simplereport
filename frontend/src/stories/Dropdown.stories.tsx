effd0b5de53f3ee72a17cd51c26aaccdc2a6d701
Maya Benari <hello@maya-benari.com>
2021-05-06 17:58:52 -0700

    add dropdown story

---
+++ frontend/src/stories/Dropdown.stories.tsx
@@ -0,0 +1,36 @@
+import { Story, Meta } from "@storybook/react";
+// import { useState } from "react";
+
+import Dropdown from "../app/commonComponents/Dropdown";
+
+const fruits = ["Apple", "Orange", "Pear"];
+
+const fruitOptions = Object.entries(fruits).map(([value, key]) => ({
+  value: value,
+  label: key,
+}));
+
+export default {
+  title: "Components/Form controls/Dropdown",
+  component: Dropdown,
+  parameters: { layout: "fullscreen" },
+  argTypes: {},
+  args: {
+    name: "dropdown",
+    label: "Dropdown",
+    errorMessage: "Helpful error message",
+    options: fruitOptions.map((c) => ({ label: c, value: c })),
+    defaultSelect: true,
+  },
+} as Meta;
+
+type Props = React.ComponentProps<typeof Dropdown>;
+
+const Template: Story<Props> = (args) => (
+  <div className="grid-container margin-top-2">
+    <Dropdown {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
