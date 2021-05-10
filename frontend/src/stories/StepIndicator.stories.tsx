19b7f0acc67c4afa6b9dc6d0191b13f8a6f22147
Maya Benari <hello@maya-benari.com>
2021-05-10 16:18:11 -0700

    add step indicator story

---
+++ frontend/src/stories/StepIndicator.stories.tsx
@@ -0,0 +1,36 @@
+import { Story, Meta } from "@storybook/react";
+
+import StepIndicator from "../app/commonComponents/StepIndicator";
+
+export default {
+  title: "Components/Step indicator",
+  component: StepIndicator,
+  argTypes: {},
+  args: {
+    steps: [
+      {
+        label: "Apple",
+        value: "apple",
+        order: 0,
+      },
+      {
+        label: "Orange",
+        value: "orange",
+        order: 1,
+      },
+      {
+        label: "Pear",
+        value: "pear",
+        order: 2,
+      },
+    ],
+    currentStepValue: "orange",
+  },
+} as Meta;
+
+type Props = React.ComponentProps<typeof StepIndicator>;
+
+const Template: Story<Props> = (args) => <StepIndicator {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
