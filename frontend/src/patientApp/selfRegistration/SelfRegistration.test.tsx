1f3516443ab197ade72253cfd07e830478da8356
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-05-10 10:53:57 -0400

    Add unit test for self registration

--- frontend/src/patientApp/selfRegistration/SelfRegistration.test.tsx
+++ frontend/src/patientApp/selfRegistration/SelfRegistration.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { MemoryRouter, Route } from "react-router";
 import createMockStore from "redux-mock-store";
+import faker from "faker";
 
 import { SelfRegistration } from "./SelfRegistration";
 
@@ -18,29 +19,24 @@ jest.mock("../PxpApiService", () => ({
         }
       });
     },
+    selfRegister: () => {
+      return Promise.resolve();
+    },
   },
 }));
 
 const mockStore = createMockStore([]);
 const store = mockStore({});
 
-describe("SelfRegistration", () => {
-  it("Renders the terms of service for a good link", async () => {
-    await waitFor(() => {
-      render(
-        <Provider store={store}>
-          <MemoryRouter initialEntries={[`/register/${VALID_LINK}`]}>
-            <Route
-              exact
-              path="/register/:registrationLink"
-              component={SelfRegistration}
-            />
-          </MemoryRouter>
-        </Provider>
-      );
-    });
+const originalConsoleError = console.error;
 
-    expect(screen.queryByText("Foo Facility")).toBeInTheDocument();
+describe("SelfRegistration", () => {
+  beforeEach(() => {
+    // For smartystreets failures
+    console.error = jest.fn();
+  });
+  afterEach(() => {
+    console.error = originalConsoleError;
   });
 
   it("Renders a 404 page for a bad link", async () => {
@@ -60,4 +56,47 @@ describe("SelfRegistration", () => {
 
     expect(screen.queryByText("Page not found")).toBeInTheDocument();
   });
+
+  it("Allows for user to register through link", async () => {
+    await waitFor(() => {
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[`/register/${VALID_LINK}`]}>
+            <Route
+              exact
+              path="/register/:registrationLink"
+              component={SelfRegistration}
+            />
+          </MemoryRouter>
+        </Provider>
+      );
+    });
+    expect(screen.queryByText("Foo Facility")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("I agree"));
+    expect(screen.getByText("General information")).toBeInTheDocument();
+    Object.entries(filledForm).forEach(([field, value]) => {
+      fireEvent.change(screen.getByLabelText(field, { exact: false }), {
+        target: { value },
+      });
+    });
+    screen.getAllByLabelText("No").forEach(fireEvent.click);
+    fireEvent.click(screen.getByText("Submit"));
+    await screen.findByText("Address validation");
+    fireEvent.click(screen.getByLabelText("Use address", { exact: false }));
+    fireEvent.click(screen.getByText("Save changes"));
+    expect(
+      await screen.findByText("Registration complete")
+    ).toBeInTheDocument();
+  });
 });
+
+const filledForm = {
+  "First name": faker.name.firstName(),
+  "Middle name": faker.name.middleName(),
+  "Last name": faker.name.lastName(),
+  "Date of birth": "1970-09-22",
+  "Phone number": "7038675309",
+  "Street address 1": faker.address.streetAddress(),
+  State: "MD",
+  "Zip code": "12345",
+};
