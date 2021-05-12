ad4f42c497509a938bb47108ecd0ad434c0b57e4
emmastephenson <emma.stephenson.24@gmail.com>
2021-05-12 16:06:04 -0700

    updating with Brett's feedback

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/UserAccountCreationRequest.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/UserAccountCreationRequest.java
@@ -1,11 +1,15 @@
 package gov.cdc.usds.simplereport.api.model.useraccountcreation;
 
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UserAccountCreationRequest {
+  @Size(max = 512)
   private String activationToken;
+
+  @Size(max = 256)
   private String password;
 }
