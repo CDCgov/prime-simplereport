c1f3735f126f428fd70306976704bf9bd446b26c
Maya Benari <hello@maya-benari.com>
2021-05-12 12:06:34 -0700

    Update radio buttons

--- frontend/src/app/commonComponents/RadioGroup.tsx
+++ frontend/src/app/commonComponents/RadioGroup.tsx
@@ -61,6 +61,8 @@ const RadioGroup = <T extends string>({
   return (
     <div
       className={classnames(
+        "usa-form-group",
+        "form-group--radio",
         validationStatus === "error" && "usa-form-group--error"
       )}
     >
@@ -91,7 +93,6 @@ const RadioGroup = <T extends string>({
           {(_, uid) => (
             <div
               className={classnames(
-                "usa-form-group",
                 variant === "horizontal" && "prime-radio--horizontal"
               )}
             >
