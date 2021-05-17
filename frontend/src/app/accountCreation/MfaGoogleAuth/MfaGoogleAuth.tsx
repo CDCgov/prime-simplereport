a0be75366b461d3cd3e36c6a82e892acdff42bd4
Nick Clyde <nick@clyde.tech>
2021-05-17 11:48:38 -0700

    Generalize TOTP cards

--- frontend/src/app/accountCreation/MfaGoogleAuth/MfaGoogleAuth.tsx
+++ frontend/src/app/accountCreation/MfaGoogleAuth/MfaGoogleAuth.tsx
@@ -1,42 +1,9 @@
-import QRCode from "react-qr-code";
-
-import { Card } from "../../commonComponents/Card/Card";
-import { CardBackground } from "../../commonComponents/CardBackground/CardBackground";
-import Button from "../../commonComponents/Button/Button";
-import StepIndicator from "../../commonComponents/StepIndicator";
-import { accountCreationSteps } from "../../../config/constants";
+import { MfaTotp } from "../MfaTotp/MfaTotp";
 
 interface Props {
   qrCode: string;
 }
 
 export const MfaGoogleAuth = (props: Props) => {
-  return (
-    <CardBackground>
-      <Card logo bodyKicker="Set up your account">
-        <StepIndicator
-          steps={accountCreationSteps}
-          currentStepValue={"2"}
-          noLabels={true}
-        />
-        <p className="margin-bottom-0">
-          Get your security code via the Google Authenticator application.
-        </p>
-        <p className="usa-hint font-ui-2xs">
-          To add an authentication application, scan this QR code in the app.
-        </p>
-        <div className="display-flex flex-column flex-align-center">
-          {props.qrCode ? (
-            <img src={props.qrCode} alt="TOTP QR Code" />
-          ) : (
-            <QRCode value={"https://bit.ly/3cRcweQ"} size={140} />
-          )}
-        </div>
-        <Button className="margin-top-3" label={"Continue"} type={"submit"} />
-      </Card>
-      <p className="margin-top-5">
-        <a href="#0">Return to previous step</a>
-      </p>
-    </CardBackground>
-  );
+  return <MfaTotp totpType="Google Authenticator" qrCode={props.qrCode} />;
 };
