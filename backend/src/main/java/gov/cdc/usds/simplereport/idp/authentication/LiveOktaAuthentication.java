6dab0eb53a12ddca2a654ed7f06f65f72bd87ebd
emmastephenson <emma.stephenson.24@gmail.com>
2021-05-20 12:38:55 -0700

    cleaning up for review

--- backend/src/main/java/gov/cdc/usds/simplereport/idp/authentication/LiveOktaAuthentication.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/idp/authentication/LiveOktaAuthentication.java
@@ -9,21 +9,17 @@ import com.okta.sdk.resource.user.RecoveryQuestionCredential;
 import com.okta.sdk.resource.user.User;
 import com.okta.sdk.resource.user.UserCredentials;
 import com.okta.spring.boot.sdk.config.OktaClientProperties;
-
 import gov.cdc.usds.simplereport.api.model.errors.InvalidActivationLinkException;
 import gov.cdc.usds.simplereport.api.model.errors.OktaAuthenticationFailureException;
 import gov.cdc.usds.simplereport.config.BeanProfiles;
 import java.util.List;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpEntity;
 
 /**
  * Created by emmastephenson on 4/28/21
@@ -33,14 +29,11 @@ import org.springframework.http.HttpEntity;
 @Profile("!" + BeanProfiles.NO_OKTA_AUTH)
 @Service
 public class LiveOktaAuthentication implements OktaAuthentication {
-  private static final Logger LOG = LoggerFactory.getLogger(LiveOktaAuthentication.class);
-
   private Client _client;
   private String _apiToken;
   private String _orgUrl;
 
   public LiveOktaAuthentication(OktaClientProperties oktaClientProperties) {
-    LOG.info("WIP: liveOktaAuthentication initialized");
     _client =
         Clients.builder()
             .setOrgUrl(oktaClientProperties.getOrgUrl())
@@ -52,109 +45,24 @@ public class LiveOktaAuthentication implements OktaAuthentication {
 
   /**
    * Converts an activation token into a user id by sending a REST request to the Okta API. (The
-   * Okta Authentication SDK does not yet support activating an activation token. If that changes, update this method to use the SDK.) 
-   * If successful, it moves the Okta state machine into a RESET_PASSWORD state.
+   * Okta Authentication SDK does not yet support activating an activation token. If that changes,
+   * update this method to use the SDK.) If successful, it moves the Okta state machine into a
+   * RESET_PASSWORD state.
+   * https://developer.okta.com/docs/reference/api/authn/#request-example-for-activation-token
    * https://developer.okta.com/docs/reference/api/authn/#response-example-for-activation-token-success-user-without-password
    *
    * @param activationToken the token passed from Okta when creating a new user.
-   * @param crossForwardedHeader the IP address of the user requesting a new account.
+   * @param crossForwardedHeader the IP address of the user requesting a new account, along with any
+   *     other IP addresses in the request chain (typically Azure).
    * @param userAgent the user agent of the user requesting a new account.
    * @return The user id returned by Okta.
    * @throws OktaAuthenticationFailureException if the state token is not returned by Okta.
-   *     <p>https://developer.okta.com/docs/reference/api/authn/#request-example-for-activation-token
    */
-
-  /**
-    * {
-  "stateToken": "005Oj4_rx1yAYP2MFNobMXlM2wJ3QEyzgifBd_T6Go",
-  "expiresAt": "2017-03-29T21:35:47.000Z",
-  "status": "PASSWORD_RESET",
-  "recoveryType": "ACCOUNT_ACTIVATION",
-  "_embedded": {
-    "user": {
-      "id": "00ub0oNGTSWTBKOLGLNR",
-      "passwordChanged": "2015-09-08T20:14:45.000Z",
-      "profile": {
-        "login": "dade.murphy@example.com",
-        "firstName": "Dade",
-        "lastName": "Murphy",
-        "locale": "en_US",
-        "timeZone": "America/Los_Angeles"
-      }
-    },
-    "policy": {
-      "expiration": {
-        "passwordExpireDays": 5
-      },
-      "complexity": {
-        "minLength": 8,
-        "minLowerCase": 1,
-        "minUpperCase": 1,
-        "minNumber": 1,
-        "minSymbol": 0
-      }
-    }
-  },
-  "_links": {
-    "next": {
-      "name": "resetPassword",
-      "href": "https://${yourOktaDomain}/api/v1/authn/credentials/reset_password",
-      "hints": {
-        "allow": [
-          "POST"
-        ]
-      }
-    },
-    "cancel": {
-      "href": "https://${yourOktaDomain}/api/v1/authn/cancel",
-      "hints": {
-        "allow": [
-          "POST"
-        ]
-      }
-    }
-  }
-}
-
-HTTP/1.1 401 Unauthorized
-Content-Type: application/json
-
-{
-  "errorCode": "E0000004",
-  "errorSummary": "Authentication failed",
-  "errorLink": "E0000004",
-  "errorId": "oae2fYYJmkuTg-NyozqBkb3sw",
-  "errorCauses": []
-}
-
-
-curl -v -X POST \
--H "Accept: application/json" \
--H "Content-Type: application/json" \
--H "Authorization: SSWS ${api_token}" \
--H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36" \
--H "X-Forwarded-For: 23.235.46.133" \
--d '{
-  "token": "o7AFoTGE9xjQiHQK6dAa"
-}' "https://${yourOktaDomain}/api/v1/authn"
-
-    */
   public String activateUser(String activationToken, String crossForwardedHeader, String userAgent)
       throws InvalidActivationLinkException {
-    System.out.println("activating user (printed)");
-    LOG.info("activating user");
     JSONObject requestBody = new JSONObject();
     requestBody.put("token", activationToken);
     String authorizationToken = "SSWS " + _apiToken;
-
-  /**
-   *  HttpHeaders headers = new HttpHeaders();
- headers.setContentType(MediaType.TEXT_PLAIN);
- HttpEntity<String> entity = new HttpEntity<String>(helloWorld, headers);
- URI location = template.postForLocation("https://example.com", entity);
- 
-   */
-
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     headers.setAccept(List.of(MediaType.APPLICATION_JSON));
@@ -163,41 +71,14 @@ curl -v -X POST \
     headers.add("Authorization", authorizationToken);
     HttpEntity<String> entity = new HttpEntity<String>(requestBody.toString(), headers);
     RestTemplate restTemplate = new RestTemplate();
-    LOG.info("HEADERS:" + entity.getHeaders());
-    LOG.info("BODY: " + entity.getBody());
-
-    // RestTemplate restTemplate =
-    //     new RestTemplateBuilder(
-    //             rt ->
-    //                 rt.getInterceptors()
-    //                     .add(
-    //                         (request, body, execution) -> {
-    //                           HttpHeaders headers = request.getHeaders();
-    //                           headers.setContentType(MediaType.APPLICATION_JSON);
-    //                           headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-    //                           headers.add(HttpHeaders.USER_AGENT, userAgent);
-    //                           headers.add("X-Forwarded-For", crossForwardedHeader);
-    //                           headers.add("Authorization", authorizationToken);
-    //                           return execution.execute(request, body);
-    //                         }))
-    //         .build();
     String postUrl = _orgUrl + "/api/v1/authn";
-
     try {
       String response = restTemplate.postForObject(postUrl, entity, String.class);
-      LOG.info("activating user response: " + response);
       JSONObject responseJson = new JSONObject(response);
       return responseJson.getJSONObject("_embedded").getJSONObject("user").getString("id");
     } catch (Exception e) {
       throw new OktaAuthenticationFailureException("activation failed", e);
     }
-    // LOG.info("activating user response: " + response);
-    // JSONObject responseJson = new JSONObject(response);
-    // try {
-    //   return responseJson.getJSONObject("_embedded").getJSONObject("user").getString("id");
-    // } catch (NullPointerException e) {
-    //   throw new InvalidActivationLinkException();
-    // }
   }
 
   /**
@@ -211,7 +92,6 @@ curl -v -X POST \
   public void setPassword(String userId, char[] password)
       throws OktaAuthenticationFailureException {
     try {
-      LOG.info("setting password for user: " + userId);
       User user = _client.getUser(userId);
       UserCredentials creds = user.getCredentials();
       PasswordCredential passwordCred =
@@ -219,9 +99,7 @@ curl -v -X POST \
       creds.setPassword(passwordCred);
       user.setCredentials(creds);
       user.update();
-      LOG.info("set password successful");
     } catch (ResourceException e) {
-      LOG.info("resource exception thrown");
       throw new OktaAuthenticationFailureException("Error setting user's password", e);
     }
   }
