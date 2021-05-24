e5769964407d61516a5752f712bee45f047205de
emmastephenson <emma.stephenson.24@gmail.com>
2021-05-24 10:00:36 -0700

    add more notes to myself

--- backend/src/test/java/gov/cdc/usds/simplereport/idp/authentication/LiveOktaAuthenticationTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/idp/authentication/LiveOktaAuthenticationTest.java
@@ -7,4 +7,7 @@ public class LiveOktaAuthenticationTest {
     // in any case, this is likely going to need a different configuration file than the rest of the the tests, since we only want it to run infrequently.
     // backend/src/test/resources/application-default.yaml
     // Ugh I suspect I'm going to have to write a new yaml file just for integration tests, the one we have in place is for unit testing only (and that's where no-auth is set up)
+
+    // maybe we don't need to go through all this trouble - what if I just made a repository instead?
+    // might still need a yaml file to provide all the right Okta links, but that's ok
 }
