f67e7815c0a8bd1358325be6c16b56196c3ce739
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-05-25 17:09:57 -0400

    Remove import

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-a11y",
     "@storybook/preset-create-react-app",
-    "storybook-addon-apollo-client",
   ],
   webpackFinal: (config) => {
     config.resolve.alias[
