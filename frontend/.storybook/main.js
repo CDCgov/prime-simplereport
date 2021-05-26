35c66aec6cd4ff36ff331c10ec561c864c1ff5fd
Volodymyr Onofriichuk <srwind18@gmail.com>
2021-05-26 12:05:23 +0300

    add webpack config to the storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -15,13 +15,27 @@ module.exports = {
       },
     },
   ],
-  webpackFinal: (config) => {
+  webpackFinal: async (config) => {
     config.resolve.alias[
       "@microsoft/applicationinsights-react-js"
     ] = require.resolve("../src/stories/__mocks__/appInsights.js");
     config.resolve.alias["./TestTimer"] = require.resolve(
       "../src/stories/__mocks__/TestTimer.ts"
     );
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: [
+              './node_modules/uswds/dist/scss/packages/_required.scss'
+            ]
+          },
+        },
+      ],
+    },);
     return config;
   },
 };
