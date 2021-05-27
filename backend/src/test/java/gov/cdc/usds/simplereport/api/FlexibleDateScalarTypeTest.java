bb571c822e191d32e38870f216db7d947dbc0e0f
Adam Weber <adam@skylight.digital>
2021-05-27 10:49:46 -0700

    Fix leading-zeroes Date Parse issue & add tests

---
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/FlexibleDateScalarTypeTest.java
@@ -0,0 +1,54 @@
+package gov.cdc.usds.simplereport.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.util.AssertionErrors.assertNull;
+
+import graphql.language.StringValue;
+import graphql.schema.CoercingParseValueException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import org.junit.jupiter.api.Test;
+
+public class FlexibleDateScalarTypeTest {
+  private final FlexibleDateCoercion converter = new FlexibleDateCoercion();
+
+  @Test
+  void convertImpl_succeeds() {
+    LocalDate y2k = LocalDate.parse("2000-01-01");
+    assertEquals(y2k, converter.convertImpl("2000-01-01"));
+    assertEquals(y2k, converter.convertImpl("01/01/2000"));
+  }
+
+  @Test
+  void convertImpl_slashFormatWorksWithoutLeadingZeros() {
+    LocalDate y2k = LocalDate.parse("2000-01-01");
+    assertEquals(y2k, converter.convertImpl("1/01/2000"));
+    assertEquals(y2k, converter.convertImpl("01/1/2000"));
+  }
+
+  @Test
+  void convertImpl_returnsNullOnNoSeparators() {
+    assertNull(null, converter.convertImpl("20000101"));
+  }
+
+  @Test
+  void parseLiteral_succeeds() {
+    LocalDate y2k = LocalDate.parse("2000-01-01");
+    assertEquals(y2k, converter.parseLiteral(new StringValue("2000-01-01")));
+    assertEquals(y2k, converter.parseLiteral(new StringValue("01/01/2000")));
+  }
+
+  @Test
+  void parseValue_succeeds() {
+    LocalDate y2k = LocalDate.parse("2000-01-01");
+    assertEquals(y2k, converter.parseValue("2000-01-01"));
+    assertEquals(y2k, converter.parseValue("01/01/2000"));
+  }
+
+  @Test
+  void parseValue_exceptions() {
+    assertThrows(CoercingParseValueException.class, () -> converter.parseValue("20000101"));
+    assertThrows(DateTimeParseException.class, () -> converter.parseValue("2000-0101"));
+  }
+}
