9d27c4f32938c58cddf967eac67f8203073269f8
Tim Best <tBest@cdc.gov>
2021-05-27 11:28:10 -0400

    cleanup logging

--- backend/src/main/java/gov/cdc/usds/simplereport/api/FlexibleDateScalarType.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/FlexibleDateScalarType.java
@@ -7,8 +7,6 @@ import graphql.schema.CoercingParseValueException;
 import graphql.schema.GraphQLScalarType;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +15,6 @@ public class FlexibleDateScalarType {
   private static final DateTimeFormatter US_DASHDATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
   private static final DateTimeFormatter US_SLASHDATE_FORMATTER =
       DateTimeFormatter.ofPattern("MM/dd/yyyy");
-  private static final Logger LOG = LoggerFactory.getLogger(FlexibleDateScalarType.class);
 
   @Bean
   public GraphQLScalarType FlexibleDateScalar() {
@@ -27,8 +24,6 @@ public class FlexibleDateScalarType {
         .coercing(
             new Coercing() {
               private LocalDate convertImpl(Object input) {
-                LOG.warn("convertImpl");
-                LOG.warn("convertImpl {}", input);
                 if (input instanceof String) {
                   if (((String) input).contains("/")) {
                     return LocalDate.parse((String) input, US_SLASHDATE_FORMATTER);
@@ -43,33 +38,26 @@ public class FlexibleDateScalarType {
 
               @Override
               public Object serialize(Object dataFetcherResult) {
-                LOG.warn("serialize");
-                LOG.warn("serialize {}", dataFetcherResult);
-
                 return convertImpl(dataFetcherResult).format(US_DASHDATE_FORMATTER);
               }
 
               @Override
               public Object parseValue(Object input) {
-                LOG.warn("parseValue");
-                LOG.warn("parseValue {}", input);
-
                 LocalDate result = convertImpl(input);
                 if (result == null) {
                   throw new CoercingParseValueException(
-                      "Invalid value sdlkhfgksil '" + input + "' for LocalDate");
+                      "Invalid value '" + input + "' for LocalDate");
                 }
                 return result;
               }
 
               @Override
               public Object parseLiteral(Object input) {
-                LOG.warn("parseLiteral {}", input);
                 String value = ((StringValue) input).getValue();
                 LocalDate result = convertImpl(value);
                 if (result == null) {
                   throw new CoercingParseLiteralException(
-                      "Invalid value jksdhfjsadh'" + input + "' for LocalDate");
+                      "Invalid value '" + input + "' for LocalDate");
                 }
 
                 return result;
--- backend/src/main/java/gov/cdc/usds/simplereport/logging/AuditLoggingInstrumentation.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/logging/AuditLoggingInstrumentation.java
@@ -78,7 +78,6 @@ public class AuditLoggingInstrumentation extends SimpleInstrumentation {
     @SuppressWarnings("checkstyle:IllegalCatch")
     public void onCompleted(ExecutionResult result, Throwable t) {
       LOG.trace("End of execution, audit entry being saved.");
-      LOG.warn("error {}", result.getErrors());
       List<String> errorPaths =
           result.getErrors().stream()
               .map(GraphQLError::getPath)
