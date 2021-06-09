b7da18915b7dfbe5667ba28f49a1b5984d1d755f
Nick Clyde <nick@clyde.tech>
2021-06-09 14:30:21 -0700

    Add LoadingCard story

---
+++ frontend/src/app/accountCreation/LoadingCard/LoadingCard.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from "@storybook/react";
+
+import { LoadingCard } from "./LoadingCard";
+
+export default {
+  title: "App/Account set up/Loading card",
+  component: LoadingCard,
+  argTypes: {},
+  args: {
+    message: "Verifying code...",
+  },
+} as Meta;
+
+type Props = React.ComponentProps<typeof LoadingCard>;
+
+const Template: Story<Props> = (args) => <LoadingCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
