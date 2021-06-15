8c4efb4347bc19ac86224bff1d16b5a7bb79b25c
Nick Clyde <nick@clyde.tech>
2021-06-15 15:02:28 -0700

    Fix MfaEmailVerify story

--- frontend/src/app/accountCreation/MfaEmailVerify/MfaEmailVerify.stories.tsx
+++ frontend/src/app/accountCreation/MfaEmailVerify/MfaEmailVerify.stories.tsx
@@ -6,14 +6,9 @@ export default {
   title: "App/Account set up/Step 3b: Verify Email security code",
   component: MfaEmailVerify,
   argTypes: {},
-  args: {
-    location: { state: { contact: "foo@bar.com" } },
-  },
 } as Meta;
 
-type Props = React.ComponentProps<typeof MfaEmailVerify>;
-
-const Template: Story<Props> = (args) => <MfaEmailVerify {...args} />;
+const Template: Story = (args) => <MfaEmailVerify {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
