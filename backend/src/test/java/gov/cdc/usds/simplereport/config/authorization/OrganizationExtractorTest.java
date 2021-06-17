6d02b8aee1c68ffff8a6e8338980ce4c4d901162
Ben Warfield <bwarfield@cdc.gov>
2021-06-17 12:09:04 -0400

    Style cleanup: no more * imports.
    Updates to checkstyle configuration to ban * imports, and retroactively applies
    that rule to src/test/java. Also fixes a couple of other style/warning issues
    that cropped up in the same files.

--- backend/src/test/java/gov/cdc/usds/simplereport/config/authorization/OrganizationExtractorTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/config/authorization/OrganizationExtractorTest.java
@@ -1,6 +1,7 @@
 package gov.cdc.usds.simplereport.config.authorization;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import gov.cdc.usds.simplereport.config.AuthorizationProperties;
 import java.util.Arrays;
