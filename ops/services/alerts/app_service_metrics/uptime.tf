791da7778c7182347cac151c578a914a12d051b0
Josh Dorothy <josh@skylight.digital>
2021-06-21 12:57:21 -0400

    Address PR comments

--- ops/services/alerts/app_service_metrics/uptime.tf
+++ ops/services/alerts/app_service_metrics/uptime.tf
@@ -25,17 +25,17 @@ resource "azurerm_application_insights_web_test" "uptime" {
   resource_group_name     = var.rg_name
   application_insights_id = var.app_insights_id
   retry_enabled           = true
-  frequency               = 300
-  timeout                 = 60
+  frequency               = 60
+  timeout                 = 30
   geo_locations           = local.geo_locations
   kind                    = "ping"
   enabled                 = contains(var.disabled_alerts, "uptime") ? false : true
 
   // Borrowed from https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/application_insights_web_test#example-usage
   configuration = <<XML
-<WebTest Name="WebTest1" Id="" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="120" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+<WebTest Name="WebTest1" Id="" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="30" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
   <Items>
-    <Request Method="GET" Guid="" Version="1.1" Url="${each.value}" ThinkTime="0" Timeout="300" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+    <Request Method="GET" Guid="" Version="1.1" Url="${each.value}" ThinkTime="0" Timeout="30" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
   </Items>
 </WebTest>
 XML
@@ -51,13 +51,13 @@ resource "azurerm_monitor_metric_alert" "uptime" {
   for_each = local.url_map
 
   name                = "uptime-${each.key}"
-  description         = "${var.env} is not responding"
+  description         = "${each.key} is not responding"
   resource_group_name = var.rg_name
   scopes              = [azurerm_application_insights_web_test.uptime[each.key].id, var.app_insights_id]
   frequency           = "PT1M"
   window_size         = "PT5M"
   severity            = var.severity
-  enabled             = true
+  enabled             = contains(var.disabled_alerts, "uptime") ? false : true
 
   application_insights_web_test_location_availability_criteria {
     web_test_id           = azurerm_application_insights_web_test.uptime[each.key].id
