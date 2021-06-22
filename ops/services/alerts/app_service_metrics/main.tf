cffff88017067d9c4c3d9552ce7c058f3d675508
Josh Dorothy <josh@skylight.digital>
2021-06-22 00:06:54 -0400

    Add alerts: anomaly detection, 2xx failed requests, change 4xx/5xx from Average to Count, omit 401s on 4xx alerts

--- ops/services/alerts/app_service_metrics/main.tf
+++ ops/services/alerts/app_service_metrics/main.tf
@@ -125,6 +125,32 @@ resource "azurerm_monitor_metric_alert" "http_2xx_failed_requests" {
   }
 }
 
+resource "azurerm_monitor_metric_alert" "http_5xx_errors" {
+  name                = "${var.env}-api-5xx-errors"
+  description         = "${local.env_title} HTTP Server 5xx Errors >= 10"
+  resource_group_name = var.rg_name
+  scopes              = [var.app_service_id]
+  frequency           = "PT1M"
+  window_size         = "PT5M"
+  severity            = var.severity
+  enabled             = contains(var.disabled_alerts, "http_5xx_errors") ? false : true
+
+  criteria {
+    aggregation      = "Count"
+    metric_name      = "Http5xx"
+    metric_namespace = "Microsoft.Web/sites"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 10
+  }
+
+  dynamic "action" {
+    for_each = var.action_group_ids
+    content {
+      action_group_id = action.value
+    }
+  }
+}
+
 resource "azurerm_monitor_metric_alert" "http_4xx_errors" {
   name                = "${var.env}-api-4xx-errors"
   description         = "${local.env_title} HTTP Server 4xx Errors (excluding 401s) >= 10"
@@ -147,32 +173,6 @@ resource "azurerm_monitor_metric_alert" "http_4xx_errors" {
       operator = "Include"
       values   = ["400", "403", "404", "410", "429"]
     }
-  }
-
-  dynamic "action" {
-    for_each = var.action_group_ids
-    content {
-      action_group_id = action.value
-    }
-  }
-}
-
-resource "azurerm_monitor_metric_alert" "http_5xx_errors" {
-  name                = "${var.env}-api-5xx-errors"
-  description         = "${local.env_title} HTTP Server 5xx Errors >= 10"
-  resource_group_name = var.rg_name
-  scopes              = [var.app_service_id]
-  frequency           = "PT1M"
-  window_size         = "PT5M"
-  severity            = var.severity
-  enabled             = contains(var.disabled_alerts, "http_5xx_errors") ? false : true
-
-  criteria {
-    aggregation      = "Count"
-    metric_name      = "Http5xx"
-    metric_namespace = "Microsoft.Web/sites"
-    operator         = "GreaterThanOrEqual"
-    threshold        = 10
   }
 
   dynamic "action" {
