b901b0e35172f58e8655825130c874ddbc93e3b6
emmastephenson <emma.stephenson.24@gmail.com>
2021-06-23 15:45:22 -0700

    add jsonproperty annotation

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/SetPasswordRequest.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/SetPasswordRequest.java
@@ -2,6 +2,9 @@ package gov.cdc.usds.simplereport.api.model.useraccountcreation;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,5 +13,6 @@ import lombok.Getter;
 public class SetPasswordRequest {
   @Size(max = RequestConstants.LARGE_REQUEST_STRING_LIMIT)
   @NotNull
+  @JsonProperty
   private String password;
 }
