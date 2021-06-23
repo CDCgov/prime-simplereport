a01d64ff8147ef6cd7e7f10946ebeb9e8482d7e9
emmastephenson <emma.stephenson.24@gmail.com>
2021-06-23 15:50:38 -0700

    remove allArgs constructor

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/ActivateAccountRequest.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/ActivateAccountRequest.java
@@ -2,14 +2,10 @@ package gov.cdc.usds.simplereport.api.model.useraccountcreation;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ActivateAccountRequest {
   @Size(max = RequestConstants.LARGE_REQUEST_STRING_LIMIT)
   @NotNull
--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/SetPasswordRequest.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/useraccountcreation/SetPasswordRequest.java
@@ -4,12 +4,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class SetPasswordRequest {
   @Size(max = RequestConstants.LARGE_REQUEST_STRING_LIMIT)
   @NotNull
