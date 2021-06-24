dbb85e1da37ee95857e44f11f203458e50f6f102
Tim Best <tBest@cdc.gov>
2021-06-24 13:22:27 -0400

    Update test infra

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,6 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
 
 jest.mock("@microsoft/applicationinsights-react-js", () => {
   return {
