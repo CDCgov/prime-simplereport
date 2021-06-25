ab3ebf4c25b3cdaae6e0ec97a8b2e2bb0410eccd
Nick Clyde <nick@clyde.tech>
2021-06-25 14:36:22 -0700

    🤦

--- frontend/src/app/utils/api.ts
+++ frontend/src/app/utils/api.ts
@@ -59,12 +59,7 @@ class FetchClient {
       this.getOptions(method, body)
     );
     if (!res.ok) {
-      console.log("response not ok: ", res);
-      try {
-        throw await res.json();
-      } catch {
-        throw res;
-      }
+      throw await res.json();
     }
     const contentType = res.headers.get("content-type");
     if (contentType && contentType.indexOf(JSON_CONTENT) !== -1) {
