764266b0e11d53dd13b5c04d39d1c7e093767759
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-06-25 15:19:36 -0400

    Add TextMessageSent repo and use in SmsService

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/TextMessageSent.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/TextMessageSent.java
@@ -6,15 +6,25 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class TextMessageSent extends EternalAuditedEntity {
+public class TextMessageSent extends AuditedEntity {
   @ManyToOne
   @JoinColumn(name = "patient_link_internal_id")
   private PatientLink patientLink;
-  
+
   @Column private String twilioMessageId;
+
+  public TextMessageSent() {}
 
   public TextMessageSent(PatientLink link, String messageId) {
     patientLink = link;
     twilioMessageId = messageId;
+  }
+
+  public PatientLink getPatientLink() {
+    return patientLink;
+  }
+
+  public String getTwilioMessageId() {
+    return twilioMessageId;
   }
 }
--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/TextMessageSentRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/TextMessageSentRepository.java
@@ -2,4 +2,4 @@ package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.TextMessageSent;
 
-public interface TextMessageSentRepository extends EternalAuditedEntityRepository<TextMessageSent> {}
+public interface TextMessageSentRepository extends AuditedEntityRepository<TextMessageSent> {}
