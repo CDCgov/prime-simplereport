812d119d007f3724dcd618126d6dc7a005b40fda
Nathan Carter <nathan@skylight.digital>
2021-06-28 16:15:42 -0400

    Factor out date in `DataHubUploaderService` test so the expected output string will match the generated document     - More correct would be to DI a `Clock`, but this is a little less       lift. If no one objects it will remain.

--- backend/src/test/java/gov/cdc/usds/simplereport/service/DataHubUploaderServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/DataHubUploaderServiceTest.java
@@ -52,6 +52,7 @@ public class DataHubUploaderServiceTest extends BaseServiceTest<DataHubUploaderS
 
     // The data factory generates unique GUIDs for model instances shared by each test event,
     // so manually overwrite those values to match
+    var date = entry1.getDateResultReleased();
     var instrumentId1 = entry1.getInstrumentID();
     var patientId1 = entry1.getPatientId();
     var resultId1 = entry1.getResultID();
@@ -61,20 +62,36 @@ public class DataHubUploaderServiceTest extends BaseServiceTest<DataHubUploaderS
 
     var serialized =
         "\"Corrected_result_ID\",\"Date_result_released\",\"Device_ID\",\"Employed_in_healthcare\",\"First_test\",\"Illness_onset_date\",\"Instrument_ID\",\"Order_test_date\",\"Ordered_test_code\",\"Ordering_facility_city\",\"Ordering_facility_county\",\"Ordering_facility_email\",\"Ordering_facility_name\",\"Ordering_facility_phone_number\",\"Ordering_facility_state\",\"Ordering_facility_street\",\"Ordering_facility_street_2\",\"Ordering_facility_zip_code\",\"Ordering_provider_ID\",\"Ordering_provider_city\",\"Ordering_provider_county\",\"Ordering_provider_first_name\",\"Ordering_provider_last_name\",\"Ordering_provider_phone_number\",\"Ordering_provider_state\",\"Ordering_provider_street\",\"Ordering_provider_street_2\",\"Ordering_provider_zip_code\",\"Organization_name\",\"Patient_DOB\",\"Patient_ID\",\"Patient_city\",\"Patient_county\",\"Patient_email\",\"Patient_ethnicity\",\"Patient_first_name\",\"Patient_gender\",\"Patient_last_name\",\"Patient_middle_name\",\"Patient_phone_number\",\"Patient_race\",\"Patient_role\",\"Patient_state\",\"Patient_street\",\"Patient_street_2\",\"Patient_suffix\",\"Patient_zip_code\",\"Processing_mode_code\",\"Resident_congregate_setting\",\"Result_ID\",\"Specimen_collection_date_time\",\"Specimen_source_site_code\",\"Specimen_type_code\",\"Symptomatic_for_disease\",\"Test_date\",\"Test_result_code\",\"Test_result_status\",\"Testing_lab_CLIA\",\"Testing_lab_city\",\"Testing_lab_county\",\"Testing_lab_name\",\"Testing_lab_phone_number\",\"Testing_lab_state\",\"Testing_lab_street\",\"Testing_lab_street_2\",\"Testing_lab_zip_code\"\n"
-            + "\"\",\"20210625\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
+            + "\"\",\""
+            + date
+            + "\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
             + instrumentId1
-            + "\",\"20210625\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
+            + "\",\""
+            + date
+            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
             + patientId1
             + "\",\"\",\"\",\"\",\"U\",\"John\",\"U\",\"Boddie\",\"Brown\",\"503-867-5309\",\"UNK\",\"STAFF\",\"\",\"\",\"\",\"Jr.\",\"\",\"P\",\"UNK\",\""
             + resultId1
-            + "\",\"20210625\",\"986543321\",\"000111222\",\"UNK\",\"20210625\",\"260415000\",\"F\",\"123456\",\"Washington\",\"Washington\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\"\n"
-            + "\"\",\"20210625\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
+            + "\",\""
+            + date
+            + "\",\"986543321\",\"000111222\",\"UNK\",\""
+            + date
+            + "\",\"260415000\",\"F\",\"123456\",\"Washington\",\"Washington\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\"\n"
+            + "\"\",\""
+            + date
+            + "\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
             + instrumentId2
-            + "\",\"20210625\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
+            + "\",\""
+            + date
+            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
             + patientId2
             + "\",\"\",\"\",\"\",\"U\",\"John\",\"U\",\"Boddie\",\"Brown\",\"503-867-5309\",\"UNK\",\"STAFF\",\"\",\"\",\"\",\"Jr.\",\"\",\"P\",\"UNK\",\""
             + resultId2
-            + "\",\"20210625\",\"986543321\",\"000111222\",\"UNK\",\"20210625\",\"260415000\",\"F\",\"123456\",\"Washington\",\"Washington\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\"\n";
+            + "\",\""
+            + date
+            + "\",\"986543321\",\"000111222\",\"UNK\",\""
+            + date
+            + "\",\"260415000\",\"F\",\"123456\",\"Washington\",\"Washington\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\"\n";
 
     try {
       var fileContents = mapper.writer(schema).writeValueAsString(List.of(entry1, entry2));
