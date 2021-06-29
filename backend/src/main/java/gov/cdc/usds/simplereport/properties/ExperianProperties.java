f037cecc9bec6256992e08b4b0fa9ad73d4b7da7
emmastephenson <emma.stephenson.24@gmail.com>
2021-06-29 11:28:10 -0700

    add method to fetch token

--- backend/src/main/java/gov/cdc/usds/simplereport/properties/ExperianProperties.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/properties/ExperianProperties.java
@@ -7,27 +7,26 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 @ConfigurationProperties(prefix = "simple-report.experian")
 @Getter
 public class ExperianProperties {
-    private final String tokenEndpoint;
-    private final String domain;
-    private final String username;
-    private final String password;
-    private final String clientId;
-    private final String clientSecret;
+  private final String tokenEndpoint;
+  private final String domain;
+  private final String username;
+  private final String password;
+  private final String clientId;
+  private final String clientSecret;
 
-    @ConstructorBinding
-    public ExperianProperties(
-        String tokenEndpoint,
-        String domain,
-        String username, 
-        String password,
-        String clientId,
-        String clientSecret
-    ) {
-        this.tokenEndpoint = tokenEndpoint;
-        this.domain = domain;
-        this.username = username;
-        this.password = password;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-    }
+  @ConstructorBinding
+  public ExperianProperties(
+      String tokenEndpoint,
+      String domain,
+      String username,
+      String password,
+      String clientId,
+      String clientSecret) {
+    this.tokenEndpoint = tokenEndpoint;
+    this.domain = domain;
+    this.username = username;
+    this.password = password;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
 }
