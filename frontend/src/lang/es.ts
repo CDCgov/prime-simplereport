e99d820ae929139bf662aa2434b54ed35728bc10
Nathan Carter <nathan@skylight.digital>
2021-07-02 18:27:21 -0400

    Add patient test result copy to English translation configuration files     - Large text sets are represented as hierarchical "document trees"       for easy human reading
    Add empty data structures of the same type to the Spanish translation
    configuration file

--- frontend/src/lang/es.ts
+++ frontend/src/lang/es.ts
@@ -3,5 +3,106 @@ import { LanguageConfig } from "./en";
 export const es: LanguageConfig = {
   translation: {
     header: "Portal de pruebas COVID-19",
+    testResult: {
+      tos: {
+        header: "",
+        introText: "",
+        consent: "",
+        submit: "",
+        document: {
+          intro: {
+            p0: "",
+          },
+          scope: {
+            heading: "",
+            p0: "",
+          },
+          dataRights: {
+            heading: "",
+            subheading: "",
+            l0: "",
+            p0: "",
+            l1: "",
+            p1: "",
+          },
+          privacy: {
+            heading: "",
+            p0: "",
+            p1: "",
+          },
+          useOfData: {
+            heading: "",
+            p0: "",
+          },
+          sharingOfData: {
+            heading: "",
+            p0: "",
+          },
+          otherResponsibilities: {
+            heading: "",
+            ul: {
+              li0: "",
+              li1: "",
+              li2: "",
+              li3: "",
+              li4: "",
+              li5: "",
+              li6: "",
+              li7: "",
+              li8: "",
+            },
+          },
+          serviceManagement: {
+            heading: "",
+            subheading: "",
+            p0: "",
+          },
+          serviceTermination: {
+            heading: "",
+            p0: "",
+            p1: "",
+          },
+          intellectualProperty: {
+            heading: "",
+            p0: "",
+          },
+          disclaimerOfWarranties: {
+            heading: "",
+            p0: "",
+          },
+          limitationOfLiability: {
+            heading: "",
+            p0: "",
+            p1: "",
+          },
+          disputes: {
+            heading: "",
+            p0: "",
+          },
+          indemnification: {
+            heading: "",
+            p0: "",
+          },
+          noWaiverOfRights: {
+            heading: "",
+            p0: "",
+          },
+          dataAnalytics: {
+            heading: "",
+            p0: "",
+          },
+        },
+      },
+      dob: {
+        dateOfBirth: "",
+        enterDOB: "",
+        enterDOB2: "",
+        error: "",
+        validating: "",
+        linkExpired: "",
+        submit: "",
+      },
+      notes: {},
+    },
   },
 };
