187844eb01d213fbc7f1ebbc98ba2d25f92278ff
Nathan Carter <nathan@skylight.digital>
2021-07-02 18:31:22 -0400

    Add translation support for terms of service parent component and document

--- frontend/src/patientApp/timeOfTest/TermsOfService.tsx
+++ frontend/src/patientApp/timeOfTest/TermsOfService.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Redirect, RouteComponentProps, withRouter } from "react-router";
 import classnames from "classnames";
+import { Trans, useTranslation } from "react-i18next";
 
 import Button from "../../app/commonComponents/Button/Button";
 
@@ -18,6 +19,8 @@ const TermsOfService: React.FunctionComponent<Props> = ({
 }) => {
   const [nextPage, setNextPage] = useState(false);
   const plid = useSelector((state: any) => state.plid);
+
+  const { t } = useTranslation();
 
   if (nextPage) {
     return (
@@ -38,20 +41,23 @@ const TermsOfService: React.FunctionComponent<Props> = ({
       )}
     >
       <form className="grid-container maxw-tablet usa-prose">
-        <h1 className="font-heading-lg margin-top-3">Terms of service</h1>
-        <p className="margin-top-105">
-          This testing site uses{" "}
-          <a href="https://simplereport.gov/">SimpleReport</a> to manage
-          COVID-19 testing and reporting. The terms below explain SimpleReport’s
-          policies and terms of service.
-        </p>
+        <h1 className="font-heading-lg margin-top-3">
+          {t("testResult.tos.header")}
+        </h1>
+        <Trans
+          t={t}
+          parent="p"
+          className="margin-top-105"
+          i18nKey="testResult.tos.introText"
+          components={[<a href="https://simplereport.gov/">SimpleReport</a>]}
+        />
         <div className="tos-content prime-formgroup usa-prose height-card-lg overflow-x-hidden font-body-3xs">
           <ToS />
         </div>
-        <p>By agreeing, you consent to our terms of service.</p>
+        <p>{t("testResult.tos.consent")}</p>
         <Button
           id="tos-consent-button"
-          label="I agree"
+          label={t("testResult.tos.submit")}
           onClick={() => {
             if (onAgree) {
               onAgree();
