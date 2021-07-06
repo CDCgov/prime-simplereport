424732a73964c23b3fd668221f529909060b4b4f
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-07-06 14:25:31 -0400

    Remove redundant registration link creation

--- backend/src/main/java/gov/cdc/usds/simplereport/service/OrganizationService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/OrganizationService.java
@@ -351,7 +351,6 @@ public class OrganizationService {
             providerAddress,
             providerTelephone,
             providerNPI);
-    _psrlService.createRegistrationLink(fac);
     return fac;
   }
 }
