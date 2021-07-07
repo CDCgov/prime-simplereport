ae58ca5ae474469d1d2945668c81df6270876140
emmastephenson <emma.stephenson.24@gmail.com>
2021-07-07 15:28:08 -0700

    add default exception handler to RestExceptionHandler

--- backend/src/main/java/gov/cdc/usds/simplereport/api/RestExceptionHandler.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/RestExceptionHandler.java
@@ -26,4 +26,9 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   public ResponseEntity<String> handleException(BadRequestException e) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
   }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+  }
 }
