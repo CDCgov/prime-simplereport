47b11aa22899d3095f057de406ee501654105c7d
Nathan Carter <nathan.andrew.carter@gmail.com>
2021-07-12 12:23:48 -0400

    Prepare self-registration for translation (#1991)
    * Add translation configuration to person form labels and person form sub-components
    
    * Enable translation for dropdown/radio options on PersonForm
    
    Translate-ify address confirmation modal component
    
    Enable translation for person form field errors
    
    HTML renders correctly in interpolated string on Confirmation component
    
    Translate default option in Dropdown component
    
    Catch some English text in the person form I missed earlier
    
    * Remove stray comment
    
    * Pass translation string for default value into dropdown component instead of translating inside the component
    
    * Revert translation work in facility select component (not used in patient self-registration)
    
    * Add rule to Gradle build file to skip SonarCloud checks on translation config files
    
    * RequiredMessage component does not translate internally - add prop to pass in message
    
    * Tweak Gradle build file syntax to ignore translation config in SonarCloud scans
    
    * Revert change in Gradle build file to ignore all SonarCloud checks on translation config files
    
    Co-authored-by: Nathan Carter <nathan@skylight.digital>

--- frontend/src/patientApp/timeOfTest/PatientFormContainer.test.tsx
+++ frontend/src/patientApp/timeOfTest/PatientFormContainer.test.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 
 import PatientFormContainer from "./PatientFormContainer";
+import "../../i18n";
 
 jest.mock("../..//app/commonComponents/ComboBox", () => () => <></>);
 const mockStore = configureStore([]);
