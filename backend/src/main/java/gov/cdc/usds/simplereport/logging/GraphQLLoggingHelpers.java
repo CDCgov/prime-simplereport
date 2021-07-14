8e6a169f7df48cb0d12932ecc4adb5bbd8f9e114
Nathan Carter <nathan@skylight.digital>
2021-07-14 13:43:06 -0400

    Add some debugging log lines

--- backend/src/main/java/gov/cdc/usds/simplereport/logging/GraphQLLoggingHelpers.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/logging/GraphQLLoggingHelpers.java
@@ -90,6 +90,9 @@ public class GraphQLLoggingHelpers {
           }
           // Clear the MDC context
           MDC.remove(LoggingConstants.REQUEST_ID_MDC_KEY);
+          System.out.println("----------------------------");
+          System.out.println(request.getProperties().get("App Version"));
+          System.out.println("----------------------------");
           client.trackRequest(request);
         });
   }
