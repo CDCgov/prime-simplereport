101696b9459969e7f8aed9ca06f08b87ded87c72
Nathan Carter <nathan@skylight.digital>
2021-07-19 18:09:57 -0400

    Add logging to AzureTelemetryInitializer

--- backend/src/main/java/gov/cdc/usds/simplereport/config/AzureTelemetryInitializer.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/AzureTelemetryInitializer.java
@@ -6,21 +6,29 @@ import gov.cdc.usds.simplereport.api.CurrentUIVersionContextHolder;
 
 import com.microsoft.applicationinsights.telemetry.Telemetry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AzureTelemetryInitializer implements TelemetryInitializer {
     private final CurrentUIVersionContextHolder _currentUIVersionContextHolder;
+    private static final Logger LOG = LoggerFactory.getLogger(AzureTelemetryInitializer.class);
 
     AzureTelemetryInitializer(CurrentUIVersionContextHolder currentUIVersionContextHolder) {
       _currentUIVersionContextHolder = currentUIVersionContextHolder;
     }
 
     public void initialize(Telemetry telemetry) {
+        LOG.info("Initializing Azure telemetry client...");
         RequestTelemetry requestTelemetry = (RequestTelemetry) telemetry;
 
         if (requestTelemetry == null) {
+            LOG.info("Request telemetry instance not found. Exiting telemetry client initialization.");
             return;
         }
 
-        requestTelemetry.getProperties().put("UI Version", _currentUIVersionContextHolder.getUiShaFromHeaders());
-        requestTelemetry.getProperties().put("HELLO", "WORLD");
+        requestTelemetry.getContext().getProperties().put("UI Version", _currentUIVersionContextHolder.getUiShaFromHeaders());
+        requestTelemetry.getContext().getProperties().put("HELLO", "WORLD");
+
+        LOG.info("Azure telemetry client intialized.");
     }
 }
