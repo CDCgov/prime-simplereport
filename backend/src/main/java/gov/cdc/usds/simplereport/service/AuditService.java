e5c1bffc89ef589e31cc01bf29a22679881d3852
emmastephenson <emma.stephenson.24@gmail.com>
2021-07-19 15:55:51 -0700

    spotless

--- backend/src/main/java/gov/cdc/usds/simplereport/service/AuditService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/AuditService.java
@@ -85,7 +85,10 @@ public class AuditService {
     LOG.trace("Saving audit event for {}", requestId);
     HttpRequestDetails reqDetails = new HttpRequestDetails(request);
     Object userIdObj = request.getSession(true).getAttribute("userId");
-    JsonNode userId = (userIdObj == null) ? null : JsonNodeFactory.instance.objectNode().put("userId", userIdObj.toString());
+    JsonNode userId =
+        (userIdObj == null)
+            ? null
+            : JsonNodeFactory.instance.objectNode().put("userId", userIdObj.toString());
     ApiUser anonymousUser = _userService.getAnonymousApiUser();
     _repo.save(new ApiAuditEvent(requestId, reqDetails, responseCode, userId, anonymousUser));
   }
