2064edb1444036605a1394b48cd1ca085d7e068e
Nathan Carter <nathan@skylight.digital>
2021-07-22 16:29:35 -0400

    More experimentation...

--- backend/src/main/java/gov/cdc/usds/simplereport/config/AzureTelemetryConfiguration.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/AzureTelemetryConfiguration.java
@@ -1,6 +1,8 @@
 package gov.cdc.usds.simplereport.config;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+
 import gov.cdc.usds.simplereport.api.CurrentUIVersionContextHolder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +20,15 @@ public class AzureTelemetryConfiguration {
   @Bean
   @Scope("singleton")
   TelemetryClient getTelemetryClient() {
-    // TelemetryConfiguration config = new TelemetryConfiguration();
-    // config.getTelemetryInitializers().add(new
-    // AzureTelemetryInitializer(_currentUIVersionContextHolder));
-    // TelemetryClient client = new TelemetryClient(config);
-    // client.getContext().setInstrumentationKey(System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY"));
-    TelemetryClient client = new TelemetryClient();
+     TelemetryConfiguration config = new TelemetryConfiguration();
+     config.setConnectionString(System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"));
+    // config.getTelemetryInitializers().add(new AzureTelemetryInitializer(_currentUIVersionContextHolder));
+    TelemetryClient client = new TelemetryClient(config);
+    //TelemetryClient client = new TelemetryClient();
+    System.out.println("============================");
+    System.out.println(System.getenv("APPINSIGHTS_INSTRUMENTATIONKEY"));
+    System.out.println(System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"));
+    System.out.println("============================");
     client.getContext().getProperties().put("HELLO", "WORLD");
     return client;
   }
