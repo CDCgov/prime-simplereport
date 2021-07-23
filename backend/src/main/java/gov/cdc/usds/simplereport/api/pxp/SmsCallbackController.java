8346bbc3cc066587804cd0c0980776aeead3b2c9
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-07-23 12:22:57 -0400

    Accept urlencoded form data and create validation exception

--- backend/src/main/java/gov/cdc/usds/simplereport/api/pxp/SmsCallbackController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/pxp/SmsCallbackController.java
@@ -4,10 +4,10 @@ import gov.cdc.usds.simplereport.db.model.auxiliary.SmsStatusCallback;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pxp/callback")
 @PreAuthorize("@smsValidationService.validateSmsCallback(#request, #body)")
 @Validated
-class SmsCallbackController {
+public class SmsCallbackController {
   private static final Logger LOG =
       LoggerFactory.getLogger(PatientSelfRegistrationController.class);
 
-  @PostMapping("")
-  public void register(@RequestBody SmsStatusCallback body, HttpServletRequest request) {
+  @PostMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public void register(SmsStatusCallback body, HttpServletRequest request) {
     LOG.info("messageSid={} status={}", body.getMessageSid(), body.getMessageStatus());
   }
 }
--- backend/src/main/java/gov/cdc/usds/simplereport/service/sms/SmsValidationService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/sms/SmsValidationService.java
@@ -1,11 +1,14 @@
 package gov.cdc.usds.simplereport.service.sms;
 
 import com.twilio.security.RequestValidator;
+import gov.cdc.usds.simplereport.api.model.errors.InvalidTwilioCallbackException;
 import gov.cdc.usds.simplereport.db.model.auxiliary.SmsStatusCallback;
 import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service("smsValidationService")
 public class SmsValidationService {
 
   private final RequestValidator validator;
@@ -13,17 +16,21 @@ public class SmsValidationService {
   @Value("${simple-report.twilio-callback-url:https://simplereport.gov/api/pxp/callback}")
   private static String twilioCallbackUrl;
 
-  public SmsValidationService(@Value("${TWILIO_AUTH_TOKEN}") String authToken) {
+  public SmsValidationService(@Value("${TWILIO_AUTH_TOKEN:MISSING}") String authToken) {
     this.validator = new RequestValidator(authToken);
   }
 
-  boolean validateSmsCallback(HttpServletRequest request, SmsStatusCallback body) {
+  public boolean validateSmsCallback(HttpServletRequest request, SmsStatusCallback body)
+      throws InvalidTwilioCallbackException {
     String twilioSignature = request.getHeader("X-Twilio-Signature");
     HashMap<String, String> params = new HashMap<>();
     params.put("SmsSid", body.getSmsSid());
     params.put("SmsStatus", body.getSmsStatus());
     params.put("MessageStatus", body.getMessageStatus());
     params.put("MessageSid", body.getMessageSid());
-    return validator.validate(twilioCallbackUrl, params, twilioSignature);
+    if (validator.validate(twilioCallbackUrl, params, twilioSignature)) {
+      return true;
+    }
+    throw new InvalidTwilioCallbackException();
   }
 }
