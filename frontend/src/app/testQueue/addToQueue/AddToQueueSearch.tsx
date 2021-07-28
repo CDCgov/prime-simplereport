1be53aa5253e6b9603ea6e67847c112369b8de25
Adam Weber <nullflux@users.noreply.github.com>
2021-07-28 07:38:47 -0700

    Update AddToQueueSearch to show 100 results (#2105)

--- frontend/src/app/testQueue/addToQueue/AddToQueueSearch.tsx
+++ frontend/src/app/testQueue/addToQueue/AddToQueueSearch.tsx
@@ -37,7 +37,7 @@ export const QUERY_PATIENT = gql`
     patients(
       facilityId: $facilityId
       pageNumber: 0
-      pageSize: 20
+      pageSize: 100
       showDeleted: false
       namePrefixMatch: $namePrefixMatch
     ) {
