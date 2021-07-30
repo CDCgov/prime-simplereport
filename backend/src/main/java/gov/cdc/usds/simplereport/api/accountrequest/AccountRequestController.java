dd0c588e6860355be782c7bda04a9ea299a576bc
emmastephenson <emma.stephenson.24@gmail.com>
2021-07-30 14:30:19 -0700

    add exception handler

--- backend/src/main/java/gov/cdc/usds/simplereport/api/accountrequest/AccountRequestController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/accountrequest/AccountRequestController.java
@@ -45,6 +45,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /** Note that this controller is automatically authorized. */
 @PreAuthorize("@" + AUTHORIZER_BEAN + ".permitAllAccountRequests()")
@@ -78,6 +81,11 @@ public class AccountRequestController {
     this._es = es;
     this._crm = crm;
     this.objectMapper = new ObjectMapper();
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<String> handleException(BadRequestException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
   }
 
   @PostConstruct
