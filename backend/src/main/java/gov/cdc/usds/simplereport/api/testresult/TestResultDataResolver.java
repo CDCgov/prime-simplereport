e141edd0bfc7c54be228f0321749d259aaa5b18b
Adam Weber <a.diamond.weber@gmail.com>
2021-08-02 17:57:01 -0700

    Revert TestResultData stuff

--- backend/src/main/java/gov/cdc/usds/simplereport/api/testresult/TestResultDataResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/testresult/TestResultDataResolver.java
@@ -3,33 +3,24 @@ package gov.cdc.usds.simplereport.api.testresult;
 import gov.cdc.usds.simplereport.api.InternalIdResolver;
 import gov.cdc.usds.simplereport.api.model.ApiFacility;
 import gov.cdc.usds.simplereport.api.model.TestDescription;
-import gov.cdc.usds.simplereport.db.model.PatientAnswers;
 import gov.cdc.usds.simplereport.db.model.PatientLink;
 import gov.cdc.usds.simplereport.db.model.Person;
 import gov.cdc.usds.simplereport.db.model.TestEvent;
 import gov.cdc.usds.simplereport.db.model.auxiliary.AskOnEntrySurvey;
 import gov.cdc.usds.simplereport.db.model.auxiliary.TestResult;
-import gov.cdc.usds.simplereport.service.dataloader.PatientAnswersDataLoader;
-import gov.cdc.usds.simplereport.service.dataloader.PatientLinkDataLoader;
 import graphql.kickstart.tools.GraphQLResolver;
-import graphql.schema.DataFetchingEnvironment;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
-
-import static gov.cdc.usds.simplereport.service.dataloader.DataLoaderRegistryBuilder.loadFuture;
 
 @Component
 public class TestResultDataResolver
     implements GraphQLResolver<TestEvent>, InternalIdResolver<TestEvent> {
 
-  private CompletableFuture<AskOnEntrySurvey> getSurvey(TestEvent testEvent, DataFetchingEnvironment dfe) {
-      CompletableFuture<PatientAnswers> answers = loadFuture(testEvent.getTestOrder(), dfe, PatientAnswersDataLoader.KEY);
-      return answers.thenApply(PatientAnswers::getSurvey);
+  private AskOnEntrySurvey getSurvey(TestEvent testEvent) {
+    return testEvent.getSurveyData();
   }
 
   public Person getPatient(TestEvent testEvent) {
@@ -40,36 +31,41 @@ public class TestResultDataResolver
     return testEvent.getDateTested();
   }
 
-  public CompletableFuture<String> getPregnancy(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getPregnancy);
+  public String getPregnancy(TestEvent testEvent) {
+    return getSurvey(testEvent).getPregnancy();
   }
 
-  public CompletableFuture<Boolean> getNoSymptoms(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getNoSymptoms);
+  public Boolean getNoSymptoms(TestEvent testEvent) {
+    return getSurvey(testEvent).getNoSymptoms();
   }
 
-  public CompletableFuture<String> getSymptoms(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getSymptomsJSON);
+  public String getSymptoms(TestEvent testEvent) {
+    Map<String, Boolean> s = getSurvey(testEvent).getSymptoms();
+    JSONObject obj = new JSONObject();
+    for (Map.Entry<String, Boolean> entry : s.entrySet()) {
+      obj.put(entry.getKey(), entry.getValue().toString());
+    }
+    return obj.toString();
   }
 
-  public CompletableFuture<LocalDate> getSymptomOnset(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getSymptomOnsetDate);
+  public LocalDate getSymptomOnset(TestEvent testEvent) {
+    return getSurvey(testEvent).getSymptomOnsetDate();
   }
 
-  public CompletableFuture<Boolean> getFirstTest(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getFirstTest);
+  public Boolean getFirstTest(TestEvent testEvent) {
+    return getSurvey(testEvent).getFirstTest();
   }
 
-  public CompletableFuture<LocalDate> getPriorTestDate(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getPriorTestDate);
+  public LocalDate getPriorTestDate(TestEvent testEvent) {
+    return getSurvey(testEvent).getPriorTestDate();
   }
 
-  public CompletableFuture<String> getPriorTestType(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getPriorTestType);
+  public String getPriorTestType(TestEvent testEvent) {
+    return getSurvey(testEvent).getPriorTestType();
   }
 
-  public CompletableFuture<TestResult> getPriorTestResult(TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return getSurvey(testEvent, dfe).thenApply(AskOnEntrySurvey::getPriorTestResult);
+  public TestResult getPriorTestResult(TestEvent testEvent) {
+    return getSurvey(testEvent).getPriorTestResult();
   }
 
   public TestDescription getTestPerformed(TestEvent event) {
@@ -80,8 +76,7 @@ public class TestResultDataResolver
     return new ApiFacility(testEvent.getFacility());
   }
 
-  public CompletableFuture<PatientLink> getPatientLink(
-      TestEvent testEvent, DataFetchingEnvironment dfe) {
-    return loadFuture(testEvent, dfe, PatientLinkDataLoader.KEY);
+  public PatientLink getPatientLink(TestEvent testEvent) {
+    return testEvent.getTestOrder().getPatientLink();
   }
 }
