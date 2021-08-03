88d03f24b075386b08118073da33437966179f06
Nick Clyde <nick@clyde.tech>
2021-08-03 11:41:45 -0700

    More debug logging

--- frontend/e2e.sh
+++ frontend/e2e.sh
@@ -40,4 +40,4 @@ fi
 
 echo 'App is online! Starting Nightwatch...'
 
-npx nightwatch -e "$@"
+npx nightwatch -e "$@" --verbose
--- frontend/e2e/utils/start-wiremock.sh
+++ frontend/e2e/utils/start-wiremock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar ./wiremock-jre8-standalone-2.29.1.jar --port 8088 --root-dir ./e2e
+java -jar ./wiremock-jre8-standalone-2.29.1.jar --port 8088 --root-dir ./e2e --verbose
