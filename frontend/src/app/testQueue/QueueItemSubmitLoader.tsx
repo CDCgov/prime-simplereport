d94e29b6e1939afeded2a9d423a9856a272958c5
Nick Scialli <49658917+nickscialli-usds@users.noreply.github.com>
2021-08-06 09:23:00 -0400

    Add saving and error states for queue item cards (#2175)
    * Adds loading effect during submit and associated storybook stories
    
    * Add to tests
    
    * Clean up async logic
    
    * Fix styling and add transition
    
    * Accommodate lifecycle of animation
    
    * Update snapshots

---
+++ frontend/src/app/testQueue/QueueItemSubmitLoader.tsx
@@ -0,0 +1,37 @@
+import classNames from "classnames";
+import { CSSTransition } from "react-transition-group";
+import iconLoader from "uswds/dist/img/loader.svg";
+
+import "./QueueItemSubmitLoader.scss";
+
+type Props = {
+  name: string;
+  show: boolean;
+};
+
+export const QueueItemSubmitLoader = ({ name, show }: Props) => {
+  const classnames = classNames(
+    "sr-queue-item-submit-loader",
+    "z-top",
+    "position-absolute",
+    "height-full",
+    "width-full",
+    "text-center",
+    "radius-lg"
+  );
+
+  return (
+    <CSSTransition
+      in={show}
+      timeout={300}
+      classNames="sr-queue-item-submit-loader"
+    >
+      <div className={classnames} aria-hidden={!show}>
+        <h4 className="margin-top-6 margin-bottom-5">
+          Submitting test data for {name}...
+        </h4>
+        <img src={iconLoader} alt="submitting" className="square-5" />
+      </div>
+    </CSSTransition>
+  );
+};
