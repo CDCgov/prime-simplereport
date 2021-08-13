629f4e8ec20a0f1218311f019a5a7d2e937b3458
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-08-13 14:19:26 -0400

    Clean up AoEForm tests

--- frontend/src/app/utils/date.ts
+++ frontend/src/app/utils/date.ts
@@ -6,12 +6,15 @@ export const daysSince = (date: moment.Moment): String => {
   return `${days} day${days === 1 ? "" : "s"}`;
 };
 
-export const formatDate = (date: string | undefined | null): ISODate | null => {
+export const formatDate = (
+  date: string | undefined | null,
+  inputFormat?: string
+): ISODate | null => {
   if (!date) {
     return null;
   }
 
-  return moment(date).format("YYYY-MM-DD") as ISODate;
+  return moment(date, inputFormat).format("YYYY-MM-DD") as ISODate;
 };
 
 export const isValidDate = (date: string): boolean => {
