c9e52b3bf9163bf87d874da8af8a749398d4c3ea
Nick Scialli <nicholas.a.scialli@omb.eop.gov>
2021-08-16 14:43:30 -0400

    Revert most non-test changes

--- frontend/src/app/patients/personSchema.ts
+++ frontend/src/app/patients/personSchema.ts
@@ -15,7 +15,6 @@ import {
 } from "../constants";
 import { Option } from "../commonComponents/Dropdown";
 import { languages } from "../../config/constants";
-import { getDateFormat } from "../utils/date";
 
 const phoneUtil = PhoneNumberUtil.getInstance();
 
@@ -105,15 +104,7 @@ export function isValidBirthdate(date: string | undefined) {
   if (date.split("/").length === 3 && date.split("/")[2].length < 4) {
     return false;
   }
-
-  const recognizedDateFormat = getDateFormat(date);
-
-  if (!recognizedDateFormat) {
-    return false;
-  }
-
-  const parsedDate = moment(date, recognizedDateFormat);
-
+  const parsedDate = moment(date);
   if (!parsedDate.isValid()) {
     return false;
   }
--- frontend/src/app/utils/date.ts
+++ frontend/src/app/utils/date.ts
@@ -6,35 +6,12 @@ export const daysSince = (date: moment.Moment): String => {
   return `${days} day${days === 1 ? "" : "s"}`;
 };
 
-const formats = [
-  { regexp: /^\d{4}-\d{2}-\d{2}$/, format: "YYYY-MM-DD" },
-  { regexp: /^\d{2}\/\d{2}\/\d{4}$/, format: "MM/DD/YYYY" },
-  { regexp: /^\d{2}\/\d{2}\/\d{2}$/, format: "MM/DD/YY" },
-  { regexp: /^\d{1,2}\/\d{1,2}\/\d{2}$/, format: "M/D/YY" },
-  { regexp: /^\d{1,2}\/\d{1,2}\/\d{4}$/, format: "M/D/YYYY" },
-  { regexp: /^\d{2}-\d{2}-\d{4}$/, format: "MM-DD-YYYY" },
-  { regexp: /^\d{1,2}-\d{1,2}-\d{4}$/, format: "M-D-YYYY" },
-];
-
-type DateInputType = string | undefined | null | Date;
-
-export function getDateFormat(date: DateInputType) {
-  if (!date || date instanceof Date) {
-    return undefined;
-  }
-
-  return formats.find(({ regexp }) => regexp.test(date))?.format;
-}
-
-export const formatDate = (date: DateInputType): ISODate | null => {
+export const formatDate = (date: string | undefined | null): ISODate | null => {
   if (!date) {
     return null;
   }
 
-  // Attempt to get date format
-  const format = getDateFormat(date);
-
-  return moment(date, format).format("YYYY-MM-DD") as ISODate;
+  return moment(date).format("YYYY-MM-DD") as ISODate;
 };
 
 export const isValidDate = (date: string): boolean => {
