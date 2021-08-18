14b93d68e83dc1aa6378d36eb36bbe4a364bb4d1
emmastephenson <emma.stephenson.24@gmail.com>
2021-08-18 12:40:44 -0700

    remove dynamics from AccountRequestController

--- backend/src/test/java/gov/cdc/usds/simplereport/api/AccountRequestControllerTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/AccountRequestControllerTest.java
@@ -43,7 +43,6 @@ import gov.cdc.usds.simplereport.service.AuthorizationService;
 import gov.cdc.usds.simplereport.service.DeviceTypeService;
 import gov.cdc.usds.simplereport.service.OrganizationService;
 import gov.cdc.usds.simplereport.service.TenantDataAccessService;
-import gov.cdc.usds.simplereport.service.crm.CrmService;
 import gov.cdc.usds.simplereport.service.email.EmailProvider;
 import gov.cdc.usds.simplereport.service.email.EmailProviderTemplate;
 import gov.cdc.usds.simplereport.service.email.EmailService;
@@ -107,7 +106,6 @@ class AccountRequestControllerTest {
   @MockBean private CurrentTenantDataAccessContextHolder tenantDataAccessContextHolder;
   @MockBean private TenantDataAuthenticationProvider tenantDataAuthProvider;
 
-  @MockBean private CrmService crmService;
   @MockBean private OktaRepository oktaRepository;
 
   @MockBean private EmailProvider mockSendGrid;
@@ -374,10 +372,8 @@ class AccountRequestControllerTest {
     assertThat(addressCaptor.getValue().getPostalCode()).isEqualTo("43675");
     assertThat(addressCaptor.getValue().getCounty()).isEqualTo("Asperiores illum in");
 
-    // make sure we passed the data along to our CRM
-    verify(crmService, times(1)).submitAccountRequestData(accountRequestCaptor.capture());
-    assertThat(accountRequestCaptor.getValue().getOrganizationName())
-        .isEqualTo("Day Hayes Trading");
+    // assertThat(accountRequestCaptor.getValue().getOrganizationName())
+    //     .isEqualTo("Day Hayes Trading");
 
     // new user should be disabled in okta
     verify(oktaRepository)
@@ -419,10 +415,8 @@ class AccountRequestControllerTest {
     verify(emailService, times(0)).sendWithProviderTemplate(anyString(), any());
     verify(mockSendGrid, times(0)).send(any());
 
-    verify(crmService, times(1))
-        .submitOrganizationAccountRequestData(organizationAccountRequestCaptor.capture());
-    assertThat(organizationAccountRequestCaptor.getValue().getOrganizationName())
-        .isEqualTo("Day Hayes Trading");
+    // assertThat(organizationAccountRequestCaptor.getValue().getOrganizationName())
+    //     .isEqualTo("Day Hayes Trading");
 
     verify(apiUserService, times(1))
         .createUser(
@@ -482,10 +476,8 @@ class AccountRequestControllerTest {
     verify(emailService, times(1)).sendWithProviderTemplate(anyString(), any());
     verify(mockSendGrid, times(2)).send(any());
 
-    verify(crmService, times(1))
-        .submitOrganizationAccountRequestData(organizationAccountRequestCaptor.capture());
-    assertThat(organizationAccountRequestCaptor.getValue().getOrganizationName())
-        .isEqualTo("Day Hayes Trading");
+    // assertThat(organizationAccountRequestCaptor.getValue().getOrganizationName())
+    //     .isEqualTo("Day Hayes Trading");
 
     verify(apiUserService, times(1))
         .createUser(
