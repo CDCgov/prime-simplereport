8b0c308f49807bf61adf1eadafa853bdc49635e6
Adam Weber <nullflux@users.noreply.github.com>
2021-08-20 10:52:54 -0700

    Improve GetFacilityQueue performance (#2358)
    - Adds an ApiTestOrderResolver and removes many fields from ApiTestOrder
    - Introduces new DataLoaders, each corresponding to one of the joins on 
      TestOrder or Patient and contributing to the previous 5N+1 query scenario:
      - PatientAnswersDataLoader
      - PatientPrimaryPhoneDataLoader
      - TestOrderDeviceTypeDataLoader
      - TestOrderPatientDataLoader
    - Adds trace logging to QueueResolver
    - Adds the FetchType.LAZY hint in many places even though it's allegedly the 
      default (this does actually change Hibernate's querying behavior in some 
      instances!)
    - Moves the static loadFuture to KeyedDataLoaderFactory so that it can be 
      reused across DataResolvers in a more DRY way
    - Tangentially related to this issue: DRYs out the AskOnEntrySurvey.getSymptoms
      JSON logic, because this PR added a third duplication of the explicit for 
      loop mapping

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTypeRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTypeRepository.java
@@ -1,6 +1,10 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.DeviceType;
+import java.util.List;
+import java.util.UUID;
 
 /** Specification of EternalAuditedEntityRepository for {@link DeviceType} manipulation. */
-public interface DeviceTypeRepository extends EternalAuditedEntityRepository<DeviceType> {}
+public interface DeviceTypeRepository extends EternalAuditedEntityRepository<DeviceType> {
+  List<DeviceType> findAllByTestOrdersInternalIdIn(List<UUID> testOrders);
+}
