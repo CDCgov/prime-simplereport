ea8ddc7199db073fcbd00651b212c471d17ce03b
Adam Weber <a.diamond.weber@gmail.com>
2021-08-23 20:34:25 -0700

    Update serialization test

--- backend/src/test/java/gov/cdc/usds/simplereport/service/DataHubUploaderServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/DataHubUploaderServiceTest.java
@@ -35,8 +35,8 @@ class DataHubUploaderServiceTest extends BaseServiceTest<DataHubUploaderService>
     TestResult r = TestResult.NEGATIVE;
     Date d = new Date();
 
-    Person p1 = _dataFactory.createMinimalPerson(o);
-    Person p2 = _dataFactory.createMinimalPerson(o);
+    Person p1 = _dataFactory.createFullPersonWithTelephone(o, "5168675309");
+    Person p2 = _dataFactory.createFullPersonWithTelephone(o, "5168675309");
 
     TestEvent te1 = _dataFactory.createTestEvent(p1, f, s, r, d);
     TestEvent te2 = _dataFactory.createTestEvent(p2, f, s, r, d);
@@ -65,16 +65,16 @@ class DataHubUploaderServiceTest extends BaseServiceTest<DataHubUploaderService>
     var siteOfCare2 = entry2.getSiteOfCare();
 
     var serialized =
-        "\"Corrected_result_ID\",\"Date_result_released\",\"Device_ID\",\"Employed_in_healthcare\",\"First_test\",\"Illness_onset_date\",\"Instrument_ID\",\"Order_test_date\",\"Ordered_test_code\",\"Ordering_facility_city\",\"Ordering_facility_county\",\"Ordering_facility_email\",\"Ordering_facility_name\",\"Ordering_facility_phone_number\",\"Ordering_facility_state\",\"Ordering_facility_street\",\"Ordering_facility_street_2\",\"Ordering_facility_zip_code\",\"Ordering_provider_ID\",\"Ordering_provider_city\",\"Ordering_provider_county\",\"Ordering_provider_first_name\",\"Ordering_provider_last_name\",\"Ordering_provider_phone_number\",\"Ordering_provider_state\",\"Ordering_provider_street\",\"Ordering_provider_street_2\",\"Ordering_provider_zip_code\",\"Organization_name\",\"Patient_DOB\",\"Patient_ID\",\"Patient_city\",\"Patient_county\",\"Patient_email\",\"Patient_ethnicity\",\"Patient_first_name\",\"Patient_gender\",\"Patient_last_name\",\"Patient_middle_name\",\"Patient_phone_number\",\"Patient_race\",\"Patient_role\",\"Patient_state\",\"Patient_street\",\"Patient_street_2\",\"Patient_suffix\",\"Patient_zip_code\",\"Processing_mode_code\",\"Resident_congregate_setting\",\"Result_ID\",\"Site_of_care\",\"Specimen_collection_date_time\",\"Specimen_source_site_code\",\"Specimen_type_code\",\"Symptomatic_for_disease\",\"Test_date\",\"Test_result_code\",\"Test_result_status\",\"Testing_lab_CLIA\",\"Testing_lab_city\",\"Testing_lab_county\",\"Testing_lab_name\",\"Testing_lab_phone_number\",\"Testing_lab_state\",\"Testing_lab_street\",\"Testing_lab_street_2\",\"Testing_lab_zip_code\"\n"
+        "\"Corrected_result_ID\",\"Date_result_released\",\"Device_ID\",\"Employed_in_healthcare\",\"First_test\",\"Illness_onset_date\",\"Instrument_ID\",\"Order_test_date\",\"Ordered_test_code\",\"Ordering_facility_city\",\"Ordering_facility_county\",\"Ordering_facility_email\",\"Ordering_facility_name\",\"Ordering_facility_phone_number\",\"Ordering_facility_state\",\"Ordering_facility_street\",\"Ordering_facility_street_2\",\"Ordering_facility_zip_code\",\"Ordering_provider_ID\",\"Ordering_provider_city\",\"Ordering_provider_county\",\"Ordering_provider_first_name\",\"Ordering_provider_last_name\",\"Ordering_provider_phone_number\",\"Ordering_provider_state\",\"Ordering_provider_street\",\"Ordering_provider_street_2\",\"Ordering_provider_zip_code\",\"Organization_name\",\"Patient_DOB\",\"Patient_ID\",\"Patient_city\",\"Patient_county\",\"Patient_email\",\"Patient_ethnicity\",\"Patient_first_name\",\"Patient_gender\",\"Patient_last_name\",\"Patient_middle_name\",\"Patient_phone_number\",\"Patient_preferred_language\",\"Patient_race\",\"Patient_role\",\"Patient_state\",\"Patient_street\",\"Patient_street_2\",\"Patient_suffix\",\"Patient_tribal_affiliation\",\"Patient_zip_code\",\"Processing_mode_code\",\"Resident_congregate_setting\",\"Result_ID\",\"Site_of_care\",\"Specimen_collection_date_time\",\"Specimen_source_site_code\",\"Specimen_type_code\",\"Symptomatic_for_disease\",\"Test_date\",\"Test_result_code\",\"Test_result_status\",\"Testing_lab_CLIA\",\"Testing_lab_city\",\"Testing_lab_county\",\"Testing_lab_name\",\"Testing_lab_phone_number\",\"Testing_lab_state\",\"Testing_lab_street\",\"Testing_lab_street_2\",\"Testing_lab_zip_code\"\n"
             + "\"\",\""
             + date
-            + "\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
+            + "\",\"SFN\",\"N\",\"UNK\",\"\",\""
             + instrumentId1
             + "\",\""
             + date
-            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
+            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"18990510\",\""
             + patientId1
-            + "\",\"\",\"\",\"\",\"U\",\"John\",\"U\",\"Boddie\",\"Brown\",\"503-867-5309\",\"UNK\",\"STAFF\",\"\",\"\",\"\",\"Jr.\",\"\",\"P\",\"UNK\",\""
+            + "\",\"Washington\",\"Washington\",\"\",\"N\",\"Fred\",\"M\",\"Astaire\",\"\",\"5168675309\",\"English\",\"2106-3\",\"RESIDENT\",\"DC\",\"736 Jackson PI NW\",\"\",\"\",\"\",\"20503\",\"P\",\"N\",\""
             + resultId1
             + "\",\""
             + siteOfCare1
@@ -85,13 +85,13 @@ class DataHubUploaderServiceTest extends BaseServiceTest<DataHubUploaderService>
             + "\",\"260415000\",\"F\",\"123456\",\"Washington\",\"Washington\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\"\n"
             + "\"\",\""
             + date
-            + "\",\"SFN\",\"UNK\",\"UNK\",\"\",\""
+            + "\",\"SFN\",\"N\",\"UNK\",\"\",\""
             + instrumentId2
             + "\",\""
             + date
-            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"\",\""
+            + "\",\"54321-BOOM\",\"Washington\",\"Washington\",\"facility@test.com\",\"Imaginary Site\",\"555-867-5309\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"DOOOOOOM\",\"Washington\",\"Washington\",\"Doctor\",\"Doom\",\"800-555-1212\",\"DC\",\"736 Jackson PI NW\",\"\",\"20503\",\"The Mall\",\"18990510\",\""
             + patientId2
-            + "\",\"\",\"\",\"\",\"U\",\"John\",\"U\",\"Boddie\",\"Brown\",\"503-867-5309\",\"UNK\",\"STAFF\",\"\",\"\",\"\",\"Jr.\",\"\",\"P\",\"UNK\",\""
+            + "\",\"Washington\",\"Washington\",\"\",\"N\",\"Fred\",\"M\",\"Astaire\",\"\",\"5168675309\",\"English\",\"2106-3\",\"RESIDENT\",\"DC\",\"736 Jackson PI NW\",\"\",\"\",\"\",\"20503\",\"P\",\"N\",\"" // "\",\"\",\"\",\"\",\"U\",\"John\",\"U\",\"Boddie\",\"Brown\",\"503-867-5309\",\"UNK\",\"STAFF\",\"\",\"\",\"\",\"Jr.\",\"\",\"P\",\"UNK\",\""
             + resultId2
             + "\",\""
             + siteOfCare2
