5b6d3576ec97b7fbdb97f84ba54071c3d1569d3b
Nathan Carter <nathan@skylight.digital>
2021-09-01 14:31:56 -0400

    Add GraphQL query to return a boolean indicating whether patient already exists in a facility or organization
    Add a `patientExists` resolver
    
    Simplify Specification filters in person service
      - Updated some assumptions that greatly simplify the "patient exists"
        logic

--- backend/src/main/java/gov/cdc/usds/simplereport/api/patient/PatientResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/patient/PatientResolver.java
@@ -1,9 +1,13 @@
 package gov.cdc.usds.simplereport.api.patient;
 
 import gov.cdc.usds.simplereport.config.AuthorizationConfiguration;
+import gov.cdc.usds.simplereport.db.model.Facility;
+import gov.cdc.usds.simplereport.db.model.Organization;
 import gov.cdc.usds.simplereport.db.model.Person;
+import gov.cdc.usds.simplereport.service.OrganizationService;
 import gov.cdc.usds.simplereport.service.PersonService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Component;
 public class PatientResolver implements GraphQLQueryResolver {
 
   @Autowired private PersonService ps;
+  @Autowired private OrganizationService os;
 
   // authorization happens in calls to PersonService
   public List<Person> getPatients(
@@ -24,6 +29,13 @@ public class PatientResolver implements GraphQLQueryResolver {
   // authorization happens in calls to PersonService
   public long patientsCount(UUID facilityId, boolean showDeleted, String namePrefixMatch) {
     return ps.getPatientsCount(facilityId, showDeleted, namePrefixMatch);
+  }
+
+  public boolean patientExists(
+      String firstName, String lastName, LocalDate birthDate, String zipCode, UUID facilityId) {
+    Organization org = os.getCurrentOrganization();
+    Facility facility = os.getFacilityInCurrentOrg(facilityId);
+    return ps.isDuplicatePatient(firstName, lastName, birthDate, zipCode, org, facility);
   }
 
   @AuthorizationConfiguration.RequirePermissionSearchTargetPatient
--- backend/src/main/java/gov/cdc/usds/simplereport/service/PersonService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/PersonService.java
@@ -86,10 +86,7 @@ public class PersonService {
 
   private Specification<Person> inOrganizationFilter(UUID orgId) {
     return (root, query, cb) ->
-        cb.and(
-            cb.or(
-                cb.isNull(root.get(SpecField.ORGANIZATION)),
-                cb.equal(root.get(SpecField.ORGANIZATION).get(SpecField.INTERNAL_ID), orgId)));
+        cb.and(cb.equal(root.get(SpecField.ORGANIZATION).get(SpecField.INTERNAL_ID), orgId));
   }
 
   // Note: Patients with NULL facilityIds appear in ALL facilities.
@@ -163,24 +160,12 @@ public class PersonService {
       String postalCode,
       UUID facilityId,
       UUID orgId) {
-    Specification<Person> filter = patientExistsFilter(firstName, lastName, birthDate, postalCode);
-
-    if (orgId != null) {
-      filter = filter.and(inOrganizationFilter(orgId)).and(inFacilityFilter(null));
-    }
+    Specification<Person> filter =
+        patientExistsFilter(firstName, lastName, birthDate, postalCode)
+            .and(inOrganizationFilter(orgId));
 
     if (facilityId != null) {
-      filter = filter.and(inFacilityFilter(facilityId));
-
-      var facilityOrganization = _os.getOrganizationByFacilityId(facilityId);
-
-      // Additionally check if patient has already registered with an org link
-      if (facilityOrganization != null) {
-        filter =
-            filter.or(
-                inOrganizationFilter(facilityOrganization.getInternalId())
-                    .and(inFacilityFilter(null)));
-      }
+      return filter.and(inFacilityFilter(facilityId));
     }
 
     return filter;
@@ -214,26 +199,6 @@ public class PersonService {
         PageRequest.of(pageOffset, pageSize, NAME_SORT));
   }
 
-  public boolean isPatientInOrg(
-      String firstName, String lastName, LocalDate birthDate, String postalCode, UUID orgId) {
-    var patients =
-        _repo.findAll(
-            buildPersonMatchFilter(firstName, lastName, birthDate, postalCode, null, orgId),
-            PageRequest.of(0, 1, NAME_SORT));
-
-    return !patients.isEmpty();
-  }
-
-  public boolean isPatientInFacility(
-      String firstName, String lastName, LocalDate birthDate, String postalCode, UUID facilityId) {
-    var patients =
-        _repo.findAll(
-            buildPersonMatchFilter(firstName, lastName, birthDate, postalCode, facilityId, null),
-            PageRequest.of(0, 1, NAME_SORT));
-
-    return !patients.isEmpty();
-  }
-
   public boolean isDuplicatePatient(
       String firstName,
       String lastName,
@@ -241,12 +206,18 @@ public class PersonService {
       String postalCode,
       Organization org,
       Facility facility) {
-    if (facility == null) {
-      return isPatientInOrg(firstName, lastName, birthDate, postalCode, org.getInternalId());
-    }
+    var patients =
+        _repo.findAll(
+            buildPersonMatchFilter(
+                firstName,
+                lastName,
+                birthDate,
+                postalCode,
+                facility.getInternalId(),
+                org.getInternalId()),
+            PageRequest.of(0, 1, NAME_SORT));
 
-    return isPatientInFacility(
-        firstName, lastName, birthDate, postalCode, facility.getInternalId());
+    return !patients.isEmpty();
   }
 
   @AuthorizationConfiguration.RequireSpecificPatientSearchPermission
