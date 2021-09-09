06e6c168b88bab998b179b8402a4e6b20a88f90f
Adam Weber <a.diamond.weber@gmail.com>
2021-09-08 17:13:36 -0700

    Address conf properties consistent w/ terraform env var

--- backend/src/main/java/gov/cdc/usds/simplereport/properties/AzureStorageQueueReportingProperties.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/properties/AzureStorageQueueReportingProperties.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
-@ConfigurationProperties(prefix = "simple-report.azure-reporting-queue")
-@ConditionalOnProperty("simple-report.azure-reporting-queue.connection-string")
+@ConfigurationProperties(prefix = "simple-report.az-reporting-queue")
+@ConditionalOnProperty("simple-report.az-reporting-queue.connection-string")
 public final class AzureStorageQueueReportingProperties {
   public String getConnectionString() {
     return connectionString;
