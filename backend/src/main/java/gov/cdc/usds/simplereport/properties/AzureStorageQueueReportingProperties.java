9c8de4ac37b10c2582e2d90407638015e408a41d
Adam Weber <a.diamond.weber@gmail.com>
2021-09-09 18:32:34 -0700

    Wrangle ConditionalOnProperty and env vars into working

--- backend/src/main/java/gov/cdc/usds/simplereport/properties/AzureStorageQueueReportingProperties.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/properties/AzureStorageQueueReportingProperties.java
@@ -1,20 +1,33 @@
 package gov.cdc.usds.simplereport.properties;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
-@ConfigurationProperties(prefix = "simple-report.az-reporting-queue")
-@ConditionalOnProperty("simple-report.az-reporting-queue.connection-string")
+@Slf4j
+@ConfigurationProperties(prefix = "simple-report.azure-reporting-queue")
 public final class AzureStorageQueueReportingProperties {
+  private final String connectionString;
+  private final String enabled;
+  private final String name;
+
+  @ConstructorBinding
+  public AzureStorageQueueReportingProperties(
+      String connectionString, String enabled, String name) {
+    this.enabled = enabled;
+    this.connectionString = connectionString;
+    this.name = name;
+  }
+
   public String getConnectionString() {
     return connectionString;
   }
 
-  private final String connectionString;
+  public String getEnabled() {
+    return enabled;
+  }
 
-  @ConstructorBinding
-  public AzureStorageQueueReportingProperties(String connectionString) {
-    this.connectionString = connectionString;
+  public String getName() {
+    return name;
   }
 }
