ae877223ec64efb078ac8d6ad8fd80e145ab3a47
Adam Weber <a.diamond.weber@gmail.com>
2021-09-17 07:12:59 -0700

    Format terraform

--- ops/services/app_functions/report_stream_batched_publisher/_var.tf
+++ ops/services/app_functions/report_stream_batched_publisher/_var.tf
@@ -1,37 +1,37 @@
 variable "prefix" {
-    type = string
-    default = "reportstream-batched-publisher"
+  type    = string
+  default = "reportstream-batched-publisher"
 }
 
 variable "location" {
-    type = string
-    default = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "environment" {
-    type = string
-    default = "dev"
+  type    = string
+  default = "dev"
 }
 
 variable "resource_group" {
-    type = string
-    default = "prime-simple-report-dev"
+  type    = string
+  default = "prime-simple-report-dev"
 }
 
 variable "function_app_source" {
-    type = string
-    default = "./functions/build/batched-rs-publisher.zip"
+  type    = string
+  default = "./functions/build/batched-rs-publisher.zip"
 }
 
 variable "storage_account_name" {
-    type = string
-    default = "simplereportdevapp" 
+  type    = string
+  default = "simplereportdevapp"
 }
 
 variable "storage_account_key" {
-    type = string
+  type = string
 }
 
 variable "storage_account_primary_connection_string" {
-    type = string
+  type = string
 }
--- ops/services/app_functions/report_stream_batched_publisher/main.tf
+++ ops/services/app_functions/report_stream_batched_publisher/main.tf
@@ -1,73 +1,73 @@
 resource "azurerm_storage_container" "deployments" {
-    name = "rs-batched-publisher-function-releases"
-    storage_account_name = "${var.storage_account_name}"
-    container_access_type = "private"
+  name                  = "rs-batched-publisher-function-releases"
+  storage_account_name  = var.storage_account_name
+  container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "appcode" {
-    name = "functionapp.zip"
-    storage_account_name = "${var.storage_account_name}"
-    storage_container_name = "${azurerm_storage_container.deployments.name}"
-    type = "Block"
-    source = "${var.function_app_source}"
+  name                   = "functionapp.zip"
+  storage_account_name   = var.storage_account_name
+  storage_container_name = azurerm_storage_container.deployments.name
+  type                   = "Block"
+  source                 = var.function_app_source
 }
 
 data "azurerm_storage_account_sas" "sas" {
-    connection_string = "${var.storage_account_primary_connection_string}"
-    https_only = true
-    start = "2021-09-01"
-    expiry = "2022-12-31"
-    resource_types {
-        object = true
-        container = false
-        service = false
-    }
-    services {
-        blob = true
-        queue = false
-        table = false
-        file = false
-    }
-    permissions {
-        read = true
-        write = false
-        delete = false
-        list = false
-        add = false
-        create = false
-        update = false
-        process = false
-    }
+  connection_string = var.storage_account_primary_connection_string
+  https_only        = true
+  start             = "2021-09-01"
+  expiry            = "2022-12-31"
+  resource_types {
+    object    = true
+    container = false
+    service   = false
+  }
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+  }
 }
 
 resource "azurerm_app_service_plan" "asp" {
-    name = "${var.prefix}-plan"
-    resource_group_name = "${var.resource_group}" 
-    location = "${var.location}"
-    kind = "FunctionApp"
-    sku {
-        tier = "Dynamic"
-        size = "Y1"
-    }
+  name                = "${var.prefix}-plan"
+  resource_group_name = var.resource_group
+  location            = var.location
+  kind                = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
 
 resource "azurerm_function_app" "functions" {
-    name = "${var.prefix}-${var.environment}"
-    location = "${var.location}"
-    resource_group_name        = "${var.resource_group}"
-    app_service_plan_id        = "${azurerm_app_service_plan.asp.id}"
-    storage_account_name       = "${var.storage_account_name}"
-    storage_account_access_key = "${var.storage_account_key}"
-    version = "~2"
+  name                       = "${var.prefix}-${var.environment}"
+  location                   = var.location
+  resource_group_name        = var.resource_group
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_key
+  version                    = "~2"
 
-    app_settings = {
-        https_only = true
-        FUNCTIONS_WORKER_RUNTIME = "node"
-        WEBSITE_NODE_DEFAULT_VERSION = "~10"
-        FUNCTION_APP_EDIT_MODE = "readonly"
-        HASH = "${base64encode(filesha256("${var.function_app_source}"))}"
-        WEBSITE_RUN_FROM_PACKAGE = "https://${var.storage_account_name}.blob.core.windows.net/${azurerm_storage_container.deployments.name}/${azurerm_storage_blob.appcode.name}${data.azurerm_storage_account_sas.sas.sas}"
-    }
+  app_settings = {
+    https_only                   = true
+    FUNCTIONS_WORKER_RUNTIME     = "node"
+    WEBSITE_NODE_DEFAULT_VERSION = "~10"
+    FUNCTION_APP_EDIT_MODE       = "readonly"
+    HASH                         = "${base64encode(filesha256("${var.function_app_source}"))}"
+    WEBSITE_RUN_FROM_PACKAGE     = "https://${var.storage_account_name}.blob.core.windows.net/${azurerm_storage_container.deployments.name}/${azurerm_storage_blob.appcode.name}${data.azurerm_storage_account_sas.sas.sas}"
+  }
 }
 
 
