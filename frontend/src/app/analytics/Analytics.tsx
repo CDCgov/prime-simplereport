4bfaecab6fe56b45b432dd1bec37a6532350820a
Nick Clyde <nick@clyde.tech>
2021-09-20 14:39:09 -0700

    Update to match newest design

--- frontend/src/app/analytics/Analytics.tsx
+++ frontend/src/app/analytics/Analytics.tsx
@@ -2,12 +2,7 @@ import { DatePicker } from "../commonComponents/DatePicker";
 import Dropdown from "../commonComponents/Dropdown";
 
 import "./Analytics.scss";
-import testsConducted from "./img/tests-conducted-graph.png";
-import positivityRate from "./img/positivity-rate-graph.png";
-import positiveTests from "./img/positive-tests-map.png";
-import totalsByRole from "./img/totals-by-role-table.png";
 import allTestingFacilities from "./img/all-testing-facilities-table.png";
-import testsConductedAllFacilities from "./img/tests-conducted-all-facilities-graph.png";
 
 export const Analytics = () => {
   return (
@@ -16,8 +11,9 @@ export const Analytics = () => {
         <h2 className="position-relative">COVID-19 testing data</h2>
         <div className="prime-container card-container padding-2">
           <div className="grid-row grid-gap">
-            <div className="grid-col-3">
+            <div className="grid-col-4">
               <Dropdown
+                label="Facility"
                 options={[
                   {
                     label: "Abby Road School District",
@@ -28,8 +24,30 @@ export const Analytics = () => {
                 selectedValue="Abby Road School District"
               />
             </div>
-            <div className="grid-col-3">
+            <div className="grid-col-4">
               <Dropdown
+                label="Role"
+                options={[
+                  {
+                    label: "All roles",
+                    value: "All roles",
+                  },
+                  {
+                    label: "Students",
+                    value: "Students",
+                  },
+                  {
+                    label: "Teachers",
+                    value: "Teachers",
+                  },
+                ]}
+                onChange={() => {}}
+                selectedValue="All roles"
+              />
+            </div>
+            <div className="grid-col-4">
+              <Dropdown
+                label="Date range"
                 options={[
                   {
                     label: "Custom date range",
@@ -40,7 +58,9 @@ export const Analytics = () => {
                 selectedValue="Custom date range"
               />
             </div>
-            <div className="grid-col-3 inline-datepicker flex-row">
+          </div>
+          <div className="grid-row grid-gap margin-top-2">
+            <div className="grid-col-4">
               <DatePicker
                 name="begin"
                 label="Begin"
@@ -48,7 +68,7 @@ export const Analytics = () => {
                 noHint
               />
             </div>
-            <div className="grid-col-3 inline-datepicker flex-row">
+            <div className="grid-col-4">
               <DatePicker
                 name="end"
                 label="End"
@@ -58,68 +78,46 @@ export const Analytics = () => {
             </div>
           </div>
           <h3>Abby Road School District</h3>
-          <p>{`12/1/2021 \u2013 12-31/2021`}</p>
-          <div className="grid-row grid-gap-6">
-            <div className="grid-col-4">
+          <p className="margin-bottom-0">All roles</p>
+          <p>{`12/1/2021 \u2013 12/31/2021`}</p>
+          <div className="grid-row grid-gap">
+            <div className="grid-col-3">
               <div className="card display-flex flex-column flex-row">
-                <h2 className="flex-align-self-center">Tests conducted</h2>
-                <h1 className="flex-align-self-center">29</h1>
-                <p className="flex-align-self-center"></p>
+                <h2>Tests conducted</h2>
+                <h1>29</h1>
+                <p></p>
               </div>
             </div>
-            <div className="grid-col-4">
+            <div className="grid-col-3">
               <div className="card display-flex flex-column flex-align-center">
                 <h2>Positive tests</h2>
                 <h1>3</h1>
                 {/* \u2BC6 is down pointing triangle */}
                 <p>
-                  <span className="red-pointing-up">{`\u2BC5`}</span> 2 from
-                  last week
+                  <span className="red-pointing-up">{`\u2BC5`} 2</span>{" "}
+                  <span className="usa-hint font-ui-md">from last week</span>
                 </p>
               </div>
             </div>
-            <div className="grid-col-4">
+            <div className="grid-col-3">
+              <div className="card display-flex flex-column flex-align-center">
+                <h2>Negative tests</h2>
+                <h1>26</h1>
+                <p></p>
+              </div>
+            </div>
+            <div className="grid-col-3">
               <div className="card display-flex flex-column flex-align-center">
                 <h2>Positivity rate</h2>
                 <h1>10.3%</h1>
-                <p>Above average for Clayton county</p>
+                <p></p>
               </div>
             </div>
           </div>
-          <div className="grid-row grid-gap margin-top-4">
-            <div className="grid-col-6">
-              <img src={testsConducted} alt="Tests conducted graph" />
-            </div>
-            <div className="grid-col-6">
-              <img src={positivityRate} alt="Positivity rate graph" />
-            </div>
-          </div>
-          <p>Positive tests</p>
-          <div className="grid-row">
-            <div className="grid-col-4">
-              <img src={positiveTests} alt="Positive tests map" />
-            </div>
-            <div className="grid-col-4">
-              <img src={positiveTests} alt="Positive tests map" />
-            </div>
-            <div className="grid-col-4">
-              <img src={positiveTests} alt="Positive tests map" />
-            </div>
-          </div>
-          <h4>Totals by role</h4>
-          <img src={totalsByRole} alt="Totals by role table" />
         </div>
-        <div className="padding-2">
+        <div>
           <h4>All testing facilities</h4>
           <img src={allTestingFacilities} alt="All testing facilities table" />
-          <div className="grid-row">
-            <div className="grid-col-6">
-              <img
-                src={testsConductedAllFacilities}
-                alt="Tests conducted all facilities graph"
-              />
-            </div>
-          </div>
         </div>
       </div>
     </main>
