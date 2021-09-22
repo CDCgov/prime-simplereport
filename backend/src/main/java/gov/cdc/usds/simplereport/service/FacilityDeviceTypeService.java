d4770d83c2aa399aab168c23536adc8772ca3c44
Nathan Carter <nathan@skylight.digital>
2021-09-22 17:31:13 -0400

    Remove unused service code

--- backend/src/main/java/gov/cdc/usds/simplereport/service/FacilityDeviceTypeService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/FacilityDeviceTypeService.java
@@ -2,11 +2,8 @@ package gov.cdc.usds.simplereport.service;
 
 import gov.cdc.usds.simplereport.api.model.errors.IllegalGraphqlArgumentException;
 import gov.cdc.usds.simplereport.db.model.DeviceSpecimenType;
-import gov.cdc.usds.simplereport.db.model.FacilityDeviceSpecimenType;
 import gov.cdc.usds.simplereport.db.repository.FacilityDeviceSpecimenTypeRepository;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,19 +21,5 @@ public class FacilityDeviceTypeService {
                 new IllegalGraphqlArgumentException(
                     "Device is not configured with a specimen type"))
         .getDeviceSpecimenType();
-  }
-
-  List<DeviceSpecimenType> getDeviceSpecimenTypes(UUID facilityId) {
-    List<FacilityDeviceSpecimenType> facilityDeviceSpecimenTypes =
-        _facilityDeviceSpecimenRepo
-            .findByFacilityInternalIdOrderByDeviceSpecimenTypeCreatedAt(facilityId)
-            .orElseThrow(
-                () ->
-                    new IllegalGraphqlArgumentException(
-                        "Device is not configured with a specimen type"));
-
-    return facilityDeviceSpecimenTypes.stream()
-        .map(FacilityDeviceSpecimenType::getDeviceSpecimenType)
-        .collect(Collectors.toList());
   }
 }
