585088df2519e30c5b81eee112d0bb13910935b6
Nick Clyde <nick@clyde.tech>
2021-09-22 14:50:03 -0700

    Add story mocks, fix date picker input display

--- frontend/src/app/analytics/Analytics.tsx
+++ frontend/src/app/analytics/Analytics.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import moment from "moment";
 
 import { DatePicker } from "../commonComponents/DatePicker";
@@ -8,12 +10,82 @@ import { LoadingCard } from "../commonComponents/LoadingCard/LoadingCard";
 import "./Analytics.scss";
 import allTestingFacilities from "./img/all-testing-facilities-table.png";
 
+const EXTERNAL_DATE_FORMAT = "MM/DD/YYYY";
+
 export const Analytics = () => {
+  const organization = useSelector(
+    (state) => (state as any).organization as Organization
+  );
+  const facilities = useSelector(
+    (state) => ((state as any).facilities as Facility[]) || []
+  );
+  const [facilityId, setFacilityId] = useState<string>("");
+  const [facilityName, setFacilityName] = useState<string>(organization.name);
+  const [dateRange, setDateRange] = useState<string>("week");
+  const [startDate, setStartDate] = useState<string>(
+    moment().subtract(1, "week").format(EXTERNAL_DATE_FORMAT)
+  );
+  const [endDate, setEndDate] = useState<string>(
+    moment().format(EXTERNAL_DATE_FORMAT)
+  );
+
+  useEffect(() => {
+    const startInput = document.getElementById("startDate") as HTMLInputElement;
+    const endInput = document.getElementById("endDate") as HTMLInputElement;
+    if (startInput) {
+      startInput.value = startDate;
+    }
+    if (endInput) {
+      endInput.value = endDate;
+    }
+  });
+
+  const updateFacility = ({
+    target: { value },
+  }: ChangeEvent<HTMLSelectElement>) => {
+    const facilityName =
+      facilities.find((f) => f.id === value)?.name || "All facilities";
+    setFacilityId(value);
+    setFacilityName(facilityName);
+  };
+
+  const updateDateRange = ({
+    target: { value },
+  }: ChangeEvent<HTMLSelectElement>) => {
+    setDateRange(value);
+    switch (value) {
+      case "day":
+        setStartDate(moment().subtract(1, "day").format(EXTERNAL_DATE_FORMAT));
+        setEndDate(moment().format(EXTERNAL_DATE_FORMAT));
+        break;
+      case "week":
+        setStartDate(moment().subtract(1, "week").format(EXTERNAL_DATE_FORMAT));
+        setEndDate(moment().format(EXTERNAL_DATE_FORMAT));
+        break;
+      case "month":
+        setStartDate(
+          moment().subtract(1, "month").format(EXTERNAL_DATE_FORMAT)
+        );
+        setEndDate(moment().format(EXTERNAL_DATE_FORMAT));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const now = moment();
+
   const { data, loading, error } = useGetTopLevelDashboardMetricsQuery({
     variables: {
-      facilityId: null,
-      startDate: moment("2020-01-01"),
-      endDate: moment("2025-01-01"),
+      facilityId,
+      startDate: moment(startDate, EXTERNAL_DATE_FORMAT)
+        .hour(now.hours())
+        .minute(now.minutes())
+        .toDate(),
+      endDate: moment(endDate, EXTERNAL_DATE_FORMAT)
+        .hour(now.hours())
+        .minute(now.minutes())
+        .toDate(),
     },
     fetchPolicy: "no-cache",
   });
@@ -46,70 +118,82 @@ export const Analytics = () => {
                 label="Facility"
                 options={[
                   {
-                    label: "Abby Road School District",
-                    value: "Abby Road School District",
+                    label: "All facilities",
+                    value: "",
                   },
+                  ...facilities.map((f) => ({
+                    label: f.name,
+                    value: f.id,
+                  })),
                 ]}
-                onChange={() => {}}
-                selectedValue="Abby Road School District"
+                onChange={updateFacility}
+                selectedValue={facilityId}
               />
             </div>
-            <div className="grid-col-4">
-              <Dropdown
-                label="Role"
-                options={[
-                  {
-                    label: "All roles",
-                    value: "All roles",
-                  },
-                  {
-                    label: "Students",
-                    value: "Students",
-                  },
-                  {
-                    label: "Teachers",
-                    value: "Teachers",
-                  },
-                ]}
-                onChange={() => {}}
-                selectedValue="All roles"
-              />
-            </div>
+            {/* TODO: filter by patient role */}
             <div className="grid-col-4">
               <Dropdown
                 label="Date range"
                 options={[
                   {
+                    label: "Last day",
+                    value: "day",
+                  },
+                  {
+                    label: "Last week",
+                    value: "week",
+                  },
+                  {
+                    label: "Last month",
+                    value: "month",
+                  },
+                  {
                     label: "Custom date range",
-                    value: "Custom date range",
+                    value: "custom",
                   },
                 ]}
-                onChange={() => {}}
-                selectedValue="Custom date range"
+                onChange={updateDateRange}
+                selectedValue={dateRange}
               />
             </div>
           </div>
-          <div className="grid-row grid-gap margin-top-2">
-            <div className="grid-col-4">
-              <DatePicker
-                name="begin"
-                label="Begin"
-                defaultValue="12/1/2021"
-                noHint
-              />
+          {dateRange === "custom" && (
+            <div className="grid-row grid-gap margin-top-2">
+              <div className="grid-col-4">
+                <DatePicker
+                  name="startDate"
+                  label="Begin"
+                  onChange={(date?: string) => {
+                    if (date && date.length === 10) {
+                      const newDate = moment(date, EXTERNAL_DATE_FORMAT);
+                      if (newDate.isValid()) {
+                        setStartDate(newDate.format(EXTERNAL_DATE_FORMAT));
+                      }
+                    }
+                  }}
+                  noHint
+                />
+              </div>
+              <div className="grid-col-4">
+                <DatePicker
+                  name="endDate"
+                  label="End"
+                  onChange={(date?: string) => {
+                    if (date && date.length === 10) {
+                      const newDate = moment(date, EXTERNAL_DATE_FORMAT);
+                      if (newDate.isValid()) {
+                        setEndDate(newDate.format(EXTERNAL_DATE_FORMAT));
+                      }
+                    }
+                  }}
+                  noHint
+                />
+              </div>
             </div>
-            <div className="grid-col-4">
-              <DatePicker
-                name="end"
-                label="End"
-                defaultValue="12/31/2021"
-                noHint
-              />
-            </div>
-          </div>
-          <h3>Abby Road School District</h3>
+          )}
+          <h3>{facilityName}</h3>
           <p className="margin-bottom-0">All roles</p>
-          <p>{`12/1/2021 \u2013 12/31/2021`}</p>
+          <p>{`${startDate} \u2013 ${endDate}`}</p>
           <div className="grid-row grid-gap">
             <div className="grid-col-3">
               <div className="card display-flex flex-column flex-row">
