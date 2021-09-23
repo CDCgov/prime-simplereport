2babf0feeb014de3f7ee5cdd7d13892f49eed7ef
Adam Weber <a.diamond.weber@gmail.com>
2021-09-23 13:24:35 -0700

    Add queue to prod & test envs

--- ops/test/api.tf
+++ ops/test/api.tf
@@ -30,6 +30,7 @@ module "simple_report_api" {
     OKTA_OAUTH2_CLIENT_SECRET             = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_client_secret.id})"
     OKTA_API_KEY                          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_api_key.id})"
     APPLICATIONINSIGHTS_CONNECTION_STRING = data.azurerm_application_insights.app_insights.connection_string
+    AZ_REPORTING_QUEUE_CXN_STRING         = data.azurerm_storage_account.app.primary_connection_string
     TWILIO_ACCOUNT_SID                    = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_account_sid.id})"
     TWILIO_AUTH_TOKEN                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_auth_token.id})"
     SIMPLE_REPORT_SENDGRID_API_KEY        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sendgrid_api_key.id})"
