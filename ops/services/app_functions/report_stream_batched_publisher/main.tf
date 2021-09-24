6c2828a565a09e16a2ea2a9e3e6a7a92a4420dc7
Adam Weber <a.diamond.weber@gmail.com>
2021-09-24 12:04:31 -0700

    Increase max to 5000 per ReportStream team

--- ops/services/app_functions/report_stream_batched_publisher/main.tf
+++ ops/services/app_functions/report_stream_batched_publisher/main.tf
@@ -66,7 +66,7 @@ resource "azurerm_function_app" "functions" {
     # REPORT_STREAM_TOKEN            = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.datahub_api_key.id})"
     REPORT_STREAM_TOKEN            = var.report_stream_api_token
     REPORT_STREAM_BATCH_MINIMUM    = "1"
-    REPORT_STREAM_BATCH_MAXIMUM    = "1000"
+    REPORT_STREAM_BATCH_MAXIMUM    = "5000"
   }
 }
 
