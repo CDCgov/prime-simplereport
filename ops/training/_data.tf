e3c3313cc12e678f042c26d8b9f568ca7a43ebb1
Adam Weber <nullflux@users.noreply.github.com>
2021-09-28 10:44:29 -0700

    Remove unused DATA_HUB_API_KEY from lower envs (#2683)

--- ops/training/_data.tf
+++ ops/training/_data.tf
@@ -52,11 +52,6 @@ data "azurerm_key_vault_secret" "sr_db_jdbc" {
 }
 
 
-data "azurerm_key_vault_secret" "datahub_api_key" {
-  name         = "datahub-api-key-dev"
-  key_vault_id = data.azurerm_key_vault.global.id
-}
-
 data "azurerm_key_vault_secret" "slack_notify_webhook_url" {
   name         = "slack-notify-webhook-url-dev"
   key_vault_id = data.azurerm_key_vault.global.id
