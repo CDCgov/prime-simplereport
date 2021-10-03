af8afae4ced8a8182e6c6c8d72c46a0ba4a44d47
Adam Weber <a.diamond.weber@gmail.com>
2021-10-02 21:54:22 -0700

    Test setups

--- ops/services/app_functions/report_stream_batched_publisher/functions/ReportStreamExceptionHandler/index.test.ts
+++ ops/services/app_functions/report_stream_batched_publisher/functions/ReportStreamExceptionHandler/index.test.ts
@@ -1,5 +1,37 @@
+import queueTrigger from "./index";
+import fetchMock, { enableFetchMocks } from "jest-fetch-mock";
+import { Context } from "@azure/functions";
+enableFetchMocks();
+
 describe("main function export", () => {
-    it("calls the SimpleReport webhook API", async () => {
-        expect(0).toBe(1);
-    });
+  const context: Context = {
+    log: jest.fn(),
+    traceContext: { traceparent: "asdf" },
+  } as any;
+  context.log.error = jest.fn();
+
+  it("calls the SimpleReport webhook API", async () => {
+    // GIVEN
+    const queueItem = "irrelevant";
+    fetchMock.mockResponseOnce("asdf");
+
+    // WHEN
+    queueTrigger(context, queueItem);
+
+    // THEN
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it("throws if SimpleReport webhook API does not return 2xx", async () => {
+    // GIVEN
+    const queueItem = "irrelephant";
+    fetchMock.mockResponseOnce("", { status: 500 });
+
+    // WHEN
+    const promise = queueTrigger(context, queueItem);
+
+    // THEN
+    await expect(promise).rejects.toThrowError();
+    expect(fetch).toHaveBeenCalled();
+  });
 });
