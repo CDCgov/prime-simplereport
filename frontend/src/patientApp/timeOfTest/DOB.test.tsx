0622f701eefa80bd4c967028cde65a07ee1b015f
Nick Clyde <nick@clyde.tech>
2021-10-06 14:28:40 -0700

    Improve birth date validation and increase expiry for patient links (#2737)
    * Improve birth date validation for texted test results
    
    * Add more specific exceptions to verifyPatientLink
    
    * Refactor testing strategy to use spyOn (#2738)
    
    * Remove @EnableAsync (#2733)
    
    Remove @EnableAsync, there is no longer any `@Async` used used in the app.
    
    * Fix backend tests
    
    * Change status codes slightly and fix tests
    
    * Fix imports in DOB.test.tsx
    
    * Code smells
    
    * Content updates
    
    * Fix tests
    
    * Spanish updates
    
    Co-authored-by: Adam Weber <nullflux@users.noreply.github.com>
    Co-authored-by: Brett Maden <brett.m.maden@omb.eop.gov>

--- frontend/src/patientApp/timeOfTest/DOB.test.tsx
+++ frontend/src/patientApp/timeOfTest/DOB.test.tsx
@@ -1,9 +1,11 @@
-import renderer from "react-test-renderer";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
+import { act } from "react-dom/test-utils";
+
 import "../../i18n";
+import { PxpApi } from "../PxpApiService";
 
 import DOB from "./DOB";
 
@@ -14,41 +16,159 @@ const mockContainer = (store: any) => (
   </Provider>
 );
 
-jest.mock("../PxpApiService", () => {
-  jest.fn();
-});
+const validateDateOfBirthSpy = jest
+  .spyOn(PxpApi, "validateDateOfBirth")
+  .mockImplementation(jest.fn());
 
 describe("DOB", () => {
-  describe("snapshot", () => {
-    let component: renderer.ReactTestRenderer;
-    beforeEach(() => {
-      const store = mockStore({
-        plid: "foo",
-      });
-      component = renderer.create(mockContainer(store));
+  beforeEach(() => {
+    const store = mockStore({
+      plid: "foo",
     });
-    it("matches", () => {
-      expect(component.toJSON()).toMatchSnapshot();
-    });
+    render(mockContainer(store));
   });
 
-  describe("behavior", () => {
-    beforeEach(() => {
-      const store = mockStore({
-        plid: "foo",
-      });
-      render(mockContainer(store));
+  it("Checks for the correct format - dashes aren't accepted", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "07-12-1987");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: Date of birth must be in MM/DD/YYYY format"
+    );
+    expect(validateDateOfBirthSpy).not.toHaveBeenCalled();
+  });
+
+  it("Checks for the correct format - missing slashes aren't accepted", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "07121987");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: Date of birth must be in MM/DD/YYYY format"
+    );
+    expect(validateDateOfBirthSpy).not.toHaveBeenCalled();
+  });
+
+  it("Checks to make sure it is actually a possible date", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "99/99/9999");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: Date of birth must be a valid date"
+    );
+    expect(validateDateOfBirthSpy).not.toHaveBeenCalled();
+  });
+
+  it("Checks to make sure it is a date after 1900", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/21/1899");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: Date of birth must be after 1900 and before the current year"
+    );
+    expect(validateDateOfBirthSpy).not.toHaveBeenCalled();
+  });
+
+  it("Checks to make sure it is a date before this year", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/21/2237");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: Date of birth must be after 1900 and before the current year"
+    );
+    expect(validateDateOfBirthSpy).not.toHaveBeenCalled();
+  });
+
+  it("Rejects the wrong date (not what is stored for the user)", async () => {
+    // GIVEN
+    validateDateOfBirthSpy.mockRejectedValue({ status: 403 });
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/22/1987");
+    userEvent.click(await screen.findByText("Continue"));
+
+    // WHEN
+    const error = await screen.findByRole("alert");
+
+    // THEN
+    expect(error.textContent).toEqual(
+      "Error: The date of birth entered is incorrect"
+    );
+    expect(validateDateOfBirthSpy).toHaveBeenCalled();
+  });
+
+  it("Accepts an otherwise valid date", async () => {
+    // GIVEN
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/21/1987");
+
+    // WHEN
+    await act(async () => {
+      userEvent.click(await screen.findByText("Continue"));
     });
 
-    it("validates birthdays", async () => {
-      userEvent.type(
-        await screen.findByLabelText("Date of birth"),
-        "99-99-9999"
-      );
-      userEvent.click(await screen.findByRole("button"));
-      const error = await screen.findByRole("alert");
+    // THEN
+    expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+  });
 
-      expect(error.textContent).toEqual("Error: Enter your date of birth");
+  it("Rejects an expired link", async () => {
+    // GIVEN
+    validateDateOfBirthSpy.mockRejectedValue({ status: 410 });
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/21/1987");
+
+    // WHEN
+    await act(async () => {
+      userEvent.click(await screen.findByText("Continue"));
     });
+
+    // THEN
+    expect(
+      await screen.findByText(
+        "This link has expired. Please contact your test provider.",
+        { exact: false }
+      )
+    ).toBeInTheDocument();
+    expect(validateDateOfBirthSpy).toHaveBeenCalled();
+  });
+
+  it("Rejects an invalid link", async () => {
+    // GIVEN
+    validateDateOfBirthSpy.mockRejectedValue({ status: 404 });
+    userEvent.type(await screen.findByLabelText("Date of birth"), "08/21/1987");
+
+    // WHEN
+    await act(async () => {
+      userEvent.click(await screen.findByText("Continue"));
+    });
+
+    // THEN
+    expect(
+      await screen.findByText(
+        "This link is invalid. Please contact your test provider.",
+        { exact: false }
+      )
+    ).toBeInTheDocument();
+    expect(validateDateOfBirthSpy).toHaveBeenCalled();
   });
 });
