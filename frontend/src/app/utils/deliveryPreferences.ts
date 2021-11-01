3613648e52d8072fcff4956104cc906ef6104906
Nathan Carter <nathan@skylight.digital>
2021-11-01 14:43:48 -0400

    (Hopefully) clarify delivery preference utility library

--- frontend/src/app/utils/deliveryPreferences.ts
+++ frontend/src/app/utils/deliveryPreferences.ts
@@ -5,56 +5,70 @@ import {
 
 const { SMS, EMAIL, ALL, NONE } = TestResultDeliveryPreferences;
 
-export const toggleDeliveryPreferenceSms = toggleDeliveryPreference(SMS);
-export const toggleDeliveryPreferenceEmail = toggleDeliveryPreference(EMAIL);
-
-export const getSelectedDeliveryPreferencesSms = getSelectedDeliveryPreference(
+export const toggleDeliveryPreferenceSms = toggleDeliveryPreference.bind(
+  null,
   SMS
 );
-export const getSelectedDeliveryPreferencesEmail = getSelectedDeliveryPreference(
+export const toggleDeliveryPreferenceEmail = toggleDeliveryPreference.bind(
+  null,
   EMAIL
 );
 
-function toggleDeliveryPreference(field: TestResultDeliveryPreference) {
-  return function handleToggle(
-    oldPreference: TestResultDeliveryPreference,
-    newPreference: TestResultDeliveryPreference
-  ): TestResultDeliveryPreference {
-    const deliveryPreferences = new Set<TestResultDeliveryPreference>();
+export const getSelectedDeliveryPreferencesSms = getSelectedDeliveryPreference.bind(
+  null,
+  SMS
+);
+export const getSelectedDeliveryPreferencesEmail = getSelectedDeliveryPreference.bind(
+  null,
+  EMAIL
+);
 
-    if (oldPreference === SMS || oldPreference === ALL) {
-      deliveryPreferences.add(SMS);
-    }
+function toggleDeliveryPreference(
+  field: TestResultDeliveryPreference,
+  oldPreference: TestResultDeliveryPreference,
+  newPreference: TestResultDeliveryPreference
+): TestResultDeliveryPreference {
+  // Place configured delivery preference(s) in a Set. If preference is `ALL`,
+  // expand into its constituent options
+  const deliveryPreferences = new Set<TestResultDeliveryPreference>();
 
-    if (oldPreference === EMAIL || oldPreference === ALL) {
-      deliveryPreferences.add(EMAIL);
-    }
+  if (oldPreference === SMS || oldPreference === ALL) {
+    deliveryPreferences.add(SMS);
+  }
 
-    if (newPreference === field) {
-      deliveryPreferences.add(field);
-    }
+  if (oldPreference === EMAIL || oldPreference === ALL) {
+    deliveryPreferences.add(EMAIL);
+  }
 
-    if (newPreference === NONE) {
-      deliveryPreferences.delete(field);
-    }
+  if (newPreference === field) {
+    deliveryPreferences.add(field);
+  }
 
-    switch (deliveryPreferences.size) {
-      case 0:
-        return NONE;
-      case 1:
-        return Array.from(deliveryPreferences)[0];
-      case 2:
-        return ALL;
-      default:
-        return NONE;
-    }
-  };
+  // Remove _only_ the delivery preference for the selected field - other
+  // user selections will remain intact
+  if (newPreference === NONE) {
+    deliveryPreferences.delete(field);
+  }
+
+  // Translate set of preferences into the corresponding enum value
+  switch (deliveryPreferences.size) {
+    case 0:
+      return NONE;
+    case 1:
+      return Array.from(deliveryPreferences)[0];
+    case 2:
+      return ALL;
+    default:
+      return NONE;
+  }
 }
 
-function getSelectedDeliveryPreference(field: TestResultDeliveryPreference) {
-  return function getSelectedDeliveryPreference(
-    testResultDeliveryPreference: TestResultDeliveryPreference
-  ) {
-    return [field, ALL].includes(testResultDeliveryPreference) ? field : NONE;
-  };
+// Given a patient's test result delivery preference, determines the radio
+// option that should be selected for test result delivery preference fields
+// in forms
+function getSelectedDeliveryPreference(
+  field: TestResultDeliveryPreference,
+  testResultDeliveryPreference: TestResultDeliveryPreference
+): TestResultDeliveryPreference {
+  return [field, ALL].includes(testResultDeliveryPreference) ? field : NONE;
 }
