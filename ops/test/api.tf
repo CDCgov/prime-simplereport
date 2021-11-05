1ee4bbf33bcee92a202217b8fea9e2701cc3587f
Corinne Concordia <rin@skylight.digital>
2021-11-05 15:43:27 -0400

    [#2635] Add Hikari maximum connection pool size environment variables to TF configurations.
    This puts an instance of this variable in each of the associated environment files. We can replace this with associated keyvault entries once I get Azure access, if we would like more readily-accessible per-environment configuration.

--- ops/test/api.tf
+++ ops/test/api.tf
@@ -23,30 +23,31 @@ module "simple_report_api" {
   }
 
   app_settings = {
-    SPRING_PROFILES_ACTIVE                = "azure-test"
-    SPRING_DATASOURCE_URL                 = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sr_db_jdbc.id})"
-    OKTA_OAUTH2_CLIENT_SECRET             = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_client_secret.id})"
-    OKTA_API_KEY                          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_api_key_nonprod.id})"
-    APPLICATIONINSIGHTS_CONNECTION_STRING = data.azurerm_application_insights.app_insights.connection_string
-    AZ_REPORTING_QUEUE_CXN_STRING         = data.azurerm_storage_account.app.primary_connection_string
-    TWILIO_ACCOUNT_SID                    = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_account_sid.id})"
-    TWILIO_AUTH_TOKEN                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_auth_token.id})"
-    SIMPLE_REPORT_SENDGRID_API_KEY        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sendgrid_api_key.id})"
-    SMARTY_AUTH_ID                        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.smarty_auth_id.id})"
-    SMARTY_AUTH_TOKEN                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.smarty_auth_token.id})"
-    EXPERIAN_TOKEN_ENDPOINT               = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_token_endpoint.id})"
-    EXPERIAN_INITIAL_REQUEST_ENDPOINT     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_initial_request_endpoint.id})"
-    EXPERIAN_DOMAIN                       = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_domain.id})"
-    EXPERIAN_CLIENT_ID                    = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_client_id.id})"
-    EXPERIAN_CLIENT_SECRET                = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_client_secret.id})"
-    EXPERIAN_CC_SUBSCRIBER_SUBCODE        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_subscriber_subcode.id})"
-    EXPERIAN_CC_USERNAME                  = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_username.id})"
-    EXPERIAN_CC_PASSWORD                  = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_password.id})"
-    EXPERIAN_PID_TENANT_ID                = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_tenant_id.id})"
-    EXPERIAN_PID_CLIENT_REFERENCE_ID      = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_client_reference_id.id})"
-    EXPERIAN_PID_USERNAME                 = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_username.id})"
-    EXPERIAN_PID_PASSWORD                 = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_password.id})"
-    RS_QUEUE_CALLBACK_TOKEN               = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.report_stream_exception_callback_token.id})"
+    SPRING_PROFILES_ACTIVE                      = "azure-test"
+    SPRING_DATASOURCE_URL                       = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sr_db_jdbc.id})"
+    SPRING_DATASOURCE_HIKARI_MAXIMUM-POOL-SIZE  = 10
+    OKTA_OAUTH2_CLIENT_SECRET                   = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_client_secret.id})"
+    OKTA_API_KEY                                = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.okta_api_key_nonprod.id})"
+    APPLICATIONINSIGHTS_CONNECTION_STRING       = data.azurerm_application_insights.app_insights.connection_string
+    AZ_REPORTING_QUEUE_CXN_STRING               = data.azurerm_storage_account.app.primary_connection_string
+    TWILIO_ACCOUNT_SID                          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_account_sid.id})"
+    TWILIO_AUTH_TOKEN                           = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.twilio_auth_token.id})"
+    SIMPLE_REPORT_SENDGRID_API_KEY              = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sendgrid_api_key.id})"
+    SMARTY_AUTH_ID                              = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.smarty_auth_id.id})"
+    SMARTY_AUTH_TOKEN                           = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.smarty_auth_token.id})"
+    EXPERIAN_TOKEN_ENDPOINT                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_token_endpoint.id})"
+    EXPERIAN_INITIAL_REQUEST_ENDPOINT           = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_initial_request_endpoint.id})"
+    EXPERIAN_DOMAIN                             = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_domain.id})"
+    EXPERIAN_CLIENT_ID                          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_client_id.id})"
+    EXPERIAN_CLIENT_SECRET                      = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_client_secret.id})"
+    EXPERIAN_CC_SUBSCRIBER_SUBCODE              = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_subscriber_subcode.id})"
+    EXPERIAN_CC_USERNAME                        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_username.id})"
+    EXPERIAN_CC_PASSWORD                        = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_crosscore_password.id})"
+    EXPERIAN_PID_TENANT_ID                      = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_tenant_id.id})"
+    EXPERIAN_PID_CLIENT_REFERENCE_ID            = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_client_reference_id.id})"
+    EXPERIAN_PID_USERNAME                       = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_username.id})"
+    EXPERIAN_PID_PASSWORD                       = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.experian_preciseid_password.id})"
+    RS_QUEUE_CALLBACK_TOKEN                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.report_stream_exception_callback_token.id})"
     # true by default, can be disabled quickly here
     # SPRING_LIQUIBASE_ENABLED                       = "true"
     # this shadows/overrides an identical declaration in application.yaml
