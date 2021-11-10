7931c483f0a93dc148d681ac6a038dd65df3dad9
Nathan Carter <nathan@skylight.digital>
2021-11-10 16:25:06 -0500

    Fix a failing test by updating assertions to check state variable instead of jest.mock() to assert a function call

--- frontend/src/app/testResults/TestResultTextModal.test.tsx
+++ frontend/src/app/testResults/TestResultTextModal.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MockedProvider } from "@apollo/client/testing";
+import { act } from "react-dom/test-utils";
 
 import * as utils from "../utils";
 
@@ -11,25 +12,12 @@ import TestResultTextModal, {
 } from "./TestResultTextModal";
 
 const mockCloseModal = jest.fn();
-const mockResendTestResultsText = jest.fn();
-let mockResendSuccessValue = true;
 
 window.scrollTo = jest.fn();
 
 jest.mock("react-modal", () => (props: any) => {
   return <div>{props.children}</div>;
 });
-
-jest.mock("../../generated/graphql", () => ({
-  useSendSmsMutation: () => [
-    (options: any) => {
-      mockResendTestResultsText(options);
-      return Promise.resolve({
-        data: { sendPatientLinkSms: mockResendSuccessValue },
-      });
-    },
-  ],
-}));
 
 describe("TestResultTextModal", () => {
   let alertSpy: jest.SpyInstance;
@@ -87,6 +75,8 @@ describe("TestResultTextModal", () => {
   });
 
   describe("clicking on Send result button", () => {
+    let sendSmsMutationCalled = false;
+
     let mocks = [
       {
         request: {
@@ -130,10 +120,14 @@ describe("TestResultTextModal", () => {
             id: "e4c1c27f-768e-44d2-b9d5-e047454c1d24",
           },
         },
-        result: {
-          data: {
-            sendPatientLinkEmail: true,
-          },
+        result: () => {
+          sendSmsMutationCalled = true;
+
+          return {
+            data: {
+              sendPatientLinkSms: true,
+            },
+          };
         },
       },
     ];
@@ -150,11 +144,14 @@ describe("TestResultTextModal", () => {
 
       await new Promise((resolve) => setTimeout(resolve, 0));
 
-      userEvent.click(screen.getByText("Send result"));
-
-      expect(mockResendTestResultsText).toHaveBeenCalledWith({
-        variables: { patientLinkId: "e4c1c27f-768e-44d2-b9d5-e047454c1d24" },
+      act(() => {
+        userEvent.click(screen.getByText("Send result"));
       });
+
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      // Also implies that the mutation was called with the correct patient link ID
+      // as the mock would fail to intercept the request otherwise
+      expect(sendSmsMutationCalled).toBe(true);
 
       expect(alertSpy).toHaveBeenCalledWith("success", "Texted test results.");
       expect(mockCloseModal).toHaveBeenCalled();
