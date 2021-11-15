f71f4b2794c5e4b8b5ad0d2fc7556d97b81b3833
Nick Clyde <nick@clyde.tech>
2021-11-15 15:26:26 -0800

    Move default country check to component initialization (#2972)

--- frontend/src/app/patients/Components/PersonForm.tsx
+++ frontend/src/app/patients/Components/PersonForm.tsx
@@ -94,6 +94,10 @@ interface Props {
 const PersonForm = (props: Props) => {
   const [formChanged, setFormChanged] = useState(false);
   const [patient, setPatient] = useState(props.patient);
+  // Default country to USA if it's not set
+  if (patient.country === null) {
+    setPatient({ ...patient, country: "USA" });
+  }
   const [errors, setErrors] = useState<PersonErrors>({});
   const [addressModalOpen, setAddressModalOpen] = useState(false);
   const [addressSuggestion, setAddressSuggestion] = useState<
@@ -259,10 +263,6 @@ const PersonForm = (props: Props) => {
         showError(t("patient.form.errors.validationMsg"), error);
       });
       return;
-    }
-    // Default country to USA if it's not set
-    if (patient.country === null) {
-      setPatient({ ...patient, country: "USA" });
     }
     if (
       JSON.stringify(getAddress(patient)) ===
