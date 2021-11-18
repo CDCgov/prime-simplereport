f310340034b0ef9b78a4546c5469387ee194e305
Alis Akers <alismxwll@gmail.com>
2021-11-18 08:15:29 -0800

    update versions for terraform, azurerm, okta and pagerduty

--- ops/prod/_config.tf
+++ ops/prod/_config.tf
@@ -10,10 +10,11 @@ terraform {
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~>2.84.0"
     }
   }
-  required_version = ">= 0.15.1"
+  required_version = ">=1.0.11"
 }
 
 provider "azurerm" {
