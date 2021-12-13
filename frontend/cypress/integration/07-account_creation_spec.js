0a3484d111d3e1dd73e85eb69c7abf450fb68e54
Nick Clyde <nick@clyde.tech>
2021-12-13 12:49:56 -0800

    Configure retries for account creation tests

--- frontend/cypress/integration/07-account_creation_spec.js
+++ frontend/cypress/integration/07-account_creation_spec.js
@@ -56,154 +56,163 @@ Cypress.Commands.add("verifySecurityCode", (code) => {
   cy.get(submitButton).first().click();
 });
 
-describe("Okta account creation", () => {
-  // Since these tests interact with Okta, we need to use
-  // Wiremock to stub out the Okta API calls.
-  before(() => {
-    cy.clearCookies();
-    cy.task("downloadWiremock");
-    cy.restartWiremock("accountCreation");
-  });
-  beforeEach(() => {
-    // Cypress clears cookies by default, but for these tests
-    // we want to preserve the Spring session cookie
-    Cypress.Cookies.preserveOnce("SESSION");
-  });
-  after(() => {
-    cy.clearCookies();
-    cy.task("stopWiremock");
-  });
-  describe("Account creation w/ SMS MFA", () => {
-    it("navigates to the activation link", () => {
-      cy.visit("/uac/?activationToken=h971awbXda7y7jGaxN8f");
-      cy.contains("Create your password");
-    });
-    it("sets a password", () => {
-      cy.setPassword();
-    });
-    it("sets a security question", () => {
-      cy.setSecurityQuestion();
-    });
-    it("selects SMS MFA", () => {
-      cy.mfaSelect("sms");
-    });
-    it("enters a phone number", () => {
-      cy.enterPhoneNumber();
-    });
-    it("enters a verification code", () => {
-      cy.verifySecurityCode("033457");
-    });
-    it("displays a success message", () => {
-      cy.contains("Account set up complete");
-    });
-  });
-
-  describe("Account creation w/ Okta Verify MFA", () => {
+describe(
+  "Okta account creation",
+  {
+    retries: {
+      runMode: 2,
+      openMode: 1,
+    },
+  },
+  () => {
+    // Since these tests interact with Okta, we need to use
+    // Wiremock to stub out the Okta API calls.
     before(() => {
+      cy.clearCookies();
+      cy.task("downloadWiremock");
       cy.restartWiremock("accountCreation");
     });
-    it("navigates to the activation link", () => {
-      cy.visit("/uac/?activationToken=NOr20VqF5M6m8AnwcSUJ");
-      cy.contains("Create your password");
+    beforeEach(() => {
+      // Cypress clears cookies by default, but for these tests
+      // we want to preserve the Spring session cookie
+      Cypress.Cookies.preserveOnce("SESSION");
     });
-    it("sets a password", () => {
-      cy.setPassword();
+    after(() => {
+      cy.clearCookies();
+      cy.task("stopWiremock");
     });
-    it("sets a security question", () => {
-      cy.setSecurityQuestion();
+    describe("Account creation w/ SMS MFA", () => {
+      it("navigates to the activation link", () => {
+        cy.visit("/uac/?activationToken=h971awbXda7y7jGaxN8f");
+        cy.contains("Create your password");
+      });
+      it("sets a password", () => {
+        cy.setPassword();
+      });
+      it("sets a security question", () => {
+        cy.setSecurityQuestion();
+      });
+      it("selects SMS MFA", () => {
+        cy.mfaSelect("sms");
+      });
+      it("enters a phone number", () => {
+        cy.enterPhoneNumber();
+      });
+      it("enters a verification code", () => {
+        cy.verifySecurityCode("033457");
+      });
+      it("displays a success message", () => {
+        cy.contains("Account set up complete");
+      });
     });
-    it("selects Okta Verify MFA", () => {
-      cy.mfaSelect("okta");
-    });
-    it("'scans' a QR code", () => {
-      cy.scanQrCode();
-    });
-    it("enters a verification code", () => {
-      cy.verifySecurityCode("543663");
-    });
-    it("displays a success message", () => {
-      cy.contains("Account set up complete");
-    });
-  });
 
-  describe("Account creation w/ Google Authenticator MFA", () => {
-    before(() => {
-      cy.restartWiremock("accountCreation");
+    describe("Account creation w/ Okta Verify MFA", () => {
+      before(() => {
+        cy.restartWiremock("accountCreation");
+      });
+      it("navigates to the activation link", () => {
+        cy.visit("/uac/?activationToken=NOr20VqF5M6m8AnwcSUJ");
+        cy.contains("Create your password");
+      });
+      it("sets a password", () => {
+        cy.setPassword();
+      });
+      it("sets a security question", () => {
+        cy.setSecurityQuestion();
+      });
+      it("selects Okta Verify MFA", () => {
+        cy.mfaSelect("okta");
+      });
+      it("'scans' a QR code", () => {
+        cy.scanQrCode();
+      });
+      it("enters a verification code", () => {
+        cy.verifySecurityCode("543663");
+      });
+      it("displays a success message", () => {
+        cy.contains("Account set up complete");
+      });
     });
-    it("navigates to the activation link", () => {
-      cy.visit("/uac/?activationToken=gqYPzH1FlPzVr0U3tQ7H");
-      cy.contains("Create your password");
-    });
-    it("sets a password", () => {
-      cy.setPassword();
-    });
-    it("sets a security question", () => {
-      cy.setSecurityQuestion();
-    });
-    it("selects Google Authenticator MFA", () => {
-      cy.mfaSelect("google");
-    });
-    it("'scans' a QR code", () => {
-      cy.scanQrCode();
-    });
-    it("enters a verification code", () => {
-      cy.verifySecurityCode("985721");
-    });
-    it("displays a success message", () => {
-      cy.contains("Account set up complete");
-    });
-  });
 
-  describe("Account creation w/ Voice Call MFA", () => {
-    before(() => {
-      cy.restartWiremock("accountCreation");
+    describe("Account creation w/ Google Authenticator MFA", () => {
+      before(() => {
+        cy.restartWiremock("accountCreation");
+      });
+      it("navigates to the activation link", () => {
+        cy.visit("/uac/?activationToken=gqYPzH1FlPzVr0U3tQ7H");
+        cy.contains("Create your password");
+      });
+      it("sets a password", () => {
+        cy.setPassword();
+      });
+      it("sets a security question", () => {
+        cy.setSecurityQuestion();
+      });
+      it("selects Google Authenticator MFA", () => {
+        cy.mfaSelect("google");
+      });
+      it("'scans' a QR code", () => {
+        cy.scanQrCode();
+      });
+      it("enters a verification code", () => {
+        cy.verifySecurityCode("985721");
+      });
+      it("displays a success message", () => {
+        cy.contains("Account set up complete");
+      });
     });
-    it("navigates to the activation link", () => {
-      cy.visit("/uac/?activationToken=wN5mR-8SXao1TP2PLaFe");
-      cy.contains("Create your password");
-    });
-    it("sets a password", () => {
-      cy.setPassword();
-    });
-    it("sets a security question", () => {
-      cy.setSecurityQuestion();
-    });
-    it("selects voice call MFA", () => {
-      cy.mfaSelect("voice");
-    });
-    it("enters a phone number", () => {
-      cy.enterPhoneNumber();
-    });
-    it("enters a verification code", () => {
-      cy.verifySecurityCode("30835");
-    });
-    it("displays a success message", () => {
-      cy.contains("Account set up complete");
-    });
-  });
 
-  describe("Account creation w/ Email MFA", () => {
-    before(() => {
-      cy.restartWiremock("accountCreation");
+    describe("Account creation w/ Voice Call MFA", () => {
+      before(() => {
+        cy.restartWiremock("accountCreation");
+      });
+      it("navigates to the activation link", () => {
+        cy.visit("/uac/?activationToken=wN5mR-8SXao1TP2PLaFe");
+        cy.contains("Create your password");
+      });
+      it("sets a password", () => {
+        cy.setPassword();
+      });
+      it("sets a security question", () => {
+        cy.setSecurityQuestion();
+      });
+      it("selects voice call MFA", () => {
+        cy.mfaSelect("voice");
+      });
+      it("enters a phone number", () => {
+        cy.enterPhoneNumber();
+      });
+      it("enters a verification code", () => {
+        cy.verifySecurityCode("30835");
+      });
+      it("displays a success message", () => {
+        cy.contains("Account set up complete");
+      });
     });
-    it("navigates to the activation link", () => {
-      cy.visit("/uac/?activationToken=4OVwdVhc6M1I-UwvLrNX");
-      cy.contains("Create your password");
+
+    describe("Account creation w/ Email MFA", () => {
+      before(() => {
+        cy.restartWiremock("accountCreation");
+      });
+      it("navigates to the activation link", () => {
+        cy.visit("/uac/?activationToken=4OVwdVhc6M1I-UwvLrNX");
+        cy.contains("Create your password");
+      });
+      it("sets a password", () => {
+        cy.setPassword();
+      });
+      it("sets a security question", () => {
+        cy.setSecurityQuestion();
+      });
+      it("selects email MFA", () => {
+        cy.mfaSelect("email");
+      });
+      it("enters a verification code", () => {
+        cy.verifySecurityCode("007781");
+      });
+      it("displays a success message", () => {
+        cy.contains("Account set up complete");
+      });
     });
-    it("sets a password", () => {
-      cy.setPassword();
-    });
-    it("sets a security question", () => {
-      cy.setSecurityQuestion();
-    });
-    it("selects email MFA", () => {
-      cy.mfaSelect("email");
-    });
-    it("enters a verification code", () => {
-      cy.verifySecurityCode("007781");
-    });
-    it("displays a success message", () => {
-      cy.contains("Account set up complete");
-    });
-  });
-});
+  }
+);
