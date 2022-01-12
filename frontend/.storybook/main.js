0596681ff668e67de9e3f3f092ca6f621f2bcaca
Nick Clyde <nick@clyde.tech>
2022-01-12 15:45:21 -0800

    Storybook fixes

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-a11y",
     "@storybook/preset-create-react-app",
-
     {
       name: "@storybook/preset-scss",
       options: {
@@ -36,5 +35,8 @@ module.exports = {
       ],
     });
     return config;
+  },
+  core: {
+    builder: "webpack5",
   },
 };
