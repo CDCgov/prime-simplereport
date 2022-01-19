f7b0bb6bf43dcdc8dab072b42e28114d788edbf2
Rin Concordia <rin@skylight.digital>
2022-01-19 00:12:26 -0500

    Initial PoC of web application firewall for SimpleReport.

---
+++ ops/services/web_application_firewall/main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_web_application_firewall_policy" "example" {
+  name                = "${var.name}-wafpolicy"
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+
+  custom_rules {
+    name      = "IP_Allow"
+    priority  = 1
+    rule_type = "MatchRule"
+
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
+
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+    }
+
+    action = "Block"
+  }
+
+  custom_rules {
+    name      = "US_Only"
+    priority  = 2
+    rule_type = "MatchRule"
+
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
+      operator           = "GeoMatch"
+      negation_condition = true
+      match_values       = ["US"]
+    }
+    action = "Block"
+  }
+
+  managed_rules {
+    managed_rule_set {
+      type    = "OWASP"
+      version = "3.1"
+    }
+  }
+
+  policy_settings {
+    enabled                     = true
+    mode                        = "Prevention" //Can use "Detection" for testing, to see which requests would be blocked.
+    request_body_check          = true
+    file_upload_limit_in_mb     = 100
+    max_request_body_size_in_kb = 1024
+  }
+}
