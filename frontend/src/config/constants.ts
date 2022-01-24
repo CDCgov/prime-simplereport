dc3fa4d4de052650bb0a5c71ea33936ce0782ac5
emmastephenson <emma.stephenson.24@gmail.com>
2022-01-24 09:30:30 -0800

    preliminary changes for Canadian states/zip

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -105,6 +105,24 @@ export const states = {
 
 export const stateCodes = Object.keys(states);
 
+export const canadianProvinces = {
+  AB: "Alberta",
+  BC: "British Columbia",
+  MB: "Manitoba",
+  NB: "New Brunswick",
+  NL: "Newfoundland and Labrador",
+  NT: "Northwest Territories",
+  NS: "Nova Scotia",
+  NU: "Nunavut",
+  ON: "Ontario",
+  PE: "Prince Edward Island",
+  QC: "Quebec",
+  SK: "Saskatchewan",
+  YT: "Yukon",
+};
+
+export const canadianProvinceCodes = Object.keys(canadianProvinces);
+
 export const countries = {
   AFG: "Afghanistan",
   ALB: "Albania",
