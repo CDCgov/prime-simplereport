4ac4b1a94a1badae69ddf12ba669f0e530b8a338
Josh Dorothy <josh@skylight.digital>
2022-01-25 18:10:01 -0500

    Fix Azure provider at 2.92.0 due to broken upstream

--- ops/global/_config.tf
+++ ops/global/_config.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.93"
+      version = "~> 2.92.0"
     }
     okta = {
       source  = "okta/okta"
--- ops/training/_config.tf
+++ ops/training/_config.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.93"
+      version = "~> 2.92.0"
     }
   }
   required_version = "~> 1.1.4"
