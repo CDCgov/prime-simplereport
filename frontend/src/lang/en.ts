f78774a1d8692a990eaff683d8e957e4444b6515
Nathan Carter <nathan.andrew.carter@gmail.com>
2022-02-03 17:32:20 -0500

    Add obfuscated patient name to date of birth verification screen (#3305)
    * Add patient name to patient link DOB validation screen
    
    * Add controller for new `/pxp/patient-name` endpoint to return obfuscated patient name from a patient link
    
    * DOB verification page calls patient name endpoint on-load and populates state variable
    
    * Obfuscate patient names as first name & last initial, not the other way around
    
    * PXP controller to return obfuscated patient name throws an
    ExpiredPatientLinkException if patient link is expired
      - Add a unit test for this case
    
    PXP controller to return obfuscated patient name uses the @RequestParam
    annotation
    
    * `GET /pxp/patient-name` correctly responds with a 410 status code if
    patient link is expired
      - Shows alert in UI
    
    * Add loading state to DOB component to show loading card during initial API call
    
    * Update Spanish DOB verification text to indicate the links expire in 10 days (up from 5)
    
    * PXP obfuscated name endpoint controller throws IllegalArgumentException when passed an invalid UUID
    
    * Tweak e2e tests to correctly find patient name on DOB verification screen
    
    * Type query param for /pxp/patient-name endpoint as UUID to produce BadRequestExceptions for malformed IDs
    
    * Throw expired patient link exception before fetching relations
    
    * Remove patient link service method for returning obfuscated name; done in the controller instead

--- frontend/src/lang/en.ts
+++ frontend/src/lang/en.ts
@@ -568,9 +568,9 @@ export const en = {
         dateOfBirth: "Date of birth",
         enterDOB: "Enter your date of birth",
         enterDOB2:
-          "Enter your date of birth to access your COVID-19 testing portal.",
+          "Enter <0>{{personName}}</0>'s date of birth to access your COVID-19 testing portal.",
         linkExpirationNotice:
-          "Note: this link will expire 5 days after the test result was recorded.",
+          "Note: this link will expire 10 days after the test result was recorded.",
         format: "MM/DD/YYYY",
         invalidFormat: "Date of birth must be in MM/DD/YYYY format",
         invalidYear:
