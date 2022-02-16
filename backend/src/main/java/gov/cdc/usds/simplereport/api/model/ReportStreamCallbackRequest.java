b6439aa70393b8505e496f044da60425952fd571
Nathan Carter <nathan.andrew.carter@gmail.com>
2022-02-16 18:49:39 -0500

    Update ReportStream callback interface on backend

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/ReportStreamCallbackRequest.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/ReportStreamCallbackRequest.java
@@ -1,11 +1,9 @@
 package gov.cdc.usds.simplereport.api.model;
 
-import java.util.UUID;
 import lombok.Getter;
 
 @Getter
 public class ReportStreamCallbackRequest {
-  private UUID testEventInternalId;
   private Boolean isError;
-  private String details;
+  private String message;
 }
