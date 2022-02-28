9d47f1588e93d79cf5d3352b03ca3a2cc6e0b825
Rin Concordia <rin@skylight.digital>
2022-02-28 12:57:13 -0600

    Reduce request size back to 128.

--- ops/services/web_application_firewall/main.tf
+++ ops/services/web_application_firewall/main.tf
@@ -31,6 +31,6 @@ resource "azurerm_web_application_firewall_policy" "sr_waf_policy" {
     mode                        = "Detection" //Can use "Detection" for testing, to see which requests would be blocked. "Prevention" turns on active blocking.
     request_body_check          = true
     file_upload_limit_in_mb     = 100
-    max_request_body_size_in_kb = 1024 //Can go to 2000 in modern provider version. Proposed is 1024.
+    max_request_body_size_in_kb = 128 //Can go to 2000 in modern provider version. Proposed is 1024.
   }
 }
