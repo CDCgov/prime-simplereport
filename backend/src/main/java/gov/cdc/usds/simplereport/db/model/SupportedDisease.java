bb2a86fdb63f19fabcef744e9b6d79ce681c2310
emmastephenson <emma.stephenson.24@gmail.com>
2022-03-08 08:46:39 -0800

    fix join table

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/SupportedDisease.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/SupportedDisease.java
@@ -1,7 +1,8 @@
 package gov.cdc.usds.simplereport.db.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConstructorBinding;
@@ -19,10 +20,25 @@ public class SupportedDisease extends IdentifiedEntity {
   @Column(nullable = false)
   private String loinc;
 
+  @ManyToMany
+  @JoinTable(
+      name = "device_supported_disease",
+      joinColumns = @JoinColumn(name = "supported_disease_id"),
+      inverseJoinColumns = @JoinColumn(name = "device_type_id"))
+  private Set<DeviceType> deviceTypes = new HashSet<>();
+
   @ConstructorBinding
   public SupportedDisease(String name, String loinc) {
     this();
     this.name = name;
     this.loinc = loinc;
+  }
+
+  public void addDevice(DeviceType deviceType) {
+    this.deviceTypes.add(deviceType);
+  }
+
+  public Set<DeviceType> getDevices() {
+    return this.deviceTypes;
   }
 }
