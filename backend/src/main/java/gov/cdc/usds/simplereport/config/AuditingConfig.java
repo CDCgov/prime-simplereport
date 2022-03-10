51922c24c80bc3c00bf8ee3383f0d51f5dab42d9
Zedd Shmais <zedd@skylight.digital>
2022-03-09 20:16:38 -0600

    audit log json to splunk (#3537)
    * logging audit log to console and changed all logs to json
    
    * handled printing json exception
    
    * removed commented out config and cleaned code
    
    * added a audit log type identifier
    
    * sonar feedback
    
    * sonar feedback
    
    * more sonar changes
    
    * fixing to not alter the original ApiAuditEvent
    
    * removed requiredArgsConstructor
    
    * checkstyle feedback
    
    * trying to reduce code duplication
    
    * removed writing to the audit log table

--- backend/src/main/java/gov/cdc/usds/simplereport/config/AuditingConfig.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/AuditingConfig.java
@@ -1,10 +1,18 @@
 package gov.cdc.usds.simplereport.config;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
 import gov.cdc.usds.simplereport.db.model.ApiUser;
 import gov.cdc.usds.simplereport.service.ApiUserService;
+import gov.cdc.usds.simplereport.service.AuditService;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -23,5 +31,25 @@ public class AuditingConfig {
       Optional<ApiUser> user = Optional.of(_userService.getCurrentApiUserInContainedTransaction());
       return user;
     };
+  }
+
+  @Bean
+  public Logger jsonLogger(@Value("${logging.pattern.json-log}") String jsonPattern) {
+    LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+    PatternLayoutEncoder patternLayoutEncoder = new PatternLayoutEncoder();
+
+    patternLayoutEncoder.setPattern(jsonPattern);
+    patternLayoutEncoder.setContext(loggerContext);
+    patternLayoutEncoder.start();
+
+    ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
+    consoleAppender.setEncoder(patternLayoutEncoder);
+    consoleAppender.setContext(loggerContext);
+    consoleAppender.start();
+
+    Logger logger = (Logger) LoggerFactory.getLogger(AuditService.class);
+    logger.addAppender(consoleAppender);
+    logger.setAdditive(false);
+    return logger;
   }
 }
