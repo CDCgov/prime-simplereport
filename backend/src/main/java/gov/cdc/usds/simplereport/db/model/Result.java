ec28223c6fdcf0c845b69c61c631cd6a5fe42782
emmastephenson <emma.stephenson.24@gmail.com>
2022-03-14 13:02:00 -0700

    update results entity with database changes

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/Result.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/Result.java
@@ -2,13 +2,11 @@ package gov.cdc.usds.simplereport.db.model;
 
 import gov.cdc.usds.simplereport.api.Translators;
 import gov.cdc.usds.simplereport.db.model.auxiliary.TestResult;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Result extends EternalAuditedEntity {
 
   @ManyToOne(optional = false)
-  @JoinColumn(name = "test_event_id", nullable = false, updatable = false)
+  @JoinColumn(name = "test_event_id")
   private TestEvent testEvent;
 
   @ManyToOne(optional = false)
@@ -31,19 +29,17 @@ public class Result extends EternalAuditedEntity {
   @Column(name = "result", nullable = false)
   private String resultLOINC;
 
+  @Column(name = "test_result", nullable = false)
+  @Type(type = "pg_enum")
+  @Enumerated(EnumType.STRING)
+  private TestResult testResult;
+
   public Result(
       TestEvent testEvent, TestOrder testOrder, SupportedDisease disease, TestResult testResult) {
     this.testEvent = testEvent;
     this.testOrder = testOrder;
     this.disease = disease;
     this.resultLOINC = Translators.convertTestResultToLoinc(testResult);
-  }
-
-  public TestResult getTestResult() {
-    return Translators.convertLoincToResult(this.resultLOINC);
-  }
-
-  public void setTestResult(TestResult result) {
-    this.resultLOINC = Translators.convertTestResultToLoinc(result);
+    this.testResult = testResult;
   }
 }
