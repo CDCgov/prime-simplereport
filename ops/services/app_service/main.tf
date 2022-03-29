e08772c0515f25cdb7038053dc4ec1c3a4403f0f
Josh Dorothy <josh@skylight.digital>
2022-03-29 15:02:01 -0400

    Fix terraform fmt

--- ops/services/app_service/main.tf
+++ ops/services/app_service/main.tf
@@ -15,7 +15,7 @@ locals {
 }
 
 resource "azurerm_app_service_plan" "service_plan" {
-  name                = "${var.az_account}-appserviceplan-${var.env}"
+  name = "${var.az_account}-appserviceplan-${var.env}"
   // This is hard-coded for stg due to a specific issue with the combination of our stg RG and East US
   // not supporting V3 SKUs - other regions seem to work fine. This needs an Azure support ticket
   // to get back to the correct region.
