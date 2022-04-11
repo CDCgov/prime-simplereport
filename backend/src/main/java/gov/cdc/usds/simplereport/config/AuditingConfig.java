347fa66dd0f14c9a402a042f36c96d710af834c5
Josh Dorothy <joshdorothy@gmail.com>
2022-04-11 16:11:48 -0400

    Revert "Audit log to console and all logs as JSON (#3643)" (#3648)
    This reverts commit 99e92e8c66bed5b0de9d88d1cbada5d2a0a12f4a.

--- backend/src/main/java/gov/cdc/usds/simplereport/config/AuditingConfig.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/AuditingConfig.java
@@ -1,18 +1,10 @@
 package gov.cdc.usds.simplereport.config;
 
-import ch.qos.logback.classic.AsyncAppender;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.ConsoleAppender;
 import gov.cdc.usds.simplereport.db.model.ApiUser;
 import gov.cdc.usds.simplereport.service.ApiUserService;
-import gov.cdc.usds.simplereport.service.AuditService;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -22,14 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @Slf4j
 public class AuditingConfig {
-  private final ApiUserService _userService;
-
-  @Value("${logging.queueSize}")
-  private int queueSize;
-
-  public AuditingConfig(ApiUserService userService) {
-    this._userService = userService;
-  }
+  @Autowired private ApiUserService _userService;
 
   @Bean
   public AuditorAware<ApiUser> getCurrentApiUserProvider() {
@@ -38,33 +23,5 @@ public class AuditingConfig {
       Optional<ApiUser> user = Optional.of(_userService.getCurrentApiUserInContainedTransaction());
       return user;
     };
-  }
-
-  @Bean
-  public Logger jsonLogger(@Value("${logging.pattern.json-log}") String jsonPattern) {
-    LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-    PatternLayoutEncoder patternLayoutEncoder = new PatternLayoutEncoder();
-
-    patternLayoutEncoder.setPattern(jsonPattern);
-    patternLayoutEncoder.setContext(loggerContext);
-    patternLayoutEncoder.start();
-
-    ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
-    consoleAppender.setEncoder(patternLayoutEncoder);
-    consoleAppender.setContext(loggerContext);
-    consoleAppender.start();
-
-    AsyncAppender asyncAppender = new AsyncAppender();
-    // queueSize value chosen based on this:
-    // https://www.overops.com/blog/how-to-instantly-improve-your-java-logging-with-7-logback-tweaks/
-    asyncAppender.setQueueSize(queueSize);
-    asyncAppender.setContext(loggerContext);
-    asyncAppender.addAppender(consoleAppender);
-    asyncAppender.start();
-
-    Logger logger = (Logger) LoggerFactory.getLogger(AuditService.class);
-    logger.addAppender(asyncAppender);
-    logger.setAdditive(false);
-    return logger;
   }
 }
