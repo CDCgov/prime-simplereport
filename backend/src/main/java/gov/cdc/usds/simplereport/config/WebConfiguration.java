6eeaabdabadf558f85a67cd5df8ca3092759fbaf
Zedd Shmais <zedd@skylight.digital>
2022-04-12 17:12:26 -0500

    disable open in view

--- backend/src/main/java/gov/cdc/usds/simplereport/config/WebConfiguration.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/WebConfiguration.java
@@ -3,11 +3,8 @@ package gov.cdc.usds.simplereport.config;
 import gov.cdc.usds.simplereport.logging.PatientExperienceLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -34,19 +31,5 @@ public class WebConfiguration implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(_loggingInterceptor);
-  }
-
-  /**
-   * We want (at least for now) to be able to lazy-load JPA relationships in the graphql resolver,
-   * even after exiting service-layer transactions.
-   *
-   * <p>Since the graphql servlet does not register a handler with Spring, the Spring
-   * HandlerInterceptor does not work: instead, register a servlet filter with the same effect.
-   *
-   * @return
-   */
-  @Bean
-  public FilterRegistrationBean<OpenEntityManagerInViewFilter> openInViewFilter() {
-    return new FilterRegistrationBean<>(new OpenEntityManagerInViewFilter());
   }
 }
