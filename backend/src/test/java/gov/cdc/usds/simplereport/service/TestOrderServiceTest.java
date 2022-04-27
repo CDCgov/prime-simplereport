de41e88b6af94e92bef1842c77ab6a708e37fcc8
emmastephenson <emma.stephenson.24@gmail.com>
2022-04-27 15:00:35 -0700

    remove obsolete comment

--- backend/src/test/java/gov/cdc/usds/simplereport/service/TestOrderServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/TestOrderServiceTest.java
@@ -1364,7 +1364,6 @@ class TestOrderServiceTest extends BaseServiceTest<TestOrderService> {
 
     OrganizationLevelDashboardMetrics metrics =
         _service.getOrganizationLevelDashboardMetrics(startDate, endDate);
-    // these will need to be adjusted once the Date bug in makedata is fixed.
     assertEquals(3, metrics.getOrganizationPositiveTestCount());
     assertEquals(12, metrics.getOrganizationTotalTestCount());
     assertEquals(5, metrics.getOrganizationNegativeTestCount());
