3117a73e302d4001b45cbffa33b0b504eb64e56b
Rin Concordia <rin@skylight.digital>
2022-04-26 19:53:29 -0500

    Incorporate legacy db removal changes.

--- ops/dev2/metabase.tf
+++ ops/dev2/metabase.tf
@@ -1,6 +1,5 @@
 module "metabase_database" {
   source = "../services/metabase/database"
 
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  postgres_server_name = data.terraform_remote_state.persistent_dev2.outputs.postgres_server_name
+  postgres_server_id = data.terraform_remote_state.persistent_dev2.outputs.postgres_server_id
 }
