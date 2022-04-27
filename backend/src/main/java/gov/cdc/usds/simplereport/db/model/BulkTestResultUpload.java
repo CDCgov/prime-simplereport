c3a8976473c2e47315e13c1fccb8ccbf5f3e93a5
Daniel Sass <dsass@skylight.digital>
2022-04-27 16:09:30 -0400

    test result bulk upload - wip

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/BulkTestResultUpload.java
@@ -0,0 +1,43 @@
+package gov.cdc.usds.simplereport.db.model;
+
+import gov.cdc.usds.simplereport.db.model.auxiliary.UploadStatus;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Type;
+
+@Getter
+@Entity
+@Slf4j
+@Table(name = "upload")
+public class BulkTestResultUpload extends AuditedEntity {
+
+  @Column private UUID reportId;
+
+  @Column
+  @Type(type = "pg_enum")
+  @Enumerated(EnumType.STRING)
+  private UploadStatus Status;
+
+  @Column private int recordsCount;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "org_id")
+  private Organization organization;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "facility_id")
+  private Facility facility;
+
+  @Column private String warnings;
+
+  @Column private String errors;
+}
