1fc453dcb7e6fa5ec6b3a447544ec4c5883f9576
Stephen Nesman <stephen.nesman@semanticbits.com>
2022-04-28 15:29:28 -0400

    minor fixes based on sonarcloud issues findings

--- backend/src/main/java/gov/cdc/usds/simplereport/service/UploadService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/UploadService.java
@@ -83,11 +83,10 @@ public class UploadService {
 
   public String getRow(Map<String, String> row, String name, boolean isRequired) {
     String value = row.get(name);
-    if (!isRequired) {
-      return value;
-    }
-    if (value == null || value.trim().isEmpty()) {
-      throw new IllegalGraphqlArgumentException(name + " is required.");
+    if (isRequired) {
+      if (value == null || value.trim().isEmpty()) {
+        throw new IllegalGraphqlArgumentException(name + " is required.");
+      }
     }
     return value;
   }
