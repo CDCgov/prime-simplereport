fb9a18eb71f31044ca7a58958a25dea489263ca7
Josh Dorothy <josh@skylight.digital>
2022-04-28 23:27:57 -0400

    TF: remove old DBs and switch to new DBs (#3695)

--- ops/services/postgres_db/_output.tf
+++ ops/services/postgres_db/_output.tf
@@ -1,17 +1,11 @@
-// TODO - change these to azurerm_postgresql_flexible_server when removing the old DB config
-
 output "server_name" {
-  value = azurerm_postgresql_server.db.name
+  value = azurerm_postgresql_flexible_server.db.name
 }
 
 output "server_id" {
-  value = azurerm_postgresql_server.db.id
-}
-
-output "flexible_server_id" {
   value = azurerm_postgresql_flexible_server.db.id
 }
 
 output "server_fqdn" {
-  value = azurerm_postgresql_server.db.fqdn
+  value = azurerm_postgresql_flexible_server.db.fqdn
 }
