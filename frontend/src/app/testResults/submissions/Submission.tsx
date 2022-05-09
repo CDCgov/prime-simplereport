7c53ced2f07e61ed038fcae5f2a2c870f367e833
Nathan Carter <nathan.andrew.carter@gmail.com>
2022-05-09 17:07:21 -0400

    Implement Submission component for upload detail view (stubbing out GraphQL for now)

--- frontend/src/app/testResults/submissions/Submission.tsx
+++ frontend/src/app/testResults/submissions/Submission.tsx
@@ -1,42 +1,32 @@
 import { useParams } from "react-router-dom";
 import moment from "moment";
 
-import { LoadingCard } from "../../commonComponents/LoadingCard/LoadingCard";
-import { gql, useQuery } from "@apollo/client";
-import { UploadStatus, UploadSubmission } from "../../../generated/graphql";
+//import { LoadingCard } from "../../commonComponents/LoadingCard/LoadingCard";
+//import { gql, useQuery } from "@apollo/client";
 import { useSelector } from "react-redux";
+
+import {
+  UploadStatus,
+  UploadSubmission,
+} from "../../../generated/graphql";
 
 const Submission = () => {
   const urlParams = useParams();
-  const reportId = urlParams.id;
+  const reportId = urlParams.id || "";
 
-  /*
-  const GET_SUBMISSION = gql`
-    query GetSubmissionUpload($id: String!) {
-      uploadSubmission(id: $id) {
-        internalId
-        reportId
-        createdAt
-        status
-        recordsCount
-      }
-    }
-  `;
-*/
-
-const submission: UploadSubmission = {
-  reportId: "foobar",
-  internalId: "barfoo",
-  status: UploadStatus.Success,
-  recordsCount: "2",
-  createdAt: Date.now(),
-}
+  const submission: UploadSubmission = {
+    reportId: "12b86a9d-a9d6-4391-a555-6618e8ac66d9",
+    internalId: "barfoo",
+    status: UploadStatus.Success,
+    recordsCount: "2",
+    createdAt: Date.now(),
+  };
   const organization = useSelector(
     (state) => (state as any).organization as Organization
   );
 
   /*
-  const { data: submission, loading, error } = useQuery<UploadSubmission>(GET_SUBMISSION, {
+  const { data: submission, loading, error } = useGetUploadSubmissionQuery({
     fetchPolicy: "no-cache",
     variables: {
       id: reportId
@@ -50,47 +40,56 @@ const submission: UploadSubmission = {
   if (error) {
     throw error;
   }
-  */
+*/
 
   const transmissionTimestamp = moment(submission?.createdAt);
-  const transmissionDate = transmissionTimestamp.format("dd Mon YYYY");
-  const transmissionTime = transmissionTimestamp.format("hh:mm:ss a");
+  const transmissionDate = transmissionTimestamp.format("DD MMM YYYY");
+  const transmissionTime = transmissionTimestamp.format("k:mm");
 
   return (
     <main className="prime-home">
-      <div className="grid-container results-wide-container">
+      <div className="grid-container">
         <div className="grid-row">
           <div className="prime-container card-container">
-            {/*Submission header*/}
-            <div className="usa-card__header">
-              <h2>COVID-19 Submissions</h2>
+            {/* Sub-heading */}
+            <div className="usa-card__body text-normal font-body-xs text-base margin-bottom-0">
+              <span>
+                <a href="/results/upload/submission">
+                  {organization.name} COVID-19 Submissions
+                </a>
+              </span>
             </div>
 
-            {/*submissions table*/}
-            <div className="usa-card__body" title="filtered-result">
-              <table className="usa-table usa-table--borderless width-full">
-                <thead>{moment(submission?.createdAt).format("DD MMM YYYY k:mm")}
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>Report ID</th>
-                    <td>{submission?.reportId}</td>
-                  </tr>
-                  <tr>{organization.name}</tr>
-                  <tr>
-                    <th>Transmission Date</th>
-                    <td>{transmissionDate}</td>
-                  </tr>
-                  <tr>
-                    <th>Transmission Time</th>
-                    <td>{transmissionTime}</td>
-                  </tr>
-                  <tr>
-                    <th>Records</th>
-                    <td>{submission?.recordsCount}</td>
-                  </tr>
-                </tbody>
-              </table>
+            {/* Submission result header */}
+            <div className="usa-card__body">
+              <h1>{[transmissionDate, transmissionTime].join(" ")}</h1>
+            </div>
+
+            {/* Submission detail */}
+            <div className="usa-card__body">
+              <div className="display-flex flex-column margin-top-2 margin-bottom-2">
+                <span className="text-base">Report ID</span>
+                <td>{submission?.reportId}</td>
+              </div>
+              <div className="display-flex flex-column"></div>
+              <h2>{organization.name}</h2>
+              <div className="display-flex flex-column margin-bottom-4">
+                <span className="text-base">Data Stream</span>
+                <span>ELR</span>
+              </div>
+              <div className="display-flex flex-column margin-bottom-4">
+                <span className="text-base">Transmission Date</span>
+                <span>{transmissionDate}</span>
+              </div>
+
+              <div className="display-flex flex-column margin-bottom-4">
+                <span className="text-base">Transmission Time</span>
+                <span>{transmissionTime}</span>
+              </div>
+              <div className="display-flex flex-column margin-bottom-4">
+                <span className="text-base">Records</span>
+                <span>{submission.recordsCount}</span>
+              </div>
             </div>
           </div>
         </div>
