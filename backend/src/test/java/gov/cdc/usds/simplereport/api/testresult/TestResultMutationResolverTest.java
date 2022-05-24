cfa259c07d19caa2c1fe414b677a73b35fc3a176
Daniel Sass <dsass@skylight.digital>
2022-05-24 09:33:30 -0400

    TestResultMutationResolver Tests

--- backend/src/test/java/gov/cdc/usds/simplereport/api/testresult/TestResultMutationResolverTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/testresult/TestResultMutationResolverTest.java
@@ -7,11 +7,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import gov.cdc.usds.simplereport.api.model.errors.IllegalGraphqlArgumentException;
 import gov.cdc.usds.simplereport.db.model.TestResultUpload;
 import gov.cdc.usds.simplereport.db.model.auxiliary.UploadStatus;
 import gov.cdc.usds.simplereport.db.repository.TestEventRepository;
-import gov.cdc.usds.simplereport.service.DataHubClient;
 import gov.cdc.usds.simplereport.service.TestEventReportingService;
 import gov.cdc.usds.simplereport.service.TestResultUploadService;
 import gov.cdc.usds.simplereport.test_util.WireMockConfig;
@@ -23,7 +22,6 @@ import javax.servlet.http.Part;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -40,11 +38,6 @@ class TestResultMutationResolverTest {
   @MockBean private TestEventReportingService _mockReportingSvc;
   @MockBean private TestResultUploadService _mockUploadSvc;
 
-  @Autowired private TestResultUploadService testResultUploadService;
-  @Autowired private DataHubClient dataHubClient;
-  @Autowired private TestResultMutationResolver resolver;
-  @Autowired private WireMockServer wireMockServer;
-
   @Test
   void uploadResults_uploadExceptionThrown_graphqlErrorCaught() throws IOException {
     var input = mock(Part.class);
@@ -56,6 +49,19 @@ class TestResultMutationResolverTest {
     assertThat(caught)
         .asInstanceOf(InstanceOfAssertFactories.type(GraphQLError.class))
         .matches(e -> e.getErrorType() == ErrorType.ExecutionAborted);
+  }
+
+  @Test
+  void uploadResults_graphqlExceptionThrown_graphqlErrorCaught() throws IOException {
+    var input = mock(Part.class);
+    when(input.getInputStream()).thenThrow(new IllegalGraphqlArgumentException("wrong args bro"));
+
+    var sut = new TestResultMutationResolver(_mockRepo, _mockReportingSvc, _mockUploadSvc);
+    var caught = assertThrows(Throwable.class, () -> sut.uploadTestResultCSV(input));
+
+    assertThat(caught)
+        .asInstanceOf(InstanceOfAssertFactories.type(GraphQLError.class))
+        .matches(e -> e.getErrorType() == ErrorType.ValidationError);
   }
 
   @Test
