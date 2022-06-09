5a7319327d478405c27c25bcb974c478f12faacd
Nathan Carter <nathan.andrew.carter@gmail.com>
2022-06-09 17:24:33 -0400

    Can't have low coverage on code that doesn't exist! 🧠

--- backend/src/main/java/gov/cdc/usds/simplereport/service/model/reportstream/TokenResponse.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/model/reportstream/TokenResponse.java
@@ -1,26 +1,12 @@
 package gov.cdc.usds.simplereport.service.model.reportstream;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TokenResponse {
-  private String sub;
-
   @JsonProperty("access_token")
   private String accessToken;
-
-  @JsonProperty("token_type")
-  private String tokenType;
-
-  @JsonProperty("expires_in")
-  private Number expiresIn;
-
-  @JsonProperty("expires_at_seconds")
-  private Date expiresAtSeconds;
-
-  private String scope;
 }
