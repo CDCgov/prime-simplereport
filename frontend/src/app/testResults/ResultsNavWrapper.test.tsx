e662240071bee8fad96faebd34bc4048aa470d17
Zedd Shmais <zedd@skylight.digital>
2022-06-13 11:26:38 -0500

    Adding test results sub tabs (#3880)

---
+++ frontend/src/app/testResults/ResultsNavWrapper.test.tsx
@@ -0,0 +1,32 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import { appPermissions } from "../permissions";
+
+import ResultsNavWrapper from "./ResultsNavWrapper";
+
+describe("ResultsNav", () => {
+  it("displays the results sub nav when user has the permission", () => {
+    const { container } = render(
+      <MemoryRouter>
+        <ResultsNavWrapper
+          userPermissions={appPermissions.featureFlags.SrCsvUploaderPilot}
+        >
+          <>Testing 123</>
+        </ResultsNavWrapper>
+      </MemoryRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("doesnt display the results sub nav when user doesnt have the permission", () => {
+    const { container } = render(
+      <MemoryRouter>
+        <ResultsNavWrapper>
+          <>Testing 123</>
+        </ResultsNavWrapper>
+      </MemoryRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
