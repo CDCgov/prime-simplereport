0f0fbd7596dafbbb98337b39e2c654ee1c5b2a39
Bob Zhao <bob@skylight.digital>
2022-06-14 13:58:31 -0500

    add check for cypress env

--- frontend/src/app/commonComponents/Page/Page.tsx
+++ frontend/src/app/commonComponents/Page/Page.tsx
@@ -2,23 +2,33 @@ import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
+
 import TouchpointsButton from "../../analytics/TouchpointsButton";
 import { getUrl } from "../../utils/url";
 import USAGovBanner from "../USAGovBanner";
 
+declare global {
+  interface Window {
+    Cypress?: any; // don't really need strict type here
+  }
+}
+
 const Page: React.FC<{}> = ({ children }) => {
   // load touchpoints script
   useEffect(() => {
-    const script = document.createElement("script");
-    const urlPrefix = getUrl(true);
+    // don't load script when running in cypress
+    if (!window.Cypress) {
+      const script = document.createElement("script");
+      const urlPrefix = getUrl(true);
 
-    script.src =
-      process.env.PUBLIC_URL && urlPrefix.includes(process.env.PUBLIC_URL)
-        ? `${urlPrefix}static/touchpoints.js`
-        : "touchpoints.js";
-    script.async = true;
+      script.src =
+        process.env.PUBLIC_URL && urlPrefix.includes(process.env.PUBLIC_URL)
+          ? `${urlPrefix}static/touchpoints.js`
+          : "touchpoints.js";
+      script.async = true;
 
-    document.body.appendChild(script);
+      document.body.appendChild(script);
+    }
   }, []);
   return (
     <div className="App">
