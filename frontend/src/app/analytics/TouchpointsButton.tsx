33370fa4eadaf43830e58aae0d4e37f37afdde52
Bob Zhao <bob@skylight.digital>
2022-06-15 14:11:16 -0500

    remove unneeded space

--- frontend/src/app/analytics/TouchpointsButton.tsx
+++ frontend/src/app/analytics/TouchpointsButton.tsx
@@ -3,7 +3,6 @@ import "./TouchpointsButton.scss";
 const TouchpointsButton: React.FC = () => {
   return (
     <div className="touchpoints-wrapper sr-app-touchpoints-button">
-      {" "}
       How can we improve SimpleReport?
     </div>
   );
