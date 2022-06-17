c2ae392ec7bba953087f9d1c9fd87ef2376e0ac6
elisa lee <emyl3@users.noreply.github.com>
2022-06-17 16:14:56 -0500

    Add flu test results to downloads (#3849)

--- frontend/src/app/utils/symptoms.test.ts
+++ frontend/src/app/utils/symptoms.test.ts
@@ -1,6 +1,6 @@
-import { symptomsStringToArray } from "./symptoms";
+import { symptomsStringToArray, hasSymptomsForView } from "./symptoms";
 
-describe("symtomsStringToArray", () => {
+describe("symptomsStringToArray", () => {
   it("translates a string with zero symptoms to an array", () => {
     const symptomString =
       '{"64531003":"false","103001002":"false","84229001":"false","68235000":"false","426000000":"false","49727002":"false","68962001":"false","422587007":"false","267036007":"false","62315008":"false","43724002":"false","36955009":"false","44169009":"false","422400008":"false","230145002":"false","25064002":"false","162397003":"false"}';
@@ -20,5 +20,27 @@ describe("symtomsStringToArray", () => {
       "Fatigue",
       "Fever over 100.4F",
     ]);
+  });
+});
+
+describe("hasSymptomsForView", () => {
+  it("returns 'Yes' if there are symptoms present", () => {
+    const noSymptomsBoolean = false;
+    const symptomString =
+      '{"64531003":"true","103001002":"false","84229001":"true","68235000":"false","426000000":"false","49727002":"false","68962001":"false","422587007":"false","267036007":"false","62315008":"false","43724002":"false","36955009":"false","44169009":"false","422400008":"false","230145002":"false","25064002":"false","162397003":"false"}';
+    expect(hasSymptomsForView(noSymptomsBoolean, symptomString)).toEqual("Yes");
+  });
+  it("returns 'No' if there are no symptoms present", () => {
+    const noSymptomsBoolean = true;
+    const symptomString =
+      '{"64531003":"false","103001002":"false","84229001":"false","68235000":"false","426000000":"false","49727002":"false","68962001":"false","422587007":"false","267036007":"false","62315008":"false","43724002":"false","36955009":"false","44169009":"false","422400008":"false","230145002":"false","25064002":"false","162397003":"false"}';
+    expect(hasSymptomsForView(noSymptomsBoolean, symptomString)).toEqual("No");
+  });
+  it("returns 'Unknown' for all other scenarios", () => {
+    const noSymptomsBoolean = false;
+    const symptomString = '{"64531003":"badData"}';
+    expect(hasSymptomsForView(noSymptomsBoolean, symptomString)).toEqual(
+      "Unknown"
+    );
   });
 });
