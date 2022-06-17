c4f860fbfbddc4bbaadc0dee506ee8a75098acd6
Alis Akers <alismxwll@gmail.com>
2022-06-17 13:22:41 -0700

    fix typo

--- backend/db-setup/anonymize-db.sh
+++ backend/db-setup/anonymize-db.sh
@@ -12,6 +12,6 @@ docker compose exec db /usr/local/lib/snapshot-db.sh && sleep 1;
 
 echo "Anonymize the snapshot"
 # TODO use our own db image for this anonymization
-cat backend/db-setup/snapshots/* backend/db-setup/anonymize-db.sql | docker run --rm -i registry.gitlab.com/dalibo/postgresql_anonymizer /anon.sh > backend/db-setup/anon_dump.sql;s
+cat backend/db-setup/snapshots/* backend/db-setup/anonymize-db.sql | docker run --rm -i registry.gitlab.com/dalibo/postgresql_anonymizer /anon.sh > backend/db-setup/anon_dump.sql;
 
 echo "Anonymization complete. The anonymized dump is in backend/db-setup/anon_dump.sql"
