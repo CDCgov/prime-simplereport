27b16a9233e2b78413fe4c0f4e325276e32a1eac
Alis Akers <alismxwll@gmail.com>
2022-06-17 13:16:55 -0700

    update to anonymization script

--- backend/db-setup/anonymize-db.sh
+++ backend/db-setup/anonymize-db.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-echo "remove current snapshots"
-sudo rm backend/db-setup/snapshots/*
+echo "Cleaning up the directory for pg dump"
+sudo mkdir -p backend/db-setup/snapshots/;
+sudo touch backend/db-setup/snapshots/anonymize-db.tmp;
+sudo rm backend/db-setup/snapshots/*;
 
-echo "create a snapshot from our running db"
-docker compose exec db /usr/local/lib/snapshot-db.sh && sleep 1
+echo "Create a snapshot from our running db"
+docker compose exec db /usr/local/lib/snapshot-db.sh && sleep 1;
 
-echo "anonymize the snapshot"
+echo "Anonymize the snapshot"
 # TODO use our own db image for this anonymization
-cat backend/db-setup/snapshots/* backend/db-setup/anonymize-db.sql | docker run --rm -i registry.gitlab.com/dalibo/postgresql_anonymizer /anon.sh > backend/db-setup/anon_dump.sql
+cat backend/db-setup/snapshots/* backend/db-setup/anonymize-db.sql | docker run --rm -i registry.gitlab.com/dalibo/postgresql_anonymizer /anon.sh > backend/db-setup/anon_dump.sql;s
+
+echo "Anonymization complete. The anonymized dump is in backend/db-setup/anon_dump.sql"
