3578b6f23cfb468bd374f7d1e5636419ad708b03
Merethe Hansen <merethe@skylight.digital>
2022-06-21 11:44:32 -0700

    Fix status in upload list table

--- frontend/src/app/testResults/submissions/Submissions.tsx
+++ frontend/src/app/testResults/submissions/Submissions.tsx
@@ -68,7 +68,7 @@ const Submissions = () => {
           </td>
           <td>{formatDateWithTimeOption(submission.createdAt, true)}</td>
           <td>{submission.recordsCount}</td>
-          <td>{submission.status}</td>
+          <td>Success</td>
         </tr>
       );
     });
@@ -148,7 +148,7 @@ const Submissions = () => {
                     <th scope="col">Report ID</th>
                     <th scope="col">Date/Time submitted</th>
                     <th scope="col">Records</th>
-                    <th scope="col">Status</th>
+                    <th scope="col">Submission Status</th>
                   </tr>
                 </thead>
                 <tbody>{SubmissionsTableRows(submissions)}</tbody>
