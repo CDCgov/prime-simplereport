0e64021632294e00c648639bf7b0c271f05a7f60
Merethe Hansen <merethe@skylight.digital>
2022-07-12 09:37:38 -0700

    Uptake main element moved to ResultsNavWrapper

--- frontend/src/app/testResults/submissions/Submission.tsx
+++ frontend/src/app/testResults/submissions/Submission.tsx
@@ -16,13 +16,7 @@ const Submission = () => {
   });
 
   if (loading) {
-    return (
-      <main className="prime-home">
-        <div className="grid-container">
-          <p> Loading... </p>
-        </div>
-      </main>
-    );
+    return <p> Loading... </p>;
   }
 
   if (error) {
@@ -35,45 +29,41 @@ const Submission = () => {
   );
 
   return (
-    <main className="prime-home">
-      <div className="grid-container">
-        <div className="grid-row">
-          <div className="prime-container card-container">
-            {/* Sub-heading */}
-            <div className="usa-card__body text-normal font-body-xs text-base margin-bottom-0">
-              <span>
-                <LinkWithQuery
-                  to={`/results/upload/submissions/`}
-                  className="sr-link__primary"
-                >
-                  Return to spreadsheet upload history
-                </LinkWithQuery>
-              </span>
-            </div>
+    <div className="grid-row">
+      <div className="prime-container card-container">
+        {/* Sub-heading */}
+        <div className="usa-card__body text-normal font-body-xs text-base margin-bottom-0">
+          <span>
+            <LinkWithQuery
+              to={`/results/upload/submissions/`}
+              className="sr-link__primary"
+            >
+              Return to spreadsheet upload history
+            </LinkWithQuery>
+          </span>
+        </div>
 
-            {/* Submission detail */}
-            <div className="usa-card__body">
-              <div className="display-flex flex-column margin-top-2 margin-bottom-4">
-                <span className="text-base">Report ID</span>
-                <td>{submission?.uploadSubmission.reportId}</td>
-              </div>
-              <div className="display-flex flex-column margin-bottom-4">
-                <span className="text-base">Data stream</span>
-                <span>ELR</span>
-              </div>
-              <div className="display-flex flex-column margin-bottom-4">
-                <span className="text-base">Transmission date</span>
-                <span>{transmissionDate}</span>
-              </div>
-              <div className="display-flex flex-column margin-bottom-4">
-                <span className="text-base">Records</span>
-                <span>{submission?.uploadSubmission.recordsCount}</span>
-              </div>
-            </div>
+        {/* Submission detail */}
+        <div className="usa-card__body">
+          <div className="display-flex flex-column margin-top-2 margin-bottom-4">
+            <span className="text-base">Report ID</span>
+            <td>{submission?.uploadSubmission.reportId}</td>
+          </div>
+          <div className="display-flex flex-column margin-bottom-4">
+            <span className="text-base">Data stream</span>
+            <span>ELR</span>
+          </div>
+          <div className="display-flex flex-column margin-bottom-4">
+            <span className="text-base">Transmission date</span>
+            <span>{transmissionDate}</span>
+          </div>
+          <div className="display-flex flex-column margin-bottom-4">
+            <span className="text-base">Records</span>
+            <span>{submission?.uploadSubmission.recordsCount}</span>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
