3cf24741d327c44e4a4ec2b078000eb38b50b8e8
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-07-22 15:16:06 -0400

    remove comment

--- backend/src/main/java/gov/cdc/usds/simplereport/service/TestOrderService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/TestOrderService.java
@@ -96,7 +96,6 @@ public class TestOrderService {
       Date startDate,
       Date endDate) {
     return (root, query, cb) -> {
-      // DOES NOT ADD RESULT FROM RESULT TABLE -> STILL USES RESULT COLUMN!!!!!!!!
       Join<TestEvent, Result> resultJoin = root.join(TestEvent_.results);
       Join<TestEvent, TestOrder> order = root.join(TestEvent_.order);
       order.on(cb.equal(root.get(AuditedEntity_.internalId), order.get(TestOrder_.testEvent)));
