938e2a64906976dff0f0c7c9823402c9353cf060
Bob Zhao <bob@skylight.digital>
2022-08-16 09:08:55 -0500

    update test

--- frontend/src/app/patients/Components/ManageEmails.tsx
+++ frontend/src/app/patients/Components/ManageEmails.tsx
@@ -148,6 +148,7 @@ const ManageEmails: React.FC<Props> = ({
               <div className="flex-align-self-end">
                 <button
                   className="usa-button--unstyled padding-105 height-5"
+                  data-testid={`delete-email-${idx}`}
                   onClick={() => onEmailRemove(idx)}
                   aria-label={`Delete email ${email}`.trim()}
                 >
