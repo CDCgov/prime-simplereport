f30467be44dd61f8dd00cbd94c5ed5c731679adb
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-08-25 07:39:06 -0700

    put focus on the first aria-invalid item in the document on error.

--- frontend/src/app/signUp/Organization/OrganizationForm.tsx
+++ frontend/src/app/signUp/Organization/OrganizationForm.tsx
@@ -104,9 +104,12 @@ const OrganizationForm = () => {
         body="Please check the form to make sure you complete all of the required fields."
       />
     );
-    window.scrollTo(0, 0);
     showNotification(alert);
     setLoading(false);
+    let firstError = document.querySelector(
+      "[aria-invalid=true]"
+    ) as HTMLInputElement | null;
+    firstError?.focus();
   };
 
   if (orgExternalId) {
