7b0524103dcd5308a040f413b61970f778aaa00e
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-08-31 16:21:07 -0400

    add card tests

---
+++ frontend/src/app/commonComponents/Card/Card.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+
+import Card from "./Card";
+
+describe("card", () => {
+  describe("header", () => {
+    it("should contain a logo", () => {
+      render(<Card logo={true} />);
+      expect(screen.getByAltText("SimpleReport")).toBeInTheDocument();
+    });
+    it("should be hidden", () => {
+      render(<Card logo={true} ariaHidden={true} />);
+      expect(screen.getByAltText("SimpleReport")).toBeInTheDocument();
+      expect(screen.getByAltText("SimpleReport").parentElement).toHaveAttribute(
+        "aria-hidden",
+        "true"
+      );
+    });
+  });
+  describe("kicker", () => {
+    it("should exist", () => {
+      render(<Card bodyKicker={"body kicker"} />);
+      expect(screen.getByText("body kicker")).toBeInTheDocument();
+    });
+    it("should be centered", () => {
+      render(<Card bodyKicker={"body kicker"} bodyKickerCentered={true} />);
+      expect(screen.getByText("body kicker")).toBeInTheDocument();
+      expect(screen.getByText("body kicker").parentElement).toHaveClass(
+        "display-flex flex-column flex-align-center"
+      );
+    });
+  });
+});
