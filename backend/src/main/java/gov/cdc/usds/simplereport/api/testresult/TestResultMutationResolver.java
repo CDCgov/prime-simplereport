9f04ff33f7da69cde5b30761bebc14b4e9b66ece
Zedd Shmais <zedd@skylight.digital>
2022-09-13 09:21:24 -0500

    fix resendToReportStream graphql endpoint

--- backend/src/main/java/gov/cdc/usds/simplereport/api/testresult/TestResultMutationResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/testresult/TestResultMutationResolver.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,7 +25,7 @@ public class TestResultMutationResolver {
   private final TestEventReportingService testEventReportingService;
   private final TestResultUploadService testResultUploadService;
 
-  @QueryMapping
+  @MutationMapping
   public boolean resendToReportStream(@Argument List<UUID> testEventIds) {
     testEventRepository
         .findAllByInternalIdIn(testEventIds)
