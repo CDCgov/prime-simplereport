8c665d586fb2a29e0bcba5d733e70acf8e462699
Zedd Shmais <zedd@skylight.digital>
2022-09-22 09:35:46 -0500

    adding sonar feedback

--- frontend/src/app/testResults/uploads/Uploads.tsx
+++ frontend/src/app/testResults/uploads/Uploads.tsx
@@ -111,20 +111,18 @@ const Uploads = () => {
         );
       }
       const response = await res.json();
-      if (response) {
-        if (response?.reportId) {
-          setReportId(response?.reportId);
-        }
+      if (response?.reportId) {
+        setReportId(response?.reportId);
+      }
 
-        if (response?.errors?.length) {
-          setErrorMessageText(
-            "Please resolve the errors below and upload your edited file. Your file has not been accepted."
-          );
-        }
+      if (response?.errors?.length) {
+        setErrorMessageText(
+          "Please resolve the errors below and upload your edited file. Your file has not been accepted."
+        );
+      }
 
-        if (response?.errors && response.errors.length > 0) {
-          setErrors(response.errors);
-        }
+      if (response?.errors && response.errors.length > 0) {
+        setErrors(response.errors);
       }
     });
 
