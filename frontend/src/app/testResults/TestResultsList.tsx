7dd4c490fd097a50db608af9e615cfca90e031cd
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-10-03 15:06:25 -0400

    change query selector to get element by

--- frontend/src/app/testResults/TestResultsList.tsx
+++ frontend/src/app/testResults/TestResultsList.tsx
@@ -411,11 +411,11 @@ export const DetachedTestResultsList = ({
                 onClick={() => {
                   setDebounced("");
                   clearFilterParams();
-                  (document.querySelector(
-                    "input[id=start-date]"
+                  (document.getElementById(
+                    "start-date"
                   ) as HTMLInputElement).value = "";
-                  (document.querySelector(
-                    "input[id=end-date]"
+                  (document.getElementById(
+                    "end-date"
                   ) as HTMLInputElement).value = "";
                   setStartDateError("");
                   setEndDateError("");
