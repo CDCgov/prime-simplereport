501f12196c419bb3459c8bf44e103844230b2a8c
Zedd Shmais <zedd@skylight.digital>
2022-10-04 19:01:35 -0500

    add disclaimer to csv uploader (#4379)
    * add disclaimer to csv uploader
    
    * addressing lauras feedback
    
    * Update frontend/src/app/testResults/uploads/Uploads.tsx
    
    Co-authored-by: lauraykerr <lkerry@skylight.digital>
    
    * apply laura kerry's edits
    
    Co-authored-by: lauraykerr <lkerry@skylight.digital>

--- frontend/src/app/testResults/uploads/Uploads.tsx
+++ frontend/src/app/testResults/uploads/Uploads.tsx
@@ -140,6 +140,31 @@ const Uploads = () => {
         <div className="usa-card__header">
           <h2>Upload your results</h2>
         </div>
+        <div className="usa-alert usa-alert--info margin-left-105em margin-right-105em maxw-tablet-lg">
+          <div className="usa-alert__body">
+            <h4 className="usa-alert__heading">
+              What is the bulk results uploader?
+            </h4>
+            <p className="usa-alert__text">
+              <em>
+                This feature is in beta. That means it’s new, and we’ll continue
+                to update and improve it for our users. Though the feature is in
+                beta, it will route all the results you submit to the
+                appropriate public health department(s).
+              </em>{" "}
+              <br />
+              <br /> The results uploader allows you to report test results in
+              bulk using a CSV file. While we expect most SimpleReport users
+              will continue to report through the regular process, this feature
+              can serve labs and others with an information system that exports
+              spreadsheets, such as an EMR.{" "}
+              <LinkWithQuery to="/results/upload/submit/guide">
+                <strong>Learn more about how it works</strong>
+              </LinkWithQuery>
+              .
+            </p>
+          </div>
+        </div>
         <div className="usa-card__body padding-y-2 maxw-prose">
           <p>
             Report results in bulk using a comma-separated values (CSV)
