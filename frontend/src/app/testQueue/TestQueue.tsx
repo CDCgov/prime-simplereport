ac92d06217b37d94d545902a961e3809dcde2063
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-10-05 18:23:02 -0400

    rename main to div and add flex to TestQueue.tsx

--- frontend/src/app/testQueue/TestQueue.tsx
+++ frontend/src/app/testQueue/TestQueue.tsx
@@ -331,7 +331,7 @@ const TestQueue: React.FC<Props> = ({ activeFacilityId }) => {
   );
 
   return (
-    <main className="prime-home">
+    <div className="prime-home flex-1">
       <div className="grid-container queue-container-wide">
         <h1 className="font-sans-lg">Conduct tests</h1>
         <div className="position-relative">
@@ -345,7 +345,7 @@ const TestQueue: React.FC<Props> = ({ activeFacilityId }) => {
         </div>
         {createQueueItems(data.queue)}
       </div>
-    </main>
+    </div>
   );
 };
 
