7f8800343343cfefe7d41b5c985fb8d21ec62835
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-10-05 10:38:36 -0400

    replace main with div and add main-wrapper to extend style

--- frontend/src/app/Settings/Settings.tsx
+++ frontend/src/app/Settings/Settings.tsx
@@ -13,7 +13,7 @@ const Settings = () => {
   useDocumentTitle("Settings");
 
   return (
-    <main className="prime-home">
+    <div className="prime-home main-wrapper">
       <div className="grid-container">
         <SettingsNav />
         <Routes>
@@ -34,7 +34,7 @@ const Settings = () => {
           <Route path="/" element={<ManageUsersContainer />} />
         </Routes>
       </div>
-    </main>
+    </div>
   );
 };
 
