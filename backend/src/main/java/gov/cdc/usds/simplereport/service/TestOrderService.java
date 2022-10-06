43e772c0bdeeb0170b3d0fb3e0820409dc3ee367
Nathan Carter <nathan.andrew.carter@gmail.com>
2022-10-06 11:53:20 -0400

    Test corrections are re-opened with original submission date (#4361)
    * Set date_tested_backdate on all test corrections
    
    * Add unit tests for backdating test corrections
    
    * Had a null assertion in the wrong unit test - fixed
    
    * Missing import
    
    * Better date comparison in unit test

--- backend/src/main/java/gov/cdc/usds/simplereport/service/TestOrderService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/TestOrderService.java
@@ -612,6 +612,10 @@ public class TestOrderService {
       order.setCorrectionStatus(status);
       order.setReasonForCorrection(reasonForCorrection);
       order.markPending();
+
+      if (order.getDateTestedBackdate() == null) {
+        order.setDateTestedBackdate(event.getDateTested());
+      }
       _repo.save(order);
 
       return event;
