0781d5a86c25d3e79005a62bf1ac7190c2932840
Rin Concordia <rin@skylight.digital>
2022-10-11 12:50:56 -0500

    Add additional informational endpoint designations to prod.

--- ops/services/app_service/main.tf
+++ ops/services/app_service/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_app_service" "service" {
     min_tls_version  = "1.2"
     ftps_state       = "Disabled"
 
-    //TODO: Evaluate if this can be hit from somewhere else in Azure. If so, use a private link.
+    // TODO: Evaluate if this can be hit from somewhere else in Azure. If so, use a private link.
     // NOTE: If this code is removed, TF will not automatically delete it with the current provider version! It must be removed manually from the App Service -> Networking blade!
     ip_restriction {
       virtual_network_subnet_id = var.lb_subnet_id
