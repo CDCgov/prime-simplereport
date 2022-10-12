7746e8b48b6f27f6d98540b2fb11324889ad6280
Daniel Sass <dsass@skylight.digital>
2022-10-12 16:41:55 -0400

    adding some tests

---
+++ frontend/src/app/uploads/DeviceLookup/DeviceLookupContainer.test.tsx
@@ -0,0 +1,65 @@
+import { render } from "@testing-library/react";
+import { ToastContainer } from "react-toastify";
+
+import { SpecimenType } from "../../../generated/graphql";
+
+import DeviceLookupContainer from "./DeviceLookupContainer";
+
+const mockCreateDeviceType = jest.fn();
+
+jest.mock("../../../generated/graphql", () => {
+  return {
+    useGetSpecimenTypesQuery: () => {
+      return {
+        data: {
+          specimenTypes: [
+            {
+              internalId: "1234",
+              name: "NOSE POKE",
+              typeCode: "4321-a",
+            },
+          ] as SpecimenType[],
+        },
+      };
+    },
+    useGetDeviceTypeListQuery: () => {
+      return {
+        data: {
+          deviceTypes: [
+            {
+              internalId: "1",
+              name: "Abbott Tomorrow",
+              loincCode: "loinc-1",
+              manufacturer: "Abbott",
+              model: "A",
+              swabTypes: ["1234"],
+            },
+          ],
+        },
+      };
+    },
+  };
+});
+
+let container: any;
+
+describe("DeviceLookupContainer", () => {
+  beforeEach(() => {
+    container = render(
+      <>
+        <DeviceLookupContainer />
+        <ToastContainer
+          autoClose={5000}
+          closeButton={false}
+          limit={2}
+          position={"bottom-center"}
+          hideProgressBar={true}
+        />
+      </>
+    );
+  });
+
+  it("should render device lookup container", async () => {
+    expect(container).toMatchSnapshot();
+  });
+});
