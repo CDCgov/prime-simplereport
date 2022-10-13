db7d61ef858dfde8767eef6f3808fb911070688b
Daniel Sass <dsass@skylight.digital>
2022-10-13 11:54:05 -0400

    device lookup page

---
+++ frontend/src/app/uploads/DeviceLookup/DeviceLookup.tsx
@@ -0,0 +1,261 @@
+import React, { useEffect, useState } from "react";
+import { ComboBox } from "@trussworks/react-uswds";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faCopy } from "@fortawesome/free-solid-svg-icons";
+
+import TextInput from "../../commonComponents/TextInput";
+import { DeviceType } from "../../../generated/graphql";
+import LabeledText from "../../commonComponents/LabeledText";
+import Optional from "../../commonComponents/Optional";
+import "./DeviceLookup.scss";
+
+interface Device {
+  internalId?: string;
+  name: string;
+  manufacturer: string;
+  model: string;
+  loincCode: string;
+  swabTypes: Array<string>;
+}
+
+interface SwabType {
+  swabName: string;
+  typeCode: string;
+  internalId: string;
+}
+
+interface Props {
+  formTitle: string;
+  swabOptions: Array<SwabType>;
+  deviceOptions?: DeviceType[];
+}
+
+const DeviceLookup = (props: Props) => {
+  const [device, updateDevice] = useState<Device | undefined>();
+
+  const [copiedSlug, setCopiedSlug] = useState<string>();
+
+  useEffect(() => {
+    const timeout = copiedSlug
+      ? setTimeout(() => {
+          setCopiedSlug(undefined);
+        }, 3000)
+      : undefined;
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [copiedSlug]);
+
+  async function copySlug(slug: string) {
+    try {
+      await navigator.clipboard.writeText(slug);
+      setCopiedSlug(slug);
+    } catch (e: any) {
+      console.error(e);
+    }
+  }
+
+  const onChange = () => {};
+
+  const getDeviceOptions = () =>
+    props.deviceOptions
+      ? props.deviceOptions
+          .map((deviceType) => ({
+            label: deviceType.name,
+            value: deviceType.internalId,
+          }))
+          .sort((a, b) => a.label.localeCompare(b.label))
+      : [];
+
+  const getDeviceFromDeviceType = (device?: DeviceType): Device | undefined => {
+    return device
+      ? {
+          internalId: device.internalId,
+          name: device.name,
+          manufacturer: device.manufacturer,
+          model: device.model,
+          swabTypes: device.swabTypes?.map((swab) => swab.internalId),
+          loincCode: device.loincCode,
+        }
+      : undefined;
+  };
+
+  const getCopyToClipboardButton = (
+    copiedAttribute: string | undefined,
+    label: string
+  ) => {
+    return (
+      <div className="copy-button-container copy-button-tooltip">
+        {device && (
+          <button
+            className="usa-button usa-button--unstyled"
+            onClick={() => copiedAttribute && copySlug(copiedAttribute)}
+            aria-label={`${label}`}
+          >
+            <FontAwesomeIcon
+              icon={copiedSlug === copiedAttribute ? faCheck : faCopy}
+            />
+          </button>
+        )}
+        {device && copiedSlug === copiedAttribute && <CopyTooltip />}
+      </div>
+    );
+  };
+
+  return (
+    <main className="prime-home device-lookup-container">
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="prime-container card-container">
+            <div className="usa-card__header">
+              <div>
+                <h2>{props.formTitle}</h2>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              ></div>
+            </div>
+            <div className="usa-card__body margin-top-1">
+              {props.deviceOptions ? (
+                <div className="grid-row grid-gap">
+                  <div className="tablet:grid-col">
+                    <LabeledText label={"Select device"} />
+                    <ComboBox
+                      className="usa-combo-box__full-width"
+                      id="selectDevice"
+                      name="selectDevice"
+                      options={getDeviceOptions()}
+                      onChange={(id) => {
+                        updateDevice(
+                          getDeviceFromDeviceType(
+                            props.deviceOptions?.find(
+                              (d) => id === d.internalId
+                            )
+                          )
+                        );
+                      }}
+                      defaultValue={device?.internalId || ""}
+                    />
+                  </div>
+                </div>
+              ) : null}
+              <div className="grid-row grid-gap">
+                <div className="tablet:grid-col">
+                  <div className="text-field">
+                    <TextInput
+                      label="Equipment model name"
+                      name="Equipment model name"
+                      hintText={<code>equipment_model_name</code>}
+                      value={device?.model}
+                      onChange={onChange}
+                      disabled={true}
+                    />
+                    {getCopyToClipboardButton(
+                      device?.model,
+                      `Copy equipment model name for ${device?.name}`
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="grid-row grid-gap">
+                <div className="tablet:grid-col">
+                  <div className="text-field">
+                    <TextInput
+                      label="Test performed code "
+                      name="Test performed code "
+                      hintText={<code>test_performed_code</code>}
+                      value={device?.loincCode}
+                      onChange={onChange}
+                      disabled={true}
+                    />
+                    {getCopyToClipboardButton(
+                      device?.loincCode,
+                      `Copy Test performed code for ${device?.name}`
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="grid-row grid-gap">
+                <div className="tablet:grid-col">
+                  <div style={{ position: "relative" }}>
+                    <div className="usa-form-group">
+                      <label className="usa-label">
+                        <Optional label="Specimen Type" />
+                      </label>
+                      <span className="usa-hint">
+                        <code>specimen_type</code>
+                      </span>
+                      <table className={"usa-table"}>
+                        <tbody>
+                          {device &&
+                            props.swabOptions
+                              .filter((swab) =>
+                                device.swabTypes.includes(swab.internalId)
+                              )
+                              .map(({ swabName, typeCode }) => (
+                                <tr key={swabName}>
+                                  <td>{swabName}</td>
+                                  <td>
+                                    <div
+                                      style={{ position: "relative" }}
+                                      className="display-flex flex-justify"
+                                    >
+                                      <span>{typeCode}</span>
+                                      <div className={"copy-button-container"}>
+                                        {device && (
+                                          <button
+                                            className="usa-button usa-button--unstyled copy-button"
+                                            onClick={() =>
+                                              typeCode && copySlug(typeCode)
+                                            }
+                                            aria-label={`Copy SNOMED code for ${swabName} (${typeCode})`}
+                                          >
+                                            <FontAwesomeIcon
+                                              icon={
+                                                copiedSlug === typeCode
+                                                  ? faCheck
+                                                  : faCopy
+                                              }
+                                            />
+                                          </button>
+                                        )}
+                                        {device && copiedSlug === typeCode && (
+                                          <CopyTooltip />
+                                        )}
+                                      </div>
+                                    </div>
+                                  </td>
+                                </tr>
+                              ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DeviceLookup;
+
+const CopyTooltip = () => {
+  return (
+    <span
+      className="usa-tooltip__body usa-tooltip__body--right is-set is-visible"
+      style={{ right: 0, marginRight: -75 }}
+    >
+      Copied!
+    </span>
+  );
+};
