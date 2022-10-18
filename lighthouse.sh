88fde60e733fda0a1cd6a7359950946dd9b4ef37
George Hager <george@skylight.digital>
2022-10-18 13:43:07 -0400

    switch back to lhci

--- lighthouse.sh
+++ lighthouse.sh
@@ -12,6 +12,7 @@ echo "Waiting for backend to start at ${TEST_ENV}${BACKEND_URL_PATH}"
 http_response=0
 polls=0
 while [[ $http_response != "200" && $polls -lt 360 ]]; do
+  echo "checking backend health"
   ((polls++))
   sleep 1
   http_response=$(curl -skL -w "%{http_code}" "${TEST_ENV}${BACKEND_URL_PATH}")
@@ -27,6 +28,7 @@ echo "Waiting for frontend to start at ${TEST_ENV}${PUBLIC_URL}${FRONTEND_URL_PA
 result=0
 polls=0
 while [[ $result -ne 1 && $polls -lt 240 ]]; do
+  echo "checking frontend health"
   ((polls++))
   sleep 1
   result=$(curl -skL "${TEST_ENV}${PUBLIC_URL}${FRONTEND_URL_PATH}" | grep -c '<title>SimpleReport</title>')
@@ -42,17 +44,18 @@ echo 'Waiting two minutes in case something went wrong...'
 sleep 2m
 echo
 
+# start server command
 echo 'applesaredelicious'
 
 echo 'App is online!'
 echo
 
-echo 'Installing chromium and lighthouse...'
-apt-get install chromium
-npm install -g lighthouse
-echo
-
-echo 'Starting lighthouse...'
-echo
-mkdir lighthouse
-lighthouse https://localhost.simplereport.gov --output json --output-path lighthouse/output.json --chrome-flags="--headless"
+#echo 'Installing chromium and lighthouse...'
+#apt-get install chromium
+#npm install -g lighthouse
+#echo
+#
+#echo 'Starting lighthouse...'
+#echo
+#mkdir lighthouse -p
+#lighthouse https://localhost.simplereport.gov --output json --output-path lighthouse/output.json --chrome-flags="--headless"
