95d47db65a4923fc0cb0469f338ab53679e4dc1f
Boban <BobanL@users.noreply.github.com>
2022-10-20 12:04:51 -0400

    add optional chaining to tribal affiliation and set default to empty string (#4533)

--- frontend/src/app/utils/testResultCSV.ts
+++ frontend/src/app/utils/testResultCSV.ts
@@ -72,7 +72,8 @@ export function parseDataForCSV(data: any, multiplexEnabled: boolean) {
       "Patient gender": r.patient.gender,
       "Patient race": r.patient.race,
       "Patient ethnicity": r.patient.ethnicity,
-      "Patient tribal affiliation": r.patient.tribalAffiliation.join(", "),
+      "Patient tribal affiliation":
+        r.patient.tribalAffiliation?.join(", ") || "",
       "Patient is a resident in a congregate setting":
         r.patient.residentCongregateSetting,
       "Patient is employed in healthcare": r.patient.employedInHealthcare,
