abba28f47e2c89910a320a98951a3376552f98fe
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-10-24 16:39:14 -0400

    add styling to match designs

--- frontend/src/app/patients/UploadPatients.tsx
+++ frontend/src/app/patients/UploadPatients.tsx
@@ -13,73 +13,92 @@ const UploadPatients = () => {
   return (
     <div className={"prime-edit-patient prime-home flex-1"}>
       <div className={"grid-container margin-bottom-4"}>
-        <div className="patient__header">
+        <div className="patient__header padding-bottom-0">
           <AddPatientHeader />
         </div>
         <div className={"card"}>
-          1. Setup your spreadsheet
-          <div className="usa-alert usa-alert--info usa-alert--no-icon margin-left-105em margin-right-105em maxw-tablet-lg">
-            <div className="usa-alert__body">
-              <h3 className="usa-alert__heading">
-                Follow formatting guidelines
-              </h3>
-              <p className="usa-alert__text">
-                To upload patients in bulk, spreadsheets need to match
-                SimpleReport data requirements. Follow the Patient bulk upload
-                guide closely to format your CSV before uploading it below.
-              </p>
-            </div>
+          <span style={{ fontSize: "22px", marginTop: "20px" }}>
+            1. Setup your spreadsheet
+          </span>
+          <div
+            className={"usa-summary-box"}
+            style={{
+              marginTop: "24px",
+              marginBottom: "24px",
+              marginLeft: "20px",
+            }}
+          >
+            <h3 className="usa-alert__heading">Follow formatting guidelines</h3>
+            <p className="usa-alert__text">
+              To upload patients in bulk, spreadsheets need to match
+              SimpleReport data requirements. Follow the Patient bulk upload
+              guide closely to format your CSV before uploading it below.
+            </p>
           </div>
-          <div>
+          <div
+            style={{
+              marginLeft: "20px",
+            }}
+          >
             <Button>View patient bulk upload guide</Button>
             <Button variant={"outline"}>Download spreadsheet template</Button>
           </div>
-          <div>
+          <div
+            style={{ fontSize: "22px", marginTop: "56px", marginBottom: "8px" }}
+          >
             2. Would you like to import these patients to one facility OR all
             facilities?
           </div>
-          <div>
+          <div style={{ marginLeft: "20px" }}>
             If you plan to test patients at more than one facility, we recommend
             adding them to all facilities. You can't select multiple facilities
             individually.
           </div>
-          <RadioGroup
-            wrapperClassName="margin-top-1"
-            inputClassName={"usa-radio__input--tile"}
-            name="facilitySector"
-            legend="Select facility"
-            legendSrOnly
-            buttons={[
-              {
-                value: "current",
-                label: "One facility",
-              },
-              {
-                value: "all",
-                label: "All facilities",
-              },
-            ]}
-            // selectedRadio={selectedFacility}
-            onChange={() => {}}
-            variant="horizontal"
-          />
-          <div>3. Upload your spreadsheet.</div>
-          <div>
+          <div style={{ marginLeft: "20px" }}>
+            <RadioGroup
+              wrapperClassName="margin-top-1"
+              inputClassName={"usa-radio__input--tile"}
+              name="facilitySector"
+              legend="Select facility"
+              legendSrOnly
+              buttons={[
+                {
+                  value: "current",
+                  label: "One facility",
+                },
+                {
+                  value: "all",
+                  label: "All facilities",
+                },
+              ]}
+              // selectedRadio={selectedFacility}
+              onChange={() => {}}
+              variant="horizontal"
+            />
+          </div>
+          <div
+            style={{ fontSize: "22px", marginTop: "56px", marginBottom: "8px" }}
+          >
+            3. Upload your spreadsheet.
+          </div>
+          <div style={{ marginLeft: "20px" }}>
             The spreadsheet may take 10 or more minutes to upload. You do not
             need to stay on this page. We'll email you if you need to fix any
             errors or when the upload is complete.
           </div>
-          <FormGroup className="margin-bottom-3">
-            <FileInput
-              // key={fileInputResetValue}
-              id="upload-csv-input"
-              name="upload-csv-input"
-              aria-label="Choose CSV file"
-              accept="text/csv, .csv"
-              // onChange={(e) => handleFileChange(e)}
-              required
-            />
-          </FormGroup>
+          <div style={{ marginLeft: "20px", marginTop: "8px" }}>
+            <FormGroup className="margin-bottom-3">
+              <FileInput
+                // key={fileInputResetValue}
+                id="upload-csv-input"
+                name="upload-csv-input"
+                aria-label="Choose CSV file"
+                accept="text/csv, .csv"
+                // onChange={(e) => handleFileChange(e)}
+                required
+              />
+            </FormGroup>
+          </div>
           <div>
             <Button>Upload CSV file</Button>
           </div>
