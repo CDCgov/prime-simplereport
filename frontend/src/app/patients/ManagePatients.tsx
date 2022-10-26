f5e21e4512d1b4d4a19cf40e337964142dce7b4d
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-10-26 10:28:53 -0400

    remove unused id

--- frontend/src/app/patients/ManagePatients.tsx
+++ frontend/src/app/patients/ManagePatients.tsx
@@ -324,7 +324,6 @@ export const DetachedManagePatients = ({
                         },
                       },
                     ]}
-                    id={"1"}
                   />
                 ) : null}
               </div>
