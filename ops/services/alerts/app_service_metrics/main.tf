50e4bfea12755c20afc825bb29c93d7c8157c289
Daniel Sass <DanielSass@users.noreply.github.com>
2022-11-04 12:27:40 -0400

    CSV Uploader Error handling improvements (#4640)
    * add alerting for bulk result uploader when RS is down
    * update http codes for failure to match frontend expectations

--- ops/services/alerts/app_service_metrics/main.tf
+++ ops/services/alerts/app_service_metrics/main.tf
@@ -253,3 +253,33 @@ ${local.skip_on_weekends}
     action_group = var.action_group_ids
   }
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "bulk_results_upload" {
+  name                = "${var.env}-bulk_results_upload"
+  description         = "${local.env_title} alert when bulk uploads fail"
+  location            = data.azurerm_resource_group.app.location
+  resource_group_name = var.rg_name
+
+  action {
+    action_group = var.action_group_ids
+  }
+
+  data_source_id = var.app_insights_id
+  enabled        = contains(var.disabled_alerts, "bulk_results_upload") ? false : true
+
+  query = <<-QUERY
+requests
+| where true
+| where timestamp >= ago(6m)
+    and name == "/upload/results"
+    and toint(resultCode) >= 500
+  QUERY
+
+  severity    = 1
+  frequency   = 5
+  time_window = 6
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 0
+  }
+}
