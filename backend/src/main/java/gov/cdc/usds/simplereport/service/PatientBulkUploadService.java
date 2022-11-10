4fbccb843ecd41cfd03d125c8a4fa38e188f33cd
George Hager <george@skylight.digital>
2022-11-10 18:22:07 -0500

    fix backend linting 2

--- backend/src/main/java/gov/cdc/usds/simplereport/service/PatientBulkUploadService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/PatientBulkUploadService.java
@@ -25,7 +25,11 @@ import gov.cdc.usds.simplereport.validators.PatientBulkUploadFileValidator.Patie
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
