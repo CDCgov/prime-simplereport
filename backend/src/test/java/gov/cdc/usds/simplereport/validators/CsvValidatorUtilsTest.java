f281d32108777f92395969b8b64c9909a703ac24
Daniel Sass <DanielSass@users.noreply.github.com>
2022-11-14 10:42:11 -0500

    updating error messaging (#4688)
    improved error handling for malformed csv files for both the patient and results uploaders
    improved display of indices in the results upload UI

--- backend/src/test/java/gov/cdc/usds/simplereport/validators/CsvValidatorUtilsTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/validators/CsvValidatorUtilsTest.java
@@ -7,9 +7,19 @@ import static gov.cdc.usds.simplereport.validators.CsvValidatorUtils.validateEth
 import static gov.cdc.usds.simplereport.validators.CsvValidatorUtils.validatePhoneNumber;
 import static gov.cdc.usds.simplereport.validators.CsvValidatorUtils.validateZipCode;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.io.ContentReference;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
+import gov.cdc.usds.simplereport.api.model.errors.CsvProcessingException;
+import java.io.IOException;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class CsvValidatorUtilsTest {
 
@@ -63,6 +73,25 @@ class CsvValidatorUtilsTest {
     ValueOrError actual = getValue(row, "biological_sex", true);
     String expectedMessage = "biological_sex is a required column.";
     assertThat(actual.getError().get(0).getMessage()).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  void getNextRow_jsonException_throwsCsvProcessingException() throws IOException {
+    CsvProcessingException thrown;
+    try (var mockIterator = Mockito.mock(MappingIterator.class)) {
+      var mockContentRef = Mockito.mock(ContentReference.class);
+      RuntimeJsonMappingException exception = new RuntimeJsonMappingException("it broke");
+      var location = new JsonLocation(mockContentRef, 10, 1, 5);
+      when(mockIterator.next()).thenThrow(exception);
+      when(mockIterator.getCurrentLocation()).thenReturn(location);
+      thrown =
+          assertThrows(
+              CsvProcessingException.class, () -> CsvValidatorUtils.getNextRow(mockIterator));
+    }
+
+    assertEquals("it broke", thrown.getMessage());
+    assertEquals(1, thrown.getLineNumber());
+    assertEquals(5, thrown.getColumnNumber());
   }
 
   @Test
