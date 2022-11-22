501596b09a417807935b0142d34880f5697fd60e
Merethe Hansen <merethe@skylight.digital>
2022-11-22 12:58:47 -0800

    Make device lookup card same size as other pages

--- frontend/src/app/uploads/DeviceLookup/DeviceLookup.tsx
+++ frontend/src/app/uploads/DeviceLookup/DeviceLookup.tsx
@@ -91,59 +91,52 @@ const DeviceLookup = (props: Props) => {
   }, [selectedDevice]);
 
   return (
-    <div className="prime-home device-lookup-container flex-1">
+    <div className="device-lookup-container prime-container card-container">
       <ScrollToTopOnMount />
-      <div className="grid-container">
-        <div className="prime-container card-container">
-          <div className="usa-card__header">
-            <div>
-              <div className="display-flex flex-align-center">
-                <svg
-                  className="usa-icon text-base margin-left-neg-2px"
-                  aria-hidden="true"
-                  focusable="false"
-                  role="img"
-                >
-                  <use xlinkHref={iconSprite + "#arrow_back"}></use>
-                </svg>
-                <LinkWithQuery
-                  to={`/results/upload/submit/guide`}
-                  className="margin-left-05"
-                >
-                  Bulk results upload guide
-                </LinkWithQuery>
-              </div>
-              <div>
-                <h1 className="font-sans-lg">Device code lookup</h1>
-              </div>
-            </div>
-          </div>
-          <div className="usa-card__body margin-top-1">
-            <strong
-              className="display-block margin-bottom-1em"
-              id="select-device"
+      <div className="usa-card__header">
+        <div>
+          <div className="display-flex flex-align-center">
+            <svg
+              className="usa-icon text-base margin-left-neg-2px"
+              aria-hidden="true"
+              focusable="false"
+              role="img"
             >
-              Select device
-            </strong>
-            <div className="position-relative">
-              <SearchInput
-                onSearchClick={onSearchClick}
-                onInputChange={onInputChange}
-                queryString={debounced}
-                placeholder={"Search for a device"}
-                labeledBy="select-device"
-              />
-              <DeviceSearchResults
-                devices={searchDevices(props.deviceOptions, queryString)}
-                setSelectedDevice={setSelectedDevice}
-                shouldShowSuggestions={showDropdown}
-                loading={debounced !== queryString}
-                queryString={queryString}
-                dropDownRef={dropDownRef}
-              />
-              {selectedDevice && <DeviceDetails device={selectedDevice} />}
-            </div>
+              <use xlinkHref={iconSprite + "#arrow_back"}></use>
+            </svg>
+            <LinkWithQuery
+              to={`/results/upload/submit/guide`}
+              className="margin-left-05"
+            >
+              Bulk results upload guide
+            </LinkWithQuery>
           </div>
+          <div>
+            <h1 className="font-sans-lg">Device code lookup</h1>
+          </div>
+        </div>
+      </div>
+      <div className="usa-card__body margin-top-1">
+        <strong className="display-block margin-bottom-1em" id="select-device">
+          Select device
+        </strong>
+        <div className="position-relative">
+          <SearchInput
+            onSearchClick={onSearchClick}
+            onInputChange={onInputChange}
+            queryString={debounced}
+            placeholder={"Search for a device"}
+            labeledBy="select-device"
+          />
+          <DeviceSearchResults
+            devices={searchDevices(props.deviceOptions, queryString)}
+            setSelectedDevice={setSelectedDevice}
+            shouldShowSuggestions={showDropdown}
+            loading={debounced !== queryString}
+            queryString={queryString}
+            dropDownRef={dropDownRef}
+          />
+          {selectedDevice && <DeviceDetails device={selectedDevice} />}
         </div>
       </div>
     </div>
