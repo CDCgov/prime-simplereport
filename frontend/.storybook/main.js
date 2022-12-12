3b312db7111474896562d758a625251747701b03
Johanna <johanna@skylight.digital>
2022-12-12 16:41:25 -0500

    Fixed eslint check

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
   ],
-  // eslint-disable-next-line
   webpackFinal: async (config) => {
     config.resolve.alias["@microsoft/applicationinsights-react-js"] =
       require.resolve("../src/stories/__mocks__/appInsights.ts");
