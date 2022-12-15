3ad882928b4e786af270e3ad8d12a7896841edd7
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2022-12-15 09:48:16 -0500

    fix code smell

--- frontend/src/app/patients/PatientsNav.tsx
+++ frontend/src/app/patients/PatientsNav.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { LinkWithQuery } from "../commonComponents/LinkWithQuery";
 import { PATIENT_TERM } from "../../config/constants";
 
-const PatientsNav = () => {
-  function classNameByActive({ isActive }: { isActive: boolean }) {
-    return isActive ? "active" : "";
-  }
+function classNameByActive({ isActive }: { isActive: boolean }) {
+  return isActive ? "active" : "";
+}
 
+const PatientsNav = () => {
   return (
     <nav
       className="prime-secondary-nav padding-top-2"
