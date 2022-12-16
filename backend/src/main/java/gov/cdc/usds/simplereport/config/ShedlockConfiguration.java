e9dc3ff88a2fcb3567f125f7e341cd489ba88f95
Merethe Hansen <merethe@skylight.digital>
2022-12-15 16:43:01 -0800

    don't hardcode schema name

--- backend/src/main/java/gov/cdc/usds/simplereport/config/ShedlockConfiguration.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/ShedlockConfiguration.java
@@ -2,6 +2,7 @@ package gov.cdc.usds.simplereport.config;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,12 +11,16 @@ import javax.sql.DataSource;
 
 @Configuration
 public class ShedlockConfiguration {
+
+    @Value("${spring.jpa.properties.hibernate.default_schema}")
+    private String schemaName;
+
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
         return new JdbcTemplateLockProvider(
                 JdbcTemplateLockProvider.Configuration.builder()
                         .withJdbcTemplate(new JdbcTemplate(dataSource))
-                        .withTableName("simple_report.shedlock")
+                        .withTableName(schemaName + ".shedlock")
                         .usingDbTime() // lock provider will use UTC time based on the DB server clock instead of app server clocks
                         .build()
         );
