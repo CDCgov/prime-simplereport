1b0b84e2aa8fde327ef6647e1e2ed29ff742ab87
George Hager <89797785+georgehager@users.noreply.github.com>
2022-12-21 09:57:59 -0500

    Email notification for Patient Bulk Upload (#4852)
    * batch save for patients and phone numbers
    
    * fix backend linting 1
    
    * fix backend linting 2
    
    * remove unnecessary comments
    
    * move adding patient to list
    
    * cleanup
    
    * try request context
    
    * extract async service
    
    * fix java linting
    
    * try/catch scope
    
    * add new person constructor and test for phone numbers assignment
    
    * clean up code reuse
    
    * refactor person constructor and deduplicate persons
    
    * cleanup
    
    * add test for no phone type
    
    * try request context
    
    * extract async service
    
    * try/catch scope
    
    * remove unnecessary executor bean and friends
    
    * sync with latest changes from george/4662-batch-save-patients
    
    * have I mentioned how much I dislike rebasing
    
    * update async service with latest changes
    
    * testing wip
    
    * integration test wip
    
    * tests still failing, but different fails
    
    * tests wip
    
    * some service tests working
    
    * patientBulkUploadServiceTest updated
    
    * wip, tests use BaseFullStackTest
    
    * ugly but WORKING
    
    * tests passing; comments cleaned
    
    * update imports for bulkuploadasync
    
    * hopefully fix the frontend linter?
    
    * update hikari pool size
    
    * increase total database connections available
    
    * Modify db setup scripts as central location for increasing number of connections.
    
    * Add the ALTER SYSTEM call.
    
    * implement email notification for patient bulk upload
    
    * turn on jdbc batching
    
    * add naive batching for saves
    
    * fix code smells
    
    * take off order_inserts
    
    * fix duplicate checkiing
    
    * remove comment
    
    * optimize batching
    
    * rename to follow style guide, fix code smells
    
    * turn off sendgrid
    
    Co-authored-by: emmastephenson <emma.stephenson.24@gmail.com>
    Co-authored-by: Rin Concordia <rin@skylight.digital>

--- backend/src/test/java/gov/cdc/usds/simplereport/service/PatientBulkUploadServiceAsyncTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/PatientBulkUploadServiceAsyncTest.java
@@ -4,6 +4,8 @@ import static gov.cdc.usds.simplereport.api.Translators.parsePhoneType;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -13,12 +15,16 @@ import gov.cdc.usds.simplereport.db.model.Person;
 import gov.cdc.usds.simplereport.db.model.PhoneNumber;
 import gov.cdc.usds.simplereport.db.model.auxiliary.PersonRole;
 import gov.cdc.usds.simplereport.db.model.auxiliary.PhoneType;
+import gov.cdc.usds.simplereport.db.repository.PersonRepository;
 import gov.cdc.usds.simplereport.db.repository.PhoneNumberRepository;
+import gov.cdc.usds.simplereport.service.email.EmailProviderTemplate;
+import gov.cdc.usds.simplereport.service.email.EmailService;
 import gov.cdc.usds.simplereport.test_util.SliceTestConfiguration;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -27,6 +33,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.TestPropertySource;
@@ -48,6 +55,9 @@ class PatientBulkUploadServiceAsyncTest extends BaseGraphqlTest {
   @SpyBean PersonService _personService;
   @Autowired PhoneNumberRepository phoneNumberRepository;
 
+  @MockBean private EmailService _emailService;
+  @SpyBean private PersonRepository personRepository;
+
   public static final int PATIENT_PAGE_OFFSET = 0;
   public static final int PATIENT_PAGE_SIZE = 1000;
 
@@ -56,6 +66,8 @@ class PatientBulkUploadServiceAsyncTest extends BaseGraphqlTest {
 
   @BeforeEach
   void setupData() {
+    reset(personRepository);
+
     List<UUID> facilityIds =
         _orgService.getFacilities(_orgService.getCurrentOrganization()).stream()
             .map(Facility::getInternalId)
@@ -101,6 +113,12 @@ class PatientBulkUploadServiceAsyncTest extends BaseGraphqlTest {
     assertThat(phoneNumber.getNumber()).isEqualTo("410-867-5309");
     assertThat(phoneNumber.getType()).isEqualTo(PhoneType.MOBILE);
     assertThat(patient.getEmail()).isEqualTo("jane@testingorg.com");
+
+    verify(_emailService, times(1))
+        .sendWithDynamicTemplate(
+            List.of("bobbity@example.com"),
+            EmailProviderTemplate.SIMPLE_REPORT_PATIENT_UPLOAD,
+            Map.of("patients_url", "https://simplereport.gov/patients?facility=null"));
   }
 
   @Test
@@ -200,7 +218,8 @@ class PatientBulkUploadServiceAsyncTest extends BaseGraphqlTest {
   }
 
   @Test
-  void invalidData_throwsException() throws IOException, ExecutionException, InterruptedException {
+  void invalidData_throwsExceptionAndTriggersErrorEmail()
+      throws IOException, ExecutionException, InterruptedException {
     // GIVEN
     InputStream inputStream = loadCsv("patientBulkUpload/missingRequiredFields.csv");
     byte[] content = inputStream.readAllBytes();
@@ -212,6 +231,35 @@ class PatientBulkUploadServiceAsyncTest extends BaseGraphqlTest {
     // THEN
     assertThrows(ExecutionException.class, futurePatients::get);
     assertThat(fetchDatabasePatients()).isEmpty();
+
+    verify(_emailService, times(1))
+        .sendWithDynamicTemplate(
+            List.of("bobbity@example.com"),
+            EmailProviderTemplate.SIMPLE_REPORT_PATIENT_UPLOAD_ERROR,
+            Map.of("simplereport_url", "https://simplereport.gov/"));
+  }
+
+  @Test
+  void personSaveAllFailure_triggersErrorEmail()
+      throws IOException, ExecutionException, InterruptedException {
+    // GIVEN
+    InputStream inputStream = loadCsv("patientBulkUpload/valid.csv");
+    byte[] content = inputStream.readAllBytes();
+
+    doThrow(IllegalArgumentException.class).when(personRepository).saveAll(any());
+
+    // WHEN
+    CompletableFuture<Set<Person>> futurePatients =
+        this._service.savePatients(content, firstFacilityId);
+
+    // THEN
+    assertThrows(ExecutionException.class, futurePatients::get);
+
+    verify(_emailService, times(1))
+        .sendWithDynamicTemplate(
+            List.of("bobbity@example.com"),
+            EmailProviderTemplate.SIMPLE_REPORT_PATIENT_UPLOAD_ERROR,
+            Map.of("simplereport_url", "https://simplereport.gov/"));
   }
 
   @Test
