f10fd7447f503065497a8d92645abba1c00c0c26
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-01-18 06:49:06 -0800

    Revert "remove base service test from storage queue reporting test"
    This reverts commit 706d1f416053d437843e1ccfe340f0a9b32ab882.

--- backend/src/test/java/gov/cdc/usds/simplereport/service/AzureStorageQueueTestEventReportingServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/AzureStorageQueueTestEventReportingServiceTest.java
@@ -13,38 +13,18 @@ import com.azure.storage.queue.QueueAsyncClient;
 import com.azure.storage.queue.models.SendMessageResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.cdc.usds.simplereport.api.model.errors.TestEventSerializationFailureException;
-import gov.cdc.usds.simplereport.db.model.DeviceSpecimenType;
-import gov.cdc.usds.simplereport.db.model.DeviceType;
-import gov.cdc.usds.simplereport.db.model.Facility;
-import gov.cdc.usds.simplereport.db.model.Organization;
-import gov.cdc.usds.simplereport.db.model.Person;
-import gov.cdc.usds.simplereport.db.model.PhoneNumber;
-import gov.cdc.usds.simplereport.db.model.Provider;
-import gov.cdc.usds.simplereport.db.model.Result;
-import gov.cdc.usds.simplereport.db.model.SpecimenType;
-import gov.cdc.usds.simplereport.db.model.SupportedDisease;
 import gov.cdc.usds.simplereport.db.model.TestEvent;
-import gov.cdc.usds.simplereport.db.model.TestOrder;
-import gov.cdc.usds.simplereport.db.model.auxiliary.PersonName;
-import gov.cdc.usds.simplereport.db.model.auxiliary.PersonRole;
-import gov.cdc.usds.simplereport.db.model.auxiliary.PhoneType;
-import gov.cdc.usds.simplereport.db.model.auxiliary.StreetAddress;
-import gov.cdc.usds.simplereport.db.model.auxiliary.TestResult;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatcher;
-import org.springframework.test.util.ReflectionTestUtils;
 import reactor.core.publisher.Mono;
 
 @SuppressWarnings("unchecked")
-class AzureStorageQueueTestEventReportingServiceTest {
+class AzureStorageQueueTestEventReportingServiceTest
+    extends BaseServiceTest<TestEventReportingService> {
   private final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
 
   @Test
@@ -130,59 +110,19 @@ class AzureStorageQueueTestEventReportingServiceTest {
   }
 
   private TestEvent createTestEvent() {
-    var address = new StreetAddress(List.of("736 Jackson PI NW"), "Washington", "IL", "60614", "");
-    var deviceType = new DeviceType("name", "manufacturer", "model", "loinc", "nasal", 0);
-    var specimenType = new SpecimenType("name", "typeCode");
-    var deviceSpecimenType = new DeviceSpecimenType(deviceType, specimenType);
-    var provider =
-        new Provider(new PersonName("Michaela", null, "Quinn", ""), "1", address, "7735551235");
-    var organization = new Organization("District", "school", "1", true);
-    var facility =
-        new Facility(
-            organization,
-            "School",
-            "1",
-            address,
-            "7735551234",
-            "school@example.com",
-            provider,
-            deviceSpecimenType,
-            Collections.emptyList());
-    var person =
-        new Person(
-            organization,
-            null,
-            "Tracy",
-            null,
-            "Jordan",
-            null,
-            LocalDate.of(2022, 12, 13),
-            address,
-            "USA",
-            PersonRole.STUDENT,
-            List.of("tj@example.com"),
-            "black",
-            "not hispanic",
-            Collections.emptyList(),
-            "male",
-            false,
-            false,
-            "",
-            null);
-    var testOrder = new TestOrder(person, facility);
-    var testEvent =
-        new TestEvent(
-            testOrder,
-            false,
-            Set.of(
-                new Result(
-                    null, new SupportedDisease("COVID-19", "96741-4"), TestResult.POSITIVE)));
+    return createTestEvents(1).stream().findFirst().orElseThrow();
+  }
 
-    ReflectionTestUtils.setField(testEvent, "internalId", UUID.randomUUID());
-    ReflectionTestUtils.setField(testEvent, "createdAt", new Date());
-    ReflectionTestUtils.setField(
-        person, "phoneNumbers", List.of(new PhoneNumber(PhoneType.LANDLINE, "7735551234")));
+  private Set<TestEvent> createTestEvents(int count) {
+    var org = _dataFactory.saveValidOrganization();
+    var facility = _dataFactory.createValidFacility(org);
+    var patient = _dataFactory.createFullPerson(org);
 
-    return testEvent;
+    var events = new HashSet<TestEvent>();
+    for (int i = 0; i < count; i++) {
+      events.add(_dataFactory.createTestEvent(patient, facility));
+    }
+
+    return events;
   }
 }
