16e02a1ca170b83daf9511c09f794dfcd6f8f436
Merethe Hansen <merethe@skylight.digital>
2023-01-20 13:28:13 -0800

    Use Shedlock for account reminder emails (#4891)
    * Enable and configure Shedlock
    * switch to using scheduled annotation and shedlock for account reminder emails
    * remove old schedule bean
    * remove unused ScheduledTasksService class
    * Update ReminderService tests
    * increase heap size for tests

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/AdvisoryLockManager.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/AdvisoryLockManager.java
@@ -1,6 +1,5 @@
 package gov.cdc.usds.simplereport.db.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 
 /** A mix-in repository for entity repositories that manage processes that use advisory locks. */
@@ -15,22 +14,6 @@ public interface AdvisoryLockManager {
 
   int TEST_ORDER_LOCK_SCOPE =
       895417283; // another arbitrary number - this one specific to TestOrder
-
-  /**
-   * Take the advisory lock defined by the two arguments, waiting until the lock is available and
-   * releasing it at the end of the current transaction.
-   *
-   * @param lockCategory the high-level group of locks that contains the lock we are trying to
-   *     obtain (in this case, almost always {{@link #CORE_API_LOCK_SCOPE}).
-   * @param lock the specific lock we are trying to obtain (usually a constant in the implementing
-   *     repository or the service that calls it).
-   */
-  @Query(
-      nativeQuery = true,
-      // can't tell hibernate that Types.OTHER is a "void" result in this case:
-      // just cast it to text
-      value = "select cast(pg_advisory_xact_lock(:lockCategory, :lock) as text)")
-  void waitForTransactionLock(int lockCategory, int lock);
 
   /**
    * Attempt to take the advisory lock defined by the two arguments. If the lock is available,
--- backend/src/test/java/gov/cdc/usds/simplereport/service/ReminderServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/ReminderServiceTest.java
@@ -1,38 +1,44 @@
 package gov.cdc.usds.simplereport.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import gov.cdc.usds.simplereport.db.model.OrganizationQueueItem;
+import gov.cdc.usds.simplereport.service.email.EmailProviderTemplate;
+import gov.cdc.usds.simplereport.service.email.EmailService;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 class ReminderServiceTest extends BaseServiceTest<ReminderService> {
 
   @Autowired private JdbcTemplate _jdbc;
+  @MockBean private EmailService _emailService;
 
   @Test
-  void sendAccountReminderEmails_noEmails_success() {
-    Map<String, OrganizationQueueItem> orgEmailsSent = _service.sendAccountReminderEmails();
-    assertEquals(Set.of(), orgEmailsSent.keySet());
+  void sendAccountReminderEmails_noEmails_success() throws IOException {
+    _service.sendAccountReminderEmails();
+    verify(_emailService, never()).sendWithDynamicTemplate(anyString(), any());
   }
 
   @Test
-  void sendAccountReminderEmails_sendEmails_success() throws SQLException {
+  void sendAccountReminderEmails_sendEmails_success() throws SQLException, IOException {
     String email = "fake@example.org";
     OrganizationQueueItem unverifiedQueuedOrg =
         _dataFactory.saveOrganizationQueueItem("New Org Name", "NEW_ORG_NAME", email);
@@ -43,31 +49,27 @@ class ReminderServiceTest extends BaseServiceTest<ReminderService> {
         _dataFactory.saveOrganizationQueueItem("New Org Name", "NEW_ORG_NAME_AGAIN", email);
     backdateQueuedOrgCreatedAt(unverifiedQueuedOrg2);
 
-    Map<String, OrganizationQueueItem> orgEmailsSentMap = _service.sendAccountReminderEmails();
-    assertEquals(1, orgEmailsSentMap.keySet().size());
-
-    String remindedEmail = orgEmailsSentMap.keySet().iterator().next();
-    assertEquals(email, remindedEmail);
-
-    OrganizationQueueItem remindedOrg = orgEmailsSentMap.get(remindedEmail);
-    assertEquals(unverifiedQueuedOrg.getExternalId(), remindedOrg.getExternalId());
+    _service.sendAccountReminderEmails();
+    verify(_emailService, times(1)).sendWithDynamicTemplate(anyString(), any());
+    verify(_emailService, times(1))
+        .sendWithDynamicTemplate(
+            email, EmailProviderTemplate.ORGANIZATION_ID_VERIFICATION_REMINDER);
   }
 
   @Test
   void sendAccountReminderEmails_concurrencyLock_success()
-      throws InterruptedException, ExecutionException, SQLException {
+      throws InterruptedException, SQLException, IOException {
     OrganizationQueueItem unverifiedQueuedOrg = _dataFactory.saveOrganizationQueueItem();
     initAndBackdateUnverifiedQueuedOrg(unverifiedQueuedOrg);
 
     int n = 3;
-    List<Future<Map<String, OrganizationQueueItem>>> futures = new ArrayList<>();
+    List<Future<?>> futures = new ArrayList<>();
 
     ThreadPoolExecutor executor =
         new ThreadPoolExecutor(n, n, 1, TimeUnit.MINUTES, new ArrayBlockingQueue<>(n));
 
     for (int i = 0; i < n; i++) {
-      Future<Map<String, OrganizationQueueItem>> future =
-          executor.submit(() -> _service.sendAccountReminderEmails());
+      Future<?> future = executor.submit(() -> _service.scheduledSendAccountReminderEmails());
       futures.add(future);
     }
 
@@ -76,24 +78,12 @@ class ReminderServiceTest extends BaseServiceTest<ReminderService> {
 
     assertEquals(n, futures.size());
 
-    Map<String, OrganizationQueueItem> successResult = null;
-    int emptyResultCount = 0;
-
-    for (Future<Map<String, OrganizationQueueItem>> resultFuture : futures) {
-      Map<String, OrganizationQueueItem> result = resultFuture.get();
-      if (result.keySet().size() > 0) {
-        successResult = result;
-      } else {
-        emptyResultCount++;
-      }
-    }
-
-    // expect:
-    //   * 1 result with 1 key (the one that got the lock)
-    assertNotNull(successResult);
-    assertEquals(1, successResult.keySet().size());
-    //   * n-1 results with empty key set
-    assertEquals(n - 1, emptyResultCount);
+    // verify only 1 email sent
+    verify(_emailService, times(1)).sendWithDynamicTemplate(anyString(), any());
+    verify(_emailService, times(1))
+        .sendWithDynamicTemplate(
+            "org.queue.admin@example.com",
+            EmailProviderTemplate.ORGANIZATION_ID_VERIFICATION_REMINDER);
   }
 
   void initAndBackdateUnverifiedQueuedOrg(OrganizationQueueItem unverifiedQueuedOrg)
