c76d364166b4d6d4d20ba852cbfdb6104cab1d29
Johanna <johanna@skylight.digital>
2023-01-23 11:52:30 -0500

    Fixed cypress_backend_url setup

--- cypress/utils/request-utils.js
+++ cypress/utils/request-utils.js
@@ -1,1 +1,1 @@
-export const graphqlURL = `${Cypress.env('CYPRESS_BACKEND_URL') || 'http://localhost:8080'}/graphql`;
+export const graphqlURL = `${Cypress.env('BACKEND_URL') || 'http://localhost:8080'}/graphql`;
