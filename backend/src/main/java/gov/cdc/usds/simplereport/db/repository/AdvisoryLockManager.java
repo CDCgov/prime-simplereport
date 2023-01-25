1d646e9d9d9ad84de090bf478e7d4397dfaaf03d
Merethe Hansen <merethe@skylight.digital>
2023-01-25 08:36:03 -0800

    Remove unused DataHubUpload code (#5105)
    * remove unused datahubupload code

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/AdvisoryLockManager.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/AdvisoryLockManager.java
@@ -4,30 +4,12 @@ import org.springframework.data.jpa.repository.query.Procedure;
 
 /** A mix-in repository for entity repositories that manage processes that use advisory locks. */
 public interface AdvisoryLockManager {
-
   /**
-   * The "scope" constant that defines the category of all postgresql advisory locks this
-   * application should obtain. To be used as the first argument to postgresql two-argument lock
-   * functions.
+   * The "scope" constant that defines the category of the advisory locks this application should
+   * obtain when locking test orders. To be used as the first argument to postgresql two-argument
+   * lock functions.
    */
-  int CORE_API_LOCK_SCOPE = 110506458; // some arbitrary 32-bit number that defines "our" locks
-
-  int TEST_ORDER_LOCK_SCOPE =
-      895417283; // another arbitrary number - this one specific to TestOrder
-
-  /**
-   * Attempt to take the advisory lock defined by the two arguments. If the lock is available,
-   * return {@code true} and retain the lock until the end of the current transaction. If the lock
-   * is not available, return {@code false} immediately.
-   *
-   * @param lockCategory the high-level group of locks that contains the lock we are trying to
-   *     obtain (in this case, almost always {{@link #CORE_API_LOCK_SCOPE}).
-   * @param lock the specific lock we are trying to obtain (usually a constant in the implementing
-   *     repository or the service that calls it).
-   * @return true if the lock was obtained, false otherwise
-   */
-  @Procedure("pg_try_advisory_xact_lock")
-  boolean tryTransactionLock(int lockCategory, int lock);
+  int TEST_ORDER_LOCK_SCOPE = 895417283;
 
   @Procedure("pg_try_advisory_lock")
   boolean tryLock(int lockCategory, int lock);
