aa78adf5d6388f77d5c6d98bf255fd388f4ca2df
Johanna <johanna@skylight.digital>
2023-01-30 12:53:50 -0500

    Added common formatting to function files.

--- ops/services/app_functions/report_stream_batched_publisher/functions/FHIRTestEventReporter/dataHandlers.ts
+++ ops/services/app_functions/report_stream_batched_publisher/functions/FHIRTestEventReporter/dataHandlers.ts
@@ -1,33 +1,33 @@
-import {
-    DequeuedMessageItem,
-} from "@azure/storage-queue";
+import { DequeuedMessageItem } from "@azure/storage-queue";
 
 export type SimpleReportTestEvent = Record<any, any>;
 
-export type ProcessedTestEvents ={
-    testEvents: SimpleReportTestEvent[];
-    parseFailure: Record<string, boolean>;
-    parseFailureCount: number;
-    parseSuccessCount: number;
-}
-
-export function processTestEvents(messages: DequeuedMessageItem[]): ProcessedTestEvents {
-    const parseFailure: { [k: string]: boolean } = {};
-    let parseFailureCount = 0;
-    const testEventsAsJSON = messages
-        .map((m:DequeuedMessageItem) => {
-            try {
-                return JSON.parse(m.messageText);
-            } catch (e) {
-                parseFailure[m.messageId] = true;
-                parseFailureCount++;
-            }
-        });
-
-    return {
-        testEvents: testEventsAsJSON,
-        parseFailure,
-        parseFailureCount,
-        parseSuccessCount: testEventsAsJSON.length,
-    };
+export type ProcessedTestEvents = {
+  testEvents: SimpleReportTestEvent[];
+  parseFailure: Record<string, boolean>;
+  parseFailureCount: number;
+  parseSuccessCount: number;
 };
+
+export function processTestEvents(
+  messages: DequeuedMessageItem[]
+): ProcessedTestEvents {
+  const parseFailure: { [k: string]: boolean } = {};
+  let parseFailureCount = 0;
+  const testEventsAsJSON = messages.flatMap((m: DequeuedMessageItem) => {
+    try {
+      return [JSON.parse(m.messageText)];
+    } catch (e) {
+      parseFailure[m.messageId] = true;
+      parseFailureCount++;
+      return [];
+    }
+  });
+
+  return {
+    testEvents: testEventsAsJSON,
+    parseFailure,
+    parseFailureCount,
+    parseSuccessCount: testEventsAsJSON.length,
+  };
+}
