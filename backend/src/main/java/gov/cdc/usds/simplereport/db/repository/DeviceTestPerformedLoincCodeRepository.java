64923905d600c417447a6849527ccc0158080d40
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-02-01 11:17:29 -0700

    add method to find by device type internal ids

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTestPerformedLoincCodeRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/DeviceTestPerformedLoincCodeRepository.java
@@ -1,8 +1,13 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.DeviceTestPerformedLoincCode;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DeviceTestPerformedLoincCodeRepository
-    extends CrudRepository<DeviceTestPerformedLoincCode, UUID> {}
+    extends CrudRepository<DeviceTestPerformedLoincCode, UUID> {
+  List<DeviceTestPerformedLoincCode> findAllByDeviceTypeInternalIdIn(
+      Set<UUID> deviceTypeInternalIds);
+}
