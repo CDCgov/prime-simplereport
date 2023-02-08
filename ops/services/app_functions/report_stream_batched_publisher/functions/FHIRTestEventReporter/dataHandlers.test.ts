005d6ba2a7728503d82a2fcfb47e4f1da061172f
Johanna <johanna@skylight.digital>
2023-02-08 09:04:32 -0500

    Added unit testing for ndjson converter

--- ops/services/app_functions/report_stream_batched_publisher/functions/FHIRTestEventReporter/dataHandlers.test.ts
+++ ops/services/app_functions/report_stream_batched_publisher/functions/FHIRTestEventReporter/dataHandlers.test.ts
@@ -1,4 +1,4 @@
-import { processTestEvents } from "./dataHandlers";
+import { processTestEvents, serializeTestEventsAsNdjson } from "./dataHandlers";
 import { DequeuedMessageItem } from "@azure/storage-queue";
 
 describe("data handlers", () => {
@@ -31,6 +31,18 @@ describe("data handlers", () => {
       expect(processedEvents).toHaveProperty("parseFailure", { "1": true });
       expect(processedEvents).toHaveProperty("parseFailureCount", 1);
       expect(processedEvents).toHaveProperty("parseSuccessCount", 0);
+    });
+  });
+
+  describe('serializeTestEventsAsNdjson',()=>{
+    it('returns empty string for empty array',()=>{
+      expect(serializeTestEventsAsNdjson([])).toStrictEqual("");
+    });
+    it('transform array with one event and returns string with no [enter] at the end',()=>{
+      expect(serializeTestEventsAsNdjson([{"foo":"bar"}])).toStrictEqual("{\"foo\":\"bar\"}");
+    });
+    it('transform array with multiple events and returns string with no [enter] at the end',()=>{
+      expect(serializeTestEventsAsNdjson([{"foo":"bar"},{"foo":"123"}])).toStrictEqual("{\"foo\":\"bar\"}\n{\"foo\":\"123\"}");
     });
   });
 });
