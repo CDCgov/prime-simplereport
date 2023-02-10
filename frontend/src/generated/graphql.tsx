34f8b34fb86a47d28b3d121cdae1858de8afc569
Rin Concordia <rin@skylight.digital>
2023-02-10 12:59:41 -0600

    Yarn codegen.

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -7,13 +7,9 @@ export type Exact<T extends { [key: string]: unknown }> = {
   [K in keyof T]: T[K];
 };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  {
-    [SubKey in K]?: Maybe<T[SubKey]>;
-  };
+  { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  {
-    [SubKey in K]: Maybe<T[SubKey]>;
-  };
+  { [SubKey in K]: Maybe<T[SubKey]> };
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
