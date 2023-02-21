354cb5cd5bde59cd6c7b188d0411dc40b9bb6cb8
Alis Akers <alismxwll@gmail.com>
2023-02-21 12:01:41 -0800

    upgrade azurerm provider

--- ops/pentest/persistent/_config.tf
+++ ops/pentest/persistent/_config.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.34"
+      version = "~> 3.44"
     }
     okta = {
       source  = "okta/okta"
