b44a81a359193064b05e4448805214525c9f4919
Johanna <103958711+johanna-skylight@users.noreply.github.com>
2023-03-01 13:28:05 -0500

    Johanna/fixes apollo client upgrade (#5366)
    * Bump @apollo/client from 3.7.3 to 3.7.9 in /frontend
    
    Bumps [@apollo/client](https://github.com/apollographql/apollo-client) from 3.7.3 to 3.7.9.
    - [Release notes](https://github.com/apollographql/apollo-client/releases)
    - [Changelog](https://github.com/apollographql/apollo-client/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/apollographql/apollo-client/compare/v3.7.3...v3.7.9)
    
    ---
    updated-dependencies:
    - dependency-name: "@apollo/client"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Reworked logic to download file with test results.
    
    * Adding eslint inline exception
    
    * Added unit testing and removed unnecessary loading state
    
    * fixing codegen checks
    
    * Fixed formatting
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- frontend/src/app/utils/hooks.tsx
+++ frontend/src/app/utils/hooks.tsx
@@ -1,10 +1,3 @@
-import {
-  DocumentNode,
-  OperationVariables,
-  QueryHookOptions,
-  QueryResult,
-  useQuery,
-} from "@apollo/client";
 import React, { useEffect, useRef } from "react";
 
 const useOutsideClick = (
@@ -46,31 +39,4 @@ const useDocumentTitle = (title: string, retainOnUnmount = false) => {
   }, [retainOnUnmount, title]);
 };
 
-/**
- * Small wrapper around `useQuery` so that we can use it imperatively.
- *
- * @see Credit: https://github.com/apollographql/react-apollo/issues/3499#issuecomment-610649667
- *
- * @example
- * const callQuery = useImperativeQuery(query, options)
- * const handleClick = async () => {
- *   const{ data, error } = await callQuery()
- * }
- */
-function useImperativeQuery<TData = any, TVariables = OperationVariables>(
-  query: DocumentNode,
-  options: QueryHookOptions<TData, TVariables> = {}
-): QueryResult<TData, TVariables>["refetch"] {
-  const { refetch } = useQuery<TData, TVariables>(query, {
-    ...options,
-    skip: true,
-  });
-
-  const imperativelyCallQuery = (queryVariables: TVariables) => {
-    return refetch(queryVariables);
-  };
-
-  return imperativelyCallQuery;
-}
-
-export { useOutsideClick, useDocumentTitle, useImperativeQuery };
+export { useOutsideClick, useDocumentTitle };
