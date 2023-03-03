1da86a9a494490d9dfc447e05c406648f45974cc
Boban Ljuljdjurovic <boban@skylight.digital>
2023-03-03 15:01:54 -0500

    update wiremock

--- cypress/support/wiremock/download-wiremock.sh
+++ cypress/support/wiremock/download-wiremock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ ! -f "wiremock-jre8-standalone-2.29.1.jar" ]]; then
-  curl -s -O https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/2.29.1/wiremock-jre8-standalone-2.29.1.jar
+if [[ ! -f "wiremock-jre8-standalone-2.34.0.jar" ]]; then
+  curl -s -O https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/2.34.0/wiremock-jre8-standalone-2.34.0.jar
 fi
