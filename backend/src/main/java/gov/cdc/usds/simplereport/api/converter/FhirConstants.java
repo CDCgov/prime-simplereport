2da871395645fad8780c9073dd93ef933072ffee
Merethe Hansen <merethe@skylight.digital>
2023-03-09 10:08:02 -0800

    Add AoE answers to FHIR bundle (#5411)

--- backend/src/main/java/gov/cdc/usds/simplereport/api/converter/FhirConstants.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/converter/FhirConstants.java
@@ -24,6 +24,8 @@ public class FhirConstants {
       "http://terminology.hl7.org/CodeSystem/v3-TribalEntityUS";
   public static final String SNOMED_CODE_SYSTEM = "http://snomed.info/sct";
   public static final String LOINC_CODE_SYSTEM = "http://loinc.org";
+  public static final String YESNO_CODE_SYSTEM = "http://terminology.hl7.org/ValueSet/v2-0136";
+
   public static final String EVENT_TYPE_CODE_SYSTEM =
       "http://terminology.hl7.org/CodeSystem/v2-0003";
   public static final String EVENT_TYPE_CODE = "R01";
@@ -31,6 +33,10 @@ public class FhirConstants {
       "ORU/ACK - Unsolicited transmission of an observation message";
   public static final String DEFAULT_COUNTRY = "USA";
   public static final String UNIVERSAL_ID_SYSTEM = "http://terminology.hl7.org/CodeSystem/v2-0301";
+
+  public static final String LOINC_AOE_IDENTIFIER = "81959-9";
+  public static final String LOINC_AOE_SYMPTOMATIC = "95419-8";
+  public static final String LOINC_AOE_SYMPTOM_ONSET = "11368-8";
 
   public static final Map<String, String> PROCESSING_ID_DISPLAY =
       Map.of(
