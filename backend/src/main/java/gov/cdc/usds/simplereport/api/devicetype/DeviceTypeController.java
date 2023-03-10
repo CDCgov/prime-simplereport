13a67119f9aefb42f2409b785e982781206d1529
Zedd Shmais <zedd@skylight.digital>
2023-03-10 14:31:22 -0600

    clean up

--- backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
@@ -1,11 +1,15 @@
 package gov.cdc.usds.simplereport.api.devicetype;
 
+import static gov.cdc.usds.simplereport.config.BeanProfiles.PROD;
+
 import gov.cdc.usds.simplereport.service.DeviceTypeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("!" + PROD)
 @RestController
 @Slf4j
 public class DeviceTypeController {
