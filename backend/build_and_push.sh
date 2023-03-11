2daec4f628fde41498fc1fcba1116515c30cb9ee
Boban Ljuljdjurovic <boban@skylight.digital>
2023-03-10 23:55:32 -0500

    add a log to echo java version

--- backend/build_and_push.sh
+++ backend/build_and_push.sh
@@ -5,6 +5,7 @@
 GIT_SHA=${GIT_SHA:-$(git rev-parse --short HEAD)}
 ACR_TAG="simplereportacr.azurecr.io/api/simple-report-api-build:$GIT_SHA"
 
+echo $(java --version)
 export DOCKER_CLI_EXPERIMENTAL=enabled # to get "manifest inspect"
 
 if docker manifest inspect $ACR_TAG > /dev/null 2>&1; then
