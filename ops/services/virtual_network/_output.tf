841ec03d2eb633d8f3cee96e835de0ac13287833
Alis Akers <alismxwll@gmail.com>
2023-03-28 12:07:57 -0700

    update container instances to be launched in the correct subnet

--- ops/services/virtual_network/_output.tf
+++ ops/services/virtual_network/_output.tf
@@ -14,14 +14,14 @@ output "subnet_db_id" {
   value = azurerm_subnet.db.id
 }
 
+output "subnet_container_instances_id" {
+  value = azurerm_subnet.container_instances.id
+}
+
 output "private_dns_zone_id" {
   value = azurerm_private_dns_zone.default.id
 }
 
 output "network" {
   value = azurerm_virtual_network.vn
-}
-
-output "network_profile_id" {
-  value = azurerm_network_profile.container_instances.id
 }
