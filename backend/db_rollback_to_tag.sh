e6a3ceb63ae07cba72ad395c82dc5c1244ed9518
Alis Akers <alismxwll@gmail.com>
2023-03-29 14:13:50 -0700

    update db-rollback Dockerfile

--- backend/db_rollback_to_tag.sh
+++ backend/db_rollback_to_tag.sh
@@ -6,10 +6,6 @@ if [[ -n $LIQUIBASE_ROLLBACK_TAG ]];
         echo "Rollback tag provided! Rolling back to: $LIQUIBASE_ROLLBACK_TAG"
         echo "***************************************************************"
 
-        # echo "Clearing checksums..."
-        # gradle liquibaseClearChecksums
-        # gradle liquibaseUpdate
-        # Roll back to the provided tag
         gradle -Dorg.gradle.java.home=$JAVA_HOME liquibaseRollback -PliquibaseCommandValue="$LIQUIBASE_ROLLBACK_TAG"
     else
         echo "*******************************************"
