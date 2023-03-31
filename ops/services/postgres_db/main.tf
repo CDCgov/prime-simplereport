0931d8213d93d75ba5c11c2a7e89cff4cdf66a8f
Rin Concordia <93527746+rin-skylight@users.noreply.github.com>
2023-03-31 17:19:22 -0500

    Enable DB HA in Production. (#5465)
    * Enable DB HA in Production.
    
    * Add zone redundancy.
    
    * Perform terraform fmt.
    
    * Undo lockfile changes.
    
    * Further purge lockfile changes.

--- ops/services/postgres_db/main.tf
+++ ops/services/postgres_db/main.tf
@@ -17,11 +17,20 @@ resource "azurerm_postgresql_flexible_server" "db" {
 
   tags = var.tags
 
-  // Time is Eastern
+  # Time is Eastern
   maintenance_window {
     day_of_week  = 0
     start_hour   = 0
     start_minute = 0
+  }
+
+  # Only activate high availability in production for now.
+  dynamic "high_availability" {
+    for_each = var.env == "prod" ? [1] : []
+    content {
+      mode                      = "ZoneRedundant"
+      standby_availability_zone = 2
+    }
   }
 
   # See note at https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_flexible_server#high_availability
