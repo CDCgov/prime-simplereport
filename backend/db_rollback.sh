cc764a976a69d5f6b8ea85dc1e2267d5f9a43542
Alis Akers <alismxwll@gmail.com>
2023-04-05 14:23:34 -0700

    revert rollback script

--- backend/db_rollback.sh
+++ backend/db_rollback.sh
@@ -5,19 +5,21 @@ tag="$LIQUIBASE_ROLLBACK_TAG"
 count="$LIQUIBASE_ROLLBACK_COUNT"
 
 echo "Running validations..."
-# Check if the task failed (exit code is non-zero)
-if ! gradle liquibaseValidate; then
-    echo "Validation failed. Clearing checksums..."
-    gradle liquibaseClearCheckSums
-fi
+gradle liquibaseValidate
 
-# if [ -n "$tag" ]; then
-#   echo "Rolling back to tag: $tag"
-#   gradle liquibaseRollback -PliquibaseCommandValue=$tag
-# elif [ -n "$count" ]; then
-#   echo "Rolling back by count: $count"
-#   gradle liquibaseRollbackCount -PliquibaseCommandValue=$count
-# else
-#   echo "No rollback tag or count provided! Taking no action."
-#   exit 0
+# Check if the task failed (exit code is non-zero)
+# if ! gradle liquibaseValidate; then
+#     echo "Validation failed. Clearing checksums..."
+#     gradle liquibaseClearCheckSums
 # fi
+
+if [ -n "$tag" ]; then
+  echo "Rolling back to tag: $tag"
+  gradle liquibaseRollback -PliquibaseCommandValue=$tag
+elif [ -n "$count" ]; then
+  echo "Rolling back by count: $count"
+  gradle liquibaseRollbackCount -PliquibaseCommandValue=$count
+else
+  echo "No rollback tag or count provided! Taking no action."
+  exit 0
+fi
