6458f87c53a4bbfd1865ccdd14af2e6e99319fb3
Boban Ljuljdjurovic <boban@skylight.digital>
2023-04-06 08:58:05 -0400

    return empty collection instead of null

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/errors/ConflictingUserException.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/errors/ConflictingUserException.java
@@ -4,6 +4,7 @@ import graphql.ErrorClassification;
 import graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.language.SourceLocation;
+import java.util.Collections;
 import java.util.List;
 
 /** Exception to throw when a user can't be added because of a conflict */
@@ -17,7 +18,7 @@ public class ConflictingUserException extends RuntimeException implements GraphQ
 
   @Override // should-be-defaulted unused interface method
   public List<SourceLocation> getLocations() {
-    return null;
+    return Collections.emptyList();
   }
 
   @Override
