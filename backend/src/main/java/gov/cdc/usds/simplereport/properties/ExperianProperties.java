8e07939fdb85f4f8212428c5f326cae531f73e8b
Johanna <johanna@skylight.digital>
2023-04-24 12:21:09 -0400

    Added allArgConstructor to ExperianProperties

--- backend/src/main/java/gov/cdc/usds/simplereport/properties/ExperianProperties.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/properties/ExperianProperties.java
@@ -1,9 +1,10 @@
 package gov.cdc.usds.simplereport.properties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "simple-report.experian")
 @Getter
 public class ExperianProperties {
@@ -22,32 +23,4 @@ public class ExperianProperties {
   private final String preciseidClientReferenceId;
   private final String preciseidUsername;
   private final String preciseidPassword;
-
-  @ConstructorBinding
-  public ExperianProperties(
-      String tokenEndpoint,
-      String initialRequestEndpoint,
-      String domain,
-      String clientId,
-      String clientSecret,
-      String crosscoreSubscriberSubcode,
-      String crosscoreUsername,
-      String crosscorePassword,
-      String preciseidTenantId,
-      String preciseidClientReferenceId,
-      String preciseidUsername,
-      String preciseidPassword) {
-    this.tokenEndpoint = tokenEndpoint;
-    this.initialRequestEndpoint = initialRequestEndpoint;
-    this.domain = domain;
-    this.clientId = clientId;
-    this.clientSecret = clientSecret;
-    this.crosscoreSubscriberSubcode = crosscoreSubscriberSubcode;
-    this.crosscoreUsername = crosscoreUsername;
-    this.crosscorePassword = crosscorePassword;
-    this.preciseidTenantId = preciseidTenantId;
-    this.preciseidClientReferenceId = preciseidClientReferenceId;
-    this.preciseidUsername = preciseidUsername;
-    this.preciseidPassword = preciseidPassword;
-  }
 }
