0ba43a3e80df7ac44a0a74b5798ba88383005ffc
Zedd Shmais <zedd@skylight.digital>
2023-04-28 13:34:06 -0500

    make device sync endpoint more available in prod

--- backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
@@ -1,22 +1,18 @@
 package gov.cdc.usds.simplereport.api.devicetype;
 
-import static gov.cdc.usds.simplereport.config.BeanProfiles.PROD;
-
 import gov.cdc.usds.simplereport.service.DeviceTypeSyncService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("!" + PROD)
 @RestController
 @Slf4j
 public class DeviceTypeController {
-  @Autowired private DeviceTypeSyncService dtss;
+  @Autowired private DeviceTypeSyncService deviceTypeSyncService;
 
   @GetMapping("/devices/sync")
   public void syncDevices() {
-    dtss.syncDevices();
+    deviceTypeSyncService.syncDevices();
   }
 }
