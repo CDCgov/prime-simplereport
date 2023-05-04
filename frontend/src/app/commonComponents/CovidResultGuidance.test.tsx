c5cd5afd1e67f03cbc04a15b6297d6e90b1f6bf3
Johanna <johanna@skylight.digital>
2023-05-04 14:20:30 -0400

    Removed unnecessary describe

--- frontend/src/app/commonComponents/CovidResultGuidance.test.tsx
+++ frontend/src/app/commonComponents/CovidResultGuidance.test.tsx
@@ -5,31 +5,29 @@ import { TEST_RESULTS } from "../testResults/constants";
 import CovidResultGuidance from "./CovidResultGuidance";
 
 describe("CovidResultGuidance", () => {
-  describe("COVID-19 result guidance", () => {
-    const cases = [
-      TEST_RESULTS.POSITIVE,
-      TEST_RESULTS.NEGATIVE,
-      TEST_RESULTS.UNDETERMINED,
-      TEST_RESULTS.UNKNOWN,
-    ];
-    test.each(cases)("displays guidance for %p result", (testResult) => {
-      const { container: containerReporting } = render(
-        <CovidResultGuidance
-          result={testResult}
-          isPatientApp={false}
-          needsHeading={true}
-        />
-      );
+  const cases = [
+    TEST_RESULTS.POSITIVE,
+    TEST_RESULTS.NEGATIVE,
+    TEST_RESULTS.UNDETERMINED,
+    TEST_RESULTS.UNKNOWN,
+  ];
+  test.each(cases)("displays guidance for %p result", (testResult) => {
+    const { container: containerReporting } = render(
+      <CovidResultGuidance
+        result={testResult}
+        isPatientApp={false}
+        needsHeading={true}
+      />
+    );
 
-      expect(containerReporting).toMatchSnapshot();
-      const { container: containerPXP } = render(
-        <CovidResultGuidance
-          result={testResult}
-          isPatientApp={true}
-          needsHeading={false}
-        />
-      );
-      expect(containerPXP).toMatchSnapshot();
-    });
+    expect(containerReporting).toMatchSnapshot();
+    const { container: containerPXP } = render(
+      <CovidResultGuidance
+        result={testResult}
+        isPatientApp={true}
+        needsHeading={false}
+      />
+    );
+    expect(containerPXP).toMatchSnapshot();
   });
 });
