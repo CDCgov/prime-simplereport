93a69c328612dfa89d227941939980cdb394f51c
Rin Concordia <rin@skylight.digital>
2023-05-12 17:07:05 -0500

    Add operational logic for database maintenance container.

--- ops/dev7/container_instances.tf
+++ ops/dev7/container_instances.tf
@@ -11,3 +11,14 @@ module "db_liquibase_action" {
   spring_datasource_url   = data.azurerm_key_vault_secret.sr_dev7_db_jdbc.value
   subnet_id               = data.terraform_remote_state.persistent_dev7.outputs.subnet_container_instances_id
 }
+
+module "db_client" {
+  source                  = "../services/container_instances/infra/db_client"
+  count                   = 1
+  name                    = local.name
+  env                     = local.env
+  resource_group_name     = data.azurerm_resource_group.rg.name
+  resource_group_location = data.azurerm_resource_group.rg.location
+  acr_password            = data.terraform_remote_state.global.outputs.acr_simeplereport_admin_password
+  subnet_id               = data.terraform_remote_state.persistent_dev7.outputs.subnet_container_instances_id
+}
---
+++ ops/services/container_instances/db_client/infra/db_client.tf
@@ -0,0 +1,37 @@
+resource "azurerm_container_group" "db_liquibase_action" {
+  name                = "${var.name}-${var.env}-db-client"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  ip_address_type     = "Private"
+  subnet_ids          = [var.subnet_id]
+  os_type             = "Linux"
+  restart_policy      = "Always"
+
+  image_registry_credential {
+    username = var.acr_username
+    password = var.acr_password
+    server   = var.acr_server
+  }
+
+  container {
+    name   = "${var.name}-${var.env}-db-client"
+    image  = "${var.acr_server}/api/simple-report-db-client:latest"
+    cpu    = "1"
+    memory = "1.5"
+
+    // Not a real port but *a* port is required, so expose a port the container hopefully doesn't respond to
+    ports {
+      port     = 1234
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      //LIQUIBASE_ROLLBACK_TAG = var.rollback_tag
+      //LIQUIBASE_ACTION       = var.image_action
+    }
+
+    secure_environment_variables = {
+      //SPRING_DATASOURCE_URL = var.spring_datasource_url
+    }
+  }
+}
