1f93a78c452949ccb728c5c79bb7904c7df05da4
Johanna <johanna@skylight.digital>
2023-05-24 14:51:17 -0400

    Form migrated to react-hook-forms. Validation errors, focus and form tag added.

--- frontend/src/app/Settings/ManageOrganization.tsx
+++ frontend/src/app/Settings/ManageOrganization.tsx
@@ -1,136 +1,127 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 import TextInput from "../commonComponents/TextInput";
 import Button from "../commonComponents/Button/Button";
 import RequiredMessage from "../commonComponents/RequiredMessage";
 import Alert from "../commonComponents/Alert";
 import Select from "../commonComponents/Select";
-import { showError } from "../utils/srToast";
 import { OrganizationTypeEnum } from "../signUp/Organization/utils";
 
 import { EditableOrganization } from "./ManageOrganizationContainer";
 
-interface Props {
+type OrganizationFormData = {
+  name: string;
+  type: OrganizationType;
+};
+
+interface ManageOrganizationProps {
   organization: EditableOrganization;
   onSave: (organization: EditableOrganization) => void;
   canEditOrganizationName: boolean;
 }
 
-const ManageOrganization: React.FC<Props> = (props) => {
-  const [organization, setOrganization] = useState(props.organization);
-  const [errors, setErrors] = useState<
-    Partial<Record<keyof EditableOrganization, string>>
-  >({});
-  const [formChanged, setFormChanged] = useState(false);
-  const onChange =
-    <K extends keyof EditableOrganization>(key: K) =>
-    (value: EditableOrganization[K]) => {
-      setOrganization({ ...organization, [key]: value });
-      setFormChanged(true);
+const ManageOrganization: React.FC<ManageOrganizationProps> = ({
+  organization,
+  onSave,
+  canEditOrganizationName,
+}: ManageOrganizationProps) => {
+  /**
+   * Form state setup
+   */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isDirty },
+    watch,
+  } = useForm<OrganizationFormData>({
+    defaultValues: { type: organization.type, name: organization.name },
+  });
+  const formCurrentValues = watch();
+
+  /**
+   * Submit organization data
+   */
+  const onSubmit = async (orgData: OrganizationFormData) => {
+    const updatedOrganization = {
+      ...organization,
+      name: canEditOrganizationName ? orgData.name : organization.name,
+      type: orgData.type,
     };
 
-  const validateField = (
-    field: keyof EditableOrganization
-  ): string | undefined => {
-    if (field === "name") {
-      if (organization[field].trim().length === 0) {
-        return "The organization's name cannot be blank";
-      }
-    }
-    if (field === "type") {
-      if (!Object.keys(OrganizationTypeEnum).includes(organization[field])) {
-        return "An organization type must be selected";
-      }
-    }
-    return undefined;
+    onSave(updatedOrganization);
   };
 
-  const validateAndSave = () => {
-    const nameError = validateField("name");
-    const typeError = validateField("type");
-
-    if (!nameError && !typeError) {
-      props.onSave(organization);
-    } else {
-      const updatedErrors = {
-        ...errors,
-        name: nameError,
-        type: typeError,
-      };
-      let ulAlertBody = (
-        <ul>
-          {[nameError, typeError]
-            .filter((msg) => !!msg)
-            .map((msg) => (
-              <li key={msg}>{msg}</li>
-            ))}
-        </ul>
-      );
-      showError(ulAlertBody, "Information missing");
-      setErrors(updatedErrors);
-    }
-  };
-
+  /**
+   * HTML
+   */
   return (
     <div className="grid-row position-relative">
       <div className="prime-container card-container settings-tab">
-        <div className="usa-card__header">
-          <h1>Manage organization</h1>
-          <Button
-            onClick={validateAndSave}
-            label="Save settings"
-            disabled={!formChanged}
-          />
-        </div>
-        <div className="usa-card__body">
-          <RequiredMessage />
-          {!props.canEditOrganizationName && (
-            <Alert type="info">
-              The organization name is used for reporting to public health
-              departments. Please contact{" "}
-              <a href="mailto:support@simplereport.gov">
-                support@simplereport.gov
-              </a>{" "}
-              if you need to change it.
-            </Alert>
-          )}
-          {props.canEditOrganizationName ? (
-            <TextInput
-              label="Organization name"
-              name="name"
-              value={organization.name}
-              onChange={(e) => onChange("name")(e.target.value)}
-              required
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="usa-card__header">
+            <h1>Manage organization</h1>
+            <Button
+              type="submit"
+              label="Save settings"
+              disabled={isSubmitting || !isDirty}
             />
-          ) : (
-            <div className="usa-form-group">
-              <span>Organization name</span>
-              <p>{organization.name}</p>
-            </div>
-          )}
-          <Select
-            name="type"
-            options={
-              Object.entries(OrganizationTypeEnum).map(([key, value]) => ({
-                label: value,
-                value: key,
-              })) as {
-                value: OrganizationType;
-                label: string;
-              }[]
-            }
-            label="Organization type"
-            onChange={onChange("type")}
-            value={organization.type}
-            defaultSelect
-            onBlur={() => {
-              validateField("type");
-            }}
-            errorMessage={errors["type"]}
-            validationStatus={errors["type"] ? "error" : "success"}
-            required
-          />
-        </div>
+          </div>
+          <div className="usa-card__body">
+            <RequiredMessage />
+            {canEditOrganizationName ? (
+              <TextInput
+                label="Organization name"
+                name="name"
+                value={formCurrentValues.name}
+                validationStatus={
+                  errors?.name?.type === "required" ? "error" : undefined
+                }
+                errorMessage="The organization's name cannot be blank"
+                required
+                registrationProps={register("name", {
+                  required: true,
+                })}
+              />
+            ) : (
+              <>
+                <Alert type="info">
+                  The organization name is used for reporting to public health
+                  departments. Please contact{" "}
+                  <a href="mailto:support@simplereport.gov">
+                    support@simplereport.gov
+                  </a>{" "}
+                  if you need to change it.
+                </Alert>
+                <div className="usa-form-group">
+                  <span>Organization name</span>
+                  <p>{organization.name}</p>
+                </div>
+              </>
+            )}
+            <Select
+              name="type"
+              options={
+                Object.entries(OrganizationTypeEnum).map(([key, value]) => ({
+                  label: value,
+                  value: key,
+                })) as {
+                  value: OrganizationType;
+                  label: string;
+                }[]
+              }
+              label="Organization type"
+              value={formCurrentValues.type}
+              defaultSelect
+              errorMessage="An organization type must be selected"
+              validationStatus={
+                errors?.type?.type === "required" ? "error" : undefined
+              }
+              required
+              registrationProps={register("type", { required: true })}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
