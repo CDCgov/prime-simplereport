a24754d48d1748ab158ce998e287504cf5b76efa
Boban Ljuljdjurovic <boban@skylight.digital>
2023-06-02 09:36:03 -0400

    make combobox optionally wrapped in a controller

--- frontend/src/app/supportAdmin/Components/OrganizationComboDropdown.tsx
+++ frontend/src/app/supportAdmin/Components/OrganizationComboDropdown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ComboBox } from "@trussworks/react-uswds";
+import { Control, Controller } from "react-hook-form";
 
 import Required from "../../commonComponents/Required";
 import { showError } from "../../utils/srToast";
@@ -7,10 +8,6 @@ import { showError } from "../../utils/srToast";
 export interface OrganizationOption {
   name: string;
   externalId: string;
-}
-
-export interface OrganizationOptions {
-  organizations: [OrganizationOption];
 }
 
 export const useOrganizationDropDownValidation = (
@@ -34,21 +31,17 @@ export const useOrganizationDropDownValidation = (
 
 interface Props {
   selectedExternalId: string | undefined;
-  updateSelectedExternalId: (selectedId: string | undefined) => void;
+  updateSelectedExternalId?: (selectedId: string | undefined) => void;
   organizationOptions: OrganizationOption[];
+  control?: Control<any>;
 }
 
 const OrganizationComboDropDown: React.FC<Props> = ({
   selectedExternalId,
   updateSelectedExternalId,
   organizationOptions,
+  control,
 }) => {
-  const onOrganizationChange = (
-    newOrganizationExternalId: string | undefined
-  ) => {
-    updateSelectedExternalId(newOrganizationExternalId);
-  };
-
   const dropdownOptions = organizationOptions.map(({ name, externalId }) => {
     return {
       label: name,
@@ -66,15 +59,46 @@ const OrganizationComboDropDown: React.FC<Props> = ({
         </h2>
       </div>
       <div className="usa-card__body usa-form usa-form--large">
-        <ComboBox
-          options={dropdownOptions}
-          id="org-dropdown-select"
-          name="orgSelect"
-          defaultValue={selectedExternalId}
-          onChange={(value) => {
-            onOrganizationChange(value);
-          }}
-        />
+        {control ? (
+          <Controller
+            render={({
+              field: { onChange, value, name, ref },
+              fieldState: { error },
+            }) => (
+              <div className={error?.message && "usa-form-group--error"}>
+                {error && (
+                  <span className={"usa-error-message"} role={"alert"}>
+                    <span className="usa-sr-only">Error: </span>{" "}
+                    {error?.message}
+                  </span>
+                )}
+                <ComboBox
+                  options={dropdownOptions}
+                  id="org-dropdown-select"
+                  name={name}
+                  defaultValue={value}
+                  onChange={onChange}
+                  ref={ref}
+                />
+              </div>
+            )}
+            name="organizationExternalId"
+            control={control}
+            rules={{ required: "Organization is missing" }}
+          />
+        ) : (
+          <ComboBox
+            options={dropdownOptions}
+            id="org-dropdown-select"
+            name="orgSelect"
+            defaultValue={selectedExternalId}
+            onChange={(value) => {
+              if (updateSelectedExternalId) {
+                updateSelectedExternalId(value);
+              }
+            }}
+          />
+        )}
       </div>
     </div>
   );
