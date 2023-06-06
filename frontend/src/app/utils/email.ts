95585cb12bdbf8916f94fc8755f91964b3690e34
Johanna <johanna@skylight.digital>
2023-06-06 12:41:02 -0400

    Fixed form implementation in manage users

--- frontend/src/app/utils/email.ts
+++ frontend/src/app/utils/email.ts
@@ -3,3 +3,6 @@ export const emailIsValid = (emailAddress = "") => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(emailAddress).toLowerCase());
 };
+
+export const emailRegex =
+  /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
