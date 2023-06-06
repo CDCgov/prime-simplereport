0c178c877ff52cdd85e2306cb9069fadf22e3c35
Boban Ljuljdjurovic <boban@skylight.digital>
2023-06-06 11:04:18 -0400

    Remove validation from user schema

--- frontend/src/app/Settings/Users/CreateUserSchema.ts
+++ frontend/src/app/Settings/Users/CreateUserSchema.ts
@@ -1,7 +1,7 @@
-import * as yup from "yup";
-
-import { getValues } from "../../patients/personSchema";
 import { Role } from "../../permissions";
+import { Maybe, UserPermission } from "../../../generated/graphql";
+
+import { UserFacilitySetting } from "./ManageUsersContainer";
 
 export const ROLE_OPTIONS: { value: Role; label: string }[] = [
   {
@@ -24,25 +24,8 @@ export interface CreateUser {
   lastName: string;
   email: string;
   role: Role;
+  organization?: Maybe<{
+    testingFacility: UserFacilitySetting[];
+  }>;
+  permissions: UserPermission[];
 }
-
-export type CreateUserErrors = Record<keyof CreateUser, string>;
-
-export const initCreateUserErrors = (): CreateUserErrors => ({
-  firstName: "",
-  lastName: "",
-  email: "",
-  role: "",
-});
-
-export const createUserSchema: yup.SchemaOf<CreateUser> = yup.object().shape({
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string().required("Last name is required"),
-  email: yup
-    .string()
-    .email("Email must be a valid email address")
-    .required("Email is required"),
-  role: yup
-    .mixed()
-    .oneOf(getValues(ROLE_OPTIONS), "Phone type is missing or invalid"),
-});
