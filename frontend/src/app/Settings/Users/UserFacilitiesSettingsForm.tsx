483c3395322029c7183953e83e52dce63e12c239
Boban Ljuljdjurovic <boban@skylight.digital>
2023-06-07 10:34:45 -0400

    replace nested ternary with if statements

--- frontend/src/app/Settings/Users/UserFacilitiesSettingsForm.tsx
+++ frontend/src/app/Settings/Users/UserFacilitiesSettingsForm.tsx
@@ -104,15 +104,15 @@ const UserFacilitiesSettingsForm: React.FC<Props> = ({
     }
   }, [hasAllFacilityAccess, activeUser, onUpdateUser, allFacilities]);
 
-  const userFacilities = useMemo(
-    () =>
-      hasAllFacilityAccess
-        ? [...allFacilities]
-        : activeUser.organization
-        ? [...activeUser.organization.testingFacility]
-        : [],
-    [activeUser, allFacilities, hasAllFacilityAccess]
-  );
+  const userFacilities = useMemo(() => {
+    if (hasAllFacilityAccess) {
+      return [...allFacilities];
+    } else if (activeUser.organization?.testingFacility) {
+      return [...activeUser.organization.testingFacility];
+    } else {
+      return [];
+    }
+  }, [activeUser, allFacilities, hasAllFacilityAccess]);
 
   userFacilities.sort(alphabeticalFacilitySort);
 
