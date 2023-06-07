c87ee068486a6cc3436a8c8181663d077c12033c
Johanna <johanna@skylight.digital>
2023-06-07 13:43:04 -0400

    Replaced old email regex with the owasp email regex.

--- frontend/src/app/utils/email.ts
+++ frontend/src/app/utils/email.ts
@@ -1,7 +1,5 @@
 export const emailIsValid = (emailAddress = "") => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(emailAddress).toLowerCase());
+  return emailRegex.test(String(emailAddress).toLowerCase());
 };
 
 export const emailRegex =
