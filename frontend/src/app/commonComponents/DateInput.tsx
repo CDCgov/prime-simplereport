ef350537e35fbe0aabff5e0adba4a6251017dc69
Boban Ljuljdjurovic <boban@skylight.digital>
2023-06-12 11:02:55 -0400

    use react hook validation on DateInput

--- frontend/src/app/commonComponents/DateInput.tsx
+++ frontend/src/app/commonComponents/DateInput.tsx
@@ -4,125 +4,169 @@ import {
   DateInputGroup,
 } from "@trussworks/react-uswds";
 import { useTranslation } from "react-i18next";
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  UseFormWatch,
+} from "react-hook-form";
+import moment from "moment/moment";
 
-export type HTMLInputElementType =
-  | "date"
-  | "datetime-local"
-  | "email"
-  | "month"
-  | "number"
-  | "password"
-  | "search"
-  | "tel"
-  | "text"
-  | "time"
-  | "url"
-  | "week";
+import { dateFromStrings } from "../utils/date";
+
+import Required from "./Required";
 
 interface Props {
   name: string;
   label: string;
-  monthName: string;
-  dayName: string;
-  yearName: string;
-  monthValue: any;
-  dayValue: any;
-  yearValue: any;
-  monthOnChange: any;
-  dayOnChange: any;
-  yearOnChange: any;
   className?: string;
-  validationStatus?: "error" | "success";
-  errorMessage?: React.ReactNode;
   labelSrOnly?: boolean;
   labelClassName?: string;
-  defaultValue?: string;
   noHint?: boolean;
-  type?: HTMLInputElementType;
+  control: Control<DateForm>;
+  errors: FieldErrors<DateForm>;
+  watch: UseFormWatch<DateForm>;
+}
+export interface DateForm {
+  month: string;
+  day: string;
+  year: string;
 }
 
 export const DateInput = ({
   name,
   label,
-  monthName,
-  dayName,
-  yearName,
-  monthValue,
-  dayValue,
-  yearValue,
-  monthOnChange,
-  dayOnChange,
-  yearOnChange,
-  type,
   className,
-  validationStatus,
-  errorMessage,
   labelSrOnly,
   labelClassName,
-  defaultValue,
   noHint,
+  control,
+  errors,
+  watch,
 }: Props) => {
   const { t } = useTranslation();
 
   return (
     <fieldset
       className={classnames("usa-form-group usa-fieldset", className, {
-        "usa-form-group--error": validationStatus === "error",
+        "usa-form-group--error": errors.month ?? errors.day ?? errors.year,
       })}
     >
       <legend
         className={classnames("usa-label", labelClassName, {
           "usa-sr-only": labelSrOnly,
-          "usa-label--error": validationStatus === "error",
+          "usa-label--error": errors.month ?? errors.day ?? errors.year,
         })}
       >
-        {label}
+        <Required label={label} />
       </legend>
       {noHint ? null : (
         <span className="usa-hint">{t("testResult.dob.exampleText")}</span>
       )}
-      {validationStatus === "error" && (
-        <span className="usa-error-message" id={`error_${name}`} role="alert">
+      {(errors.month || errors.day || errors.year) && (
+        <span
+          className="usa-error-message text-pre-line"
+          id={`error_${name}`}
+          role="alert"
+        >
           <span className="usa-sr-only">Error: </span>
-          {errorMessage}
+          {Array.from(
+            new Set([
+              errors.month?.message,
+              errors.day?.message,
+              errors.year?.message,
+            ])
+          )
+            .filter((x) => x !== undefined)
+            .join("\n")}
         </span>
       )}
       <DateInputGroup>
-        <TrussworksDateInput
-          id={monthName}
-          name={monthName}
-          value={monthValue}
-          required={true}
-          defaultValue={defaultValue}
-          label={t("constants.date.month")}
-          unit={"month"}
-          maxLength={2}
-          type={type || "text"}
-          onChange={monthOnChange}
+        <Controller
+          name={"month"}
+          control={control}
+          rules={{
+            required: t("testResult.dob.invalidDate"),
+            min: { value: 1, message: t("testResult.dob.invalidDate") },
+            max: { value: 12, message: t("testResult.dob.invalidDate") },
+          }}
+          render={({ field: { value, name, ref, onChange } }) => (
+            <>
+              <TrussworksDateInput
+                id={name}
+                name={name}
+                value={value}
+                label={t("constants.date.month")}
+                unit={"month"}
+                maxLength={2}
+                type={"number"}
+                inputRef={ref}
+                onChange={onChange}
+              />
+            </>
+          )}
         />
-        <TrussworksDateInput
-          id={dayName}
-          name={dayName}
-          value={dayValue}
-          required={true}
-          defaultValue={defaultValue}
-          label={t("constants.date.day")}
-          unit={"day"}
-          maxLength={2}
-          type={type || "text"}
-          onChange={dayOnChange}
+        <Controller
+          name={"day"}
+          control={control}
+          rules={{
+            required: t("testResult.dob.invalidDate"),
+            min: { value: 1, message: t("testResult.dob.invalidDate") },
+            max: { value: 31, message: t("testResult.dob.invalidDate") },
+            validate: (day) => {
+              const month = watch("month");
+              const year = watch("year");
+              const date = dateFromStrings(month, day, year);
+              if (month && day && year && !date.isValid()) {
+                return t("testResult.dob.invalidDate");
+              }
+              return true;
+            },
+          }}
+          render={({ field: { value, name, ref, onChange } }) => (
+            <>
+              <TrussworksDateInput
+                id={name}
+                name={name}
+                value={value}
+                label={t("constants.date.day")}
+                unit={"day"}
+                maxLength={2}
+                type={"number"}
+                inputRef={ref}
+                onChange={onChange}
+              />
+            </>
+          )}
         />
-        <TrussworksDateInput
-          id={yearName}
-          name={yearName}
-          value={yearValue}
-          required={true}
-          defaultValue={defaultValue}
-          label={t("constants.date.year")}
-          unit={"year"}
-          maxLength={4}
-          type={type || "text"}
-          onChange={yearOnChange}
+        <Controller
+          name={"year"}
+          control={control}
+          rules={{
+            required: t("testResult.dob.invalidDate"),
+            maxLength: { value: 4, message: t("testResult.dob.invalidDate") },
+            minLength: { value: 4, message: t("testResult.dob.invalidDate") },
+            min: { value: 1900, message: t("testResult.dob.invalidYear") },
+            max: {
+              value: moment().year(),
+              message: t("testResult.dob.invalidYear"),
+            },
+          }}
+          render={({ field: { ref, value, name, onChange } }) => (
+            <>
+              <TrussworksDateInput
+                id={name}
+                name={name}
+                value={value}
+                label={t("constants.date.year")}
+                unit={"year"}
+                maxLength={4}
+                type={"number"}
+                inputRef={ref}
+                onChange={onChange}
+              />
+            </>
+          )}
         />
       </DateInputGroup>
     </fieldset>
