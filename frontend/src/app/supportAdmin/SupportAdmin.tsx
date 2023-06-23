5f77803f7e6e05f2483c624087d67ce8cdc6b49d
Johanna <johanna@skylight.digital>
2023-06-23 15:33:47 -0400

    Modified landing page

--- frontend/src/app/supportAdmin/SupportAdmin.tsx
+++ frontend/src/app/supportAdmin/SupportAdmin.tsx
@@ -1,4 +1,5 @@
 import { useFeature } from "flagged";
+import React from "react";
 
 import { LinkWithQuery } from "../commonComponents/LinkWithQuery";
 import { useDocumentTitle } from "../utils/hooks";
@@ -20,38 +21,61 @@ const SupportAdmin = () => {
               </div>
             </div>
             <div className="usa-card__body">
-              <div>
-                <LinkWithQuery to={`/admin/pending-organizations`}>
-                  Organizations pending identify verification
-                </LinkWithQuery>
-              </div>
-              <div>
-                <LinkWithQuery to="/admin/add-organization-admin">
-                  Add organization admin
-                </LinkWithQuery>
-              </div>
-              <div>
-                <LinkWithQuery to="/admin/create-device-type">
-                  Add a new testing device
-                </LinkWithQuery>
-              </div>
-              <div>
-                <LinkWithQuery to="/admin/manage-devices">
-                  Edit existing testing device
-                </LinkWithQuery>
-              </div>
-              <div>
-                <LinkWithQuery to="/admin/tenant-data-access">
-                  Organization data
-                </LinkWithQuery>
-              </div>
-              {hivEnabled && (
-                <div>
-                  <LinkWithQuery to="/admin/hiv-csv-upload">
-                    Beta - HIV CSV Upload
-                  </LinkWithQuery>
+              <div className="grid-row grid-gap">
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <h2 className="font-heading-md margin-bottom-0 margin-top-2">
+                    Organization
+                  </h2>
+                  <ul className="usa-list padding-left-2">
+                    <li>
+                      <LinkWithQuery to={`/admin/pending-organizations`}>
+                        Identify verification
+                      </LinkWithQuery>
+                    </li>
+                    <li>
+                      <LinkWithQuery to="/admin/add-organization-admin">
+                        Add organization admin
+                      </LinkWithQuery>
+                    </li>
+                    <li>
+                      <LinkWithQuery to="/admin/tenant-data-access">
+                        Organization data
+                      </LinkWithQuery>
+                    </li>
+                  </ul>
                 </div>
-              )}
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <h2 className="font-heading-md margin-bottom-0 margin-top-2">
+                    Test Devices
+                  </h2>
+                  <ul className="usa-list padding-left-2">
+                    <li>
+                      <LinkWithQuery to="/admin/create-device-type">
+                        Add a new testing device
+                      </LinkWithQuery>
+                    </li>
+                    <li>
+                      <LinkWithQuery to="/admin/manage-devices">
+                        Edit existing testing device
+                      </LinkWithQuery>
+                    </li>
+                  </ul>
+                </div>
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <h2 className="font-heading-md margin-bottom-0 margin-top-2">
+                    Beta
+                  </h2>
+                  <ul className="usa-list padding-left-2">
+                    {hivEnabled && (
+                      <li>
+                        <LinkWithQuery to="/admin/hiv-csv-upload">
+                          Beta - HIV CSV Upload
+                        </LinkWithQuery>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
