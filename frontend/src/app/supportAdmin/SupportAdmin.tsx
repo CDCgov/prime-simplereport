d6d7da1e9d1c094598d39aa74a8de698e8557968
Johanna <johanna@skylight.digital>
2023-06-28 11:49:44 -0400

    Created a resuable wrapper component

--- frontend/src/app/supportAdmin/SupportAdmin.tsx
+++ frontend/src/app/supportAdmin/SupportAdmin.tsx
@@ -3,6 +3,21 @@ import { useFeature } from "flagged";
 import { LinkWithQuery } from "../commonComponents/LinkWithQuery";
 import { useDocumentTitle } from "../utils/hooks";
 
+type CategoryMenuProps = {
+  heading: string;
+  children: React.ReactNode;
+};
+
+const CategoryMenu: React.FC<CategoryMenuProps> = ({
+  heading,
+  children,
+}: CategoryMenuProps) => (
+  <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+    <h2 className="font-heading-md margin-bottom-0 margin-top-2">{heading}</h2>
+    <ul className="usa-list padding-left-2">{children}</ul>
+  </div>
+);
+
 const SupportAdmin = () => {
   useDocumentTitle("Support admin");
   const hivEnabled = useFeature("hivEnabled") as boolean;
@@ -21,58 +36,43 @@ const SupportAdmin = () => {
             </div>
             <div className="usa-card__body">
               <div className="grid-row grid-gap">
-                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                  <h2 className="font-heading-md margin-bottom-0 margin-top-2">
-                    Organization
-                  </h2>
-                  <ul className="usa-list padding-left-2">
-                    <li>
-                      <LinkWithQuery to={`/admin/pending-organizations`}>
-                        Identify verification
-                      </LinkWithQuery>
-                    </li>
-                    <li>
-                      <LinkWithQuery to="/admin/add-organization-admin">
-                        Add organization admin
-                      </LinkWithQuery>
-                    </li>
-                    <li>
-                      <LinkWithQuery to="/admin/tenant-data-access">
-                        Organization data
-                      </LinkWithQuery>
-                    </li>
-                  </ul>
-                </div>
-                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                  <h2 className="font-heading-md margin-bottom-0 margin-top-2">
-                    Test Devices
-                  </h2>
-                  <ul className="usa-list padding-left-2">
-                    <li>
-                      <LinkWithQuery to="/admin/create-device-type">
-                        Add a new testing device
-                      </LinkWithQuery>
-                    </li>
-                    <li>
-                      <LinkWithQuery to="/admin/manage-devices">
-                        Edit existing testing device
-                      </LinkWithQuery>
-                    </li>
-                  </ul>
-                </div>
+                <CategoryMenu heading="Organization">
+                  <li>
+                    <LinkWithQuery to={`/admin/pending-organizations`}>
+                      Identify verification
+                    </LinkWithQuery>
+                  </li>
+                  <li>
+                    <LinkWithQuery to="/admin/add-organization-admin">
+                      Add organization admin
+                    </LinkWithQuery>
+                  </li>
+                  <li>
+                    <LinkWithQuery to="/admin/tenant-data-access">
+                      Organization data
+                    </LinkWithQuery>
+                  </li>
+                </CategoryMenu>
+                <CategoryMenu heading="Test Devices">
+                  <li>
+                    <LinkWithQuery to="/admin/create-device-type">
+                      Add a new testing device
+                    </LinkWithQuery>
+                  </li>
+                  <li>
+                    <LinkWithQuery to="/admin/manage-devices">
+                      Edit existing testing device
+                    </LinkWithQuery>
+                  </li>
+                </CategoryMenu>
                 {hivEnabled && (
-                  <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                    <h2 className="font-heading-md margin-bottom-0 margin-top-2">
-                      Beta
-                    </h2>
-                    <ul className="usa-list padding-left-2">
-                      <li>
-                        <LinkWithQuery to="/admin/hiv-csv-upload">
-                          Beta - HIV CSV Upload
-                        </LinkWithQuery>
-                      </li>
-                    </ul>
-                  </div>
+                  <CategoryMenu heading="Beta">
+                    <li>
+                      <LinkWithQuery to="/admin/hiv-csv-upload">
+                        Beta - HIV CSV Upload
+                      </LinkWithQuery>
+                    </li>
+                  </CategoryMenu>
                 )}
               </div>
             </div>
