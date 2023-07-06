026967190db576f40bc49ab30d8beee2b633047e
Zedd Shmais <zedd@skylight.digital>
2023-07-06 09:41:16 -0500

    add intellij config for running e2e

--- cypress/e2e/07-organization_settings.cy.js
+++ cypress/e2e/07-organization_settings.cy.js
@@ -20,15 +20,17 @@ describe("Updating organization settings", () => {
     cy.checkAccessibility();
   });
   it("attempts an empty selection for organization type", () => {
+    cy.get('select[name="type"]').find('option:selected').should('have.text','Camp');
     cy.get('select[name="type"]').select("- Select -");
-    cy.contains("Save settings").click();
+    cy.contains("Save settings").should("be.enabled").click();
   });
   it("displays a validation toast", () => {
     cy.contains("An organization type must be selected");
   });
   it("attempts a valid selection for organization type", () => {
     cy.get('select[name="type"]').select("Nursing home");
-    cy.contains("Save settings").click();
+    cy.contains("Save settings").should("be.enabled").click();
+    cy.wait("@AdminSetOrganization")
   });
   it("displays a success toast", () => {
     cy.get(".Toastify").contains("Updated organization");
