d1c527ab4c2d4b2bad74faa8360d800823201fac
Bob Zhao <bob@skylight.digital>
2023-07-26 15:29:02 -0400

    extra line

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -201,7 +201,7 @@ export type Mutation = {
   setOrganizationIdentityVerified?: Maybe<Scalars["Boolean"]>;
   setPatientIsDeleted?: Maybe<Patient>;
   setRegistrationLinkIsDeleted?: Maybe<Scalars["String"]>;
-  /** @deprecated Use reactivateAndResetPassword version i */
+  /** @deprecated Use reactivateAndResetPassword version */
   setUserIsDeleted?: Maybe<User>;
   submitQueueItem?: Maybe<AddTestResultResponse>;
   updateDeviceType?: Maybe<DeviceType>;
