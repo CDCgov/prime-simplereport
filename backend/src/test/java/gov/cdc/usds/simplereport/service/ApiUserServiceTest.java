42b34d45114038a43ea647721427ad0c1c972118
Bob Zhao <bob@skylight.digital>
2023-07-26 15:15:37 -0400

    fix import

--- backend/src/test/java/gov/cdc/usds/simplereport/service/ApiUserServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/ApiUserServiceTest.java
@@ -3,7 +3,8 @@ package gov.cdc.usds.simplereport.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.okta.sdk.resource.user.UserStatus;
 import gov.cdc.usds.simplereport.api.model.ApiUserWithStatus;
