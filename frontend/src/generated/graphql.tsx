e101b317a725159d8d0b0db93c5cbc6d68afd99b
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-07-27 08:58:49 -0400

    add isDeleted to `GetFacilityResultsMultiplexWithCount` query

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -2262,7 +2262,11 @@ export type GetFacilityResultsMultiplexWithCountQuery = {
         __typename?: "PatientLink";
         internalId?: string | null;
       } | null;
-      facility?: { __typename?: "Facility"; name: string } | null;
+      facility?: {
+        __typename?: "Facility";
+        name: string;
+        isDeleted?: boolean | null;
+      } | null;
     } | null> | null;
   } | null;
 };
@@ -6500,6 +6504,7 @@ export const GetFacilityResultsMultiplexWithCountDocument = gql`
         }
         facility {
           name
+          isDeleted
         }
       }
       totalElements
