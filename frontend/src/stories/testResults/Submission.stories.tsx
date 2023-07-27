f4e04b7085f952bf70059ba78750d4f82e846e7a
Alis Akers <alismxwll@gmail.com>
2023-07-27 11:42:28 -0700

    revert msw version

--- frontend/src/stories/testResults/Submission.stories.tsx
+++ frontend/src/stories/testResults/Submission.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta, StoryFn } from "@storybook/react";
-import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import createMockStore from "redux-mock-store";
