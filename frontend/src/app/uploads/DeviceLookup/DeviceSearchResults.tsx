70c1cd0560c4443d78c79932c3e0e3f4265c3339
Nathan Carter <nathan.andrew.carter@gmail.com>
2023-07-31 12:24:08 -0400

    Use alternate no-results text for device management dropdown

--- frontend/src/app/uploads/DeviceLookup/DeviceSearchResults.tsx
+++ frontend/src/app/uploads/DeviceLookup/DeviceSearchResults.tsx
@@ -42,12 +42,21 @@ const DeviceSearchResults = (props: SearchResultsProps) => {
         <div className="margin-bottom-105">
           No device found matching <strong>{queryString}</strong>.
         </div>
-        <span>
-          Please try a different search term from the device's name or
-          manufacturer. <br /> If you need help, contact{" "}
-          <a href="mailto:support@simplereport.gov">support@simplereport.gov</a>
-          .
-        </span>
+        {multiSelect ? (
+          <span>
+            Please check the list of added devices or try a different search
+            term from the device's name or manufacturer.
+          </span>
+        ) : (
+          <span>
+            Please try a different search term from the device's name or
+            manufacturer. <br /> If you need help, contact{" "}
+            <a href="mailto:support@simplereport.gov">
+              support@simplereport.gov
+            </a>
+            .
+          </span>
+        )}
       </div>
     );
   } else {
