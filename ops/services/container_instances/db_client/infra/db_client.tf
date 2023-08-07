06f7741d015aec140b134704fcbc995fb6139289
Rin Concordia <rin@skylight.digital>
2023-08-07 15:52:56 -0500

    Apply results of terraform fmt.

--- ops/services/container_instances/db_client/infra/db_client.tf
+++ ops/services/container_instances/db_client/infra/db_client.tf
@@ -36,7 +36,7 @@ resource "azurerm_container_group" "db_client" {
     }
 
     readiness_probe {
-      exec = ["/bin/sh","-c","touch /tmp/healthy"]
+      exec = ["/bin/sh", "-c", "touch /tmp/healthy"]
     }
 
     liveness_probe {
