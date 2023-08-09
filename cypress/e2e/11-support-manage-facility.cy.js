1eda3b94380788412f62129b80ada18f1421fd67
Johanna <johanna@skylight.digital>
2023-08-09 13:04:37 -0400

    Fixes selection for org and facility

--- cypress/e2e/11-support-manage-facility.cy.js
+++ cypress/e2e/11-support-manage-facility.cy.js
@@ -2,12 +2,24 @@ import {loginHooks} from "../support/e2e";
 import {graphqlURL} from "../utils/request-utils";
 import {aliasGraphqlOperations} from "../utils/graphql-test-utils";
 
+loginHooks();
 describe("Support admin: manage facility", () => {
-  loginHooks();
+  let organizationId="";
+  let facilityId = "";
 
-  beforeEach(() => {
+  before(() => {
     cy.intercept("POST", graphqlURL, (req) => {
       aliasGraphqlOperations(req)
+    });
+
+    cy.makePOSTRequest({
+      operationName: "WhoAmI",
+      variables: {},
+      query:
+        "query WhoAmI {\n  whoami {\n organization {\n  id\n  facilities {\n      id\n    }\n  }\n} \n}",
+    }).then((res) => {
+      organizationId = res.body.data.whoami.organization.id;
+      facilityId = res.body.data.whoami.organization.facilities[0].id;
     });
   });
 
@@ -22,11 +34,11 @@ describe("Support admin: manage facility", () => {
 
     // selects organization
     cy.wait("@GetAllOrganizations");
-    cy.get("div.bg-base-lightest select").first().select("Dis Organization");
+    cy.get("div.bg-base-lightest select").first().select(organizationId);
 
     // selects facility
     cy.wait("@GetFacilitiesByOrgId");
-    cy.get("div.bg-base-lightest select").eq(1).select("Testing Site");
+    cy.get("div.bg-base-lightest select").eq(1).select(facilityId);
 
     // displays facility information
     cy.wait("@GetFacilityStats");
