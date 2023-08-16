5226474933a214cad13fa2619324169206e11d38
Bob Zhao <bob@skylight.digital>
2023-08-16 08:22:04 -0400

    remove the extra annotation

--- backend/src/main/java/gov/cdc/usds/simplereport/service/supportescalation/SlackWebhookService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/supportescalation/SlackWebhookService.java
@@ -1,11 +1,9 @@
 package gov.cdc.usds.simplereport.service.supportescalation;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 @AllArgsConstructor
 public class SlackWebhookService {
 
