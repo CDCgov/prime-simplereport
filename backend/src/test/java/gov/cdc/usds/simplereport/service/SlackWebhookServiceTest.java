552e7c3159816392c62acbbef43154c8517b920b
Bob Zhao <bob@skylight.digital>
2023-08-16 12:40:33 -0400

    change test to have right permissions

--- backend/src/test/java/gov/cdc/usds/simplereport/service/SlackWebhookServiceTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/service/SlackWebhookServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 
 import gov.cdc.usds.simplereport.service.supportescalation.SlackConfigService;
 import gov.cdc.usds.simplereport.service.supportescalation.SlackWebhookService;
+import gov.cdc.usds.simplereport.test_util.SliceTestConfiguration;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ class SlackWebhookServiceTest extends BaseServiceTest<SlackWebhookService> {
   @Autowired @MockBean private SlackConfigService config;
 
   @Test
+  @SliceTestConfiguration.WithSimpleReportSiteAdminUser
   void sendSlackEscalationMessage_success() throws IOException {
     // GIVEN
     when(config.makeEscalationRequest()).thenReturn(true);
