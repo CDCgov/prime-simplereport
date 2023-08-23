6adaedf3634da81a3c746984f0cf686a98f6ae5d
Bob Zhao <bob@skylight.digital>
2023-08-23 15:45:45 -0400

    lint

--- backend/src/main/java/gov/cdc/usds/simplereport/api/apiuser/UserMutationResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/apiuser/UserMutationResolver.java
@@ -11,13 +11,11 @@ import gov.cdc.usds.simplereport.db.model.ApiUser;
 import gov.cdc.usds.simplereport.db.model.auxiliary.PersonName;
 import gov.cdc.usds.simplereport.service.ApiUserService;
 import gov.cdc.usds.simplereport.service.model.UserInfo;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -27,170 +25,170 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 public class UserMutationResolver {
 
-    private final ApiUserService _us;
+  private final ApiUserService _us;
 
-    public static final String MOVE_USER_ARGUMENT_ERROR =
-            "Moving a user to a new organization must specify a list of facilities for them to access or allow them access to all facilities";
+  public static final String MOVE_USER_ARGUMENT_ERROR =
+      "Moving a user to a new organization must specify a list of facilities for them to access or allow them access to all facilities";
 
-    public UserMutationResolver(ApiUserService us) {
-        _us = us;
+  public UserMutationResolver(ApiUserService us) {
+    _us = us;
+  }
+
+  @AuthorizationConfiguration.RequireGlobalAdminUser
+  @MutationMapping
+  public User addUser(@Argument UserInput user) {
+    Set<UUID> facilitySet =
+        user.getFacilities() == null ? Set.of() : new HashSet<>(user.getFacilities());
+    UserInfo userInfo =
+        _us.createUser(
+            user.getEmail(),
+            Translators.consolidateNameArguments(
+                user.getName(),
+                user.getFirstName(),
+                user.getMiddleName(),
+                user.getLastName(),
+                user.getSuffix()),
+            user.getOrganizationExternalId(),
+            user.getRole(),
+            user.isAccessAllFacilities(),
+            facilitySet);
+    return new User(userInfo);
+  }
+
+  @MutationMapping
+  public User addUserToCurrentOrg(@Argument UserInput userInput) {
+    Set<UUID> facilitySet =
+        userInput.getFacilities() == null
+            ? Collections.emptySet()
+            : new HashSet<>(userInput.getFacilities());
+    var user =
+        _us.createUserInCurrentOrg(
+            userInput.getEmail(),
+            Translators.consolidateNameArguments(
+                userInput.getName(),
+                userInput.getFirstName(),
+                userInput.getMiddleName(),
+                userInput.getLastName(),
+                userInput.getSuffix()),
+            userInput.getRole(),
+            userInput.isAccessAllFacilities(),
+            facilitySet);
+
+    return new User(user);
+  }
+
+  @MutationMapping
+  public ApiUser createApiUserNoOkta(
+      @Argument PersonName name,
+      @Argument String firstName,
+      @Argument String middleName,
+      @Argument String lastName,
+      @Argument String suffix,
+      @Argument String email) {
+    name = Translators.consolidateNameArguments(name, firstName, middleName, lastName, suffix);
+    return _us.createApiUserNoOkta(email, name);
+  }
+
+  @MutationMapping
+  public User updateUser(
+      @Argument UUID id,
+      @Argument PersonName name,
+      @Argument String firstName,
+      @Argument String middleName,
+      @Argument String lastName,
+      @Argument String suffix) {
+    name = Translators.consolidateNameArguments(name, firstName, middleName, lastName, suffix);
+    UserInfo user = _us.updateUser(id, name);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User updateUserPrivileges(
+      @Argument UUID id,
+      @Argument boolean accessAllFacilities,
+      @Argument List<UUID> facilities,
+      @Argument Role role) {
+    Set<UUID> facilitySet = facilities == null ? Set.of() : new HashSet<>(facilities);
+    UserInfo user = _us.updateUserPrivileges(id, accessAllFacilities, facilitySet, role);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User updateUserEmail(@Argument UUID id, @Argument String email) {
+    UserInfo user = _us.updateUserEmail(id, email);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User resetUserPassword(@Argument UUID id) {
+    UserInfo user = _us.resetUserPassword(id);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User reactivateUserAndResetPassword(@Argument UUID id) {
+    UserInfo user = _us.reactivateUserAndResetPassword(id);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User resetUserMfa(@Argument UUID id) {
+    UserInfo user = _us.resetUserMfa(id);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User setUserIsDeleted(@Argument UUID id, @Argument boolean deleted) {
+    UserInfo user = _us.setIsDeleted(id, deleted);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User reactivateUser(@Argument UUID id) {
+    UserInfo user = _us.reactivateUser(id);
+    return new User(user);
+  }
+
+  @MutationMapping
+  public User resendActivationEmail(@Argument UUID id) {
+    UserInfo user = _us.resendActivationEmail(id);
+    return new User(user);
+  }
+
+  @AuthorizationConfiguration.RequireGlobalAdminUser
+  @MutationMapping
+  public User setCurrentUserTenantDataAccess(
+      @Argument String organizationExternalId, @Argument String justification) {
+    UserInfo user =
+        _us.setCurrentUserTenantDataAccess(
+            Translators.parseStringNoTrim(organizationExternalId),
+            Translators.parseString(justification));
+    return new User(user);
+  }
+
+  @AuthorizationConfiguration.RequireGlobalAdminUser
+  @MutationMapping
+  public void updateUserPrivilegesAndGroupAccess(
+      @Argument String username,
+      @Argument String orgExternalId,
+      @Argument boolean accessAllFacilities,
+      @Argument List<UUID> facilities,
+      @Argument Role role) {
+    if (!accessAllFacilities && facilities.isEmpty()) {
+      throw new IllegalGraphqlArgumentException(MOVE_USER_ARGUMENT_ERROR);
     }
+    List<UUID> facilityIdsToAssign = facilities == null ? List.of() : facilities;
 
-    @AuthorizationConfiguration.RequireGlobalAdminUser
-    @MutationMapping
-    public User addUser(@Argument UserInput user) {
-        Set<UUID> facilitySet =
-                user.getFacilities() == null ? Set.of() : new HashSet<>(user.getFacilities());
-        UserInfo userInfo =
-                _us.createUser(
-                        user.getEmail(),
-                        Translators.consolidateNameArguments(
-                                user.getName(),
-                                user.getFirstName(),
-                                user.getMiddleName(),
-                                user.getLastName(),
-                                user.getSuffix()),
-                        user.getOrganizationExternalId(),
-                        user.getRole(),
-                        user.isAccessAllFacilities(),
-                        facilitySet);
-        return new User(userInfo);
+    try {
+      _us.updateUserPrivilegesAndGroupAccess(
+          username,
+          orgExternalId,
+          accessAllFacilities,
+          facilityIdsToAssign,
+          role.toOrganizationRole());
+    } catch (IllegalArgumentException e) {
+      throw new GenericGraphqlException(
+          "Error moving user to a new organization: ", e.getMessage());
     }
-
-    @MutationMapping
-    public User addUserToCurrentOrg(@Argument UserInput userInput) {
-        Set<UUID> facilitySet =
-                userInput.getFacilities() == null
-                        ? Collections.emptySet()
-                        : new HashSet<>(userInput.getFacilities());
-        var user =
-                _us.createUserInCurrentOrg(
-                        userInput.getEmail(),
-                        Translators.consolidateNameArguments(
-                                userInput.getName(),
-                                userInput.getFirstName(),
-                                userInput.getMiddleName(),
-                                userInput.getLastName(),
-                                userInput.getSuffix()),
-                        userInput.getRole(),
-                        userInput.isAccessAllFacilities(),
-                        facilitySet);
-
-        return new User(user);
-    }
-
-    @MutationMapping
-    public ApiUser createApiUserNoOkta(
-            @Argument PersonName name,
-            @Argument String firstName,
-            @Argument String middleName,
-            @Argument String lastName,
-            @Argument String suffix,
-            @Argument String email) {
-        name = Translators.consolidateNameArguments(name, firstName, middleName, lastName, suffix);
-        return _us.createApiUserNoOkta(email, name);
-    }
-
-    @MutationMapping
-    public User updateUser(
-            @Argument UUID id,
-            @Argument PersonName name,
-            @Argument String firstName,
-            @Argument String middleName,
-            @Argument String lastName,
-            @Argument String suffix) {
-        name = Translators.consolidateNameArguments(name, firstName, middleName, lastName, suffix);
-        UserInfo user = _us.updateUser(id, name);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User updateUserPrivileges(
-            @Argument UUID id,
-            @Argument boolean accessAllFacilities,
-            @Argument List<UUID> facilities,
-            @Argument Role role) {
-        Set<UUID> facilitySet = facilities == null ? Set.of() : new HashSet<>(facilities);
-        UserInfo user = _us.updateUserPrivileges(id, accessAllFacilities, facilitySet, role);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User updateUserEmail(@Argument UUID id, @Argument String email) {
-        UserInfo user = _us.updateUserEmail(id, email);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User resetUserPassword(@Argument UUID id) {
-        UserInfo user = _us.resetUserPassword(id);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User reactivateUserAndResetPassword(@Argument UUID id) {
-        UserInfo user = _us.reactivateUserAndResetPassword(id);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User resetUserMfa(@Argument UUID id) {
-        UserInfo user = _us.resetUserMfa(id);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User setUserIsDeleted(@Argument UUID id, @Argument boolean deleted) {
-        UserInfo user = _us.setIsDeleted(id, deleted);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User reactivateUser(@Argument UUID id) {
-        UserInfo user = _us.reactivateUser(id);
-        return new User(user);
-    }
-
-    @MutationMapping
-    public User resendActivationEmail(@Argument UUID id) {
-        UserInfo user = _us.resendActivationEmail(id);
-        return new User(user);
-    }
-
-    @AuthorizationConfiguration.RequireGlobalAdminUser
-    @MutationMapping
-    public User setCurrentUserTenantDataAccess(
-            @Argument String organizationExternalId, @Argument String justification) {
-        UserInfo user =
-                _us.setCurrentUserTenantDataAccess(
-                        Translators.parseStringNoTrim(organizationExternalId),
-                        Translators.parseString(justification));
-        return new User(user);
-    }
-
-    @AuthorizationConfiguration.RequireGlobalAdminUser
-    @MutationMapping
-    public void updateUserPrivilegesAndGroupAccess(
-            @Argument String username,
-            @Argument String orgExternalId,
-            @Argument boolean accessAllFacilities,
-            @Argument List<UUID> facilities,
-            @Argument Role role) {
-        if (!accessAllFacilities && facilities.isEmpty()) {
-            throw new IllegalGraphqlArgumentException(MOVE_USER_ARGUMENT_ERROR);
-        }
-        List<UUID> facilityIdsToAssign = facilities == null ? List.of() : facilities;
-
-        try {
-            _us.updateUserPrivilegesAndGroupAccess(
-                    username,
-                    orgExternalId,
-                    accessAllFacilities,
-                    facilityIdsToAssign,
-                    role.toOrganizationRole());
-        } catch (IllegalArgumentException e) {
-            throw new GenericGraphqlException(
-                    "Error moving user to a new organization: ", e.getMessage());
-        }
-    }
+  }
 }
