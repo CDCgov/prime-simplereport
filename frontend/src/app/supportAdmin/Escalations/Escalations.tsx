599547d9df118c1834a47e1f586db3f92f2b12ef
Bob Zhao <bob@skylight.digital>
2023-08-28 10:29:53 -0400

    align styles to other pages

--- frontend/src/app/supportAdmin/Escalations/Escalations.tsx
+++ frontend/src/app/supportAdmin/Escalations/Escalations.tsx
@@ -13,45 +13,45 @@ export const Escalations = () => {
 
   return (
     <div className="prime-home flex-1">
-      <div className="usa-card__header padding-top-2">
-        <div className="grid-container">
-          <div className="grid-row">
-            <form className="prime-container card-container">
-              <div className="usa-card__header padding-top-2">
-                <div className="width-full display-block  margin-top-3">
-                  <SupportHomeLink />
-                  <h1 className="desktop:grid-col-fill tablet:grid-col-fill mobile:grid-col-12 font-heading-lg margin-bottom-0">
-                    Escalate to engineering
-                  </h1>
-                </div>
+      <div className="grid-container">
+        <div className="prime-container card-container padding-bottom-3">
+          <div className="usa-card__header">
+            <div className="width-full">
+              <SupportHomeLink />
+              <div className="grid-row width-full margin-top-1">
+                <h1 className="desktop:grid-col-fill tablet:grid-col-fill mobile:grid-col-12 font-heading-lg margin-bottom-0">
+                  Escalate to engineering
+                </h1>
               </div>
-              <div className="usa-card__body margin-top-1">
-                <div className="grid-row grid-gap margin-top-2">
-                  <div className="tablet:grid-col">
-                    <p className="usa-prose">
-                      Use the below button to notify the SimpleReport
-                      engineering team about a tier 2 escalation. Make sure
-                      you've filled out information about the escalation in{" "}
-                      <a
-                        target="_blank"
-                        href="https://app.smartsheetgov.com/sheets/wwchRWw4jr55WpX8XwC2h9Mr9rVr3Q3hww64pW61"
-                        rel="noreferrer"
-                      >
-                        the Smartsheet tracker
-                      </a>
-                    </p>
-                    <Button
-                      className="width-half margin-top-2 submit-button"
-                      onClick={async () => {
-                        await escalateToSlack();
-                        showSuccess(
-                          "The engineering team has been notified that there's a new escalation and will be in touch shortly",
-                          `Escalation successfully sent`
-                        );
-                      }}
-                      label={"Submit escalation"}
-                    />
-                  </div>
+            </div>
+          </div>
+          <div className="usa-card__body margin-top-1">
+            <form>
+              <div className="grid-row grid-gap margin-top-2">
+                <div className="tablet:grid-col">
+                  <p className="usa-prose">
+                    Use the below button to notify the SimpleReport engineering
+                    team about a tier 2 escalation. Make sure you've filled out
+                    information about the escalation in{" "}
+                    <a
+                      target="_blank"
+                      href="https://app.smartsheetgov.com/sheets/wwchRWw4jr55WpX8XwC2h9Mr9rVr3Q3hww64pW61"
+                      rel="noreferrer"
+                    >
+                      the Smartsheet tracker
+                    </a>
+                  </p>
+                  <Button
+                    className="width-half margin-top-2 submit-button"
+                    onClick={async () => {
+                      await escalateToSlack();
+                      showSuccess(
+                        "The engineering team has been notified that there's a new escalation and will be in touch shortly",
+                        `Escalation successfully sent`
+                      );
+                    }}
+                    label={"Submit escalation"}
+                  />
                 </div>
               </div>
             </form>
