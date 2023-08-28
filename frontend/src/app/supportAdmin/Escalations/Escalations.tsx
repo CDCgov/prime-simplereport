47515c666352e7d368bf07a97f873fb1f3531c4b
Bob Zhao <bob@skylight.digital>
2023-08-28 10:21:22 -0400

    add backlink to escalation page

--- frontend/src/app/supportAdmin/Escalations/Escalations.tsx
+++ frontend/src/app/supportAdmin/Escalations/Escalations.tsx
@@ -5,6 +5,7 @@ import Button from "../../commonComponents/Button/Button";
 import { showSuccess } from "../../utils/srToast";
 import { useDocumentTitle } from "../../utils/hooks";
 import { escalationPageTitle } from "../pageTitles";
+import SupportHomeLink from "../SupportHomeLink";
 
 export const Escalations = () => {
   useDocumentTitle(escalationPageTitle);
@@ -12,44 +13,49 @@ export const Escalations = () => {
 
   return (
     <div className="prime-home flex-1">
-      <div className="grid-container">
-        <div className="grid-row">
-          <form className="prime-container card-container">
-            <div className="usa-card__header">
-              <h1 className="font-heading-lg margin-top-0 margin-bottom-0">
-                Escalate to engineering
-              </h1>
-            </div>
-            <div className="usa-card__body margin-top-1">
-              <div className="grid-row grid-gap margin-top-2">
-                <div className="tablet:grid-col">
-                  <p className="usa-prose">
-                    Use the below button to notify the SimpleReport engineering
-                    team about a tier 2 escalation. Make sure you've filled out
-                    information about the escalation in{" "}
-                    <a
-                      target="_blank"
-                      href="https://app.smartsheetgov.com/sheets/wwchRWw4jr55WpX8XwC2h9Mr9rVr3Q3hww64pW61"
-                      rel="noreferrer"
-                    >
-                      the Smartsheet tracker
-                    </a>
-                  </p>
-                  <Button
-                    className="width-half margin-top-2 submit-button"
-                    onClick={async () => {
-                      await escalateToSlack();
-                      showSuccess(
-                        "The engineering team has been notified that there's a new escalation and will be in touch shortly",
-                        `Escalation successfully sent`
-                      );
-                    }}
-                    label={"Submit escalation"}
-                  />
+      <div className="usa-card__header padding-top-2">
+        <div className="grid-container">
+          <div className="grid-row">
+            <form className="prime-container card-container">
+              <div className="usa-card__header padding-top-2">
+                <div className="width-full display-block  margin-top-3">
+                  <SupportHomeLink />
+                  <h1 className="desktop:grid-col-fill tablet:grid-col-fill mobile:grid-col-12 font-heading-lg margin-bottom-0">
+                    Escalate to engineering
+                  </h1>
                 </div>
               </div>
-            </div>
-          </form>
+              <div className="usa-card__body margin-top-1">
+                <div className="grid-row grid-gap margin-top-2">
+                  <div className="tablet:grid-col">
+                    <p className="usa-prose">
+                      Use the below button to notify the SimpleReport
+                      engineering team about a tier 2 escalation. Make sure
+                      you've filled out information about the escalation in{" "}
+                      <a
+                        target="_blank"
+                        href="https://app.smartsheetgov.com/sheets/wwchRWw4jr55WpX8XwC2h9Mr9rVr3Q3hww64pW61"
+                        rel="noreferrer"
+                      >
+                        the Smartsheet tracker
+                      </a>
+                    </p>
+                    <Button
+                      className="width-half margin-top-2 submit-button"
+                      onClick={async () => {
+                        await escalateToSlack();
+                        showSuccess(
+                          "The engineering team has been notified that there's a new escalation and will be in touch shortly",
+                          `Escalation successfully sent`
+                        );
+                      }}
+                      label={"Submit escalation"}
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
