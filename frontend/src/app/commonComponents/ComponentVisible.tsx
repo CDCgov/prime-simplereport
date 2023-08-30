89786f15574952c5328c5e55b4f35480fb93f2a6
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-08-30 15:47:00 -0400

    add focusin listener for useComponentVisible

--- frontend/src/app/commonComponents/ComponentVisible.tsx
+++ frontend/src/app/commonComponents/ComponentVisible.tsx
@@ -16,16 +16,18 @@ const useComponentVisible = (initialIsVisible: boolean) => {
     useState(initialIsVisible);
   const ref: LegacyRef<HTMLDivElement> | null = useRef(null);
 
-  const handleClickOutside = (event: any) => {
-    if (ref.current && !ref.current.contains(event.target)) {
+  const handleClickOutside = (event: Event) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
       setIsComponentVisible(false);
     }
   };
 
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
+    document.addEventListener("focusin", handleClickOutside, true);
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
+      document.removeEventListener("focusin", handleClickOutside, true);
     };
   });
   return { ref, isComponentVisible, setIsComponentVisible };
