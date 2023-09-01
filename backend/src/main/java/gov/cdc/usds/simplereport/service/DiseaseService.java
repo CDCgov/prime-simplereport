30f18c322ecccd7536a7727c04d970419db83bf0
Bob Zhao <bob@skylight.digital>
2023-09-01 15:07:05 -0400

    lint

--- backend/src/main/java/gov/cdc/usds/simplereport/service/DiseaseService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/DiseaseService.java
@@ -4,14 +4,12 @@ import static gov.cdc.usds.simplereport.config.CachingConfig.SUPPORTED_DISEASE_I
 
 import gov.cdc.usds.simplereport.db.model.SupportedDisease;
 import gov.cdc.usds.simplereport.db.repository.SupportedDiseaseRepository;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
@@ -22,51 +20,51 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class DiseaseService {
-    public static final String COVID19_NAME = "COVID-19";
-    public static final String FLU_A_NAME = "Flu A";
-    public static final String FLU_B_NAME = "Flu B";
-    public static final String FLU_A_AND_B_NAME = "Flu A and B";
-    public static final String FLU_RNA_NAME = "Flu RNA";
-    public static final String RSV_NAME = "RSV";
+  public static final String COVID19_NAME = "COVID-19";
+  public static final String FLU_A_NAME = "Flu A";
+  public static final String FLU_B_NAME = "Flu B";
+  public static final String FLU_A_AND_B_NAME = "Flu A and B";
+  public static final String FLU_RNA_NAME = "Flu RNA";
+  public static final String RSV_NAME = "RSV";
 
-    private final SupportedDiseaseRepository _supportedDiseaseRepo;
+  private final SupportedDiseaseRepository _supportedDiseaseRepo;
 
-    public void initDiseases() {
-        getKnownSupportedDiseasesMap();
-    }
+  public void initDiseases() {
+    getKnownSupportedDiseasesMap();
+  }
 
-    public List<SupportedDisease> fetchSupportedDiseases() {
-        return getDiseaseNameToSupportedDiseaseMap().values().stream().toList();
-    }
+  public List<SupportedDisease> fetchSupportedDiseases() {
+    return getDiseaseNameToSupportedDiseaseMap().values().stream().toList();
+  }
 
-    @Cacheable(SUPPORTED_DISEASE_ID_MAP)
-    public Map<UUID, SupportedDisease> getKnownSupportedDiseasesMap() {
-        return _supportedDiseaseRepo.findAll().stream()
-                .collect(Collectors.toMap(SupportedDisease::getInternalId, Function.identity()));
-    }
+  @Cacheable(SUPPORTED_DISEASE_ID_MAP)
+  public Map<UUID, SupportedDisease> getKnownSupportedDiseasesMap() {
+    return _supportedDiseaseRepo.findAll().stream()
+        .collect(Collectors.toMap(SupportedDisease::getInternalId, Function.identity()));
+  }
 
-    public Map<String, SupportedDisease> getDiseaseNameToSupportedDiseaseMap() {
-        return getKnownSupportedDiseasesMap().values().stream()
-                .collect(Collectors.toMap(SupportedDisease::getName, Function.identity()));
-    }
+  public Map<String, SupportedDisease> getDiseaseNameToSupportedDiseaseMap() {
+    return getKnownSupportedDiseasesMap().values().stream()
+        .collect(Collectors.toMap(SupportedDisease::getName, Function.identity()));
+  }
 
-    public SupportedDisease getDiseaseByName(String name) {
-        return getDiseaseNameToSupportedDiseaseMap().get(name);
-    }
+  public SupportedDisease getDiseaseByName(String name) {
+    return getDiseaseNameToSupportedDiseaseMap().get(name);
+  }
 
-    public SupportedDisease covid() {
-        return getDiseaseByName(COVID19_NAME);
-    }
+  public SupportedDisease covid() {
+    return getDiseaseByName(COVID19_NAME);
+  }
 
-    public SupportedDisease fluA() {
-        return getDiseaseByName(FLU_A_NAME);
-    }
+  public SupportedDisease fluA() {
+    return getDiseaseByName(FLU_A_NAME);
+  }
 
-    public SupportedDisease fluB() {
-        return getDiseaseByName(FLU_B_NAME);
-    }
+  public SupportedDisease fluB() {
+    return getDiseaseByName(FLU_B_NAME);
+  }
 
-    public SupportedDisease rsv() {
-        return getDiseaseByName(RSV_NAME);
-    }
+  public SupportedDisease rsv() {
+    return getDiseaseByName(RSV_NAME);
+  }
 }
