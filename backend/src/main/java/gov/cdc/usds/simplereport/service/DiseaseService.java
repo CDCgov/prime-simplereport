03aaad75443765fe6dbb07aeb5f91d00a748190c
Bob Zhao <bob@skylight.digital>
2023-09-01 13:58:55 -0400

    naive implementation?

--- backend/src/main/java/gov/cdc/usds/simplereport/service/DiseaseService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/DiseaseService.java
@@ -2,74 +2,57 @@ package gov.cdc.usds.simplereport.service;
 
 import gov.cdc.usds.simplereport.db.model.SupportedDisease;
 import gov.cdc.usds.simplereport.db.repository.SupportedDiseaseRepository;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import static gov.cdc.usds.simplereport.config.CachingConfig.*;
 
 @Service
 @Transactional
 @Slf4j
 @RequiredArgsConstructor
 public class DiseaseService {
-  public static final String COVID19_NAME = "COVID-19";
-  public static final String FLU_A_NAME = "Flu A";
-  public static final String FLU_B_NAME = "Flu B";
-  public static final String FLU_A_AND_B_NAME = "Flu A and B";
-  public static final String FLU_RNA_NAME = "Flu RNA";
-  public static final String RSV_NAME = "RSV";
+    public static final String COVID19_NAME = "COVID-19";
+    public static final String FLU_A_NAME = "Flu A";
+    public static final String FLU_B_NAME = "Flu B";
+    public static final String FLU_A_AND_B_NAME = "Flu A and B";
+    public static final String FLU_RNA_NAME = "Flu RNA";
+    public static final String RSV_NAME = "RSV";
 
-  private final SupportedDiseaseRepository _supportedDiseaseRepo;
+    private final SupportedDiseaseRepository _supportedDiseaseRepo;
+    
+    public void initDiseases() {
+        getKnownSupportedDiseasesMap();
+    }
 
-  private SupportedDisease covid;
-  private SupportedDisease fluA;
-  private SupportedDisease fluB;
+    public List<SupportedDisease> fetchSupportedDiseases() {
+        return getDiseaseNameToSupportedDiseaseMap().values().stream().toList();
+    }
 
-  private final Map<UUID, SupportedDisease> supportedDiseaseMap = new HashMap<>();
+    @Cacheable(SUPPORTED_DISEASE_ID_MAP)
+    public Map<UUID, SupportedDisease> getKnownSupportedDiseasesMap() {
+        return _supportedDiseaseRepo.findAll().stream()
+                .collect(Collectors.toMap(SupportedDisease::getInternalId, Function.identity()));
+    }
 
-  public void initDiseases() {
-    covid = _supportedDiseaseRepo.findByName(COVID19_NAME).orElse(null);
-    fluA = _supportedDiseaseRepo.findByName(FLU_A_NAME).orElse(null);
-    fluB = _supportedDiseaseRepo.findByName(FLU_B_NAME).orElse(null);
+    public Map<String, SupportedDisease> getDiseaseNameToSupportedDiseaseMap() {
+        return getKnownSupportedDiseasesMap().values().stream()
+                .collect(Collectors.toMap(SupportedDisease::getName, Function.identity()));
+    }
 
-    Optional.ofNullable(covid).ifPresent(sd -> supportedDiseaseMap.put(sd.getInternalId(), sd));
-    Optional.ofNullable(fluA).ifPresent(sd -> supportedDiseaseMap.put(sd.getInternalId(), sd));
-    Optional.ofNullable(fluB).ifPresent(sd -> supportedDiseaseMap.put(sd.getInternalId(), sd));
-  }
+    public SupportedDisease getDiseaseByName(String name) {
+        return getDiseaseNameToSupportedDiseaseMap().get(name);
+    }
 
-  public List<SupportedDisease> fetchSupportedDiseases() {
-    return _supportedDiseaseRepo.findAll();
-  }
-
-  public Map<UUID, SupportedDisease> getKnownSupportedDiseasesMap() {
-    return supportedDiseaseMap;
-  }
-
-  public SupportedDisease getDiseaseByName(String name) {
-    return switch (name) {
-      case COVID19_NAME -> covid;
-      case FLU_A_NAME -> fluA;
-      case FLU_B_NAME -> fluB;
-      default -> _supportedDiseaseRepo
-          .findByName(name)
-          .orElseThrow(() -> new IllegalArgumentException("Disease not found"));
-    };
-  }
-
-  public SupportedDisease covid() {
-    return covid;
-  }
-
-  public SupportedDisease fluA() {
-    return fluA;
-  }
-
-  public SupportedDisease fluB() {
-    return fluB;
-  }
 }
