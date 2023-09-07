36c099f74ea91716547b4a33a04eaec92b841fd2
Bob Zhao <bob@skylight.digital>
2023-09-07 12:21:58 -0400

    add frontend rsv feature flag

--- frontend/src/app/supportAdmin/DeviceType/ManageDeviceTypeFormContainer.tsx
+++ frontend/src/app/supportAdmin/DeviceType/ManageDeviceTypeFormContainer.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import { useFeature } from "flagged";
 
 import {
   DeviceType,
@@ -19,6 +20,7 @@ import DeviceForm from "./DeviceForm";
 
 const ManageDeviceTypeFormContainer = () => {
   useDocumentTitle(editDevicePageTitle);
+  const rsvEnabled = useFeature("rsvEnabled");
 
   const [submitted, setSubmitted] = useState(false);
   const [activeFacility] = useSelectedFacility();
@@ -72,14 +74,18 @@ const ManageDeviceTypeFormContainer = () => {
       }))
     );
 
-    const supportedDiseaseOptions = Array.from(
-      supportedDiseaseResults.supportedDiseases
-        .map((disease) => ({
-          label: disease.name,
-          value: disease.internalId,
-        }))
-        .filter((d) => d.label !== "RSV")
+    const supportedDiseaseData = supportedDiseaseResults.supportedDiseases.map(
+      (disease) => ({
+        label: disease.name,
+        value: disease.internalId,
+      })
     );
+
+    const supportedDiseaseOptions = rsvEnabled
+      ? supportedDiseaseData
+      : supportedDiseaseData.filter((d) => d.label !== "RSV");
+
+    console.log(supportedDiseaseOptions);
 
     const devices = Array.from(
       deviceTypeResults.deviceTypes.map(
