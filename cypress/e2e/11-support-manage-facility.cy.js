8b99a2743cfec24643acdde6cd361a5c8e0c5135
fzhao99 <bob@skylight.digital>
2023-09-07 10:52:16 -0400

    replace manage facility admin screen dropdowns with combos (#6439)
    * refactor using truss
    
    * add tests
    
    * temporarily wrap things in act
    
    * fix accessibility violations and refactor tests
    
    * scope down unnecessary changes
    
    * use recommended userEvent setup pattern
    
    * fix mocks
    
    * move gql alias to before each hook
    
    * switch selector
    
    * rework e2e to work with combo box
    
    * add org util query to simplify selectors
    
    * add extracted combo box component
    
    * add forwardRef hook
    
    * relax label selectors

--- cypress/e2e/11-support-manage-facility.cy.js
+++ cypress/e2e/11-support-manage-facility.cy.js
@@ -1,29 +1,37 @@
-import {loginHooks, testNumber} from "../support/e2e";
-import {addMockFacility, whoAmI} from "../utils/testing-data-utils";
-import {graphqlURL} from "../utils/request-utils";
-import {aliasGraphqlOperations} from "../utils/graphql-test-utils";
+import { loginHooks, testNumber } from "../support/e2e";
+import { addMockFacility, whoAmI, getOrganizationById } from "../utils/testing-data-utils";
+import { graphqlURL } from "../utils/request-utils";
+import { aliasGraphqlOperations } from "../utils/graphql-test-utils";
 
 loginHooks();
 describe("Support admin: manage facility", () => {
-  let organizationId="";
+  let organizationId = "";
+  let organizationName = "";
   let facilityId = "";
   let facilityCreated = {
-    id:"",
-    name:`RainbowCenter-${testNumber()}`,
-  }
+    id: "",
+    name: `RainbowCenter-${testNumber()}`
+  };
 
   before(() => {
-    addMockFacility(facilityCreated.name).then(response=>{
+    addMockFacility(facilityCreated.name).then(response => {
       facilityCreated.id = response.body.data.addFacility.id;
     });
 
     whoAmI().then((res) => {
       organizationId = res.body.data.whoami.organization.id;
       facilityId = res.body.data.whoami.organization.facilities[0].id;
+
+      getOrganizationById(organizationId).then((res) => {
+        organizationName = res.body.data.organization.name;
+      });
     });
 
+  });
+
+  beforeEach(() => {
     cy.intercept("POST", graphqlURL, (req) => {
-      aliasGraphqlOperations(req)
+      aliasGraphqlOperations(req);
     });
   });
 
@@ -38,11 +46,13 @@ describe("Support admin: manage facility", () => {
 
     // selects organization
     cy.wait("@GetAllOrganizations");
-    cy.get("div.bg-base-lightest select").first().select(organizationId);
 
-    // selects facility
+    // selects org combo box
+    cy.get("input[role=\"combobox\"]").first().type(`${organizationName}{enter}`);
+
+    // selects facility combo box
     cy.wait("@GetFacilitiesByOrgId");
-    cy.get("div.bg-base-lightest select").eq(1).select(facilityCreated.id);
+    cy.get("input[role=\"combobox\"]").last().type(`${facilityCreated.name}{enter}`);
 
     // clicks search button
     cy.get("button").contains("Search").click();
@@ -62,7 +72,7 @@ describe("Support admin: manage facility", () => {
     cy.contains(`Delete ${facilityCreated.name}`).should("not.exist");
   });
 
-  it("Deletes a facility",()=>{
+  it("Deletes a facility", () => {
     cy.get("button").contains("Delete facility").click();
     cy.get("button").contains("Yes, delete facility").click();
     cy.get(".Toastify").contains(`Facility ${facilityCreated.name} successfully deleted`);
---
+++ frontend/src/app/commonComponents/ComboBox.tsx
@@ -0,0 +1,35 @@
+import { ComboBox as TrussComboBox, Label } from "@trussworks/react-uswds";
+import React, { ComponentPropsWithRef, forwardRef } from "react";
+
+import Required from "./Required";
+
+type TrussComponentProps = ComponentPropsWithRef<typeof TrussComboBox>;
+export type ComboBoxProps = TrussComponentProps & {
+  required?: boolean;
+};
+
+// props here are only the required ones from Truss and can be extended
+// as needed with additional values.
+const ComboBox: React.FC<ComboBoxProps> = forwardRef(function (
+  { id, name, required, options, onChange, disabled }: ComboBoxProps,
+  ref
+) {
+  return (
+    <>
+      <Label htmlFor={id}>
+        {name} {required && <Required />}
+      </Label>
+      <TrussComboBox
+        name={name}
+        id={id}
+        options={options}
+        onChange={onChange}
+        disabled={disabled}
+        ref={ref}
+        inputProps={{ "aria-required": required }}
+      />
+    </>
+  );
+});
+
+export default ComboBox;
