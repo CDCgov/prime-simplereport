174b00a39181e9bda85901f510fa99dc902dc867
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-09-12 15:10:33 -0400

    add toString to types used during sync

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/SupportedDiseaseTestPerformedInput.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/SupportedDiseaseTestPerformedInput.java
@@ -3,9 +3,11 @@ package gov.cdc.usds.simplereport.api.model;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class SupportedDiseaseTestPerformedInput {
   UUID supportedDisease;
   String testPerformedLoincCode;
