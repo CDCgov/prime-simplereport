57b214809d6090ae3e8bc5a1ba78152f6db06e50
Bob Zhao <bob@skylight.digital>
2023-09-13 14:10:28 -0400

    try some more stuff

--- backend/src/main/java/gov/cdc/usds/simplereport/service/DataHubClient.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/DataHubClient.java
@@ -5,9 +5,11 @@ import gov.cdc.usds.simplereport.config.DataHubClientConfiguration;
 import gov.cdc.usds.simplereport.service.model.reportstream.LIVDResponse;
 import gov.cdc.usds.simplereport.service.model.reportstream.TokenResponse;
 import gov.cdc.usds.simplereport.service.model.reportstream.UploadResponse;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,27 +18,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
-    name = "datahub",
-    url = "${datahub.url}",
-    configuration = DataHubClientConfiguration.class)
+        name = "datahub",
+        url = "${datahub.url}",
+        configuration = DataHubClientConfiguration.class)
 public interface DataHubClient {
-  public String TOKEN_HEADER_TYPE = "application/x-www-form-urlencoded";
+    String TOKEN_HEADER_TYPE = "application/x-www-form-urlencoded";
 
-  @PostMapping(value = "/api/reports?processing=async", consumes = "text/csv")
-  UploadResponse uploadCSV(@Param("file") byte[] file);
+    @PostMapping(value = "/api/reports?processing=async", consumes = "text/csv")
+    UploadResponse uploadCSV(@Param("file") byte[] file);
 
-  @PostMapping(value = "/api/waters", consumes = "application/fhir+ndjson")
-  UploadResponse uploadFhir(
-      @RequestBody() String fhirNDJson, @RequestHeader(value = "Authorization") String authHeader);
+    @PostMapping(value = "/api/waters", consumes = "application/fhir+ndjson")
+    UploadResponse uploadFhir(
+            @RequestBody() String fhirNDJson, @RequestHeader(value = "Authorization") String authHeader);
 
-  @PostMapping(value = "/api/token", headers = "Content-Type: " + TOKEN_HEADER_TYPE)
-  TokenResponse fetchAccessToken(@RequestBody() Map<String, String> parameters);
+    @PostMapping(value = "/api/token", headers = "Content-Type: " + TOKEN_HEADER_TYPE)
+    TokenResponse fetchAccessToken(@RequestBody() Map<String, String> parameters);
 
-  @GetMapping(value = "/api/waters/report/{id}/history", consumes = "application/text")
-  UploadResponse getSubmission(
-      @PathVariable("id") UUID id,
-      @RequestHeader(value = "Authorization", required = true) String authorizationHeader);
+    @GetMapping(value = "/api/waters/report/{id}/history", consumes = "application/text")
+    UploadResponse getSubmission(
+            @PathVariable("id") UUID id,
+            @RequestHeader(value = "Authorization", required = true) String authorizationHeader);
 
-  @GetMapping(value = "/api/metadata/livd", consumes = "application/text")
-  List<LIVDResponse> getLIVDTable();
+    @GetMapping(value = "/api/metadata/livd", consumes = "application/text")
+    List<LIVDResponse> getLIVDTable();
 }
