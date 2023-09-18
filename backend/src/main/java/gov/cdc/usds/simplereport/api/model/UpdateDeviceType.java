7ee5d5e4766cd1c1fa648fdbd405a60d91e58d93
Boban <BobanL@users.noreply.github.com>
2023-09-18 14:12:24 -0400

    add rsv to livd table sync (#6544)
    * add toString to types used during sync
    
    * add dry run to sync
    
    * add rsv to sync
    
    * refactor syncDevice to own method
    
    * refactor createDevice to own method
    
    * refactor additional method to add to specimenIdsByDevice

--- backend/src/main/java/gov/cdc/usds/simplereport/api/model/UpdateDeviceType.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/model/UpdateDeviceType.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class UpdateDeviceType {
   private UUID internalId;
   private String name;
