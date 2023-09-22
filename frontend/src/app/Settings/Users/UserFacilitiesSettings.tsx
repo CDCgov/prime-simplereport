90c1a386029fd450dfd972a0ba6aa0ac93192c82
Johanna <103958711+johanna-skylight@users.noreply.github.com>
2023-09-22 11:19:23 -0400

    Johanna/6163 site admin update user access tab (#6499)
    * Rough implementation of manage user access
    
    * removed comment
    
    * Happy path working
    
    * Fixed admin setup
    
    * Added integration with backend
    
    * Added org update confirmation modal
    
    * Implemented confirmation modal
    
    * Implemented warning modals
    
    * Happy path implemented
    
    * Use useController instead of controller.
    
    * Addressed code smells and bug
    
    * Renaming of tab
    
    * Fixed flickering of save changes button
    
    * Removed todo
    
    * Fixed radio button selection disappearing
    
    * Added unit testing
    
    * fixed assertion
    
    * added unit testing and fixed race condition
    
    * Fixed feedback
    
    * Org tab will be disabled only for deleted users
    
    * Added apollo config property
    
    * removed space
    
    * Fixed glitch of radio button dissapearing
    
    * Added key to force component re-render
    
    * Resets form inner state if a search with no found user is executed.
    
    * Test taking too long

--- frontend/src/app/Settings/Users/UserFacilitiesSettings.tsx
+++ frontend/src/app/Settings/Users/UserFacilitiesSettings.tsx
@@ -1,85 +1,79 @@
 import React from "react";
-import { FieldErrors, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { UseFormRegister, UseFormSetValue, FieldError } from "react-hook-form";
 
 import Checkboxes from "../../commonComponents/Checkboxes";
-import Required from "../../commonComponents/Required";
+import { Facility } from "../../../generated/graphql";
 
-import { UserFacilitySetting } from "./ManageUsersContainer";
-import "./ManageUsers.scss";
-import { CreateUser } from "./CreateUserSchema";
 import { alphabeticalFacilitySort } from "./UserFacilitiesSettingsForm";
 
 interface UserFacilitiesSettingProps {
-  formValues: CreateUser;
-  allFacilities: UserFacilitySetting[];
-  register: UseFormRegister<CreateUser>;
-  errors: FieldErrors<CreateUser>;
-  setValue: UseFormSetValue<CreateUser>;
+  roleSelected: string;
+  facilityList: Pick<Facility, "id" | "name">[];
+  register: UseFormRegister<any>;
+  error?: FieldError;
+  setValue: UseFormSetValue<any>;
+  disabled?: boolean;
 }
 
 // This is the react-hook-form supported version of UserFacilitiesSettingsForm.
 const UserFacilitiesSettings: React.FC<UserFacilitiesSettingProps> = ({
-  formValues,
-  allFacilities,
+  roleSelected,
+  facilityList,
   register,
-  errors,
+  error,
   setValue,
+  disabled,
 }) => {
-  const isAdmin = formValues.role === "ADMIN";
+  const isAdmin = roleSelected === "ADMIN";
+
   const facilityAccessDescription = isAdmin
     ? "Admins have access to all facilities"
     : "All users must have access to at least one facility";
-  allFacilities.sort(alphabeticalFacilitySort);
+
+  const allFacilities = [
+    {
+      name: `Access all facilities (${facilityList?.length || 0})`,
+      id: "ALL_FACILITIES",
+    },
+  ].concat(facilityList?.sort(alphabeticalFacilitySort) || []);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, checked } = e.target;
     if (value === "ALL_FACILITIES" && checked && setValue) {
       setValue("facilityIds", [
-        "ALL_FACILITIES",
         ...allFacilities.map((facility) => facility.id),
       ]);
     }
   };
 
-  let boxes = [
-    {
-      value: "ALL_FACILITIES",
-      label: `Access all facilities (${allFacilities.length})`,
-      ...register("facilityIds", {
-        required: "At least one facility must be selected",
-        disabled: isAdmin,
-        onChange,
-      }),
-    },
-    ...allFacilities.map((facility) => ({
-      value: facility.id,
-      label: facility.name,
-      ...register("facilityIds", {
-        required: "At least one facility must be selected",
-        disabled: isAdmin,
-        onChange,
-      }),
-    })),
-  ];
+  const validateAtLeastOneCheck = (selectedOptions: string[]) => {
+    return isAdmin || selectedOptions.length > 0;
+  };
+
+  const formRegistrationProps =
+    register("facilityIds", {
+      validate: validateAtLeastOneCheck,
+      onChange,
+    }) || {};
+
+  let boxes = allFacilities?.map((facility) => ({
+    value: facility.id,
+    label: facility.name,
+    ...formRegistrationProps,
+  }));
 
   return (
-    <>
-      <h4 className="testing-facility-access-subheader margin-bottom-0">
-        <Required label={"Testing facility access"} />
-      </h4>
-      <p className="testing-facility-access-subtext">
-        {facilityAccessDescription}
-      </p>
-      <Checkboxes
-        boxes={boxes}
-        legend="Facilities"
-        legendSrOnly
-        name="facilities"
-        onChange={onChange}
-        validationStatus={errors?.facilityIds?.type ? "error" : undefined}
-        errorMessage={errors?.facilityIds?.message}
-      />
-    </>
+    <Checkboxes
+      boxes={boxes}
+      legend="Facilities access"
+      hintText={facilityAccessDescription}
+      name="facilities"
+      required
+      onChange={() => {}}
+      validationStatus={error?.type ? "error" : undefined}
+      errorMessage={"At least one facility must be selected"}
+      disabled={disabled}
+    />
   );
 };
 
