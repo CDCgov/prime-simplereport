bc207f7f8bbd11fa720fea583be12aaddc051a64
Johanna <johanna@skylight.digital>
2023-09-27 13:29:13 -0400

    Fist batch of clean up

--- frontend/src/app/ReportingApp.test.tsx
+++ frontend/src/app/ReportingApp.test.tsx
@@ -29,6 +29,7 @@ import {
 import { getAppInsights } from "./TelemetryService";
 import mockSupportedDiseaseMultiplex from "./testQueue/mocks/mockSupportedDiseaseMultiplex";
 import mockSupportedDiseaseCovid from "./testQueue/mocks/mockSupportedDiseaseCovid";
+import { WhoAmIQueryMock } from "./OperationMocks";
 
 const mockDispatch = jest.fn();
 
@@ -96,50 +97,6 @@ const store = {
 
 store.dispatch = mockDispatch;
 
-export const WhoAmIQueryMock = {
-  request: {
-    query: WHOAMI_QUERY,
-    fetchPolicy: "no-cache",
-  },
-  result: {
-    data: {
-      whoami: {
-        id: "05b2f71a-9392-442b-aab5-4eb550a864c0",
-        firstName: "Bob",
-        middleName: null,
-        lastName: "Bobberoo",
-        suffix: null,
-        email: "bob@example.com",
-        isAdmin: false,
-        permissions: [
-          "EDIT_PATIENT",
-          "ARCHIVE_PATIENT",
-          "READ_PATIENT_LIST",
-          "EDIT_ORGANIZATION",
-          "START_TEST",
-          "EDIT_FACILITY",
-          "ACCESS_ALL_FACILITIES",
-          "READ_RESULT_LIST",
-          "READ_ARCHIVED_PATIENT_LIST",
-          "SUBMIT_TEST",
-          "MANAGE_USERS",
-          "SEARCH_PATIENTS",
-          "UPDATE_TEST",
-        ],
-        roleDescription: "Admin user",
-        organization: {
-          name: "Dis Organization",
-          testingFacility: [
-            {
-              id: "fec4de56-f4cc-4c61-b3d5-76869ca71296",
-              name: "Testing Site",
-            },
-          ],
-        },
-      },
-    },
-  },
-};
 const facilityQueryMock = {
   request: {
     query: GetFacilityQueueDocument,
