7c91d8dbaaf1aeb56ec78b19c32433377250c2a8
Johanna <johanna@skylight.digital>
2023-09-28 15:25:19 -0400

    Final round of clean up

--- frontend/src/app/signUp/IdentityVerification/PersonalDetailsForm.test.tsx
+++ frontend/src/app/signUp/IdentityVerification/PersonalDetailsForm.test.tsx
@@ -1,10 +1,4 @@
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  act,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import PersonalDetailsForm from "./PersonalDetailsForm";
@@ -18,41 +12,42 @@ const fillInText = (label: string, text: string) => {
 };
 
 describe("PersonalDetailsForm", () => {
-  beforeEach(() => {
-    render(
+  const renderWithUser = () => ({
+    user: userEvent.setup(),
+    ...render(
       <PersonalDetailsForm
         orgExternalId="foo"
         firstName={"Bob"}
         middleName={"Rob"}
         lastName={"Bobberton"}
       />
-    );
+    ),
   });
-
   it("displays the users full name", () => {
+    renderWithUser();
     expect(screen.getByText("Bob Rob Bobberton")).toBeInTheDocument();
   });
 
   it("initializes with the submit button disabled", () => {
+    renderWithUser();
     expect(screen.getByText("Submit")).toBeDisabled();
   });
 
   describe("Filling out the form", () => {
-    beforeEach(async () => {
-      await act(
-        async () =>
-          await userEvent.type(
-            screen.getByLabelText("Email *", { exact: false }),
-            "bob@bob.bob"
-          )
-      );
-    });
+    const renderAndTypeEmail = async () => {
+      const { user, ...renderControls } = renderWithUser();
+      await user.click(screen.getByLabelText("Email *", { exact: false }));
+      await user.paste("bob@bob.bob");
+      return { user, ...renderControls };
+    };
 
-    it("enables the submit button", () => {
+    it("enables the submit button", async () => {
+      await renderAndTypeEmail();
       expect(screen.getByText("Submit")).toBeEnabled();
     });
     describe("focusing and not adding a value", () => {
       it("shows a single error", async () => {
+        await renderAndTypeEmail();
         screen.getByLabelText("Phone number *", { exact: false }).focus();
         screen.getByLabelText("Street address 1", { exact: false }).focus();
         expect(
@@ -62,16 +57,13 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On submitting with an invalid phone number", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText("Phone number", { exact: false }),
-              "123"
-            )
+        const { user } = await renderAndTypeEmail();
+
+        await user.type(
+          screen.getByLabelText("Phone number", { exact: false }),
+          "123"
         );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        await user.click(screen.getByText("Submit"));
         expect(
           await screen.findByText("A valid phone number is required")
         ).toBeInTheDocument();
@@ -82,16 +74,12 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On submitting an invalid street address 1", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText("Street address 1", { exact: false }),
-              "111 greendale dr,"
-            )
+        const { user } = await renderAndTypeEmail();
+        await user.click(
+          screen.getByLabelText("Street address 1", { exact: false })
         );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        await user.paste("111 greendale dr,");
+        await user.click(screen.getByText("Submit"));
         expect(
           await screen.findByText("A valid street address is required")
         ).toBeInTheDocument();
@@ -99,16 +87,12 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On entering an invalid date of birth and submitting", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText("Date of birth", { exact: false }),
-              "01/01/9999"
-            )
+        const { user } = await renderAndTypeEmail();
+        await user.click(
+          screen.getByLabelText("Date of birth", { exact: false })
         );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        await user.paste("01/01/9999");
+        await user.click(screen.getByText("Submit"));
         expect(
           await screen.findByText("A valid date of birth is required")
         ).toBeInTheDocument();
@@ -116,16 +100,10 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On clicking a valid date of birth and submitting", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText(/Date of birth/i),
-              "09/15/2022"
-            )
-        );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        const { user } = await renderAndTypeEmail();
+        await user.click(screen.getByLabelText(/Date of birth/i));
+        await user.paste("09/15/2022");
+        await user.click(screen.getByText("Submit"));
         await waitFor(() =>
           expect(screen.queryByText("A valid date of birth is required"))
         );
@@ -133,16 +111,12 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On submitting an invalid street address 2", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText("Street address 2", { exact: false }),
-              "111 greendale dr,"
-            )
+        const { user } = await renderAndTypeEmail();
+        await user.click(
+          screen.getByLabelText("Street address 2", { exact: false })
         );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        await user.paste("111 greendale dr,");
+        await user.click(screen.getByText("Submit"));
         expect(
           await screen.findByText("Street 2 contains invalid symbols")
         ).toBeInTheDocument();
@@ -150,16 +124,10 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On submitting an invalid zip code", () => {
       it("shows an error", async () => {
-        await act(
-          async () =>
-            await userEvent.type(
-              screen.getByLabelText("ZIP code", { exact: false }),
-              "1234"
-            )
-        );
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        const { user } = await renderAndTypeEmail();
+        await user.click(screen.getByLabelText("ZIP code", { exact: false }));
+        await user.paste("1234");
+        await user.click(screen.getByText("Submit"));
         expect(
           await screen.findByText("A valid ZIP code is required")
         ).toBeInTheDocument();
@@ -167,10 +135,9 @@ describe("PersonalDetailsForm", () => {
     });
     describe("On submitting an incomplete form", () => {
       it("shows an error", async () => {
+        const { user } = await renderAndTypeEmail();
         fillInText("Email", "bob@bob.bob");
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        await user.click(screen.getByText("Submit"));
         await waitFor(() => {
           expect(
             screen.queryAllByText("is required", { exact: false }).length
@@ -181,21 +148,17 @@ describe("PersonalDetailsForm", () => {
   });
 
   describe("Completed form", () => {
-    beforeEach(() => {
-      fillInText("Date of birth", "09/15/2022");
-      fillInText("Email", "bob@bob.bob");
-      fillInText("Phone number", "530-867-5309");
-      fillInText("Street address 1", "123 Bob St");
-      fillInText("City", "Bobtown");
-      fillInText("State", "CA");
-      fillInText("ZIP code", "74783");
-    });
-
     describe("On submit", () => {
       it("does not shows an error", async () => {
-        await act(
-          async () => await userEvent.click(screen.getByText("Submit"))
-        );
+        const { user } = renderWithUser();
+        fillInText("Date of birth", "09/15/2022");
+        fillInText("Email", "bob@bob.bob");
+        fillInText("Phone number", "530-867-5309");
+        fillInText("Street address 1", "123 Bob St");
+        fillInText("City", "Bobtown");
+        fillInText("State", "CA");
+        fillInText("ZIP code", "74783");
+        await user.click(screen.getByText("Submit"));
         await waitFor(() => {
           expect(screen.queryAllByText("is required").length).toBe(0);
         });
