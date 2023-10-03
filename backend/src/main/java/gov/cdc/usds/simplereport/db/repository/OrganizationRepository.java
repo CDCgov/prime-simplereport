7a58da60549abcae6ccbe44c05055492c420a682
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-10-03 13:50:00 -0400

    Revert "add @Repository to OrganizationRepository"
    This reverts commit ab58356a92137fc4d8b4e795e94f45a67c60a423.

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepository.java
@@ -5,9 +5,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
+/** Interface specification for fetching and manipulating {@link Organization} entities */
 public interface OrganizationRepository extends EternalAuditedEntityRepository<Organization> {
 
   @Query(EternalAuditedEntityRepository.BASE_QUERY + " and e.externalId = :externalId")
