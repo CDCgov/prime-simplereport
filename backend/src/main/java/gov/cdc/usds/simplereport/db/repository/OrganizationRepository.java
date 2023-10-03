ab58356a92137fc4d8b4e795e94f45a67c60a423
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-10-03 13:16:08 -0400

    add @Repository to OrganizationRepository
    to potentially fix this error:
    Error creating bean with name 'organizationRepository' defined in gov.cdc.usds.simplereport.db.repository.OrganizationRepository defined in @EnableJpaRepositories declared on JpaRepositoriesRegistrar.EnableJpaRepositoriesConfiguration: Cannot resolve reference to bean 'jpaSharedEM_entityManagerFactory' while setting bean property 'entityManager'

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/OrganizationRepository.java
@@ -5,8 +5,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-/** Interface specification for fetching and manipulating {@link Organization} entities */
+@Repository
 public interface OrganizationRepository extends EternalAuditedEntityRepository<Organization> {
 
   @Query(EternalAuditedEntityRepository.BASE_QUERY + " and e.externalId = :externalId")
