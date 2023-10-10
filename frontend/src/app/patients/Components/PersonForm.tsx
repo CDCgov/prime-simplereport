fef9c2834c470836b5ef5465f3ccdbea36e6cc52
Johanna <103958711+johanna-skylight@users.noreply.github.com>
2023-10-10 13:08:18 -0400

    Created new form field component (#6717)

--- frontend/src/app/patients/Components/PersonForm.tsx
+++ frontend/src/app/patients/Components/PersonForm.tsx
@@ -1,13 +1,8 @@
 import React, { useCallback, useState, useEffect, useRef } from "react";
 import { SchemaOf } from "yup";
 import { useTranslation } from "react-i18next";
-import { Checkbox, ComboBox, Label, Textarea } from "@trussworks/react-uswds";
+import { ComboBox, Label, Textarea } from "@trussworks/react-uswds";
 
-import {
-  canadianProvinceCodes,
-  countryOptions,
-  stateCodes,
-} from "../../../config/constants";
 import getLanguages from "../../utils/languages";
 import i18n from "../../../i18n";
 import {
@@ -48,6 +43,7 @@ import FacilitySelect from "./FacilitySelect";
 import ManagePhoneNumbers from "./ManagePhoneNumbers";
 import ManageEmails from "./ManageEmails";
 import "./PersonForm.scss";
+import PersonAddressField from "./PersonAddressField";
 
 export type ValidateField = (field: keyof PersonErrors) => Promise<void>;
 
@@ -539,125 +535,7 @@ const PersonForm = (props: Props) => {
         <div className="sr-patient-sub-header">
           {t("patient.form.contact.addressHeading")}
         </div>
-        <Checkbox
-          id={"unknownAddress"}
-          name={"unknownAddress"}
-          label={t("patient.form.contact.unknownAddress")}
-          checked={patient.unknownAddress ?? undefined}
-          onChange={(e) => {
-            onPersonChange("unknownAddress")(e.target.checked);
-          }}
-        />
-        {!patient.unknownAddress && (
-          <div>
-            <div className="usa-form">
-              <Select
-                label={t("patient.form.contact.country")}
-                name="country"
-                value={patient.country || "USA"}
-                options={countryOptions}
-                onChange={onPersonChange("country")}
-                onBlur={() => {
-                  onBlurField("country");
-                }}
-                validationStatus={validationStatus("country")}
-                errorMessage={errors.country}
-                required
-              />
-            </div>
-            <div className="usa-form">
-              <Input
-                {...commonInputProps}
-                field="street"
-                label={t("patient.form.contact.street1")}
-                required
-              />
-            </div>
-            <div className="usa-form">
-              <Input
-                {...commonInputProps}
-                field="streetTwo"
-                label={t("patient.form.contact.street2")}
-              />
-            </div>
-            <div className="usa-form">
-              <Input
-                {...commonInputProps}
-                field="city"
-                label={t("patient.form.contact.city")}
-                required
-              />
-              {view !== PersonFormView.SELF_REGISTRATION && (
-                <Input
-                  {...commonInputProps}
-                  field="county"
-                  label={t("patient.form.contact.county")}
-                />
-              )}
-              {patient.country === "USA" ? (
-                <div className="grid-row grid-gap">
-                  <div className="mobile-lg:grid-col-6">
-                    <Select
-                      label={t("patient.form.contact.state")}
-                      name="state"
-                      value={patient.state || ""}
-                      options={stateCodes.map((c) => ({ label: c, value: c }))}
-                      defaultOption={t("common.defaultDropdownOption")}
-                      defaultSelect
-                      onChange={onPersonChange("state")}
-                      onBlur={() => {
-                        onBlurField("state");
-                      }}
-                      validationStatus={validationStatus("state")}
-                      errorMessage={errors.state}
-                      required
-                    />
-                  </div>
-                  <div className="mobile-lg:grid-col-6">
-                    <Input
-                      {...commonInputProps}
-                      field="zipCode"
-                      label={t("patient.form.contact.zip")}
-                      required
-                    />
-                  </div>
-                </div>
-              ) : null}
-              {patient.country === "CAN" ? (
-                <div className="grid-row grid-gap">
-                  <div className="mobile-lg:grid-col-6">
-                    <Select
-                      label={t("patient.form.contact.state")}
-                      name="state"
-                      value={patient.state || ""}
-                      options={canadianProvinceCodes.map((c) => ({
-                        label: c,
-                        value: c,
-                      }))}
-                      defaultOption={t("common.defaultDropdownOption")}
-                      defaultSelect
-                      onChange={onPersonChange("state")}
-                      onBlur={() => {
-                        onBlurField("state");
-                      }}
-                      validationStatus={validationStatus("state")}
-                      errorMessage={errors.state}
-                      required
-                    />
-                  </div>
-                  <div className="mobile-lg:grid-col-6">
-                    <Input
-                      {...commonInputProps}
-                      field="zipCode"
-                      label={t("patient.form.contact.zip")}
-                      required
-                    />
-                  </div>
-                </div>
-              ) : null}
-            </div>
-          </div>
-        )}
+        <PersonAddressField {...commonInputProps} view={view} />
         <div className="usa-form" style={{ maxWidth: "30rem" }}>
           <div className="usa-form-group">
             <Label htmlFor="patient-notes-textarea">
