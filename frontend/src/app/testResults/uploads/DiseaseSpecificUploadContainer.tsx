e08eaee79ff1ecfe863cfaecd1dcc52b3d133318
Johanna <johanna@skylight.digital>
2023-10-18 09:20:40 -0400

    Updated banner

--- frontend/src/app/testResults/uploads/DiseaseSpecificUploadContainer.tsx
+++ frontend/src/app/testResults/uploads/DiseaseSpecificUploadContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { FileUploadService } from "../../../fileUploadService/FileUploadService";
 import { getAppInsights } from "../../TelemetryService";
+import { LinkWithQuery } from "../../commonComponents/LinkWithQuery";
 
 import UploadForm from "./UploadForm";
 
@@ -12,33 +13,31 @@ const DiseaseSpecificUploadContainer = () => {
     <div className="usa-alert usa-alert--info margin-left-105em margin-right-105em maxw-tablet-lg">
       <div className="usa-alert__body">
         <h2 className="usa-alert__heading">
-          New: Report flu results to California
+          New: Report flu and RSV results to California
         </h2>
-        <p className="usa-alert__text">
-          Organizations sending results to the California Department of Public
-          Health (CDPH) can now use the bulk results upload feature to report
-          positive flu tests. Report COVID-19, flu A, and flu B results all from
-          a single spreadsheet.&nbsp;
-          <a
-            href="https://www.simplereport.gov/assets/resources/bulk_results_upload_guide-flu_pilot.pdf"
-            onClick={() => {
-              appInsights?.trackEvent({
-                name: "Access bulk uploader flu guide",
-              });
-            }}
-          >
-            See more information and guidance about flu reporting
-          </a>
-          .
-          <br />
-          <br />
-          All organizations can continue to report COVID-19 results to their
-          public health department using the bulk uploader. Flu reporting will
-          be available in other jurisdictions soon.
-        </p>
+        <div className="usa-alert__text">
+          <p>
+            Organizations sending results to the California Department of Public
+            Health (CDPH) can now use the bulk results upload feature to report
+            positive flu and RSV tests. Report COVID-19, flu A and B, and RSV
+            results all from a single spreadsheet.{" "}
+            <LinkWithQuery to="/results/upload/submit/guide">
+              <strong>
+                See more information and guidance about flu and RSV reporting
+              </strong>
+            </LinkWithQuery>
+            .
+          </p>
+          <p>
+            All organizations can continue to report COVID-19 results to their
+            public health department using the bulk uploader. Flu and RSV
+            reporting will be available in other jurisdictions soon.
+          </p>
+        </div>
       </div>
     </div>
   );
+
   return (
     <UploadForm
       alert={alert}
