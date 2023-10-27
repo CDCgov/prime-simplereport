7a3e2c51a52c41d451fd87ae96cf3292b2ceb9f5
Boban Ljuljdjurovic <ljuljdjurovic@gmail.com>
2023-10-26 22:43:31 -0400

    revert changes to feature flag config
    the warning is intellij only and does not effect the code

--- backend/src/main/java/gov/cdc/usds/simplereport/config/FeatureFlagsConfig.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/FeatureFlagsConfig.java
@@ -1,18 +1,49 @@
 package gov.cdc.usds.simplereport.config;
 
+import gov.cdc.usds.simplereport.db.model.FeatureFlag;
+import gov.cdc.usds.simplereport.db.repository.FeatureFlagRepository;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "features")
 @Component
-@Getter
+@ConfigurationProperties(prefix = "features")
+@EnableScheduling
 @Setter
+@Getter
+@RequiredArgsConstructor
+@Slf4j
 public class FeatureFlagsConfig {
+  @Getter(AccessLevel.NONE)
+  @Setter(AccessLevel.NONE)
+  private final FeatureFlagRepository _repo;
+
   private boolean hivEnabled;
   private boolean singleEntryRsvEnabled;
   private boolean agnosticEnabled;
   private boolean agnosticBulkUploadEnabled;
   private boolean testCardRefactorEnabled;
+
+  @Scheduled(fixedRateString = "60000") // 1 min
+  private void loadFeatureFlagsFromDB() {
+    Iterable<FeatureFlag> flags = _repo.findAll();
+    flags.forEach(flag -> flagMapping(flag.getName(), flag.getValue()));
+  }
+
+  private void flagMapping(String flagName, Boolean flagValue) {
+    switch (flagName) {
+      case "hivEnabled" -> setHivEnabled(flagValue);
+      case "singleEntryRsvEnabled" -> setSingleEntryRsvEnabled(flagValue);
+      case "agnosticEnabled" -> setAgnosticEnabled(flagValue);
+      case "agnosticBulkUploadEnabled" -> setAgnosticBulkUploadEnabled(flagValue);
+      case "testCardRefactorEnabled" -> setTestCardRefactorEnabled(flagValue);
+      default -> log.info("no mapping for " + flagName);
+    }
+  }
 }
