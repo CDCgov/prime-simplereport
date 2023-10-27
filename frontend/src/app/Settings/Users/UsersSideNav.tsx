7828a3894d99638a7b6b454fe950802a791ea065
Daniel Sass <DanielSass@users.noreply.github.com>
2023-10-27 12:49:24 -0400

    Dan/6338 org user list bug (#6802)
    * paginate okta response on backend, add scrollbar to frontend
    
    * updating snapshot
    
    * updating unit tests
    
    * update mock to ensure it doesn't page
    
    * moving 'magic number' to constant

--- frontend/src/app/Settings/Users/UsersSideNav.tsx
+++ frontend/src/app/Settings/Users/UsersSideNav.tsx
@@ -29,7 +29,10 @@ const UsersSideNav: React.FC<Props> = ({
   return (
     <div className="display-block users-sidenav">
       <h2 className="users-sidenav-header">Users</h2>
-      <nav className="prime-secondary-nav" aria-label="Tertiary navigation">
+      <nav
+        className="prime-secondary-nav users-secondary-nav"
+        aria-label="Tertiary navigation"
+      >
         <div
           role="tablist"
           aria-owns={getIdsAsString(users)}
