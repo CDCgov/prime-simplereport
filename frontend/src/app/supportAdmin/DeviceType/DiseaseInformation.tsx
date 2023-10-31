e41a037665bc1fc88e08d2c1a6be18505e0cb822
Bob Zhao <bob@skylight.digital>
2023-10-31 11:31:26 -0400

    frontend changes

--- frontend/src/app/supportAdmin/DeviceType/DiseaseInformation.tsx
+++ frontend/src/app/supportAdmin/DeviceType/DiseaseInformation.tsx
@@ -71,7 +71,7 @@ const DiseaseInformation = ({
         <div className="tablet:grid-col">
           <TextInput
             name={`supportedDiseases.${index}.testPerformedLoincCode`}
-            label="Test performed code"
+            label="Test performed"
             disabled={disabled}
             required
             className={"margin-top-1"}
@@ -93,7 +93,7 @@ const DiseaseInformation = ({
         <div className="tablet:grid-col">
           <TextInput
             name={`supportedDiseases.${index}.testOrderedLoincCode`}
-            label="Test ordered code"
+            label="Test ordered"
             disabled={disabled}
             required
             className={"margin-top-1"}
@@ -133,6 +133,18 @@ const DiseaseInformation = ({
             value={values?.[index]?.equipmentUid}
             registrationProps={register(
               `supportedDiseases.${index}.equipmentUid` as const
+            )}
+          />
+        </div>
+        <div className="tablet:grid-col">
+          <TextInput
+            name={`supportedDiseases.${index}.equipmentUidType`}
+            label="Equipment Uid Type"
+            disabled={disabled}
+            className={"margin-top-1"}
+            value={values?.[index]?.equipmentUidType}
+            registrationProps={register(
+              `supportedDiseases.${index}.equipmentUidType` as const
             )}
           />
         </div>
