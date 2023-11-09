57f9d0e89a94f5f7f0035e10fb13b8218da48f4b
elisa lee <elisa@skylight.digital>
2023-11-09 12:38:00 -0600

    wip

--- ops/services/app_gateway/main.tf
+++ ops/services/app_gateway/main.tf
@@ -115,7 +115,7 @@ resource "azurerm_application_gateway" "load_balancer" {
   probe {
     name                                      = "frontend-http"
     interval                                  = 10
-    path                                      = "${local.app_url}"
+    path                                      = local.app_url
     pick_host_name_from_backend_http_settings = false
     protocol                                  = "Https"
     timeout                                   = 10
