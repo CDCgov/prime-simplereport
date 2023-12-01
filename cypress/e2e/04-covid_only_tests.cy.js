daf72a2b863808524463fcf3786dce24c64582a4
Bob Zhao <bob@skylight.digital>
2023-12-01 08:02:10 -0500

    maybe consolidate tests

--- cypress/e2e/04-covid_only_tests.cy.js
+++ cypress/e2e/04-covid_only_tests.cy.js
@@ -1,4 +1,4 @@
-import { loginHooks } from "../support/e2e";
+import { generatePatient, loginHooks, testNumber } from "../support/e2e";
 import { graphqlURL } from "../utils/request-utils";
 import { aliasGraphqlOperations } from "../utils/graphql-test-utils";
 
@@ -150,6 +150,72 @@ describe("Conducting a COVID test from:", () => {
     cy.contains("Start test").click({ force: true });
 
     cy.wait("@GetFacilityQueue", { timeout: 20000 });
+    cy.contains("Newly added patients go to the bottom of the queue").click();
+
+    cy.contains("legend", "COVID-19 result")
+      .next("div")
+      .within(() => {
+        cy.contains("label", "Negative (-)").click();
+      });
+
+    cy.contains("legend", "Is the patient pregnant?")
+      .next("div")
+      .within(() => {
+        cy.contains("label", "No").click();
+      });
+
+    cy.contains("legend", "Is the patient currently experiencing any symptoms?")
+      .next("div")
+      .within(() => {
+        cy.contains("label", "No").click();
+      });
+
+    cy.contains("Submit results").click();
+    cy.wait("@SubmitQueueItem");
+  });
+
+  it("starts and submits a test for a new patient from the patient form", () => {
+    const patient = generatePatient();
+    patient.email = "myemail@test.com";
+
+    cy.visit("/");
+    cy.get(".usa-nav-container");
+    cy.get("#desktop-patient-nav-link").click();
+    cy.get("#add-patient").click();
+    cy.get("#individual_add-patient").click();
+    cy.get(".prime-edit-patient").contains("Add new patient");
+    cy.injectSRAxe();
+    cy.checkAccessibility(); // New Patient page
+
+    cy.get('input[name="firstName"]').type(patient.firstName);
+    cy.get('input[name="lastName"]').type(patient.lastName);
+    cy.get('select[name="facilityId"]').select("All facilities");
+    cy.get('input[name="birthDate"]').type(patient.dobForInput);
+    cy.get('input[name="number"]').type(patient.phone);
+    cy.get('input[value="LANDLINE"]+label').click();
+    cy.get('input[name="email-0"]').type(patient.email);
+    cy.get('input[name="street"]').type(patient.address);
+    cy.get('input[name="city"]').type(patient.city);
+    cy.get('select[name="state"]').select(patient.state);
+    cy.get('input[name="zipCode"]').type(patient.zip);
+    cy.get('input[name="race"][value="other"]+label').click();
+    cy.get('input[name="ethnicity"][value="refused"]+label').click();
+    cy.get('input[name="gender"][value="male"]+label').click();
+    cy.get(".prime-save-patient-changes-start-test").first().click();
+    cy.get(
+      '.modal__container input[name="addressSelect-person"][value="userAddress"]+label',
+    ).click();
+    cy.checkAccessibility();
+
+    cy.get(".modal__container #save-confirmed-address").click();
+    cy.url().should("include", "queue");
+
+    cy.wait("@GetFacilityQueue", { timeout: 20000 });
+
+    cy.get(".prime-home").contains(patient.firstName);
+    cy.url().should("include", "queue");
+    cy.wait("@GetFacilityQueue", { timeout: 20000 });
+    cy.contains("Patient record created").click();
     cy.contains("Newly added patients go to the bottom of the queue").click();
 
     cy.contains("legend", "COVID-19 result")
