1eb81633a7bcddcfc118f7e1d0882a4b25401acf
Bob Zhao <bob@skylight.digital>
2023-12-15 10:34:21 -0500

    add okta call and update script config

--- backend/src/main/java/gov/cdc/usds/simplereport/api/heathcheck/BackendAndDatabaseHealthIndicator.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/heathcheck/BackendAndDatabaseHealthIndicator.java
@@ -1,26 +1,38 @@
 package gov.cdc.usds.simplereport.api.heathcheck;
 
+import com.okta.sdk.resource.api.GroupApi;
+import com.okta.sdk.resource.client.ApiException;
 import gov.cdc.usds.simplereport.db.repository.FeatureFlagRepository;
+import gov.cdc.usds.simplereport.idp.repository.LiveOktaRepository;
+import gov.cdc.usds.simplereport.idp.repository.OktaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.JDBCConnectionException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-@Component("prod-smoke-test")
+@Component("backend-and-db-smoke-test")
 @Slf4j
 @RequiredArgsConstructor
 public class BackendAndDatabaseHealthIndicator implements HealthIndicator {
-  private final FeatureFlagRepository _ffRepo;
+    private final FeatureFlagRepository _ffRepo;
+    private final OktaRepository _oktaRepo;
 
-  @Override
-  public Health health() {
-    try {
-      _ffRepo.findAll();
-      return Health.up().build();
-    } catch (JDBCConnectionException e) {
-      return Health.down().build();
+    @Override
+    public Health health() {
+        try {
+            _ffRepo.findAll();
+            _oktaRepo.getConnectTimeoutForHealthCheck();
+
+            return Health.up().build();
+        } catch (JDBCConnectionException e) {
+            return Health.down().build();
+            // Okta API call errored
+        } catch (ApiException e) {
+            log.info(e.getMessage());
+            return Health.down().build();
+        }
     }
-  }
 }
