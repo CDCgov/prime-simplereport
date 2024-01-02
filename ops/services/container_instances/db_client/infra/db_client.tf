b011a36e7e47b731c05af673359117f697247a93
Alis Akers <alismxwll@gmail.com>
2024-01-02 15:57:21 -0800

    feat(db_client): update docker image and probes to check PSQL version, bump version to 2.0.0

--- ops/services/container_instances/db_client/infra/db_client.tf
+++ ops/services/container_instances/db_client/infra/db_client.tf
@@ -15,7 +15,7 @@ resource "azurerm_container_group" "db_client" {
 
   container {
     name   = "${var.name}-${var.env}-db-client"
-    image  = "${var.acr_server}/api/simple-report-db-client:1.0.0"
+    image  = "${var.acr_server}/api/simple-report-db-client:2.0.0"
     cpu    = "1"
     memory = "1.5"
 
@@ -36,12 +36,12 @@ resource "azurerm_container_group" "db_client" {
     }
 
     readiness_probe {
-      exec                  = ["cat", "/tmp/healthy"]
+      exec                  = ["cat", "/tmp/psql_version"]
       initial_delay_seconds = 30
     }
 
     liveness_probe {
-      exec                  = ["cat", "/tmp/healthy"]
+      exec                  = ["cat", "/tmp/psql_version"]
       initial_delay_seconds = 30
       period_seconds        = 10
     }
