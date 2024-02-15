6a0c543cd4fab9a7f672d9ad3165419a91adeb51
elisa lee <elisa@skylight.digital>
2024-02-15 15:18:59 -0600

    Make e2e spec 3 rerunnable (#7137)
    * Make e2e spec 3 rerunnable
    
    * add after hook for cleanup

--- cypress/e2e/03-add_devices.cy.js
+++ cypress/e2e/03-add_devices.cy.js
@@ -1,129 +1,134 @@
 import {
   generateCovidOnlyDevice,
   generateMultiplexDevice,
-  loginHooks,
+  loginHooks, testNumber,
 } from "../support/e2e";
 import { graphqlURL } from "../utils/request-utils";
 import { aliasGraphqlOperations } from "../utils/graphql-test-utils";
-const covidOnlyDevice = generateCovidOnlyDevice();
-const multiplexDevice = generateMultiplexDevice();
+import {
+  accessOrganizationByName,
+  cleanUpPreviousRunSetupData,
+  cleanUpRunOktaOrgs,
+  setupRunData
+} from "../utils/setup-utils";
 
-describe("Adding covid only and multiplex devices", () => {
-  let facility;
+const specRunName = "spec03";
+const currentSpecRunVersionName = `${testNumber()}-cypress-${specRunName}`;
+
+describe("Adding testing devices", () => {
+  const covidOnlyDevice = generateCovidOnlyDevice();
+  const multiplexDevice = generateMultiplexDevice();
   loginHooks();
 
-  before("store patient info", () => {
-    cy.task("setCovidOnlyDeviceName", covidOnlyDevice.name);
-    cy.task("setMultiplexDeviceName", multiplexDevice.name);
+  before("setup spec data", () => {
+    cy.task("getSpecRunVersionName", specRunName)
+      .then((prevSpecRunVersionName) => {
+        if (prevSpecRunVersionName) {
+          cleanUpPreviousRunSetupData(prevSpecRunVersionName);
+          cleanUpRunOktaOrgs(prevSpecRunVersionName);
+        }
+        let data = {
+          specRunName: specRunName,
+          versionName: currentSpecRunVersionName
+        };
+        cy.task("setSpecRunVersionName", data)
+        setupRunData(currentSpecRunVersionName);
+      })
   });
 
-  context("Add devices", () => {
-    beforeEach(() => {
-      cy.intercept("POST", graphqlURL, (req) => {
-        aliasGraphqlOperations(req);
-      });
+  beforeEach("alias graphql operations", () => {
+    cy.intercept("POST", graphqlURL, (req) => {
+      aliasGraphqlOperations(req);
     });
+  })
 
-    it("Adds a covid only device", () => {
-      cy.visit("/admin/create-device-type");
-      cy.wait("@getSpecimenTypes");
-      cy.wait("@getSupportedDiseases");
-      cy.contains("Add new device");
-      cy.contains("Save changes").should("be.not.enabled");
-      cy.injectSRAxe();
-      cy.checkAccessibility();
-      cy.addDevice(covidOnlyDevice);
-    });
-
-    it("Adds a multiplex device", () => {
-      cy.visit("/admin/create-device-type");
-      cy.wait("@getSpecimenTypes");
-      cy.wait("@getSupportedDiseases");
-      cy.addDevice(multiplexDevice);
-    });
-
-    it("Reviews multiplex device in edit page", () => {
-      cy.visit("/admin/manage-devices");
-      cy.wait("@getSpecimenTypes");
-      cy.wait("@getSupportedDiseases");
-      cy.wait("@getDeviceTypeList");
-
-      cy.get('input[role="combobox"]').first().type(multiplexDevice.name);
-      cy.get('li[id="selectDevice--list--option-0"]')
-        .contains(multiplexDevice.name)
-        .click();
-      cy.get('input[name="name"]').should("have.value", multiplexDevice.name);
-      cy.injectSRAxe();
-      cy.checkAccessibility();
-      cy.get('input[name="model"]').should("have.value", multiplexDevice.model);
-      cy.get('input[name="manufacturer"]').should(
-        "have.value",
-        multiplexDevice.manufacturer,
-      );
-      cy.get(".pill").should("have.length", 1);
-      cy.get('select[name="supportedDiseases.0.supportedDisease"')
-        .find(":selected")
-        .should("have.text", "COVID-19");
-      cy.get('input[name="supportedDiseases.0.testPerformedLoincCode"]').should(
-        "have.value",
-        "123-456",
-      );
-      cy.get('select[name="supportedDiseases.1.supportedDisease"')
-        .find(":selected")
-        .should("have.text", "Flu A");
-      cy.get('input[name="supportedDiseases.1.testPerformedLoincCode"]').should(
-        "have.value",
-        "456-789",
-      );
-      cy.get('select[name="supportedDiseases.2.supportedDisease"')
-        .find(":selected")
-        .should("have.text", "Flu B");
-      cy.get('input[name="supportedDiseases.2.testPerformedLoincCode"]').should(
-        "have.value",
-        "789-123",
-      );
-    });
+  after("clean up spec data", () => {
+    cleanUpPreviousRunSetupData(currentSpecRunVersionName);
+    cleanUpRunOktaOrgs(currentSpecRunVersionName);
   });
 
-  context("Manage facilities - add devices", () => {
-    beforeEach(() => {
-      cy.makePOSTRequest({
-        operationName: "WhoAmI",
-        variables: {},
-        query:
-          "query WhoAmI {\n  whoami {\n organization {\n    facilities {\n      id\n      name\n      __typename\n    }\n    __typename\n  }\n} \n}",
-      }).then((res) => {
-        facility = res.body.data.whoami.organization.facilities[0];
-      });
-      cy.intercept("POST", graphqlURL, (req) => {
-        aliasGraphqlOperations(req);
-      });
-    });
+  it("Adds devices to SimpleReport via admin dashboard", () => {
+    visitAdminCreateDevicePage();
+    cy.contains("Add new device");
+    cy.contains("Save changes").should("be.not.enabled");
+    cy.injectSRAxe();
+    cy.checkAccessibility();
+    cy.addDevice(covidOnlyDevice);
 
-    it("Adds covid only and multiplex devices to the facility", () => {
-      cy.visit(`/settings/facility/${facility.id}`);
-      cy.wait("@GetFacilities");
-      cy.contains("Manage devices");
-      cy.injectSRAxe();
-      cy.checkAccessibility();
-      cy.get('input[role="combobox"]').first().type(covidOnlyDevice.name);
-      cy.get(
-        `button[aria-label="Select ${covidOnlyDevice.manufacturer} ${covidOnlyDevice.model}"]`,
-      ).click();
-      cy.get('input[role="combobox"]').first().type(multiplexDevice.name);
-      cy.get(
-        `button[aria-label="Select ${multiplexDevice.manufacturer} ${multiplexDevice.model}"]`,
-      ).click();
-      cy.contains("Save changes").click();
-      cy.get(".modal__content")
-        .find("fieldset")
-        .each((fieldset) => {
-          fieldset.find("label").first().trigger("click");
-        });
-      cy.get('button[id="save-confirmed-address"]').click();
-      cy.wait("@UpdateFacility");
-      cy.get(".Toastify").contains("Updated Facility");
-      cy.wait("@GetManagedFacilities"); // waits until it goes back to manage facilities page
-    });
-  });
+    visitAdminCreateDevicePage();
+    cy.addDevice(multiplexDevice);
+
+    cy.visit("/admin/manage-devices");
+    cy.wait("@getSpecimenTypes");
+    cy.wait("@getSupportedDiseases");
+    cy.wait("@getDeviceTypeList");
+
+    cy.get('input[role="combobox"]').first().type(multiplexDevice.name);
+    cy.get('li[id="selectDevice--list--option-0"]')
+      .contains(multiplexDevice.name)
+      .click();
+    cy.get('input[name="name"]').should("have.value", multiplexDevice.name);
+    cy.injectSRAxe();
+    cy.checkAccessibility();
+    cy.get('input[name="model"]').should("have.value", multiplexDevice.model);
+    cy.get('input[name="manufacturer"]').should(
+      "have.value",
+      multiplexDevice.manufacturer,
+    );
+    cy.get(".pill").should("have.length", 1);
+    cy.get('select[name="supportedDiseases.0.supportedDisease"')
+      .find(":selected")
+      .should("have.text", "COVID-19");
+    cy.get('input[name="supportedDiseases.0.testPerformedLoincCode"]').should(
+      "have.value",
+      "123-456",
+    );
+    cy.get('select[name="supportedDiseases.1.supportedDisease"')
+      .find(":selected")
+      .should("have.text", "Flu A");
+    cy.get('input[name="supportedDiseases.1.testPerformedLoincCode"]').should(
+      "have.value",
+      "456-789",
+    );
+    cy.get('select[name="supportedDiseases.2.supportedDisease"')
+      .find(":selected")
+      .should("have.text", "Flu B");
+    cy.get('input[name="supportedDiseases.2.testPerformedLoincCode"]').should(
+      "have.value",
+      "789-123",
+    );
+  })
+
+  it("Adds devices to a testing facility", () => {
+    accessOrganizationByName(`${currentSpecRunVersionName}-org`);
+    cy.visit(`/settings/facilities`);
+    cy.get(`[data-cy="${currentSpecRunVersionName}-facility-link"]`).click();
+    cy.contains("Manage devices");
+    cy.injectSRAxe();
+    cy.checkAccessibility();
+    cy.get('input[role="combobox"]').first().type(covidOnlyDevice.name);
+    cy.get(
+      `button[aria-label="Select ${covidOnlyDevice.manufacturer} ${covidOnlyDevice.model}"]`,
+    ).click();
+    cy.get('input[role="combobox"]').first().type(multiplexDevice.name);
+    cy.get(
+      `button[aria-label="Select ${multiplexDevice.manufacturer} ${multiplexDevice.model}"]`,
+    ).click();
+    cy.contains("Save changes").click();
+    cy.get(".modal__content")
+      .find("fieldset")
+      .each((fieldset) => {
+        fieldset.find("label").first().trigger("click");
+      });
+    cy.get('button[id="save-confirmed-address"]').click();
+    cy.wait("@UpdateFacility");
+    cy.get(".Toastify").contains("Updated Facility");
+    cy.wait("@GetManagedFacilities"); // waits until it goes back to manage facilities page
+  })
 });
+
+const visitAdminCreateDevicePage = () => {
+  cy.visit("/admin/create-device-type");
+  cy.wait("@getSpecimenTypes");
+  cy.wait("@getSupportedDiseases");
+}
