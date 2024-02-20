c1740921d2a9a2d05b0da9ca83a34a7c7f535ccb
Mike Brown <mike@skylight.digital>
2024-02-20 15:33:14 -0500

    Make spec 7 rerunnable (#7300)

--- cypress/e2e/07-organization_settings.cy.js
+++ cypress/e2e/07-organization_settings.cy.js
@@ -1,13 +1,45 @@
-import { loginHooks } from "../support/e2e";
+import { loginHooks, testNumber } from "../support/e2e";
 import { graphqlURL } from "../utils/request-utils";
 import { aliasGraphqlOperations } from "../utils/graphql-test-utils";
+import {
+  cleanUpPreviousRunSetupData,
+  cleanUpRunOktaOrgs,
+  setupRunData,
+} from "../utils/setup-utils";
+
+const specRunName = "spec07";
+const currentSpecRunVersionName = `${testNumber()}-cypress-${specRunName}`;
 
 describe("Updating organization settings", () => {
   loginHooks();
+
+  before("setup run data", () => {
+    cy.task("getSpecRunVersionName", specRunName).then(
+      (prevSpecRunVersionName) => {
+        if (prevSpecRunVersionName) {
+          cleanUpPreviousRunSetupData(prevSpecRunVersionName);
+          cleanUpRunOktaOrgs(prevSpecRunVersionName);
+        }
+        let data = {
+          specRunName: specRunName,
+          versionName: currentSpecRunVersionName,
+        };
+        cy.task("setSpecRunVersionName", data);
+
+        setupRunData(currentSpecRunVersionName);
+      },
+    );
+  });
+
   beforeEach(() => {
     cy.intercept("POST", graphqlURL, (req) => {
       aliasGraphqlOperations(req);
     });
+  });
+
+  after("clean up run data", () => {
+    cleanUpRunOktaOrgs(currentSpecRunVersionName);
+    cleanUpPreviousRunSetupData(currentSpecRunVersionName);
   });
 
   it("navigates to the org settings page", () => {
