a3bc3ead01107846f69518795b129485bf598593
Bob Zhao <bob@skylight.digital>
2024-03-14 09:36:02 -0400

    unhardcode local host

--- cypress/utils/request-utils.js
+++ cypress/utils/request-utils.js
@@ -1,5 +1,5 @@
 export const frontendURL = `${
-  Cypress.env("REACT_APP_BASE_URL") || "http://localhost:3000"
+  Cypress.env("REACT_APP_BASE_URL") || "https://localhost.simplereport.gov/app"
 }/`;
 const backendURL = Cypress.env("BACKEND_URL") || "http://localhost:8080";
 export const graphqlURL = `${backendURL}/graphql`;
