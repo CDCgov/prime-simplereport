d764b3e8439f39109ec5471b113c5674ae4438e5
Bob Zhao <bob@skylight.digital>
2024-03-20 12:29:42 -0400

    add video deletion to cypress config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -87,6 +87,18 @@ module.exports = {
 
         return launchOptions;
       });
+      on("after:spec", (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === "failed"),
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
     },
     baseUrl: "http://localhost.simplereport.gov",
   },
