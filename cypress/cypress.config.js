69c9032c2821342752a0792818df5415264f8940
Bob Zhao <bob@skylight.digital>
2024-04-12 10:41:15 -0500

    add debug log statements

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,8 +5,6 @@ module.exports = {
   viewportWidth: 1200,
   viewportHeight: 800,
   defaultCommandTimeout: 10000,
-  video: true,
-  videoCompression: false,
   retries: {
     runMode: 1,
     openMode: 1,
@@ -88,19 +86,9 @@ module.exports = {
 
         return launchOptions;
       });
-      on("after:spec", (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === "failed"),
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video);
-          }
-        }
-      });
     },
     baseUrl: "http://localhost.simplereport.gov",
+
+    // another comment change
   },
 };
