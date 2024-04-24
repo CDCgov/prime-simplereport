0424ca8d0ad600c9ba95cffb0e19a4382f75ce2e
Bob Zhao <bob@skylight.digital>
2024-04-24 14:45:24 -0400

    use new config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,12 +1,10 @@
 global.specRunVersions = new Map();
-const fs = require("fs");
+const { defineConfig } = require("cypress");
 
-module.exports = {
+module.exports = defineConfig({
   viewportWidth: 1200,
   viewportHeight: 800,
   defaultCommandTimeout: 10000,
-  video: true,
-  videoCompression: false,
   retries: {
     runMode: 1,
     openMode: 1,
@@ -88,19 +86,7 @@ module.exports = {
 
         return launchOptions;
       });
-      on("after:spec", (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === "failed"),
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video);
-          }
-        }
-      });
     },
     baseUrl: "http://localhost.simplereport.gov",
   },
-};
+});
