44a4d97ad0d78c9cd3c6d8a589485df93337634e
elisa lee <elisa@skylight.digital>
2024-04-24 15:40:43 -0500

    wip

--- cypress/e2e/06-self_registration.cy.js
+++ cypress/e2e/06-self_registration.cy.js
@@ -44,56 +44,59 @@ describe("Patient self registration", () => {
     cy.injectSRAxe();
     cy.checkAccessibility();
 
-    cy.get("#org-link").then(($link) => cy.visit($link.val()));
-    cy.contains("Terms of service");
+    cy.get("#org-link").then(($link) => cy.visit($link.val(), {
+      onLoad: () => {
+        cy.contains("Terms of service");
 
-    cy.injectSRAxe();
-    cy.checkAccessibility(); // Terms of Service
-    cy.contains("I agree").click();
-    cy.get("#registration-container").contains("General information");
+        cy.injectSRAxe();
+        cy.checkAccessibility(); // Terms of Service
+        cy.contains("I agree").click();
+        cy.get("#registration-container").contains("General information");
 
-    cy.checkAccessibility(); // Info form
-    // fills out some of the form fields
-    cy.get('input[name="firstName"]').type(patient.firstName);
-    cy.get('input[name="birthDate"]').type(patient.dobForInput);
-    cy.get('input[name="number"]').type(patient.phone);
-    cy.get('input[value="MOBILE"]+label').click();
-    cy.get('input[name="gender"][value="female"]+label').click();
-    cy.get('input[name="genderIdentity"][value="female"]+label').click();
-    cy.get('input[name="street"]').type(patient.address);
-    cy.get('select[name="state"]').select(patient.state);
-    cy.get('input[name="zipCode"]').type(patient.zip);
-    cy.get('select[name="role"]').select("Student");
-    cy.get(".prime-formgroup").contains("Student ID");
-    cy.get('input[name="lookupId"]').type(patient.studentId);
-    cy.get('input[name="race"][value="other"]+label').click();
-    cy.get('input[name="ethnicity"][value="refused"]+label').click();
-    cy.get('input[name="residentCongregateSetting"][value="NO"]+label').click();
-    cy.get('input[name="employedInHealthcare"][value="NO"]+label').click();
+        cy.checkAccessibility(); // Info form
+        // fills out some of the form fields
+        cy.get('input[name="firstName"]').type(patient.firstName);
+        cy.get('input[name="birthDate"]').type(patient.dobForInput);
+        cy.get('input[name="number"]').type(patient.phone);
+        cy.get('input[value="MOBILE"]+label').click();
+        cy.get('input[name="gender"][value="female"]+label').click();
+        cy.get('input[name="genderIdentity"][value="female"]+label').click();
+        cy.get('input[name="street"]').type(patient.address);
+        cy.get('select[name="state"]').select(patient.state);
+        cy.get('input[name="zipCode"]').type(patient.zip);
+        cy.get('select[name="role"]').select("Student");
+        cy.get(".prime-formgroup").contains("Student ID");
+        cy.get('input[name="lookupId"]').type(patient.studentId);
+        cy.get('input[name="race"][value="other"]+label').click();
+        cy.get('input[name="ethnicity"][value="refused"]+label').click();
+        cy.get('input[name="residentCongregateSetting"][value="NO"]+label').click();
+        cy.get('input[name="employedInHealthcare"][value="NO"]+label').click();
 
-    // shows what fields are missing on submit
-    cy.get(".self-registration-button").first().click();
-    cy.get(".prime-formgroup").contains("Last name is missing");
-    cy.get(".prime-formgroup").contains("City is missing");
+        // shows what fields are missing on submit
+        cy.get(".self-registration-button").first().click();
+        cy.get(".prime-formgroup").contains("Last name is missing");
+        cy.get(".prime-formgroup").contains("City is missing");
 
-    // fills out the remaining fields and submits
-    cy.get('input[name="lastName"]').type(patient.lastName);
-    cy.get('input[name="city"]').type(patient.city);
-    cy.get(".self-registration-button").first().click();
-    cy.get(
-      '.modal__container input[name="addressSelect-person"][value="userAddress"]+label',
-    ).scrollIntoView();
-    cy.get(
-      '.modal__container input[name="addressSelect-person"][value="userAddress"]+label',
-    ).click();
+        // fills out the remaining fields and submits
+        cy.get('input[name="lastName"]').type(patient.lastName);
+        cy.get('input[name="city"]').type(patient.city);
+        cy.get(".self-registration-button").first().click();
+        cy.get(
+          '.modal__container input[name="addressSelect-person"][value="userAddress"]+label',
+        ).scrollIntoView();
+        cy.get(
+          '.modal__container input[name="addressSelect-person"][value="userAddress"]+label',
+        ).click();
 
-    cy.checkAccessibility();
+        cy.checkAccessibility();
 
-    cy.get(".modal__container #save-confirmed-address").click();
-    cy.get("#self-reg-confirmation").contains(
-      "thanks for completing your patient profile",
-    );
+        cy.get(".modal__container #save-confirmed-address").click();
+        cy.get("#self-reg-confirmation").contains(
+          "thanks for completing your patient profile",
+        );
 
-    cy.checkAccessibility(); // Confirmation page
+        cy.checkAccessibility(); // Confirmation page
+      }
+    }));
   });
 });
