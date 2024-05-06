108866e02fd7e97cce236a69334baa8cdedb8ea1
Bob Zhao <bob@skylight.digital>
2024-05-06 11:04:29 -0400

    test name

--- frontend/src/app/commonComponents/Checkboxes.test.ts
+++ frontend/src/app/commonComponents/Checkboxes.test.ts
@@ -1,6 +1,6 @@
 import { generateCheckboxColumns } from "./Checkboxes";
 
-describe("generateSubArrayForColumnDisplay", () => {
+describe("generateCheckboxColumns", () => {
   it("creates array of arrays, subarrays of length n, when passed an array of items and n", () => {
     const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     expect(generateCheckboxColumns(testArray, 3)).toEqual([
