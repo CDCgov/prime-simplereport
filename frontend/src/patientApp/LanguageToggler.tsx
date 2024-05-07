971414726f46ca2029ee3ffd7bd15367c2cc96bf
elisa lee <elisa@skylight.digital>
2024-05-07 15:32:30 -0500

    Upgrade trussworks/react-uswds v9 and uswds v3.8 (#7650)
    * Upgrade trussworks/react-uswds v9 and uswds v3.8
    
    Co-authored-by: fzhao99 <bob@skylight.digital>
    
    * Adjust styles for tooltip and buttons with icons
    
    * update snapshots
    
    * add newline for new scss file
    
    * Fix more styling for button w/ icon
    
    * Fix disabled input style
    
    ---------
    
    Co-authored-by: fzhao99 <bob@skylight.digital>

--- frontend/src/patientApp/LanguageToggler.tsx
+++ frontend/src/patientApp/LanguageToggler.tsx
@@ -1,13 +1,14 @@
 import Button from "../app/commonComponents/Button/Button";
 import i18n from "../i18n";
 import { setLanguage } from "../app/utils/languages";
+import "./LanguageToggler.scss";
 
 export default function LanguageToggler() {
   return (
     <div lang={i18n.language === "en" ? "es" : "en"}>
       <Button
         icon={"globe"}
-        className="usa-button--unstyled"
+        className="sr-language-toggler usa-button--unstyled"
         onClick={() => {
           const displayLanguage = i18n.language === "en" ? "es" : "en";
           setLanguage(displayLanguage);
