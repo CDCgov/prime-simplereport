04ef11b048adbb42bc9d3bdaff15a38c78e98464
Bob Zhao <bob@skylight.digital>
2024-07-08 10:49:31 -0400

    add in okta messages

--- frontend/src/app/DeploySmokeTest.tsx
+++ frontend/src/app/DeploySmokeTest.tsx
@@ -2,26 +2,77 @@ import { useEffect, useState } from "react";
 
 import FetchClient from "./utils/api";
 
+const APP_STATUS_LOADING = "App status loading...";
+const APP_STATUS_SUCCESS = "App status returned success :)";
+const APP_STATUS_FAILURE = "App status returned failure :(";
+
+const OKTA_STATUS_LOADING = "Okta status loading...";
+const OKTA_STATUS_SUCCESS = "Okta status returned success :)";
+const OKTA_STATUS_FAILURE = "Okta status returned failure :(";
+
 const api = new FetchClient(undefined, { mode: "cors" });
 const DeploySmokeTest = (): JSX.Element => {
-  const [success, setSuccess] = useState<boolean>();
+  const [appStatus, setAppStatus] = useState<boolean>();
+  const [oktaStatus, setOktaStatus] = useState<boolean>();
+
   useEffect(() => {
     api
       .getRequest("/actuator/health/backend-and-db-smoke-test")
       .then((response) => {
         const status = JSON.parse(response);
-        if (status.status === "UP") return setSuccess(true);
-        setSuccess(false);
+        if (status.status === "UP") return setAppStatus(true);
+        setAppStatus(false);
       })
       .catch((e) => {
         console.error(e);
-        setSuccess(false);
+        setAppStatus(false);
       });
   }, []);
 
-  if (success === undefined) return <>Status loading...</>;
-  if (success) return <> Status returned success :) </>;
-  return <> Status returned failure :( </>;
+  useEffect(() => {
+    api
+      .getRequest("/actuator/health")
+      .then((response) => {
+        const status = JSON.parse(response);
+        if (status.okta === "UP") return setOktaStatus(true);
+        setOktaStatus(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setOktaStatus(false);
+      });
+  }, []);
+
+  let appStatusMessage;
+  switch (appStatus) {
+    case undefined:
+      appStatusMessage = APP_STATUS_LOADING;
+      break;
+    case true:
+      appStatusMessage = APP_STATUS_SUCCESS;
+      break;
+    case false:
+      appStatusMessage = APP_STATUS_FAILURE;
+      break;
+  }
+  let oktaStatusMessage;
+  switch (oktaStatus) {
+    case undefined:
+      oktaStatusMessage = OKTA_STATUS_LOADING;
+      break;
+    case true:
+      oktaStatusMessage = OKTA_STATUS_SUCCESS;
+      break;
+    case false:
+      oktaStatusMessage = OKTA_STATUS_FAILURE;
+      break;
+  }
+  return (
+    <>
+      <div className={"display-block"}>{appStatusMessage} </div>
+      <div className={"display-block"}> {oktaStatusMessage} </div>
+    </>
+  );
 };
 
 export default DeploySmokeTest;
