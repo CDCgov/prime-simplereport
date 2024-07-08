d130f3c8e694d98881a0f4506851151926d1d86a
Bob Zhao <bob@skylight.digital>
2024-07-08 12:13:24 -0400

    fix frontend test

--- frontend/src/app/DeploySmokeTest.test.tsx
+++ frontend/src/app/DeploySmokeTest.test.tsx
@@ -1,7 +1,11 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { FetchMock } from "jest-fetch-mock";
 
-import DeploySmokeTest from "./DeploySmokeTest";
+import DeploySmokeTest, {
+  APP_STATUS_FAILURE,
+  APP_STATUS_LOADING,
+  APP_STATUS_SUCCESS,
+} from "./DeploySmokeTest";
 
 describe("DeploySmokeTest", () => {
   beforeEach(() => {
@@ -10,21 +14,26 @@ describe("DeploySmokeTest", () => {
 
   it("renders success when returned from the API endpoint", async () => {
     (fetch as FetchMock).mockResponseOnce(JSON.stringify({ status: "UP" }));
+    (fetch as FetchMock).mockResponseOnce(
+      JSON.stringify({ okta: "UP", status: "UP" })
+    );
 
     render(<DeploySmokeTest />);
     await waitFor(() =>
-      expect(screen.queryByText("Status loading...")).not.toBeInTheDocument()
+      expect(screen.queryByText(APP_STATUS_LOADING)).not.toBeInTheDocument()
     );
-    expect(screen.getByText("Status returned success :)"));
+    expect(screen.getByText(APP_STATUS_SUCCESS));
   });
 
   it("renders failure when returned from the API endpoint", async () => {
-    (fetch as FetchMock).mockResponseOnce(JSON.stringify({ status: "DOWN" }));
+    (fetch as FetchMock).mockResponse(
+      JSON.stringify({ okta: "UP", status: "DOWN" })
+    );
 
     render(<DeploySmokeTest />);
     await waitFor(() =>
-      expect(screen.queryByText("Status loading...")).not.toBeInTheDocument()
+      expect(screen.queryByText(APP_STATUS_LOADING)).not.toBeInTheDocument()
     );
-    expect(screen.getByText("Status returned failure :("));
+    expect(screen.getByText(APP_STATUS_FAILURE));
   });
 });
