6308c6adc017b50d8053f0e2dde9a35c9e22a0d6
Bob Zhao <bob@skylight.digital>
2024-07-09 12:17:54 -0400

    add in old api exception

--- backend/src/main/java/gov/cdc/usds/simplereport/api/heathcheck/OktaHealthIndicator.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/heathcheck/OktaHealthIndicator.java
@@ -1,5 +1,6 @@
 package gov.cdc.usds.simplereport.api.heathcheck;
 
+import com.okta.sdk.resource.client.ApiException;
 import gov.cdc.usds.simplereport.idp.repository.OktaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +17,19 @@ public class OktaHealthIndicator implements HealthIndicator {
 
   @Override
   public Health health() {
+    Health.Builder oktaDegradedWarning = Health.status("OKTA_DEGRADED");
     try {
       String oktaStatus = _oktaRepo.getApplicationStatusForHealthCheck();
       if (!ACTIVE_LITERAL.equals(oktaStatus)) {
         log.info("Okta status didn't return ACTIVE, instead returned " + oktaStatus);
-        Health.Builder oktaDegradedWarning = Health.status("OKTA_DEGRADED");
         return oktaDegradedWarning.build();
       }
     } catch (NullPointerException e) {
       log.info("Call to Okta repository status returned null");
-      Health.Builder oktaDegradedWarning = Health.status("OKTA_DEGRADED");
+      return oktaDegradedWarning.build();
+    } catch (ApiException e) {
+      // Okta API call errored
+      log.info("Okta status call raised an exception: " + e.getMessage());
       return oktaDegradedWarning.build();
     }
 
--- backend/src/test/java/gov/cdc/usds/simplereport/api/healthcheck/OktaHealthIndicatorTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/healthcheck/OktaHealthIndicatorTest.java
@@ -3,9 +3,10 @@ package gov.cdc.usds.simplereport.api.healthcheck;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import com.okta.sdk.resource.client.ApiException;
 import gov.cdc.usds.simplereport.api.heathcheck.OktaHealthIndicator;
 import gov.cdc.usds.simplereport.db.repository.BaseRepositoryTest;
-import gov.cdc.usds.simplereport.idp.repository.DemoOktaRepository;
+import gov.cdc.usds.simplereport.idp.repository.OktaRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +18,29 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 @EnableConfigurationProperties
 class OktaHealthIndicatorTest extends BaseRepositoryTest {
 
-  @SpyBean private DemoOktaRepository mockOktaRepo;
+  @SpyBean private OktaRepository mockOktaRepo;
 
   @Autowired private OktaHealthIndicator indicator;
 
   @Test
-  void health_SUCCEEDSWhenOktaRepoReturnsActive() {
+  void health_SucceedsWhenOktaRepoReturnsActive() {
     when(mockOktaRepo.getApplicationStatusForHealthCheck()).thenReturn("ACTIVE");
     assertThat(indicator.health()).isEqualTo(Health.up().build());
+  }
+
+  @Test
+  void health_FailsWhenOktaApiThrowsErrors() {
+    when(mockOktaRepo.getApplicationStatusForHealthCheck())
+        .thenThrow(new ApiException("some api error"));
+    Health.Builder oktaDegradedWarning = Health.status("OKTA_DEGRADED");
+    assertThat(indicator.health()).isEqualTo(oktaDegradedWarning.build());
+  }
+
+  @Test
+  void health_FailsWhenOktaApiThrowsNPE() {
+    when(mockOktaRepo.getApplicationStatusForHealthCheck()).thenThrow(new NullPointerException());
+    Health.Builder oktaDegradedWarning = Health.status("OKTA_DEGRADED");
+    assertThat(indicator.health()).isEqualTo(oktaDegradedWarning.build());
   }
 
   @Test
