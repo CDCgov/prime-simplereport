1b6683f12c4413ea75c1c785062e852e95cb6dfd
shanice-skylight <shanice@skylight.digital>
2024-08-12 23:07:13 -0400

    added logic app back

--- ops/services/alerts/alert_app/main.tf
+++ ops/services/alerts/alert_app/main.tf
@@ -9,6 +9,40 @@ locals {
 }
 
 
+#Define the Logic App Workflow
+resource "azurerm_logic_app_workflow" "slack_workflow" {
+  name                = "alert-logic-app"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+
+}
+
+data "local_file" "trigger_schema" {
+  filename = "./logicapp_trigger_schema.json"
+}
+# Define the Logic App Workflow Trigger
+resource "azurerm_logic_app_trigger_http_request" "workflow_trigger" {
+  logic_app_id = azurerm_logic_app_workflow.slack_workflow.id
+  name         = "Alert_monitor"
+  schema       = data.local_file.trigger_schema.content
+}
+
+# Define the Logic App Workflow Action
+resource "azurerm_logic_app_action_http" "workflow_action" {
+  logic_app_id = azurerm_logic_app_workflow.slack_workflow.id
+  name         = "Post_message_to_Slack"
+  method       = "POST"
+  uri          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.azure_alert_slack_webhook.value})"
+  headers = {
+    "Content-Type" = "application/json"
+  }
+  body = <<BODY
+{
+  "text": "@{triggerBody()}"
+}
+BODY
+}
 
 
 #Define the action group, call it as local variable
@@ -24,3 +58,22 @@ resource "azurerm_monitor_action_group" "on_call_action_group" {
 }
 
 
+
+# Define the Alert Rule for the action group to be notified
+resource "azurerm_monitor_activity_log_alert" "log_alert" {
+  name                = "slack-logic-app-alert"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  scopes              = [azurerm_logic_app_workflow.slack_workflow.id]
+  description         = "Alert when the Logic App workflow is triggered."
+  enabled             = true
+
+  criteria {
+    category       = "ResourceHealth"
+    operation_name = "Microsoft.Logic/workflows/workflowRuns/write"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.on_call_action_group.id
+  }
+}
