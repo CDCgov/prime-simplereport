55a7200c9f88b28c53c281c7503a80e000185a06
Daniel Sass <dsass@skylight.digital>
2024-11-13 16:41:58 -0500

    poc for using a json view for public device data

--- backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/devicetype/DeviceTypeController.java
@@ -1,17 +1,23 @@
 package gov.cdc.usds.simplereport.api.devicetype;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import gov.cdc.usds.simplereport.api.model.errors.DryRunException;
+import gov.cdc.usds.simplereport.db.model.DeviceType;
+import gov.cdc.usds.simplereport.service.DeviceTypeService;
 import gov.cdc.usds.simplereport.service.DeviceTypeSyncService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class DeviceTypeController {
-  @Autowired private DeviceTypeSyncService deviceTypeSyncService;
+  private final DeviceTypeSyncService deviceTypeSyncService;
+  private final DeviceTypeService deviceTypeService;
 
   @GetMapping("/devices/sync")
   public void syncDevices(@RequestParam boolean dryRun) {
@@ -19,6 +25,16 @@ public class DeviceTypeController {
       deviceTypeSyncService.syncDevices(dryRun);
     } catch (DryRunException e) {
       log.info("Dry run");
+    }
+  }
+
+  @GetMapping("/devices")
+  @JsonView(PublicDeviceType.class)
+  public List<DeviceType> getDevices() {
+    try {
+      return deviceTypeService.fetchDeviceTypes();
+    } catch (Exception e) {
+      return null;
     }
   }
 }
