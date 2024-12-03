df2e3d5696d289b53f8aa98304a2a70b1f55cd68
David McClatchey <david@skylight.digital>
2024-12-03 10:49:49 -0500

    timezone agnostic tests

--- backend/src/test/java/gov/cdc/usds/simplereport/api/DateTimeScalarTypeTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/DateTimeScalarTypeTest.java
@@ -8,10 +8,14 @@ import gov.cdc.usds.simplereport.config.scalars.datetime.DateTimeScalarCoercion;
 import graphql.GraphQLContext;
 import graphql.execution.CoercedVariables;
 import graphql.language.StringValue;
+import graphql.schema.CoercingParseLiteralException;
 import graphql.schema.CoercingParseValueException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Locale;
 import org.junit.jupiter.api.Assertions;
@@ -23,38 +27,55 @@ class DateTimeScalarTypeTest {
   @MockBean GraphQLContext graphQLContext;
   @MockBean Locale locale;
   @MockBean CoercedVariables coercedVariables;
+
+  Instant thisInstant = Instant.now(); // Gives UTC instant
   DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+  LocalDateTime localDateTime = thisInstant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+  // Format ISO_LOCAL_DATE_TIME String
+  String isoLocalDateTime =
+      localDateTime.truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+  // Format ISO_INSTANT String
+  String formattedUTCInstantString = DateTimeFormatter.ISO_INSTANT.format(thisInstant);
 
   @Test
-  void convertImpl_succeedsForBothFormatters() throws ParseException {
-    Date y2k = dateFormat.parse("2000-01-01T00:00:00Z");
-    assertEquals(y2k, converter.convertImpl("2000-01-01T05:00:00Z"));
-    assertEquals(y2k, converter.convertImpl("2000-01-01T05:00:00"));
+  void convertImpl_succeedsForBothFormats() throws ParseException {
+    Date isoLocalDateTimeDate = dateFormat.parse(isoLocalDateTime);
+    assertEquals(
+        isoLocalDateTimeDate.toInstant().toEpochMilli(),
+        converter.convertImpl(isoLocalDateTimeDate).getTime());
+    assertEquals(
+        thisInstant.toEpochMilli(), converter.convertImpl(formattedUTCInstantString).getTime());
   }
 
   @Test
-  void convertImpl_returnsNullOnNoSeparators() {
+  void convertImpl_returnsNullOnNoSeparatorsAndEmptyString() {
     assertNull(null, converter.convertImpl("20000101"));
+    assertNull(null, converter.convertImpl(""));
   }
 
   @Test
-  void parseLiteral_succeedsForBothFormatters() throws ParseException {
-    Date y2k = dateFormat.parse("2000-01-01T00:00:00Z");
+  void parseLiteral_succeeds() {
     assertEquals(
-        y2k,
-        converter.parseLiteral(
-            new StringValue("2000-01-01T05:00:00Z"), coercedVariables, graphQLContext, locale));
-    assertEquals(
-        y2k,
-        converter.parseLiteral(
-            new StringValue("2000-01-01T05:00:00"), coercedVariables, graphQLContext, locale));
+        thisInstant.toEpochMilli(),
+        converter
+            .parseLiteral(
+                new StringValue(formattedUTCInstantString),
+                coercedVariables,
+                graphQLContext,
+                locale)
+            .getTime());
+    assertThrows(
+        CoercingParseLiteralException.class,
+        () ->
+            converter.parseLiteral(new StringValue(""), coercedVariables, graphQLContext, locale));
   }
 
   @Test
-  void parseValue_succeedsForBothFormatters() throws ParseException {
-    Date y2k = dateFormat.parse("2000-01-01T00:00:00Z");
-    assertEquals(y2k, converter.parseValue("2000-01-01T05:00:00Z", graphQLContext, locale));
-    assertEquals(y2k, converter.parseValue("2000-01-01T05:00:00", graphQLContext, locale));
+  void parseValue_succeeds() {
+    assertEquals(
+        thisInstant.toEpochMilli(),
+        converter.parseValue(formattedUTCInstantString, graphQLContext, locale).getTime());
     Assertions.assertNull(converter.parseValue("", graphQLContext, locale));
   }
 
