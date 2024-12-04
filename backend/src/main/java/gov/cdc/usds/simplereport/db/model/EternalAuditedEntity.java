b384d600f6eae535a77879d1df5ccb432ce6b33a
David McClatchey <david@skylight.digital>
2024-12-04 14:31:37 -0500

    more comment detail

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/EternalAuditedEntity.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/EternalAuditedEntity.java
@@ -23,6 +23,7 @@ public abstract class EternalAuditedEntity extends AuditedEntity implements Eter
 
   // This getter was created in response to a startup failure where
   // Spring graphql was failing to map the schema without this getter.
+  // It seems to be looking for a method with "get" in the name.
   // The app will fail to start without this getter.
   public boolean getIsDeleted() {
     return isDeleted;
