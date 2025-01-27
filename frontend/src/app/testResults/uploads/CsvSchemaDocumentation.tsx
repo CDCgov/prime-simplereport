022f74a71522bcce65b0a4757dc1c02b8ee0b2b6
David McClatchey <david@skylight.digital>
2025-01-27 12:12:48 -0500

    standardizing margin bottom on subsections

--- frontend/src/app/testResults/uploads/CsvSchemaDocumentation.tsx
+++ frontend/src/app/testResults/uploads/CsvSchemaDocumentation.tsx
@@ -81,7 +81,7 @@ export const CsvSchemaDocumentationItem: React.FC<CsvSchemaItemProps> = ({
           <div className="grid-col-8">
             {item.description?.map((line) => (
               <div
-                className="margin-bottom-1"
+                className=""
                 key={`${item.colHeader}-description-${line}`}
                 dangerouslySetInnerHTML={{ __html: `${line}` }}
               />
