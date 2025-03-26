fc24ab0e6bce69ead03e077a53512b07c160e83d
Mike Brown <mike@skylight.digital>
2025-03-26 16:29:18 -0400

    Populate condition table with TES client

--- backend/src/main/java/gov/cdc/usds/simplereport/api/ConditionController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/ConditionController.java
@@ -1,9 +1,10 @@
 package gov.cdc.usds.simplereport.api;
 
+import gov.cdc.usds.simplereport.db.model.Condition;
 import gov.cdc.usds.simplereport.service.ConditionService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class ConditionController {
   private final ConditionService conditionService;
 
   @GetMapping("/universal/conditions")
-  public Bundle conditions() {
-    return conditionService.getConditions();
+  public List<Condition> syncConditions() {
+    return conditionService.syncConditions();
   }
 }
