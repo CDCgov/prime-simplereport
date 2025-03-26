b86c38fc5dc6278867c9db06217ff4df52da87df
elisa lee <elisa@skylight.digital>
2025-03-26 17:05:48 +0000

    Add production client (#8455)
    * Add production client
    
    * add mock
    
    * Adjust performance test to use 4000 rows
    
    * Add annotations to configuration
    
    ---------
    
    Co-authored-by: elisa lee <emyl3@users.noreply.github.com>

--- backend/src/main/java/gov/cdc/usds/simplereport/service/DeviceTypeProdSyncService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/DeviceTypeProdSyncService.java
@@ -1,5 +1,9 @@
 package gov.cdc.usds.simplereport.service;
 
+import gov.cdc.usds.simplereport.api.model.errors.BadRequestException;
+import gov.cdc.usds.simplereport.db.model.DeviceType;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
@@ -9,10 +13,24 @@ import org.springframework.transaction.annotation.Transactional;
 /** Service to fetch and save DeviceTypes from our prod env */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DeviceTypeProdSyncService {
   @Value("${simple-report.production.devices-token}")
   private String token;
+
+  @Value("${simple-report.production.device-sync-enabled}")
+  private boolean deviceSyncEnabled;
+
+  private final SRProductionClient _client;
+
+  public List<DeviceType> getDeviceTypesFromProduction() {
+    if (deviceSyncEnabled) {
+      return _client.getProdDeviceTypes();
+    }
+    throw new BadRequestException(
+        "The device sync with production is not enabled for this environment");
+  }
 
   public boolean validateToken(String headerToken) throws AccessDeniedException {
     if (token.equals(headerToken)) {
