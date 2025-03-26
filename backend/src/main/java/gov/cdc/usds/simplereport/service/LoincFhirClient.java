8dcef58ca81545fbeef8c07228794862ab395441
Mike Brown <mike@skylight.digital>
2025-03-26 11:17:55 -0400

    Add LOINC FHIR client

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/LoincFhirClient.java
@@ -0,0 +1,18 @@
+package gov.cdc.usds.simplereport.service;
+
+import gov.cdc.usds.simplereport.config.LoincFhirClientConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/** Client to connect with LOINC FHIR API */
+@FeignClient(
+    name = "loinc-fhir-terminology-service",
+    url = "${loinc-fhir.url}",
+    configuration = LoincFhirClientConfig.class)
+public interface LoincFhirClient {
+  @GetMapping(
+      value = "/CodeSystem/$lookup?system=http://loinc.org&code={code}",
+      consumes = "application/text")
+  String getCodeSystemLookup(@PathVariable("code") String code);
+}
