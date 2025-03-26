bb057f74c327f34a9483d20efa974b76dca07b1b
Mike Brown <mike@skylight.digital>
2025-03-26 16:29:18 -0400

    Populate condition table with TES client

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/Condition.java
@@ -0,0 +1,24 @@
+package gov.cdc.usds.simplereport.db.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Condition extends EternalAuditedEntity {
+
+  @Column(nullable = false)
+  private String code;
+
+  @Column(nullable = false)
+  private String display;
+
+  public Condition(String code, String display) {
+    super();
+    this.code = code;
+    this.display = display;
+  }
+}
