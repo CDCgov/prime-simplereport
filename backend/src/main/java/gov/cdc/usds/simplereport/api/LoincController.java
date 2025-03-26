db49e9d493778f41885eb9bfd0f154df7efa51d3
Mike Brown <mike@skylight.digital>
2025-03-26 11:17:55 -0400

    Add LOINC FHIR client

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/LoincController.java
@@ -0,0 +1,20 @@
+package gov.cdc.usds.simplereport.api;
+
+import gov.cdc.usds.simplereport.service.LoincService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class LoincController {
+  private final LoincService loincService;
+
+  @GetMapping("/universal/loinc/codeSystem")
+  public String getCodeSystemLookup(@RequestParam String code) {
+    return loincService.getCodeSystemLookup(code);
+  }
+}
