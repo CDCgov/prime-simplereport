a5b99c828b769d6274cc44dd8c60888a98930642
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-03-27 12:49:21 -0400

    sycLabs returns top of loinc staging

--- backend/src/main/java/gov/cdc/usds/simplereport/api/LoincController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/LoincController.java
@@ -1,11 +1,14 @@
 package gov.cdc.usds.simplereport.api;
 
+import gov.cdc.usds.simplereport.db.model.LoincStaging;
 import gov.cdc.usds.simplereport.service.LoincService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,5 +22,5 @@ public class LoincController {
   }
 
   @GetMapping("universal/syncLabs")
-  public String syncLabs() { return loincService.syncLabs(); }
+  public List<LoincStaging> syncLabs() { return loincService.syncLabs(); }
 }
