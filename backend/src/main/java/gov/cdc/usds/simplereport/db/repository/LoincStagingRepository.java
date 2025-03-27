f959556063749d09c8e262cd5741c6166b5691da
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-03-27 12:49:21 -0400

    sycLabs returns top of loinc staging

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
@@ -1,8 +1,17 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.LoincStaging;
+
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /** Specification of EternalAuditedEntityRepository for {@link LoincStaging} manipulation. */
-public interface LoincStagingRepository extends CrudRepository<LoincStaging, UUID> {}
+public interface LoincStagingRepository extends CrudRepository<LoincStaging, UUID> {
+    @Query(value = "SELECT * FROM simple_report.loinc_staging LIMIT :limit", nativeQuery = true)
+    List<LoincStaging> findByLimit(@Param("limit") int limit);
+
+}
