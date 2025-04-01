21ae32101a876ee4664b8c55b8876c9d7138b399
Mike Brown <mike@skylight.digital>
2025-04-01 18:37:06 -0400

    Spotless apply

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
@@ -1,10 +1,8 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.LoincStaging;
-
 import java.util.List;
 import java.util.UUID;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +11,8 @@ import org.springframework.data.repository.query.Param;
 
 /** Specification of EternalAuditedEntityRepository for {@link LoincStaging} manipulation. */
 public interface LoincStagingRepository extends CrudRepository<LoincStaging, UUID> {
-    @Query(value = "SELECT * FROM simple_report.loinc_staging LIMIT :limit", nativeQuery = true)
-    List<LoincStaging> findByLimit(@Param("limit") int limit);
+  @Query(value = "SELECT * FROM simple_report.loinc_staging LIMIT :limit", nativeQuery = true)
+  List<LoincStaging> findByLimit(@Param("limit") int limit);
 
-    Page<LoincStaging> findAll(Pageable p);
+  Page<LoincStaging> findAll(Pageable p);
 }
