6b0af6511dabbb1de80d1d8d560ea539de93f2ef
Mike Brown <mike@skylight.digital>
2025-04-01 15:45:57 -0400

    Hide universal reporting based on feature flag

--- backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/UniversalReportMutationResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/UniversalReportMutationResolver.java
@@ -5,6 +5,7 @@ import gov.cdc.usds.simplereport.api.model.universalreporting.PatientReportInput
 import gov.cdc.usds.simplereport.api.model.universalreporting.ProviderReportInput;
 import gov.cdc.usds.simplereport.api.model.universalreporting.SpecimenInput;
 import gov.cdc.usds.simplereport.api.model.universalreporting.TestDetailsInput;
+import gov.cdc.usds.simplereport.config.FeatureFlagsConfig;
 import gov.cdc.usds.simplereport.service.UniversalReportService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class UniversalReportMutationResolver {
   private final UniversalReportService universalReportService;
+  private final FeatureFlagsConfig featureFlagsConfig;
 
   @MutationMapping
   public String submitLabReport(
@@ -24,6 +26,11 @@ public class UniversalReportMutationResolver {
       @Argument FacilityReportInput facility,
       @Argument SpecimenInput specimen,
       @Argument List<TestDetailsInput> testDetailsList) {
+
+    if (!featureFlagsConfig.isUniversalReportingEnabled()) {
+      return "Universal lab reporting is not available at this time.";
+    }
+
     return universalReportService.processLabReport(
         patient, provider, facility, specimen, testDetailsList);
   }
