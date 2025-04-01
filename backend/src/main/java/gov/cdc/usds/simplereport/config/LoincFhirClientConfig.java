7697d4f3242bf0ad95ba5ab52a1b895c2973990a
Mike Brown <mike@skylight.digital>
2025-04-01 18:37:06 -0400

    Spotless apply

--- backend/src/main/java/gov/cdc/usds/simplereport/config/LoincFhirClientConfig.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/config/LoincFhirClientConfig.java
@@ -4,11 +4,6 @@ import feign.RequestInterceptor;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 public class LoincFhirClientConfig {
 
@@ -22,5 +17,4 @@ public class LoincFhirClientConfig {
   public RequestInterceptor headerRequestInterceptor() {
     return new BasicAuthRequestInterceptor(username, password);
   }
-
 }
