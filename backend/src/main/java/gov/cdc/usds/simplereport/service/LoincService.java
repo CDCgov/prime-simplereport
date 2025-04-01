3a07702739586dd24f05ece1e5e6221fe4f94f4f
Mike Brown <mike@skylight.digital>
2025-04-01 18:37:06 -0400

    Spotless apply

--- backend/src/main/java/gov/cdc/usds/simplereport/service/LoincService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/LoincService.java
@@ -9,11 +9,18 @@ import gov.cdc.usds.simplereport.db.model.Lab;
 import gov.cdc.usds.simplereport.db.model.LoincStaging;
 import gov.cdc.usds.simplereport.db.repository.LabRepository;
 import gov.cdc.usds.simplereport.db.repository.LoincStagingRepository;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
-import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Parameters;
 import org.hl7.fhir.r4.model.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -40,7 +47,7 @@ public class LoincService {
   private final FhirContext context = FhirContext.forR4();
   private IParser parser = context.newJsonParser();
 
-  public List<Lab>  syncLabs() {
+  public List<Lab> syncLabs() {
     log.info("Sync Labs");
     PageRequest pageRequest = PageRequest.of(0, 100);
     List<CompletableFuture<Response>> futures = new ArrayList<>();
@@ -52,9 +59,14 @@ public class LoincService {
     while (loincPage.hasNext()) {
       List<LoincStaging> loincs = loincPage.getContent();
       log.info("Found {} Labs", loincs.size());
-      loincs.forEach(loinc -> futures.add(CompletableFuture.supplyAsync(() -> loincFhirClient.getCodeSystemLookup(loinc.getCode()))));
+      loincs.forEach(
+        loinc ->
+            futures.add(
+                CompletableFuture.supplyAsync(
+                    () -> loincFhirClient.getCodeSystemLookup(loinc.getCode()))));
       log.info("Futures created");
-      CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+      CompletableFuture<Void> allFutures =
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
       allFutures.join();
       log.info("Futures completed");
       for (int i = 0; i < futures.size(); i++) {
@@ -62,7 +74,10 @@ public class LoincService {
         LoincStaging loinc = loincs.get(i);
         if (response.status() != 200) {
           failedLoincs.add(loinc);
-          log.error("Received a {} status code from the LOINC API response for: {}", response.status(), loinc.getCode());
+          log.error(
+            "Received a {} status code from the LOINC API response for: {}",
+            response.status(),
+            loinc.getCode());
           continue;
         }
         Optional<Parameters> parameters = parseResponseToParameters(response);
@@ -111,11 +126,14 @@ public class LoincService {
 
   private Optional<Lab> parametersToLab(LoincStaging loinc, Parameters parameters) {
     List<Parameters.ParametersParameterComponent> parameter = parameters.getParameter();
-    
+
     String display = "";
-    Optional<Parameters.ParametersParameterComponent> displayParam = parameter.stream().filter(component -> Objects.equals(component.getName(), "display")).findFirst();
+    Optional<Parameters.ParametersParameterComponent> displayParam =
+        parameter.stream()
+            .filter(component -> Objects.equals(component.getName(), "display"))
+            .findFirst();
     if (displayParam.isPresent()) {
-       display = displayParam.get().getValue().toString();
+      display = displayParam.get().getValue().toString();
     }
 
     String description = null;
@@ -129,61 +147,84 @@ public class LoincService {
     boolean panel = false;
     boolean isLab = false;
 
-    List<Parameters.ParametersParameterComponent> properties= parameter.stream().filter(component -> Objects.equals(component.getName(), "property")).toList();
+    List<Parameters.ParametersParameterComponent> properties =
+        parameter.stream()
+            .filter(component -> Objects.equals(component.getName(), "property"))
+            .toList();
     for (Parameters.ParametersParameterComponent property : properties) {
       String firstValue = property.getPart().get(0).getValue().toString();
       Type lastValue = property.getPart().get(1).getValue();
 
       switch (firstValue) {
-        case "category": {
-          Coding coding = (Coding) lastValue;
-          if (coding.getCode().equals("LP29693-6")){
-            isLab = true;
+        case "category":
+          {
+            Coding coding = (Coding) lastValue;
+            if (coding.getCode().equals("LP29693-6")) {
+              isLab = true;
+            }
+            break;
           }
-          break;
-        }
-        case "DefinitionDescription": {
-          description = lastValue.toString();
-          break;
-        }
-        case "LONG_COMMON_NAME":{
-          longCommonName = lastValue.toString();
-          break;
-        }
-        case "SCALE_TYP": {
-          Coding coding = (Coding) lastValue;
-          scaleCode = coding.getCode();
-          scaleDisplay = coding.getDisplay();
-          break;
-        }
-        case "system-core": {
-          Coding coding = (Coding) lastValue;
-          systemCode = coding.getCode();
-          systemDisplay = coding.getDisplay();
-          break;
-        }
-        case "answer-list": {
-          answerList = lastValue.toString();
-          break;
-        }
-        case "ORDER_OBS": {
-          orderOrObservation = lastValue.toString();
-          break;
-        }
-        case "PanelType": {
-          if (lastValue.toString().equals("Panel")) {
-            panel = true;
+        case "DefinitionDescription":
+          {
+            description = lastValue.toString();
+            break;
           }
-          break;
-        }
+        case "LONG_COMMON_NAME":
+          {
+            longCommonName = lastValue.toString();
+            break;
+          }
+        case "SCALE_TYP":
+          {
+            Coding coding = (Coding) lastValue;
+            scaleCode = coding.getCode();
+            scaleDisplay = coding.getDisplay();
+            break;
+          }
+        case "system-core":
+          {
+            Coding coding = (Coding) lastValue;
+            systemCode = coding.getCode();
+            systemDisplay = coding.getDisplay();
+            break;
+          }
+        case "answer-list":
+          {
+            answerList = lastValue.toString();
+            break;
+          }
+        case "ORDER_OBS":
+          {
+            orderOrObservation = lastValue.toString();
+            break;
+          }
+        case "PanelType":
+          {
+            if (lastValue.toString().equals("Panel")) {
+              panel = true;
+            }
+            break;
+          }
       }
     }
 
-    if (!isLab){
+    if (!isLab) {
       return Optional.empty();
     }
 
-    return Optional.of(new Lab(loinc.getCode(), display, description, longCommonName, scaleCode, scaleDisplay, systemCode, systemDisplay, answerList, orderOrObservation, panel));
+    return Optional.of(
+        new Lab(
+            loinc.getCode(),
+            display,
+            description,
+            longCommonName,
+            scaleCode,
+            scaleDisplay,
+            systemCode,
+            systemDisplay,
+            answerList,
+            orderOrObservation,
+            panel));
   }
 
   @Autowired
