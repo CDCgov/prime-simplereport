dc6c48aa07434fb344085039b4c3970167206eab
Mike Brown <mike@skylight.digital>
2025-04-01 15:45:57 -0400

    Hide universal reporting based on feature flag

--- backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/LabReportMutationResolver.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/LabReportMutationResolver.java
@@ -5,6 +5,7 @@ import gov.cdc.usds.simplereport.api.model.universalreporting.PatientReportInput
 import gov.cdc.usds.simplereport.api.model.universalreporting.ProviderReportInput;
 import gov.cdc.usds.simplereport.api.model.universalreporting.SpecimenInput;
 import gov.cdc.usds.simplereport.api.model.universalreporting.TestDetailsInput;
+import gov.cdc.usds.simplereport.service.UniversalReportService;
 import java.util.List;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
