10a87af161ea3bbd98dc00420a1693e53362165d
Mike Brown <mike@skylight.digital>
2025-04-01 18:37:06 -0400

    Spotless apply

--- backend/src/main/java/gov/cdc/usds/simplereport/api/LoincController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/LoincController.java
@@ -1,15 +1,12 @@
 package gov.cdc.usds.simplereport.api;
 
 import gov.cdc.usds.simplereport.db.model.Lab;
-import gov.cdc.usds.simplereport.db.model.LoincStaging;
 import gov.cdc.usds.simplereport.service.LoincService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,5 +15,7 @@ public class LoincController {
   private final LoincService loincService;
 
   @GetMapping("universal/syncLabs")
-  public List<Lab> syncLabs() { return loincService.syncLabs(); }
+  public List<Lab> syncLabs() {
+    return loincService.syncLabs();
+  }
 }
--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
@@ -3,5 +3,4 @@ package gov.cdc.usds.simplereport.db.repository;
 import gov.cdc.usds.simplereport.db.model.Lab;
 
 /** Specification of EternalAuditedEntityRepository for {@link Lab} manipulation. */
-public interface LabRepository extends EternalAuditedEntityRepository<Lab> {
-}
+public interface LabRepository extends EternalAuditedEntityRepository<Lab> {}
