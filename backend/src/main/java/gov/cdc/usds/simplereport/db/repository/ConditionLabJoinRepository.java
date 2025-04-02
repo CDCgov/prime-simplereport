582cdecd11ff49737f1ba2b146deb14777fbb367
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-04-02 12:16:15 -0400

    Saving to condition_lab_join and linting

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/ConditionLabJoinRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/ConditionLabJoinRepository.java
@@ -1,8 +1,12 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.ConditionLabJoin;
+import java.util.List;
+import java.util.UUID;
 
 /** Specification of EternalAuditedEntityRepository for {@link ConditionLabJoin} manipulation. */
-public interface ConditionLabJoinRepository extends EternalAuditedEntityRepository<ConditionLabJoin> {
+public interface ConditionLabJoinRepository
+    extends EternalAuditedEntityRepository<ConditionLabJoin> {
 
+  List<ConditionLabJoin> findByConditionIdAndLabId(UUID conditionId, UUID labId);
 }
--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
@@ -4,5 +4,5 @@ import gov.cdc.usds.simplereport.db.model.Lab;
 
 /** Specification of EternalAuditedEntityRepository for {@link Lab} manipulation. */
 public interface LabRepository extends EternalAuditedEntityRepository<Lab> {
-    Lab findByCode(String code);
+  Lab findByCode(String code);
 }
