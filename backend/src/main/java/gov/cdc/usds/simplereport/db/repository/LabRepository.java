970e0b2b5a837e111147fb0d4af5456495ac075a
Mike Brown <mike@skylight.digital>
2025-04-02 15:31:25 -0400

    Update reduce labs method

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LabRepository.java
@@ -1,8 +1,9 @@
 package gov.cdc.usds.simplereport.db.repository;
 
 import gov.cdc.usds.simplereport.db.model.Lab;
+import java.util.Optional;
 
 /** Specification of EternalAuditedEntityRepository for {@link Lab} manipulation. */
 public interface LabRepository extends EternalAuditedEntityRepository<Lab> {
-  Lab findByCode(String code);
+  Optional<Lab> findByCode(String code);
 }
