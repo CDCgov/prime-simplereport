4fd3334d66ece9917475652f845bd2818b7640a5
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-04-03 11:48:30 -0400

    Create specimen controller and service

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/SpecimenController.java
@@ -0,0 +1,21 @@
+package gov.cdc.usds.simplereport.api.reporting;
+
+import gov.cdc.usds.simplereport.service.SpecimenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class SpecimenController {
+  private final SpecimenService specimenService;
+
+  @GetMapping("/universal/specimen/sync")
+  public String syncConditions() {
+    return specimenService.syncSpecimens();
+  }
+
+}
