b56cdd0c3d0a06768c60b367692034082e15755f
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-04-03 22:41:41 -0400

    Specimen model

---
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/Specimen.java
@@ -0,0 +1,42 @@
+package gov.cdc.usds.simplereport.db.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Setter
+public class Specimen extends EternalAuditedEntity {
+
+  @Getter
+  @Column(nullable = false)
+  private String loincSystemCode;
+
+  @Getter
+  @Column(nullable = false)
+  private String loincSystemDisplay;
+
+  @Getter
+  @Column(nullable = false)
+  private String snomedCode;
+
+  @Getter
+  @Column(nullable = false)
+  private String snomedDisplay;
+
+
+  public Specimen(String loincSystemCode, String loincSystemDisplay, String snomedCode, String snomedDisplay) {
+    super();
+    this.loincSystemCode = loincSystemCode;
+    this.loincSystemDisplay = loincSystemDisplay;
+    this.snomedCode = snomedCode;
+    this.snomedDisplay = snomedDisplay;
+  }
+
+  protected Specimen() {
+    /* for hibernate */
+  }
+}
