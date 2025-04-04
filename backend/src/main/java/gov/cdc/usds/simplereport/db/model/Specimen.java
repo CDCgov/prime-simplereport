1afc2aae4af7e170f7f6cefa6e93fa7ab64d1e9f
DanPaseltiner <daniel.paseltiner@gmail.com>
2025-04-03 22:45:31 -0400

    Specimen repository

--- backend/src/main/java/gov/cdc/usds/simplereport/db/model/Specimen.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/model/Specimen.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Setter
---
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/SpecimenRepository.java
@@ -0,0 +1,11 @@
+package gov.cdc.usds.simplereport.db.repository;
+
+import gov.cdc.usds.simplereport.db.model.Specimen;
+
+import java.util.List;
+
+/** Specification of EternalAuditedEntityRepository for {@link Specimen} manipulation. */
+public interface SpecimenRepository extends EternalAuditedEntityRepository<Specimen> {
+
+  List<Specimen> findByLoincSystemCode(String loincCode);
+}
