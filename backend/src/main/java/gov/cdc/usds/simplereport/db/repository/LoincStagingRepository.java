8422db54e6fa7658618456236c35610a41e671ca
Mike Brown <mike@skylight.digital>
2025-04-09 10:43:13 -0400

    Clear page of loinc staging during lab sync

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/LoincStagingRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 /** Specification of EternalAuditedEntityRepository for {@link LoincStaging} manipulation. */
 public interface LoincStagingRepository extends CrudRepository<LoincStaging, UUID> {
@@ -15,4 +17,9 @@ public interface LoincStagingRepository extends CrudRepository<LoincStaging, UUI
   List<LoincStaging> findByLimit(@Param("limit") int limit);
 
   Page<LoincStaging> findAll(Pageable p);
+
+  @Query(value = "DELETE FROM simple_report.loinc_staging", nativeQuery = true)
+  @Modifying
+  @Transactional
+  void deleteAllLoincStaging();
 }
