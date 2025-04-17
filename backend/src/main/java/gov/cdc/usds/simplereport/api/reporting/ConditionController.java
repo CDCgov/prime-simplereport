d3bfc70d158292c272b801082b3693275c272b10
Brady Fausett <brady@skylight.digital>
2025-04-17 09:43:00 -0600

    Update ConditionController.java

--- backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/ConditionController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/ConditionController.java
@@ -4,6 +4,7 @@ import gov.cdc.usds.simplereport.service.ConditionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
