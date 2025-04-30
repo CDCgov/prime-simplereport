a08cdebc1a825ce47438936f116f7cacdfbe6eb9
Merethe Hansen <merethe@skylight.digital>
2025-04-30 12:40:48 -0700

    endpoints for clearing tables

--- backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/SpecimenController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/reporting/SpecimenController.java
@@ -4,19 +4,28 @@ import gov.cdc.usds.simplereport.service.SpecimenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/universal/specimen")
 @Slf4j
 @RequiredArgsConstructor
 @SuppressWarnings({"checkstyle:TodoComment"})
 public class SpecimenController {
   private final SpecimenService specimenService;
 
-  @GetMapping("/universal/specimen/sync")
+  @GetMapping("/sync")
   // TODO: Test that syncConditions returns the expected success message
   public String syncConditions() {
     specimenService.syncSpecimens();
     return "Specimen sync completed successfully";
+  }
+
+  @GetMapping("/clear")
+  public String clearSpecimensAndBodySites() {
+    // todo can we make this harder to do accidentally?
+    specimenService.clearSpecimensAndBodySites();
+    return "Completed attempting to delete all specimens and specimen body sites.";
   }
 }
