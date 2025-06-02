8f4f76531a6e3773bc0e17ff4539b20593a02ee8
Andrew Schreiber <schreiaj@gmail.com>
2025-06-02 15:09:29 -0700

    Linter fixes.

--- backend/src/main/java/gov/cdc/usds/simplereport/service/ConditionService.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/ConditionService.java
@@ -14,7 +14,6 @@ import gov.cdc.usds.simplereport.db.repository.LoincStagingRepository;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
