cafb84c24489bd689bf0362ec93f7a7a51b3c6d5
Mike Brown <mike@skylight.digital>
2025-06-02 20:10:34 -0400

    Add pilot app landing page

---
+++ frontend/src/app/PilotApp.tsx
@@ -0,0 +1,197 @@
+import { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { useDispatch, connect } from "react-redux";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  Location,
+} from "react-router-dom";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import jwtDecode from "jwt-decode";
+import { useFeature } from "flagged";
+
+import ProtectedRoute from "./commonComponents/ProtectedRoute";
+import Page from "./commonComponents/Page/Page";
+import { setInitialState } from "./store";
+import { appPermissions } from "./permissions";
+import { getAppInsights } from "./TelemetryService";
+import VersionEnforcer from "./VersionEnforcer";
+import { TrainingNotification } from "./commonComponents/TrainingNotification";
+import LabReportForm from "./universalReporting/LabReportForm";
+import PilotHeader from "./commonComponents/PilotHeader";
+import ReportLandingPage from "./universalReporting/ReportLandingPage";
+
+export const WHOAMI_QUERY = gql`
+  query WhoAmI {
+    whoami {
+      id
+      firstName
+      middleName
+      lastName
+      suffix
+      email
+      isAdmin
+      permissions
+      roleDescription
+      organization {
+        name
+        testingFacility {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+const checkOktaLoginStatus = (
+  accessToken: string | null,
+  location: Location
+) => {
+  if (process.env.REACT_APP_OKTA_ENABLED === "true") {
+    if (!accessToken) {
+      // If Okta login has been attempted and returned to SR with an error, don't redirect back to Okta
+      const params = new URLSearchParams(location.hash.slice(1));
+      if (params.get("error")) {
+        throw new Error(
+          params.get("error_description") ?? "Unknown Okta error"
+        );
+      }
+      throw new Error("Not authenticated, redirecting to Okta...");
+    }
+  }
+};
+
+const PilotApp = () => {
+  const universalReportingEnabled = useFeature("universalReportingEnabled");
+  const appInsights = getAppInsights();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const accessToken = localStorage.getItem("access_token");
+
+  // Check if the user is logged in, if not redirect to Okta
+  checkOktaLoginStatus(accessToken, location);
+
+  const { data, loading, error } = useQuery(WHOAMI_QUERY, {
+    fetchPolicy: "no-cache",
+  });
+
+  useEffect(() => {
+    if (!data) return;
+
+    appInsights?.setAuthenticatedUserContext(data.whoami.id, undefined, false);
+    if (window?.visualViewport?.width) {
+      appInsights?.trackMetric(
+        {
+          name: "userViewport_reporting",
+          average: window.visualViewport.width,
+        },
+        {
+          width: window.visualViewport.width,
+          height: window.visualViewport.height,
+        }
+      );
+    }
+
+    dispatch(
+      setInitialState({
+        dataLoaded: true,
+        organization: {
+          name: data.whoami.organization?.name,
+        },
+        facilities: data.whoami.organization?.testingFacility || [],
+        facility: null,
+        user: {
+          id: data.whoami.id,
+          firstName: data.whoami.firstName,
+          middleName: data.whoami.middleName,
+          lastName: data.whoami.lastName,
+          suffix: data.whoami.suffix,
+          email: data.whoami.email,
+          roleDescription: data.whoami.roleDescription,
+          isAdmin: data.whoami.isAdmin,
+          permissions: data.whoami.permissions,
+        },
+      })
+    );
+  }, [data, dispatch, appInsights]);
+
+  if (loading) {
+    return <p>Loading account information...</p>;
+  }
+
+  if (error) {
+    if (appInsights instanceof ApplicationInsights) {
+      let decoded: any;
+      let validToken = null;
+      if (accessToken) {
+        try {
+          decoded = jwtDecode(accessToken);
+          validToken = true;
+        } catch (e) {
+          validToken = false;
+          console.error("Failed to decode access token", e);
+        }
+      }
+      const rolesFieldName = `${process.env.REACT_APP_OKTA_TOKEN_ROLE_CLAIM}`;
+      appInsights.trackException({
+        exception: error,
+        properties: {
+          "user message": "Server connection error",
+          "valid access token": validToken,
+          "token subject": decoded?.sub,
+          "token roles": decoded?.[rolesFieldName],
+        },
+      });
+    }
+    return <p>Server connection error...</p>;
+  }
+
+  const homepagePath: string = "report";
+
+  const canViewResults = appPermissions.results.canView;
+
+  if (!universalReportingEnabled) {
+    return <Navigate to={{ pathname: "/", search: location.search }} />;
+  }
+
+  return (
+    <>
+      <VersionEnforcer />
+      {process.env.REACT_APP_IS_TRAINING_SITE === "true" && (
+        <TrainingNotification />
+      )}
+      <Page
+        header={<PilotHeader />}
+        children={
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Navigate
+                  to={{ pathname: homepagePath, search: location.search }}
+                />
+              }
+            />
+            <Route path="report" element={<ReportLandingPage />} />
+            <Route
+              path="report/lab"
+              element={
+                <ProtectedRoute
+                  requiredPermissions={canViewResults}
+                  userPermissions={data.whoami.permissions}
+                  element={<LabReportForm />}
+                />
+              }
+            />
+            <Route path="settings/*" element={<Navigate to={"/pilot"} />} />
+          </Routes>
+        }
+      />
+    </>
+  );
+};
+
+export default connect()(PilotApp);
---
+++ frontend/src/app/commonComponents/PilotHeader.tsx
@@ -0,0 +1,273 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+import { v4 as uuidv4 } from "uuid";
+import { useSelector, connect } from "react-redux";
+
+import siteLogo from "../../img/simplereport-logo-color.svg";
+import { hasPermission, appPermissions } from "../permissions";
+import { getAppInsights } from "../TelemetryService";
+import { formatFullName, formatRole } from "../utils/user";
+
+import useComponentVisible from "./ComponentVisible";
+import { LinkWithQuery } from "./LinkWithQuery";
+import "./Header.scss";
+import Button from "./Button/Button";
+import ChangeUser from "./ChangeUser";
+
+const PilotHeader: React.FC<{}> = () => {
+  const appInsights = getAppInsights();
+
+  const handleSupportClick = () => {
+    if (appInsights) {
+      appInsights.trackEvent({ name: "Support" });
+    }
+  };
+
+  const handleWhatsNewClick = () => {
+    if (appInsights) {
+      appInsights.trackEvent({ name: "What's new" });
+    }
+  };
+
+  const user = useSelector((state) => (state as any).user as User);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const {
+    ref: staffDefailsRef,
+    isComponentVisible: staffDetailsVisible,
+    setIsComponentVisible: setStaffDetailsVisible,
+  } = useComponentVisible(false);
+
+  const canViewTestQueue = hasPermission(
+    user.permissions,
+    appPermissions.tests.canView
+  );
+
+  const logout = () => {
+    // Fetch the id_token from local storage
+    const id_token = localStorage.getItem("id_token");
+    const state = uuidv4();
+    // Remove auth data from local_storage
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    // Determine which Okta domain to use for logout
+    const oktaDomain = process.env.REACT_APP_OKTA_URL;
+    window.location.replace(
+      oktaDomain +
+        "/oauth2/default/v1/logout" +
+        `?id_token_hint=${encodeURIComponent(id_token || "")}` +
+        `&post_logout_redirect_uri=${encodeURIComponent(
+          process.env.REACT_APP_BASE_URL || ""
+        )}` +
+        `&state=${state}`
+    );
+  };
+
+  const activeNavItem = "active-nav-item prime-nav-link";
+  const inactiveNavItem = "prime-nav-link";
+  const getNavItemClassName = ({ isActive }: { isActive: boolean }) =>
+    isActive ? activeNavItem : inactiveNavItem;
+
+  const mainNavContent = [
+    {
+      url: "/pilot/report/lab",
+      displayText: "Report lab results",
+      displayPermissions: canViewTestQueue,
+      className: getNavItemClassName,
+      key: "lab-report-nav-link",
+    },
+  ];
+
+  const mainNavList = (deviceType: "desktop" | "mobile") => {
+    let navList = mainNavContent
+      .map((item) => {
+        return (
+          item.displayPermissions && (
+            <li key={item.key} className="usa-nav__primary-item">
+              <LinkWithQuery
+                to={item.url}
+                onClick={() => setMenuVisible(false)}
+                className={item.className}
+                id={`${deviceType}-${item.key}`}
+                data-cy={`${deviceType}-${item.key}`}
+              >
+                {item.displayText}
+              </LinkWithQuery>
+            </li>
+          )
+        );
+      })
+      .filter((item) => item);
+    if (deviceType === "mobile" && navList?.length > 0) {
+      return (
+        <ul className="usa-nav__primary usa-accordion mobile-main-nav-container">
+          {navList}
+        </ul>
+      );
+    } else if (deviceType === "desktop" && navList?.length > 0) {
+      return <ul className="usa-nav__primary usa-accordion">{navList}</ul>;
+    }
+  };
+
+  const secondaryNavContent = [
+    {
+      url: "#",
+      displayPermissions: true,
+      onClick: (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        setStaffDetailsVisible(!staffDetailsVisible);
+      },
+      className: staffDetailsVisible ? activeNavItem : inactiveNavItem,
+      dataTestId: "user-button",
+      hasSubmenu: true,
+      icon: (
+        <FontAwesomeIcon
+          icon={"user-circle"}
+          aria-hidden={false}
+          aria-label={"My account"}
+          role={"img"}
+          style={{
+            color: staffDetailsVisible && !menuVisible ? "white" : "",
+          }}
+        />
+      ),
+      mobileDisplay: false,
+    },
+    {
+      url: "/pilot/settings/users/1",
+      displayPermissions: true,
+      onClick: () => setMenuVisible(false),
+      className: getNavItemClassName,
+      dataTestId: "settings-button",
+      icon: (
+        <FontAwesomeIcon
+          icon={"cog"}
+          aria-hidden={false}
+          role={"img"}
+          aria-label={"Settings"}
+        />
+      ),
+      mobileDisplay: true,
+      mobileDisplayText: "Settings",
+      hasSubmenu: false,
+    },
+  ];
+
+  const secondaryNavSublist = (deviceType: string) => (
+    <ul className="usa-sidenav__sublist">
+      <li className="usa-sidenav__item span-full-name">
+        {formatFullName(user)}
+      </li>
+      <li className="usa-sidenav__item role-tag">
+        {formatRole(user.roleDescription)}
+      </li>
+      <li className="usa-sidenav__item navlink__support">
+        <div className="header-link-icon sparkle-icon-mask"></div>
+        <a
+          href="https://www.simplereport.gov/using-simplereport/whats-new"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => handleWhatsNewClick()}
+          data-testid={`${deviceType}-whats-new-link`}
+        >
+          What's new
+        </a>
+      </li>
+      <li className="usa-sidenav__item navlink__support">
+        <FontAwesomeIcon
+          className={"header-link-icon"}
+          icon={"question-circle"}
+        />
+        <a
+          href="https://www.simplereport.gov/support"
+          target="none"
+          onClick={() => handleSupportClick()}
+          data-testid={`${deviceType}-support-link`}
+        >
+          Support
+        </a>
+      </li>
+      <li className="usa-sidenav__item navlink__support">
+        <FontAwesomeIcon className={"header-link-icon"} icon={"sign-out-alt"} />
+        <Button variant="unstyled" label=" Log out" onClick={logout} />
+      </li>
+      <ChangeUser />
+    </ul>
+  );
+
+  const secondaryNav = (deviceType: string) => {
+    let content = secondaryNavContent;
+    if (deviceType === "mobile") {
+      content = secondaryNavContent.filter((item) => item.mobileDisplay);
+    }
+    return content.map((item) => {
+      return (
+        <li
+          key={`${deviceType}-${item.dataTestId}`}
+          className="usa-nav__primary-item nav__primary-item-icon"
+        >
+          <LinkWithQuery
+            to={item.url}
+            onClick={item.onClick}
+            className={item.className}
+            data-testid={`${deviceType}-${item.dataTestId}`}
+            id={`${deviceType}-${item.dataTestId}`}
+            role={item.hasSubmenu ? "button" : "link"}
+            aria-expanded={item.hasSubmenu ? staffDetailsVisible : undefined}
+            aria-controls={
+              item.hasSubmenu
+                ? `${deviceType}-${item.dataTestId}-submenu`
+                : undefined
+            }
+          >
+            {deviceType === "desktop" ? item.icon : item.mobileDisplayText}
+          </LinkWithQuery>
+          {item.hasSubmenu &&
+          staffDetailsVisible &&
+          deviceType === "desktop" ? (
+            <div
+              id={`${deviceType}-${item.dataTestId}-submenu`}
+              ref={staffDefailsRef}
+              aria-label="Account navigation"
+              className={classNames("prime-staff-infobox", {
+                "is-prime-staff-infobox-visible": staffDetailsVisible,
+              })}
+            >
+              {secondaryNavSublist("desktop")}
+            </div>
+          ) : (
+            <></>
+          )}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className="usa-nav-container prime-header">
+      <div className="usa-navbar flex-align-self-center">
+        <div id="basic-logo">
+          <LinkWithQuery to={"/pilot/report"} title="Home" aria-label="Home">
+            <img
+              className="width-card desktop:width-full"
+              src={siteLogo}
+              alt={process.env.REACT_APP_TITLE}
+            />
+          </LinkWithQuery>
+        </div>
+      </div>
+      <nav
+        aria-label="Primary desktop navigation"
+        className="usa-nav prime-nav desktop-nav flex-align-stretch"
+      >
+        {mainNavList("desktop")}
+        <ul className="usa-nav__primary usa-accordion">
+          {secondaryNav("desktop")}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default connect()(PilotHeader);
