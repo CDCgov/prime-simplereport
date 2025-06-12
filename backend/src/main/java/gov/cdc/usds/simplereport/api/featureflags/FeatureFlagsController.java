857a3ae17e3901c555406aa6e33243acd89d6d2a
kevinfiol <me@kevinfiol.com>
2025-06-12 14:00:10 -0400

    Enable feature flags at a facility level (#8841)
    * add model
    
    * add mutation resolver method
    
    * dont delete cascade
    
    * run gql codegen and fix liquibase config
    
    * cache facility feature flags
    
    * remove log
    
    * remove unused methods
    
    * fix broken test
    
    * use a constant here
    
    * add graphql test file
    
    * add facility test
    
    * comment
    
    * update tests
    
    * fix bug

--- backend/src/main/java/gov/cdc/usds/simplereport/api/featureflags/FeatureFlagsController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/api/featureflags/FeatureFlagsController.java
@@ -2,9 +2,13 @@ package gov.cdc.usds.simplereport.api.featureflags;
 
 import gov.cdc.usds.simplereport.config.FeatureFlagsConfig;
 import gov.cdc.usds.simplereport.config.WebConfiguration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -14,7 +18,12 @@ public class FeatureFlagsController {
   @Autowired private FeatureFlagsConfig featureFlags;
 
   @GetMapping(WebConfiguration.FEATURE_FLAGS)
-  public FeatureFlagsConfig getFeatureFlags() {
-    return featureFlags;
+  public Map<String, Boolean> getFeatureFlags(
+      @RequestParam(value = "facility", required = false) UUID facilityId) {
+    if (facilityId == null) {
+      return featureFlags.getFeatureFlags(Optional.empty());
+    }
+
+    return featureFlags.getFeatureFlags(Optional.of(facilityId));
   }
 }
