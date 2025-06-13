9ffe691163c29dd9d7c008a5d208296291d23915
shanice-skylight <shanice@skylight.digital>
2025-06-13 15:28:56 -0400

    replaced unsupported arguments

--- ops/services/frontend/main.tf
+++ ops/services/frontend/main.tf
@@ -5,14 +5,14 @@ locals {
 
 // Frontend code
 resource "azurerm_storage_account" "frontend" {
-  account_replication_type  = "GRS"
-  account_tier              = "Standard"
-  account_kind              = "StorageV2"
-  location                  = var.rg_location
-  name                      = "simplereport${var.env}frontend"
-  resource_group_name       = var.rg_name
-  enable_https_traffic_only = true
-  min_tls_version           = "TLS1_2"
+  account_replication_type   = "GRS"
+  account_tier               = "Standard"
+  account_kind               = "StorageV2"
+  location                   = var.rg_location
+  name                       = "simplereport${var.env}frontend"
+  resource_group_name        = var.rg_name
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
 
   custom_domain {
     name = local.custom_domain
