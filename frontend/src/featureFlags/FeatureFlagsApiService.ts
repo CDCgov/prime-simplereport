4e6cf45acfb7b1e76bd6acb7185ae1fe70c912da
kevinfiol <me@kevinfiol.com>
2025-06-18 15:20:11 -0400

    fix type

--- frontend/src/featureFlags/FeatureFlagsApiService.ts
+++ frontend/src/featureFlags/FeatureFlagsApiService.ts
@@ -7,7 +7,7 @@ export type FeatureFlags = Record<string, boolean>;
 export class FeatureFlagsApiService {
   static featureFlags(
     params: Record<string, string> = {}
-  ): Promise<Record<string, boolean>> {
+  ): Promise<FeatureFlags> {
     const searchParams = new URLSearchParams(params);
     const query = "?" + searchParams.toString();
     return api.request("/feature-flags", null, "GET", query);
