a25972523304ecf0840db6f25e6e95b13d59bc07
David McClatchey <david@skylight.digital>
2025-06-25 10:36:26 -0400

    entitygraph for n+1

--- backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/db/repository/PersonRepository.java
@@ -9,15 +9,18 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 
 /** Interface specification for fetching and manipulating {@link Person} entities */
 public interface PersonRepository extends EternalAuditedEntityRepository<Person> {
 
+  @EntityGraph(attributePaths = {"facility", "organization", "phoneNumbers"})
   List<Person> findAll(Specification<Person> searchSpec, Pageable p);
 
   List<Person> findAllByInternalIdIn(Collection<UUID> ids);
 
+  @EntityGraph(attributePaths = {"facility", "organization", "phoneNumbers"})
   List<Person> findAllByOrganizationInternalId(UUID organizationInternalId, Pageable p);
 
   int count(Specification<Person> searchSpec);
