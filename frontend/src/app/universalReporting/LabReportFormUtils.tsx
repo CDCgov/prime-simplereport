2b1a1fc251457edc1932868efc704d7da79a1734
Mike Brown <mike@skylight.digital>
2025-07-07 19:47:09 -0400

    Fix pilot form default CLIA number

--- frontend/src/app/universalReporting/LabReportFormUtils.tsx
+++ frontend/src/app/universalReporting/LabReportFormUtils.tsx
@@ -50,7 +50,7 @@ export const defaultProviderReportInputState: ProviderReportInput = {
 
 export const defaultFacilityReportInputState: FacilityReportInput = {
   city: "Jefferson City",
-  clia: "00D000000000",
+  clia: "00D0000000",
   county: "Cole",
   email: "",
   name: "Dracula",
