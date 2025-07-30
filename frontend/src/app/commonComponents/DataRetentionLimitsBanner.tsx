18b7d7c5afdc4f15474125d9a23e7614aaa09995
David McClatchey <david@skylight.digital>
2025-07-30 16:01:21 -0400

    open link in new tab and simplify divs

--- frontend/src/app/commonComponents/DataRetentionLimitsBanner.tsx
+++ frontend/src/app/commonComponents/DataRetentionLimitsBanner.tsx
@@ -19,33 +19,31 @@ export const DataRetentionLimitsBanner = ({ dataRetained }: Props) => {
   };
 
   return (
-    <>
-      {
-        <div className="prime-home grid-row width-full margin-bottom-1em">
-          <Alert
-            type="warning"
-            role="alert"
-            className={"width-full margin-bottom-40px"}
-            bodyClassName={"data-retention-limits-alert-body"}
-          >
-            <strong className={"data-retention-limits-header"}>
-              {"Beginning November 1st, 2025, "} {dataRetained}{" "}
-              {" will only be stored for 30 days"}
-              <br />
-            </strong>
-            {"This change may impact how your facility uses SimpleReport. "}
-            <a
-              className={"data-retention-limits-link"}
-              href={
-                "https://dev5.simplereport.gov/using-simplereport/data-retention-limits/"
-              }
-              onClick={handleSupportClick}
-            >
-              {"Learn how to prepare for data retention limits."}
-            </a>
-          </Alert>
-        </div>
+    <Alert
+      type="warning"
+      role="alert"
+      className={
+        "width-full margin-bottom-2em margin-top-1em data-retention-limits-alert"
       }
-    </>
+      bodyClassName={"data-retention-limits-alert-body"}
+    >
+      <strong className={"data-retention-limits-header"}>
+        {"Beginning November 1st, 2025, "} {dataRetained}{" "}
+        {" will only be stored for 30 days"}
+        <br />
+      </strong>
+      {"This change may impact how your facility uses SimpleReport. "}
+      <a
+        className={"data-retention-limits-link"}
+        target="_blank"
+        rel="noopener noreferrer"
+        href={
+          "https://dev5.simplereport.gov/using-simplereport/data-retention-limits/"
+        }
+        onClick={handleSupportClick}
+      >
+        {"Learn how to prepare for data retention limits."}
+      </a>
+    </Alert>
   );
 };
