88f87cc4c8afb25e1139c1d93c843ee2a5fec471
Arindam Kulshi <arindam@skylight.digital>
2025-08-01 12:06:41 -0700

    lint

--- frontend/src/app/analytics/Analytics.tsx
+++ frontend/src/app/analytics/Analytics.tsx
@@ -147,181 +147,181 @@ export const Analytics = (props: Props) => {
 
   return (
     <div className="prime-home flex-1">
-        <div className="grid-container">
-          <div className="prime-container card-container margin-top-2">
-            <div className="usa-card__header">
-              <h1 className="font-sans-xl">Dashboard</h1>
-            </div>
-            <div id="analytics-page">
-              <div className="prime-container padding-3">
-                <div className="grid-row grid-gap">
+      <div className="grid-container">
+        <div className="prime-container card-container margin-top-2">
+          <div className="usa-card__header">
+            <h1 className="font-sans-xl">Dashboard</h1>
+          </div>
+          <div id="analytics-page">
+            <div className="prime-container padding-3">
+              <div className="grid-row grid-gap">
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <Dropdown
+                    label="Testing facility"
+                    options={[
+                      {
+                        label: "All facilities",
+                        value: "",
+                      },
+                      ...facilities.map((f) => ({
+                        label: f.name,
+                        value: f.id,
+                      })),
+                    ]}
+                    onChange={updateFacility}
+                    selectedValue={facilityId}
+                  />
+                </div>
+                {/* TODO: filter by patient role */}
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <Dropdown
+                    label="Condition"
+                    options={supportedDiseaseList.map((disease: string) => {
+                      return {
+                        label: disease,
+                        value: disease,
+                      };
+                    })}
+                    onChange={updateSelectedCondition}
+                    selectedValue={selectedCondition}
+                  />
+                </div>
+                <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
+                  <Dropdown
+                    label="Date range"
+                    options={[
+                      {
+                        label: "Last day (24 hours)",
+                        value: "day",
+                      },
+                      {
+                        label: "Last week (7 days)",
+                        value: "week",
+                      },
+                      {
+                        label: "Last month (30 days)",
+                        value: "month",
+                      },
+                      {
+                        label: "Custom date range",
+                        value: "custom",
+                      },
+                    ]}
+                    onChange={updateDateRange}
+                    selectedValue={dateRange}
+                  />
+                </div>
+              </div>
+              {dateRange === "custom" && (
+                <div className="grid-row grid-gap margin-top-2">
                   <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                    <Dropdown
-                      label="Testing facility"
-                      options={[
-                        {
-                          label: "All facilities",
-                          value: "",
-                        },
-                        ...facilities.map((f) => ({
-                          label: f.name,
-                          value: f.id,
-                        })),
-                      ]}
-                      onChange={updateFacility}
-                      selectedValue={facilityId}
-                    />
+                    <label className={classNames("usa-label")}>Begin</label>
+                    <input
+                      id={"startDate"}
+                      data-testid={"startDate"}
+                      type={"date"}
+                      max={formatDate(new Date())}
+                      className={classNames("usa-input")}
+                      aria-label={"Enter start date"}
+                      onChange={(e) => {
+                        if (Date.parse(e.target.value)) {
+                          const d = moment(e.target.value).toDate();
+                          const startDateString = setStartTimeForDateRange(
+                            new Date(d)
+                          ).toLocaleDateString();
+                          setStartDate(startDateString);
+                        }
+                      }}
+                      defaultValue={formatDate(new Date(startDate))}
+                    />{" "}
                   </div>
-                  {/* TODO: filter by patient role */}
                   <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                    <Dropdown
-                      label="Condition"
-                      options={supportedDiseaseList.map((disease: string) => {
-                        return {
-                          label: disease,
-                          value: disease,
-                        };
-                      })}
-                      onChange={updateSelectedCondition}
-                      selectedValue={selectedCondition}
-                    />
-                  </div>
-                  <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                    <Dropdown
-                      label="Date range"
-                      options={[
-                        {
-                          label: "Last day (24 hours)",
-                          value: "day",
-                        },
-                        {
-                          label: "Last week (7 days)",
-                          value: "week",
-                        },
-                        {
-                          label: "Last month (30 days)",
-                          value: "month",
-                        },
-                        {
-                          label: "Custom date range",
-                          value: "custom",
-                        },
-                      ]}
-                      onChange={updateDateRange}
-                      selectedValue={dateRange}
+                    <label className={classNames("usa-label")}>End</label>
+                    <input
+                      id={"endDate"}
+                      data-testid={"endDate"}
+                      type={"date"}
+                      min={formatDate(new Date(startDate))}
+                      max={formatDate(new Date())}
+                      className={classNames("usa-input")}
+                      aria-label={"Enter end date"}
+                      onChange={(e) => {
+                        if (Date.parse(e.target.value)) {
+                          const d = moment(e.target.value).toDate();
+                          const endDateString = setEndTimeForDateRange(
+                            new Date(d)
+                          ).toLocaleDateString();
+                          setEndDate(endDateString);
+                        }
+                      }}
+                      defaultValue={formatDate(new Date(endDate))}
                     />
                   </div>
                 </div>
-                {dateRange === "custom" && (
-                  <div className="grid-row grid-gap margin-top-2">
-                    <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                      <label className={classNames("usa-label")}>Begin</label>
-                      <input
-                        id={"startDate"}
-                        data-testid={"startDate"}
-                        type={"date"}
-                        max={formatDate(new Date())}
-                        className={classNames("usa-input")}
-                        aria-label={"Enter start date"}
-                        onChange={(e) => {
-                          if (Date.parse(e.target.value)) {
-                            const d = moment(e.target.value).toDate();
-                            const startDateString = setStartTimeForDateRange(
-                              new Date(d)
-                            ).toLocaleDateString();
-                            setStartDate(startDateString);
-                          }
-                        }}
-                        defaultValue={formatDate(new Date(startDate))}
-                      />{" "}
+              )}
+              {loading ? (
+                <p>Loading...</p>
+              ) : (
+                <>
+                  <h2>
+                    {facilityName} - {selectedCondition} testing data
+                  </h2>
+                  <p className="margin-bottom-0">
+                    All {PATIENT_TERM_PLURAL} tested
+                  </p>
+                  <p className="padding-top-1">{`${startDate} \u2013 ${endDate}`}</p>
+                  <div className="grid-row grid-gap">
+                    <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
+                      <div className="card display-flex flex-column flex-row">
+                        <h3>Tests reported</h3>
+                        <span className="font-sans-3xl text-bold margin-y-auto">
+                          {totalTests}
+                        </span>
+                        <p></p>
+                      </div>
                     </div>
-                    <div className="desktop:grid-col-4 tablet:grid-col-4 mobile:grid-col-1">
-                      <label className={classNames("usa-label")}>End</label>
-                      <input
-                        id={"endDate"}
-                        data-testid={"endDate"}
-                        type={"date"}
-                        min={formatDate(new Date(startDate))}
-                        max={formatDate(new Date())}
-                        className={classNames("usa-input")}
-                        aria-label={"Enter end date"}
-                        onChange={(e) => {
-                          if (Date.parse(e.target.value)) {
-                            const d = moment(e.target.value).toDate();
-                            const endDateString = setEndTimeForDateRange(
-                              new Date(d)
-                            ).toLocaleDateString();
-                            setEndDate(endDateString);
-                          }
-                        }}
-                        defaultValue={formatDate(new Date(endDate))}
-                      />
+                    <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
+                      <div className="card display-flex flex-column flex-align-center">
+                        <h3>Positive tests</h3>
+                        <span className="font-sans-3xl text-bold margin-y-auto">
+                          {positiveTests}
+                        </span>
+                        {/* \u2BC6 is down pointing triangle */}
+                        <p>
+                          {/* <span className="red-pointing-up">{`\u2BC5`} 2</span>{" "}
+                      <span className="usa-hint font-ui-md">from last week</span> */}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
+                      <div className="card display-flex flex-column flex-align-center">
+                        <h3>Negative tests</h3>
+                        <span className="font-sans-3xl text-bold margin-y-auto">
+                          {negativeTests}
+                        </span>
+                        <p></p>
+                      </div>
+                    </div>
+                    <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
+                      <div className="card display-flex flex-column flex-align-center">
+                        <h3>Positivity rate</h3>
+                        <span className="font-sans-3xl text-bold margin-y-auto">
+                          {positivityRate !== null
+                            ? positivityRate.toFixed(1) + "%"
+                            : "N/A"}
+                        </span>
+                        <p className="font-ui-2xs">
+                          Positives <span>÷</span> total tests
+                        </p>
+                      </div>
                     </div>
                   </div>
-                )}
-                {loading ? (
-                  <p>Loading...</p>
-                ) : (
-                  <>
-                    <h2>
-                      {facilityName} - {selectedCondition} testing data
-                    </h2>
-                    <p className="margin-bottom-0">
-                      All {PATIENT_TERM_PLURAL} tested
-                    </p>
-                    <p className="padding-top-1">{`${startDate} \u2013 ${endDate}`}</p>
-                    <div className="grid-row grid-gap">
-                      <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
-                        <div className="card display-flex flex-column flex-row">
-                          <h3>Tests reported</h3>
-                          <span className="font-sans-3xl text-bold margin-y-auto">
-                            {totalTests}
-                          </span>
-                          <p></p>
-                        </div>
-                      </div>
-                      <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
-                        <div className="card display-flex flex-column flex-align-center">
-                          <h3>Positive tests</h3>
-                          <span className="font-sans-3xl text-bold margin-y-auto">
-                            {positiveTests}
-                          </span>
-                          {/* \u2BC6 is down pointing triangle */}
-                          <p>
-                            {/* <span className="red-pointing-up">{`\u2BC5`} 2</span>{" "}
-                      <span className="usa-hint font-ui-md">from last week</span> */}
-                          </p>
-                        </div>
-                      </div>
-                      <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
-                        <div className="card display-flex flex-column flex-align-center">
-                          <h3>Negative tests</h3>
-                          <span className="font-sans-3xl text-bold margin-y-auto">
-                            {negativeTests}
-                          </span>
-                          <p></p>
-                        </div>
-                      </div>
-                      <div className="desktop:grid-col-3 tablet:grid-col-6 mobile:grid-col-1">
-                        <div className="card display-flex flex-column flex-align-center">
-                          <h3>Positivity rate</h3>
-                          <span className="font-sans-3xl text-bold margin-y-auto">
-                            {positivityRate !== null
-                              ? positivityRate.toFixed(1) + "%"
-                              : "N/A"}
-                          </span>
-                          <p className="font-ui-2xs">
-                            Positives <span>÷</span> total tests
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                )}
-              </div>
+                </>
+              )}
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
