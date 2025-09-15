991b4973b0179377dba89c8a6db9ecef3e6b11bd
Merethe Hansen <merethe@skylight.digital>
2025-09-15 16:05:32 -0700

    snapshots

---
+++ frontend/src/app/universalReporting/LabResultsFormSection.test.tsx
@@ -0,0 +1,145 @@
+import configureStore, { MockStoreEnhanced } from "redux-mock-store";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { MockedProvider } from "@apollo/client/testing";
+import { Provider } from "react-redux";
+
+import { GetAllLabsDocument } from "../../generated/graphql";
+
+import LabResultsFormSection from "./LabResultsFormSection";
+import { defaultSpecimenReportInputState } from "./LabReportFormUtils";
+
+describe("LabResultsFormSection", () => {
+  let store: MockStoreEnhanced<unknown, {}>;
+  const mockStore = configureStore([]);
+
+  beforeEach(() => {
+    store = mockStore({
+      organization: {
+        name: "Organization Name",
+      },
+      facilities: [
+        {
+          id: "a1",
+          name: "Fake Facility",
+        },
+      ],
+      user: {
+        permissions: [],
+      },
+    });
+  });
+
+  it("renders successfully", async () => {
+    const { container } = render(
+      <MemoryRouter>
+        <MockedProvider mocks={mocks}>
+          <Provider store={store}>
+            <LabResultsFormSection
+              specimen={defaultSpecimenReportInputState}
+              setSpecimen={() => {}}
+              testDetailList={[]}
+              setTestDetailList={() => {}}
+            />
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Loading test orders...")
+    );
+
+    expect(
+      await screen.findByText("Choose a specimen type to enter test results")
+    );
+    expect(
+      await screen.findByText(
+        "Please select test order and specimen type above to see test result information"
+      )
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders loading state for loading test orders", async () => {
+    const loadingMock = [
+      {
+        request: {
+          query: GetAllLabsDocument,
+        },
+        result: {
+          data: {
+            labs: [],
+          },
+          loading: true,
+        },
+      },
+    ];
+
+    const { container } = render(
+      <MemoryRouter>
+        <MockedProvider mocks={loadingMock}>
+          <Provider store={store}>
+            <LabResultsFormSection
+              specimen={defaultSpecimenReportInputState}
+              setSpecimen={() => {}}
+              testDetailList={[]}
+              setTestDetailList={() => {}}
+            />
+          </Provider>
+        </MockedProvider>
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByText("Loading test orders..."));
+    expect(container).toMatchSnapshot();
+  });
+});
+
+const mocks = [
+  {
+    request: {
+      query: GetAllLabsDocument,
+    },
+    result: {
+      data: {
+        labs: [
+          {
+            code: "101289-7",
+            description: null,
+            display:
+              "SARS-CoV-2 (COVID-19) RNA [Presence] in Throat by NAA with non-probe detection",
+            longCommonName:
+              "SARS-CoV-2 (COVID-19) RNA [Presence] in Throat by NAA with non-probe detection",
+            scaleCode: "LP7751-3",
+            scaleDisplay: "Ord",
+            systemCode: "LP7633-3",
+            systemDisplay: "Thrt",
+            answerList: "LL360-9",
+            orderOrObservation: "Both",
+            panel: false,
+          },
+          {
+            code: "8023-4",
+            description: null,
+            display:
+              "Saint Louis encephalitis virus Ab [Presence] in Serum by Immunoassay",
+            longCommonName:
+              "Saint Louis encephalitis virus Ab [Presence] in Serum by Immunoassay",
+            scaleCode: "LP7751-3",
+            scaleDisplay: "Ord",
+            systemCode: "LP7567-3",
+            systemDisplay: "Ser",
+            answerList: "",
+            orderOrObservation: "Both",
+            panel: false,
+          },
+        ],
+      },
+    },
+  },
+];
