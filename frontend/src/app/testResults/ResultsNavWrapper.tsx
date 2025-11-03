cfff6bd8415d6a7a99d72236301dd1f18ecf7c57
Daniel Sass <DanielSass@users.noreply.github.com>
2025-11-03 10:32:43 -0500

    updating data retention messaging, removing banners (#9252)
    * updating data retention messaging, removing banners
    
    * updating tests
    
    * updating verbage

--- frontend/src/app/testResults/ResultsNavWrapper.tsx
+++ frontend/src/app/testResults/ResultsNavWrapper.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement } from "react";
 
-import { DataRetentionLimitsBanner } from "../commonComponents/DataRetentionLimitsBanner";
-
 import ResultsNav from "./ResultsNav";
 
 type Props = {
@@ -12,7 +10,6 @@ const ResultsNavWrapper = ({ children }: Props) => {
   return (
     <div className="prime-home flex-1">
       <div className="grid-container results-wide-container">
-        <DataRetentionLimitsBanner dataRetained={"test results"} />
         <ResultsNav />
         {children}
       </div>
