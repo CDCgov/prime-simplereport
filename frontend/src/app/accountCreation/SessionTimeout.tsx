459a69f5b26ec08b19f80021dcce2302c09f8dc6
Mike Brown <mike@skylight.digital>
2026-01-16 14:42:07 -0500

    Add page title and h1 to session timeout

--- frontend/src/app/accountCreation/SessionTimeout.tsx
+++ frontend/src/app/accountCreation/SessionTimeout.tsx
@@ -1,11 +1,17 @@
 import CardBackground from "../commonComponents/CardBackground/CardBackground";
 import Card from "../commonComponents/Card/Card";
+import { useDocumentTitle } from "../utils/hooks";
 
 const SessionTimeout = () => {
+  useDocumentTitle("Session timeout");
+
   return (
     <main>
       <CardBackground>
-        <Card logo bodyKicker={"Your session has expired"}>
+        <Card logo>
+          <h1 className={"font-ui-md text-bold margin-top-3"}>
+            Your session has expired
+          </h1>
           <p>
             Please contact your account administrator to request a new
             activation link.
